4a83193077f8f211228cd2f6eb4882fd7c596fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 18:05:57 +0000

    Add package bcsfe/3.2.2/bcsfe-3.2.2.tar.gz

---
+++ packages/bcsfe/bcsfe-3.2.2.tar.gz/bcsfe-3.2.2/src/bcsfe/cli/__init__.py
@@ -0,0 +1,21 @@
+from bcsfe.cli import (
+    color,
+    dialog_creator,
+    main,
+    file_dialog,
+    feature_handler,
+    save_management,
+    server_cli,
+    edits,
+)
+
+__all__ = [
+    "color",
+    "dialog_creator",
+    "main",
+    "file_dialog",
+    "feature_handler",
+    "save_management",
+    "server_cli",
+    "edits",
+]
