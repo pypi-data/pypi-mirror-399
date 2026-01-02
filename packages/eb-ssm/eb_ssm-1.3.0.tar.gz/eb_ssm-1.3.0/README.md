a054e44a64a2cc3e9cf2007a6756a7d6383e8d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:05:50 +0000

    Add package eb-ssm/1.3.0/eb_ssm-1.3.0.tar.gz

---
+++ packages/eb-ssm/eb_ssm-1.3.0.tar.gz/eb_ssm-1.3.0/README.md
@@ -0,0 +1,72 @@
+# EB SSM
+
+This simple script helps you SSH into an Elastic Beanstalk server using AWS SSM.
+
+eb-ssm is desinged to combine tools from the EB CLI and the AWS CLI to provide a better alternative to `eb ssh`.
+
+It's a pip library, installed by `pip install eb-ssm`.
+
+Once it's set up, you can SSH into your Elastic Beanstalk servers with `eb-ssm [ENVIRONMENT_NAME]`.
+
+
+## Why you should use it
+
+While `eb ssh` exists, it requires each individual user to have the EC2 instance private keys locally. This is unideal
+from both an information security and access management standpoint. If you've configured SSM, users will no longer need
+SSH keys to SSH into Elastic Beanstalk instances and instead have their access managed via IAM.
+
+The main advantages of eb-ssm are the following:
+
+1. **Server SSH access is managed through IAM.**  Normally, you have to manage SSH access to Elastic Beanstalk environments yourself.  IAM is where AWS manages user access for everything else, and with eb-ssm, you can manage server SSH access for EB environments there as well.
+
+2. **No shared SSH keys.**  Sharing, tracking, and rotating SSH keys is a pain.  Using eb-ssm, you there are no SSH keys, so these problems go away.
+
+3. **No mucking around with port 22.**  The EB CLI is supposed to open port 22 just for the SSH session but it doesn't close it in the event of non-graceful termination of the SSH session.  eb-ssm does one better by never opening port 22 in the first place.
+
+4. **Audit log of SSH sessions.**  AWS SSM keeps a log of SSH sessions.  This is one more benefit that comes from using it over native SSH.
+
+5. **Ability to access non-public servers.**  If you have servers in a privative subnet, you can use eb-ssm to SSH into them without needing a bastion host.
+
+
+## Prerequisites
+
+### Set up your Elastic Beanstalk Environment to allow SSH via AWS SSM
+
+The following steps need to be done once per environment.
+
+1. Go to Elastic Beanstalk > ENVIRONMENT_NAME > Configuration > Security and find the "IAM instance profile" (by default, this is "aws-elasticbeanstalk-ec2-role").  This is ROLE_NAME in step 2.
+
+2. Go to IAM > Roles > ROLE_NAME.  Under permissions, add "AmazonSSMManagedInstanceCore".
+
+3. Go to Systems Manager > Session Manager > Preferences > Edit.  Enable "Run As Support" and set the "Run As Defualt User" to be "ec2-user" (or whatever the default user for your Elastic Beanstalk servers is).
+
+Note that it may take some time (~10 minutes) for the IAM changes to propagate.  If you have completed the AWS setup and get a "TargetNotConnected" error, wait 10-15 minutes and try again.
+
+### Configure your local computer
+
+The following steps need to be done once per computer.
+
+1. Install the AWS CLI: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+
+2. Install the Session Manager Plugin: https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
+
+## Using EB SSM
+
+Install eb-ssm via `pip install eb-ssm`.
+
+Once it is installed, all you need to do is run `eb-ssm` from your repository and it will automatically hook into your repository's EB configuration (in .elasticbeanstalk/config.yml).
+
+To ssh into a specific environment, use `eb-ssm ENVIRONMENT_NAME`.
+
+You can also optionally pass other parameters, such as an AWS CLI profile or a region to eb-ssm.  See `eb-ssm --help` for a full list of options.
+
+## Config
+
+eb-ssm uses the EB CLI configuration files.  If you have not used the EB CLI to set up a project, here is the minimal configruation needed by eb-ssm; this configraution lives in `.elasticbeanstalk/config.yml`:
+
+```
+global:
+  application_name: EB_APPLICATION_NAME
+  default_region: REGION_NAME
+  profile: PROFILE_NAME
+```
---
+++ packages/eb-ssm/eb_ssm-1.3.0.tar.gz/eb_ssm-1.3.0/eb_ssm/eb_ssm.py
@@ -0,0 +1,129 @@
+import argparse
+import os
+import sys
+
+from cement.utils.misc import minimal_logger
+from ebcli.core import io
+from ebcli.lib import aws, utils
+from ebcli.lib.elasticbeanstalk import get_all_environment_names
+from ebcli.objects.exceptions import EBCLIException
+from ebcli.operations.commonops import (get_current_branch_environment, get_default_profile,
+    get_default_region, get_instance_ids)
+
+LOG = minimal_logger(__name__)
+DEFAULT_COMMAND = "bash -l"
+
+
+class SSMWrapper:
+    def __init__(self):
+        args = self._parse_args()
+        
+        # environment_name may be None
+        self.environment_name = args.environment_name or get_current_branch_environment()
+        self.profile = self._raise_if_none(
+            args.profile,
+            get_default_profile(),
+            "Please specify a specific profile in the command or eb configuration.",
+        )
+        self.region = self._raise_if_none(
+            args.region,
+            get_default_region(),
+            "Please specify a specific region in the command or eb configuration.",
+        )
+        
+        self.command = args.command or DEFAULT_COMMAND
+        
+        self.instance_number = args.number
+
+
+    def _parse_args(self):
+        parser = argparse.ArgumentParser(description="SSH onto an Elastic Beanstalk Server")
+        parser.add_argument(
+            "environment_name",
+            default=None,
+            help="Elastic Beanstalk environment name (uses the branch default if not specified)",
+            nargs="?",
+        )
+        parser.add_argument(
+            "-p", "--profile",
+            default=None,
+            help="Use a specific profile from your credential file",
+        )
+        parser.add_argument(
+            "-r", "--region",
+            default=None,
+            help="Use a specific region",
+        )
+        parser.add_argument(
+            "-c", "--command",
+            default=None,
+            help="Command to execute (defaults to `bash -l`)",
+        )
+        parser.add_argument(
+            "-n", "--number",
+            default=None,
+            help=("Specify the instance to connect to by number (server numbers "
+                  "are 0-indexed); if there are multiple instances and no "
+                  "number is provided, you will be prompted to select and instance"),
+        )
+        return parser.parse_args()
+    
+    def _raise_if_none(self, value, default_value, error_message):
+        """
+        Return value if it is not None. If value is None, return default_value if it is not None.
+        If default_Value is also None, raise an error.
+        """
+        if value is not None:
+            return value
+        elif default_value is not None:
+            return default_value
+        else:
+            io.log_error(error_message)
+            sys.exit()
+    
+    def ssh(self):
+        aws.set_region(self.region)
+        aws.set_profile(self.profile)
+        
+        if self.environment_name is None:
+            environment_names = get_all_environment_names()
+            if environment_names:
+                error = "Please chose one of the following environment names:\n\n"
+                error += "\n".join(sorted(environment_names)) + "\n"
+                io.log_error(error)
+            else:
+                io.log_error("The current Elastic Beanstalk application has no environments")
+            sys.exit()
+        
+        instances = get_instance_ids(self.environment_name)
+        if len(instances) == 1:
+            self.instance_number = 0
+        if self.instance_number is not None:
+            instance = instances[int(self.instance_number)]
+        else:
+            io.echo()
+            io.echo('Select an instance to ssh into')
+            instance = utils.prompt_for_item_in_list(instances)
+        
+        params = [
+            "aws", "ssm", "start-session",
+            "--document-name", "AWS-StartInteractiveCommand",
+            "--parameters", "command='" + self.command + "'",
+            "--profile", self.profile,
+            "--region", self.region,
+            "--target", instance,
+        ]
+        cmd = " ".join(params)
+        os.system(cmd)
+
+
+def main():
+    try:
+        SSMWrapper().ssh()
+    except EBCLIException as e:
+        io.log_error(e)
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
