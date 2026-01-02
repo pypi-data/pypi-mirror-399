5d3d77a8c80cb21126ec425ea3b5adec20ef45de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:04:56 +0000

    Add package agentrank/0.1.3/agentrank-0.1.3.tar.gz

---
+++ packages/agentrank/agentrank-0.1.3.tar.gz/agentrank-0.1.3/agentrank/__init__.py
@@ -0,0 +1,10 @@
+"""
+AgentRank: Temporal-Aware Embedding Model for AI Agent Memory Retrieval
+
+The first embedding model that understands WHEN memories happened.
+"""
+
+from .embedder import AgentRankEmbedder
+
+__version__ = "0.1.0"
+__all__ = ["AgentRankEmbedder"]
