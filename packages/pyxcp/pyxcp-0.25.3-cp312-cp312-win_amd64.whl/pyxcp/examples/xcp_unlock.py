c72c09d236166ba0583f059bdbcd77566e9a29fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 18:04:29 +0000

    Add package pyxcp/0.25.3/pyxcp-0.25.3-cp312-cp312-win_amd64.whl

---
+++ packages/pyxcp/pyxcp-0.25.3-cp312-cp312-win_amd64.whl/pyxcp/examples/xcp_unlock.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Very basic hello-world example."""
+
+from pyxcp.cmdline import ArgumentParser
+
+
+"""
+"""
+
+
+def callout(master, args):
+    if args.sk_dll:
+        master.seedNKeyDLL = args.sk_dll
+
+
+ap = ArgumentParser(callout)
+ap.parser.add_argument(
+    "-s",
+    "--sk-dll",
+    dest="sk_dll",
+    help="Seed-and-Key .DLL name",
+    type=str,
+    default=None,
+)
+
+with ap.run() as x:
+    x.connect()
+
+    print("")
+    rps = x.getCurrentProtectionStatus()
+    print("Protection before unlocking:", rps, end="\n\n")
+
+    x.cond_unlock()
+
+    rps = x.getCurrentProtectionStatus()
+    print("Protection after unlocking:", rps)
+
+    x.disconnect()
