ee9002cd4e17518612e56c665bfa98f5bcbf7d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 18:05:53 +0000

    Add package factpulse/3.0.6/factpulse-3.0.6.tar.gz

---
+++ packages/factpulse/factpulse-3.0.6.tar.gz/factpulse-3.0.6/factpulse/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from factpulse.api.afnorpdppa_api import AFNORPDPPAApi
+from factpulse.api.afnorpdppa_directory_service_api import AFNORPDPPADirectoryServiceApi
+from factpulse.api.afnorpdppa_flow_service_api import AFNORPDPPAFlowServiceApi
+from factpulse.api.chorus_pro_api import ChorusProApi
+from factpulse.api.document_conversion_api import DocumentConversionApi
+from factpulse.api.health_api import HealthApi
+from factpulse.api.invoice_processing_api import InvoiceProcessingApi
+from factpulse.api.pdfxml_verification_api import PDFXMLVerificationApi
+from factpulse.api.user_api import UserApi
+
