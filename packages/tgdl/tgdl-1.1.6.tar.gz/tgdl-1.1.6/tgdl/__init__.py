afc24e5a62702e6b713ef94cc35a9f197bbb43e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:05:49 +0000

    Add package tgdl/1.1.6/tgdl-1.1.6.tar.gz

---
+++ packages/tgdl/tgdl-1.1.6.tar.gz/tgdl-1.1.6/tgdl/__init__.py
@@ -0,0 +1,9 @@
+"""tgdl - High-performance Telegram media downloader CLI tool."""
+
+__version__ = "1.1.6"
+__author__ = "kavidu-dilhara"
+__license__ = "MIT"
+
+from tgdl.cli import main
+
+__all__ = ["main", "__version__"]
