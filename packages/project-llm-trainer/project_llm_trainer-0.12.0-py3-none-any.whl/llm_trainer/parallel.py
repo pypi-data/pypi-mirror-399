db240ef665eddf3690eff0bb8cd0445658d99785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:05:27 +0000

    Add package project-llm-trainer/0.12.0/project_llm_trainer-0.12.0-py3-none-any.whl

---
+++ packages/project-llm-trainer/project_llm_trainer-0.12.0-py3-none-any.whl/llm_trainer/parallel.py
@@ -0,0 +1,250 @@
+import os
+from typing import Optional, Tuple
+from abc import ABC, abstractmethod
+
+import torch
+from torch import nn
+import torch.distributed as dist
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+try:
+    import deepspeed
+except: ...
+
+from .log import Logger
+
+
+class Parallel(ABC):
+    def __init__(
+            self,
+            init_process_group: bool = True,
+            use_parallel: bool = True,
+            use_compile: bool = False
+    ):
+        self._initialize(init_process_group, use_parallel, use_compile)
+
+    def _initialize(
+            self,
+            init_process_group: bool,
+            use_parallel: bool,
+            use_compile: bool
+    ):
+        self._global_rank: int = int(os.environ.get('RANK', -1))
+        self._local_rank: int = int(os.environ.get('LOCAL_RANK', -1))
+        self._use_parallel: bool = use_parallel and self._global_rank != -1
+        self._use_compile = use_compile
+
+        self._sampler: Optional[DistributedSampler] = None
+
+        self.model: Optional[nn.Module] = None
+        self.raw_model: Optional[nn.Module] = None
+
+        if use_compile:
+            torch.set_float32_matmul_precision('high')
+
+        if self._use_parallel:
+            if init_process_group:
+                dist.init_process_group(backend='nccl')
+
+            self.device: str = f'cuda:{self._local_rank}'
+            self.device_type: str = 'cuda'
+
+            torch.cuda.set_device(self.device)
+
+            Logger.std_log(f'global_rank={self._global_rank}, local_rank={self._local_rank}, world_size={self.world_size}')
+        else:
+            device = "cpu"
+            if torch.cuda.is_available():
+                device = "cuda"
+            elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+                device = "mps"
+
+            self.device: str = device
+            self.device_type: str = device
+
+
+    @abstractmethod
+    def process(
+            self,
+            model: nn.Module,
+            optimizer: torch.optim.Optimizer,
+            kwargs: Optional[dict] = None,
+            save_instance: bool = True
+    ) -> Tuple[nn.Module, torch.optim.Optimizer]: ...
+
+    def process_dataloader(
+            self,
+            dataset: Dataset,
+            data_loader_kwargs: dict,
+            sampler_kwargs: Optional[dict]=None
+    ) -> DataLoader:
+        """
+        :param dataset:
+        :param data_loader_kwargs
+                "batch_size" int,
+                "pin_memory" bool,
+                "collate_fn" collate_fn,
+                "num_workers" int
+                "shuffle" bool
+                "drop_last" bool
+        :param sampler_kwargs:
+                "shuffle" bool
+                "drop_last" bool
+        :return:
+        """
+
+        if self._use_parallel:
+            self._sampler = DistributedSampler(dataset=dataset, **sampler_kwargs)
+            return DataLoader(dataset=dataset, sampler=self._sampler, **data_loader_kwargs)
+
+        return DataLoader(dataset=dataset, **data_loader_kwargs)
+
+    def on_epoch_start(self, epoch):
+        if self._sampler:
+            self._sampler.set_epoch(epoch)
+
+    def on_epoch_end(self, epoch): ...
+
+    def synchronize(self):
+        if self._use_parallel:
+            torch.cuda.synchronize(device=self.device)
+
+    def destroy(self):
+        if self._use_parallel:
+            dist.destroy_process_group()
+
+    # def reduce_loss(self, avg_loss: torch.Tensor, loss: torch.Tensor, batch) -> torch.Tensor:
+    #     if self._use_parallel:
+    #         world_size = dist.get_world_size()
+    #         if world_size < 2:
+    #             return loss.detach()
+    #
+    #         torch.distributed.all_reduce(loss)
+    #         # 整个训练过程的滑动损失均值=在历史平均损失的基础上，加上最新损失再求平均
+    #         avg_loss = (avg_loss * batch + loss.detach()) / (batch + 1)
+    #         return avg_loss
+    #
+    #     return loss.detach()
+
+    @property
+    def parallel_train(self) -> bool:
+        return self._use_parallel
+
+    @property
+    def is_main_process(self) -> bool:
+        if self._use_parallel:
+            return self._global_rank == 0
+
+        return True
+
+    @property
+    def world_size(self) -> int:
+        if self._use_parallel:
+            return dist.get_world_size()
+        return 1
+
+    def wait(self, msg=None):
+        if self.world_size == 1:
+            return
+
+        msg = f' for {msg}' if msg else ''
+        Logger.std_log(f'wait at {self.device}{msg}')
+        dist.barrier()
+        Logger.std_log(f'continue at {self.device}{msg}')
+
+
+class DsParallel(Parallel):
+    def __init__(self):
+        deepspeed.init_distributed(dist_backend='nccl')
+        super().__init__(init_process_group=False)
+
+    def process(
+            self,
+            model: nn.Module,
+            optimizer: torch.optim.Optimizer,
+            kwargs: Optional[dict] = None,
+            save_instance: bool = True
+    ) -> Tuple[nn.Module, torch.optim.Optimizer]:
+        """
+            :param model:
+            :param optimizer:
+            :param kwargs:
+                参考deepspeed配置
+            :param save_instance
+            :return:
+        """
+
+        if save_instance:
+            self.raw_model = model
+
+        model, optim, _, _ = deepspeed.initialize(
+            model=model,
+            optimizer=optimizer,
+            dist_init_required=False,
+            config_params=kwargs
+        )
+
+        if save_instance:
+            self.model = model
+
+        return model, optim
+
+    def synchronize(self): ...
+
+    def destroy(self): ...
+
+
+class DdpParallel(Parallel):
+    def __init__(self):
+        super().__init__()
+
+    def process(
+            self,
+            model: nn.Module,
+            optimizer: torch.optim.Optimizer,
+            kwargs: Optional[dict] = None,
+            save_instance: bool = True
+    ) -> Tuple[nn.Module, torch.optim.Optimizer]:
+        model.to(self.device)
+
+        if self._use_compile:
+            model = torch.compile(model)
+
+        if self._use_parallel:
+            # self.model = DDP(module=model, broadcast_buffers=False, find_unused_parameters=True)
+            model = DDP(module=model, device_ids=[self._local_rank], output_device=self._local_rank)
+            raw_model = model.module
+        else:
+            model = model
+            raw_model = model
+
+        if save_instance:
+            self.model = model
+            self.raw_model = raw_model
+
+        return model, optimizer
+
+
+class NoneParallel(Parallel):
+    def __init__(self):
+        super().__init__(use_parallel=False)
+
+    def process(
+            self,
+            model: nn.Module,
+            optimizer: torch.optim.Optimizer,
+            kwargs: Optional[dict] = None,
+            save_instance: bool = True
+    ) -> Tuple[nn.Module, torch.optim.Optimizer]:
+        model.to(self.device)
+
+        if self._use_compile:
+            model = torch.compile(model)
+
+        if save_instance:
+            self.raw_model = model
+            self.model = model
+
+        return model, optimizer
