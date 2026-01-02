0e1ea072518c26f945123d9521416e38642916eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:05:12 +0000

    Add package anydi/0.63.0/anydi-0.63.0.tar.gz

---
+++ packages/anydi/anydi-0.63.0.tar.gz/anydi-0.63.0/anydi/ext/fastapi.py
@@ -0,0 +1,78 @@
+"""AnyDI FastAPI extension."""
+
+from __future__ import annotations
+
+import inspect
+from collections.abc import Iterator
+from typing import Annotated, Any, cast
+
+from fastapi import Depends, FastAPI, params
+from fastapi.dependencies.models import Dependant
+from fastapi.routing import APIRoute
+from starlette.requests import Request
+
+from anydi import Container, Inject
+from anydi._marker import Marker, extend_marker
+
+from .starlette.middleware import RequestScopedMiddleware
+
+__all__ = ["install", "get_container", "Inject", "RequestScopedMiddleware"]
+
+
+def get_container(request: Request) -> Container:
+    """Get the AnyDI container from a FastAPI request."""
+    return cast(Container, request.app.state.container)
+
+
+class FastAPIMarker(params.Depends, Marker):
+    def __init__(self) -> None:
+        Marker.__init__(self)
+        self._current_owner = "fastapi"
+        params.Depends.__init__(
+            self, dependency=self._fastapi_dependency, use_cache=True
+        )
+        self._current_owner = None
+
+    async def _fastapi_dependency(
+        self, container: Annotated[Container, Depends(get_container)]
+    ) -> Any:
+        return await container.aresolve(self.interface)
+
+
+# Configure Inject() and Provide[T] to use FastAPI-specific marker
+extend_marker(FastAPIMarker)
+
+
+def _iter_dependencies(dependant: Dependant) -> Iterator[Dependant]:
+    yield dependant
+    if dependant.dependencies:
+        for sub_dependant in dependant.dependencies:
+            yield from _iter_dependencies(sub_dependant)
+
+
+def _validate_route_dependencies(
+    route: APIRoute, container: Container, patched: set[tuple[Any, ...]]
+) -> None:
+    for dependant in _iter_dependencies(route.dependant):
+        if dependant.cache_key in patched:
+            continue
+        patched.add(dependant.cache_key)
+        call, *_ = dependant.cache_key
+        if not call:
+            continue  # pragma: no cover
+        for parameter in inspect.signature(call, eval_str=True).parameters.values():
+            _, should_inject, marker = container.validate_injected_parameter(
+                parameter, call=call
+            )
+            if should_inject and marker:
+                marker.set_owner("fastapi")
+
+
+def install(app: FastAPI, container: Container) -> None:
+    """Install AnyDI into a FastAPI application."""
+    app.state.container = container  # noqa
+    patched: set[tuple[Any, ...]] = set()
+    for route in app.routes:
+        if not isinstance(route, APIRoute):
+            continue
+        _validate_route_dependencies(route, container, patched)
---
+++ packages/anydi/anydi-0.63.0.tar.gz/anydi-0.63.0/anydi/ext/starlette/middleware.py
@@ -0,0 +1,23 @@
+"""Starlette RequestScopedMiddleware."""
+
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+from anydi import Container
+
+
+class RequestScopedMiddleware(BaseHTTPMiddleware):
+    """Starlette middleware for managing request-scoped AnyDI context."""
+
+    def __init__(self, app: ASGIApp, container: Container) -> None:
+        super().__init__(app)
+        self.container = container
+
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        async with self.container.arequest_context() as context:
+            context.set(Request, request)
+            return await call_next(request)
