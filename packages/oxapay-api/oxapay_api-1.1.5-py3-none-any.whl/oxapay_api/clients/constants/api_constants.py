d1306c64dec95280e7797cdc4963318c8513aa2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:05:06 +0000

    Add package oxapay-api/1.1.5/oxapay_api-1.1.5-py3-none-any.whl

---
+++ packages/oxapay-api/oxapay_api-1.1.5-py3-none-any.whl/oxapay_api/clients/constants/api_constants.py
@@ -0,0 +1,2 @@
+_GENERAL_API_URL = 'https://api.oxapay.com/v1'
+_METHODS = ['POST', 'GET']
---
+++ packages/oxapay-api/oxapay_api-1.1.5-py3-none-any.whl/oxapay_api/utils/response_models.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+@dataclass
+class OrderStatus:
+    track_id: str
+    payment_url: str
+    expired_at: int
+    date: int
+
+@dataclass
+class PaymentStatus:
+    track_id: str
+    type: str
+    amount: float
+    currency: str
+    status: str
+    mixed_payment: bool
+    fee_paid_by_payer: int
+    under_paid_coverage: float
+    lifetime: int
+    callback_url: str
+    return_url: str
+    email: str
+    order_id: str
+    description: str
+    thanks_message: str
+    expired_at: int
+    sandbox: bool
+    date: int
+    txs: list
