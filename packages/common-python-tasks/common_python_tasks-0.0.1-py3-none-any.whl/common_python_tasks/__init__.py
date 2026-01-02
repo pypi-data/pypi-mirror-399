525f95786a8b425a0b919460978f8530f0197497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:05:30 +0000

    Add package common-python-tasks/0.0.1/common_python_tasks-0.0.1-py3-none-any.whl

---
+++ packages/common-python-tasks/common_python_tasks-0.0.1-py3-none-any.whl/common_python_tasks/__init__.py
@@ -0,0 +1,16 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+from poethepoet_tasks import TaskCollection
+
+__version__ = "0.0.0"
+
+__all__ = ["TaskCollection"]
+
+
+def tasks(include_tags: Sequence[str] = tuple(), exclude_tags: Sequence[str] = tuple()):
+    from .tasks import tasks
+
+    return tasks(include_tags=include_tags, exclude_tags=exclude_tags)
