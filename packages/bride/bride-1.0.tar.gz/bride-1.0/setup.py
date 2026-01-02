eb0e15ae06685cdee8553c5b8f3f250ceca86020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:04:45 +0000

    Add package bride/1.0/bride-1.0.tar.gz

---
+++ packages/bride/bride-1.0.tar.gz/bride-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bride",
+    version="1.0",
+    author="Saif",
+    author_email="saifullahanwar00040@gmail.com",
+    description="Coming Soon",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
