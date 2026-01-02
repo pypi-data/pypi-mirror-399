d664feeabcd7366f8f2481a296c6e15a8d6fc4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:04:33 +0000

    Add package simplemp/0.7.4/simplemp-0.7.4.tar.gz

---
+++ packages/simplemp/simplemp-0.7.4.tar.gz/simplemp-0.7.4/tests/testtranscode.py
@@ -0,0 +1,10 @@
+from simplemp import transcode
+
+transcode(
+    input_file="../dump/testv0.0.7/stay1.ts", output_file="../dump/testv0.0.7/stay1.mkv",
+    audio_encoder="pcm_s16le", samplerate=11025, bitrate_audio=32000, sample_fmt="s16",
+    video_encoder="h264", bitrate_video=9000000, pixel_fmt="yuv420p", frame_rate=60, 
+    crf=24, preset="slow", profile="baseline", tune="zerolatency",
+    resolution=(1920, 1080),
+    mute=False, debug=False, overwrite=False, thread_count=3, thread_type="SLICE", loop=3
+)
