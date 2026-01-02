ae60e8b8144dd3aca2443ed35e5190eac7328a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:04:34 +0000

    Add package pysmartcocoon/1.4.2/pysmartcocoon-1.4.2.tar.gz

---
+++ packages/pysmartcocoon/pysmartcocoon-1.4.2.tar.gz/pysmartcocoon-1.4.2/pysmartcocoon/const.py
@@ -0,0 +1,45 @@
+"""SmartCocoon constants."""
+
+from enum import Enum, StrEnum
+
+# API Data
+API_URL = "https://app.mysmartcocoon.com/api/"
+API_AUTH_URL = API_URL + "auth/sign_in"
+API_FANS_URL = API_URL + "fans/"
+API_HEADERS = {
+    "Content-Type": "application/json",
+    "User-Agent": "SmartCocoon/12601 CFNetwork/1568.300.101 Darwin/24.2.0",
+}
+
+# Default fan speed is 38% in the SmartCocoon app
+# but 33% works better for HomeAssistant
+DEFAULT_FAN_POWER_PCT = 33
+
+DEFAULT_TIMEOUT: int = 30
+
+
+class EntityType(Enum):
+    """Class to define entity types"""
+
+    # SmartCocoon entity types
+
+    LOCATIONS = "client_systems"
+    THERMOSTATS = "thermostats"
+    ROOMS = "rooms"
+    FANS = "fans"
+
+
+class FanMode(StrEnum):
+    """Fan mode."""
+
+    ECO = "eco"
+    OFF = "always_off"
+    ON = "always_on"
+    AUTO = "auto"
+
+
+class FanState(StrEnum):
+    """Fan State."""
+
+    FAN_OFF = "false"
+    FAN_ON = "true"
---
+++ packages/pysmartcocoon/pysmartcocoon-1.4.2.tar.gz/pysmartcocoon-1.4.2/pysmartcocoon/location.py
@@ -0,0 +1,24 @@
+"""Define a SmartCocoon Rocation class."""
+
+# pylint: disable=too-few-public-methods,too-many-instance-attributes
+
+from typing import Any
+
+
+class Location:  # pylint: disable=too-many-instance-attributes
+    """Define the location."""
+
+    def __init__(self, data: dict[str, Any]) -> None:
+        """Initialize."""
+        self._identifier: int = data["id"]
+        self._postal_code: str = data["location"]["postal_code"]
+
+    @property
+    def identifier(self) -> int:
+        """Return location id"""
+        return self._identifier
+
+    @property
+    def postal_code(self) -> str:
+        """Return postal code"""
+        return self._postal_code
