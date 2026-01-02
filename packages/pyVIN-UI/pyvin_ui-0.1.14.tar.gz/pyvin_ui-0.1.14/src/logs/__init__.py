dd055800850055190bf8b1606633745c3eea1e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:04:42 +0000

    Add package pyVIN-UI/0.1.14/pyvin_ui-0.1.14.tar.gz

---
+++ packages/pyVIN-UI/pyvin_ui-0.1.14.tar.gz/pyvin_ui-0.1.14/src/logs/__init__.py
@@ -0,0 +1,3 @@
+from src.logs.logs import setup_logger
+
+__all__ = ["setup_logger"]
---
+++ packages/pyVIN-UI/pyvin_ui-0.1.14.tar.gz/pyvin_ui-0.1.14/src/validation/__init__.py
@@ -0,0 +1,3 @@
+from src.validation.vin import validate_and_normalize_vin
+
+__all__ = ["validate_and_normalize_vin"]
