aef80ba7a50672f65bb45f69df4433526454c69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 23:05:38 +0000

    Add package egeometry/0.20.1/egeometry-0.20.1-cp312-cp312-win32.whl

---
+++ packages/egeometry/egeometry-0.20.1-cp312-cp312-win32.whl/egeometry/_dboundingbox2dtype.hpp
@@ -0,0 +1,23 @@
+
+// generated from codegen/templates/_boundingbox2dtype.hpp
+
+#ifndef EGEOMETRY_DBoundingBox2d_TYPE_HPP
+#define EGEOMETRY_DBoundingBox2d_TYPE_HPP
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <glm/glm.hpp>
+
+typedef glm::vec<2, double, glm::defaultp> DBoundingBox2dGlmVector;
+typedef glm::vec<4, double, glm::defaultp> DBoundingBox2dGlmVector4;
+
+struct DBoundingBox2d
+{
+    PyObject_HEAD
+    PyObject *weakreflist;
+    PyObject *py_position;
+    PyObject *py_size;
+};
+
+#endif
