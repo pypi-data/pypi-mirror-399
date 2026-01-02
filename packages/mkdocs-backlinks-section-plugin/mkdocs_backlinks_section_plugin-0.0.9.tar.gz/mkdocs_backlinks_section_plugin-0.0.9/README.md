18e0c34c2a58fd4c3e6df69a47ab9146e2b51869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:05:28 +0000

    Add package mkdocs-backlinks-section-plugin/0.0.9/mkdocs_backlinks_section_plugin-0.0.9.tar.gz

---
+++ packages/mkdocs-backlinks-section-plugin/mkdocs_backlinks_section_plugin-0.0.9.tar.gz/mkdocs_backlinks_section_plugin-0.0.9/README.md
@@ -0,0 +1,224 @@
+# MkDocs Backlinks Section Plugin
+
+[![PyPI version](https://img.shields.io/pypi/v/mkdocs-backlinks-section-plugin)](https://pypi.org/project/mkdocs-backlinks-section-plugin/)
+![License](https://img.shields.io/pypi/l/mkdocs-backlinks-section-plugin)
+![Python versions](https://img.shields.io/pypi/pyversions/mkdocs-backlinks-section-plugin)
+
+Adds a backlinks section that lists every page linking to the current page.
+
+The added backlinks section looks like this, but you can also customize the title and the text show above the list:
+
+![Screenshot of the backlinks section](https://github.com/six-two/mkdocs-backlinks-section-plugin/raw/main/screenshot.png)
+
+## Comparison to similar plugins
+
+I wrote my plugin after trying some existing plugins and not being 100% happy with them.
+But depending on your intended use case, they may be a better fit.
+
+### mkdocs-backlinks
+
+My plugin is similar in concept to [mkdocs-backlinks](https://github.com/danodic-dev/mkdocs-backlinks), but I wanted a plugin that works out of the box.
+
+With [mkdocs-backlinks](https://github.com/danodic-dev/mkdocs-backlinks) you can specify exactly where and how you want to have your backlinks shown, but at the cost of having to potentially alter your template files.
+
+With my plugin you can just add it to your `mkdocs.yml` without any theme changes.
+But it can also work the same way as `mkdocs-backlinks`, so that you have full control over the output.
+
+### mkdocs-publisher
+
+[mkdocs-publisher](https://github.com/mkdocs-publisher/mkdocs-publisher) is a bundle of plugins.
+The `pub-obsidian` also has a backlinks feature.
+
+While my plugin does a single job and has minimal dependencies, mkdocs-publisher offers many more features but at the cost of many more dependencies.
+
+## Installation
+
+You can install it with `pip`:
+```bash
+pip install mkdocs-backlinks-section-plugin
+```
+
+## Usage
+
+Add the plugin to your `mkdocs.yml`:
+```yaml
+plugins:
+- search
+- backlinks_section
+```
+
+## Configuration
+
+### Text
+
+You can customize the text inserted by the plugin with the configuration values below:
+
+Option | Type | Default value
+--- | --- | ---
+`title` | string | `Backlinks`
+`description` | string | `The following pages link to this page:`
+`description_no_links` | string | `No other pages link to this page.`
+
+
+So for example if you would want the text to be in German, you could do this in your `mkdocs.yml`:
+```yaml
+plugins:
+- search
+- backlinks_section:
+    title: Rückverweise
+    description: "Die folgenden Seiten referenzieren die aktuelle Seite:"
+    description_no_links: Es gibt keine Verweise auf diese Seite.
+```
+
+### Ignore pages
+
+You can ignore source and destination pages for the backlink section.
+The values are interpreted as [glob-like](https://docs.python.org/3/library/pathlib.html#pathlib-pattern-language) patterns, which are matched against the paths of the Markdown source files.
+
+Option | Type | Default value
+--- | --- | ---
+`ignore_links_from` | list of strings | `[]`
+`ignore_links_to` | list of strings | `[]`
+
+For example you may have a page listing all [tags](https://squidfunk.github.io/mkdocs-material/setup/setting-up-tags/) (and thus linking to almost all pages) and want to prevent every page having a backlink to it:
+
+```yaml
+plugins:
+- search
+- backlinks_section:
+    ignore_links_from:
+    - path/with/globs/**/to/tags.md
+```
+
+If you do not want a backlinks section on some pages, you can disable it with the `ignore_links_to` option:
+```yaml
+plugins:
+- search
+- backlinks_section:
+    ignore_links_to:
+    - path/with/globs/**/to/files-without-backlink-section-*.md
+    - index.md
+```
+
+### Hide section
+
+Option | Type | Default value
+--- | --- | ---
+`add_to_toc` | boolean | `true`
+`hide_if_empty` | boolean | `false`
+
+If you do not want a backlinks section to be added to the table of contents of search page, you can set the `add_to_toc` parameter to `false`:
+```yaml
+plugins:
+- search
+- backlinks_section:
+    add_to_toc: false
+```
+
+If you want to hide the backlinks section from pages which have no backlinks, you can set the `hide_if_empty` attribute to `true`.
+Please note that in the current implementation this also requires always hiding the section title (even if the section exists) from the table of contents.
+To suppress the warning about this it is recommended to explicitly set `add_to_toc` to `false` too:
+```yaml
+plugins:
+- search
+- backlinks_section:
+    add_to_toc: false
+    hide_if_empty: true
+```
+
+### Jinja Template Mode
+
+Option | Type | Default value
+--- | --- | ---
+`add_section` | boolean | `true`
+`jinja_variable_name` | string | `backlinks`
+
+
+Similar to [mkdocs-backlinks](https://github.com/danodic-dev/mkdocs-backlinks), this plugin also exposes backlinks to the page template.
+This allows you much more control about how and where to display your backlinks.
+If you want to use this feature, you can stop the plugin from adding a section:
+
+```yaml
+plugins:
+- search
+- backlinks_section:
+    add_section: false
+```
+
+If you want to use this side by side with the `mkdocs-backlinks` plugin or your theme expects the variable to be named something else, you can change the name of the Jinja variable that the plugin writes to:
+
+```yaml
+plugins:
+- search
+- backlinks_section:
+    add_section: false
+    jinja_variable_name: backlinks_section_plugin
+```
+
+
+And then add some code for backlinks to your theme (see [this MkDocs Material page](https://squidfunk.github.io/mkdocs-material/customization/#extending-the-theme), for other themes you need to figure that part out on your own):
+
+```html
+{% if backlinks %}
+<h3>Backlinks:</h3>
+<ul>
+    {% for backlink in backlinks %}
+    <li><a href="/{{ backlink.url }}">{{ backlink.title }}</a></li>
+    {% endfor %}
+</ul>
+{% endif %}
+```
+
+### Troubleshooting
+
+Option | Type | Default value
+--- | --- | ---
+`debug` | boolean | `false`
+
+Used by me to debug problems with backlink generation.
+If you open a GitHub issue, it would be best to provide me a minimum working example, with which I can reproduce your problem.
+If that is not possible (for example because your project is private), it would help me if you supply the output of mkdocs when you set `debug: True`.
+
+## Notable changes
+
+### Version 0.0.9
+
+- Added Jinja support and `jinja_variable_name` option (see #6).
+    By default, this plugin works as a replacement for [danodic-dev/mkdocs-backlinks](https://github.com/danodic-dev/mkdocs-backlinks).
+    You can change the `jinja_variable_name` to something else, so that both plugins can co-exist.
+
+### Version 0.0.8
+
+- Fixed backlinks not working when folder names contain special characters (see #5)
+
+### Version 0.0.7
+
+- Fixed false positive warnings about missing `hrefs` for tags starting with `<a` (`<autoref>`, `<audio>`, etc). Thank you @kzndotsh (see #4)
+
+### Version 0.0.6
+
+- Fixed an issue with backlinks not appearing on some pages (see #3)
+- Added `debug` option to help with troubleshooting
+
+### Version 0.0.5
+
+- Ignore empty link tags created by listings with line numbers (`linenums="1"`)
+- In warning messages show which file caused the warning
+
+### Version 0.0.4
+
+- Added `add_to_toc` option, which controls whether to add the backlinks section to the table of contents.
+- Added `hide_if_empty` option, which will hide the backlinks section, if no backlinks exist.
+    This requires `add_to_toc` to be false, otherwise the table of contents would point to a potentially non-existent section.
+
+### Version 0.0.3
+
+- Fixed crash with Python <= 3.12
+
+### Version 0.0.2
+
+- Added `ignore_links_from` and `ignore_links_to` configuration options
+
+### Version 0.0.1
+
+- Initial version
---
+++ packages/mkdocs-backlinks-section-plugin/mkdocs_backlinks_section_plugin-0.0.9.tar.gz/mkdocs_backlinks_section_plugin-0.0.9/src/mkdocs_backlinks_section_plugin/__init__.py
@@ -0,0 +1,242 @@
+# builtin
+
+# The idea is based roughly on https://github.com/danodic-dev/mkdocs-backlinks, but instead of dealing with a template, this plugin just injects the backlinks into the HTML at the bottom of the page
+import os
+import html
+from html.parser import HTMLParser
+from pathlib import PurePath
+import random
+import re
+from typing import Optional
+import urllib.parse
+# pip
+from mkdocs.config.base import Config
+from mkdocs.config.config_options import Type, ListOfItems
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.plugins import BasePlugin, get_plugin_logger
+from mkdocs.structure.files import Files
+from mkdocs.structure.pages import Page
+
+# Marks URLs that should be ignored
+BAD_URL_STARTS = ["http://", "https://", "tel:", "#"]
+# Regular expression for anchor tags (excludes autoref tags)
+LINK_START_TAG_REGEX = re.compile(r"<a(?:\s[^>]*)?>", re.MULTILINE)
+# Logger
+LOGGER = get_plugin_logger(__name__)
+
+class BacklinksSectionConfig(Config):
+    add_section = Type(bool, default=True)
+    title = Type(str, default="Backlinks")
+    description = Type(str, default="The following pages link to this page:")
+    description_no_links = Type(str, default="No other pages link to this page.")
+    ignore_links_from = ListOfItems(Type(str), [])
+    ignore_links_to = ListOfItems(Type(str), [])
+    add_to_toc = Type(bool, default=True)
+    debug = Type(bool, default=False)
+    hide_if_empty = Type(bool, default=False) # Adding multiple eent handlers for the on_page_content did not work as I hoped (handler 2 ran before all other pages were processed by handler 1). So we need to remove the backlinks section from the nav or risk linking to an empty section
+    jinja_variable_name = Type(str, default="backlinks") # For compatibility with / replacement of https://github.com/danodic-dev/mkdocs-backlinks
+
+class BacklinksSectionPlugin(BasePlugin[BacklinksSectionConfig]):
+    def __init__(self):
+        # Use a random placeholder to prevent accidential collisions with strings like BACKLINK_PLUGIN
+        self.backlink_placeholder = f"BACKLINK_PLUGIN_{random.randint(0,10000000)}_PLACEHOLDER"
+
+    def debug(self, message: str):
+        if self.config.debug:
+            LOGGER.info(f"[DEBUG] {message}")
+    
+    def on_nav(self, nav, config: MkDocsConfig, files: Files):
+        # self.backlinks | normalized_url: str -> (page_url: str, page_title: str)
+        self.backlinks: dict[str,set[tuple[str,str]]] = {normalize_link(file.url): set() for file in files if is_page_url(file.url)}
+        self.debug(f"Normalized links of all pages: {', '.join(self.backlinks)}")
+        self.ignore_links_from = [normalize_link(x) for x in self.config.ignore_links_from]
+        self.ignore_links_to = [x[1:] if x.startswith("/") else x for x in self.config.ignore_links_to]
+        self.add_to_toc = self.config.add_to_toc
+        if self.add_to_toc and self.config.hide_if_empty:
+            self.add_to_toc = False
+            LOGGER.warning("'add_to_toc' and 'hide_if_empty' are mutually exclusive. Since 'hide_if_empty' has precedence, 'add_to_toc' has been disabled. You can hide this warning by explicitly setting 'add_to_toc: false'")
+
+        if not hasattr(PurePath("/"), "full_match"):
+            # Fullmatch "**" is not supported. Check if the patterns actually use it
+            if "**" in ",".join(self.ignore_links_from + self.ignore_links_to):
+                LOGGER.warning("Your Python does not support the Path.full_match method. Since you use '**' in your patterns, this may not be interpreted correctly. To fix it update your Python to 3.13")
+        return nav
+
+    def on_page_markdown(self, markdown: str, page: Page, config: MkDocsConfig, files: Files) -> str:
+        if should_ignore_page(page, self.ignore_links_to) or not self.config.add_section:
+            # Ignore the page and skip the backlinks section
+            return markdown
+        else:
+            # Add backlinks section placeholder
+            if self.add_to_toc:
+                # We need to add the heading in the Markdown phase, so that the new section is added to the table of contents
+                return f"{markdown}\n\n## {self.config.title}\n\n{self.backlink_placeholder}"
+            else:
+                return f"{markdown}\n\n{self.backlink_placeholder}"
+
+    def on_page_content(self, html: str, page: Page, config: MkDocsConfig, files: Files) -> str:
+        if should_ignore_page(page, self.ignore_links_from):
+            self.debug(f"Ignoring links from page: {page.file.src_uri}")
+        else:
+            # Collect the backlinks
+            for url in parse_links_to_other_pages(html, page.file.src_uri):
+                destination_link = normalize_link(url, page.url)
+
+                # Prevent the same page from linking back to itself
+                # @TODO: this only works for directory URLs. Also not sure how reliable it is
+                if destination_link != normalize_link("", page.url):
+                    if destination_link in self.backlinks:
+                        page_title = str(page.title) if page.title else "Untitled Page"
+                        self.backlinks[destination_link].add((page.url, page_title))
+                    else:
+                        self.debug(f"Link to unknown page ignored: {url} (normalized: {destination_link}). page.url={page.url}")
+                else:
+                    self.debug(f"Link to self on {page.url} ignored")
+        
+        return html
+
+    def on_post_page(self, output: str, page: Page, config: MkDocsConfig) -> str:
+        if should_ignore_page(page, self.ignore_links_to) or not self.config.add_section:
+            # Ignore the page and skip the backlinks section
+            return output
+        else:
+            # Insert the backlink list
+            key = normalize_link(page.url, "")
+            links: set[tuple[str,str]] = self.backlinks.get(key, set())
+            if links:
+                if self.add_to_toc:
+                    # The heading is already added, since it needed to be there before the TOC generation
+                    backlink_html = ""
+                else:
+                    backlink_html = f"<h2>{html.escape(self.config.title)}</h2>"
+
+                backlink_html += f"<p>{html.escape(self.config.description)}</p>" if self.config.description else ""
+                backlink_html += "<ul>"
+                # sort by title
+                for link, title in sorted(links, key=lambda x: x[1]):
+                    link_to_page = get_relative_path_from(page.url, link)
+                    backlink_html += f'<li><a href="{link_to_page}">{html.escape(title)}</a></li>'
+                backlink_html += "</ul>"
+            else:
+                if self.config.hide_if_empty:
+                    backlink_html = ""
+                else:
+                    if self.add_to_toc:
+                        # The heading is already added, since it needed to be there before the TOC generation
+                        backlink_html = ""
+                    else:
+                        backlink_html = f"<h2>{html.escape(self.config.title)}</h2>"
+                    backlink_html += f"<p>{html.escape(self.config.description_no_links)}</p>"
+
+            # Replace our placeholder with the actual section's text
+            output = output.replace(self.backlink_placeholder, backlink_html)
+            return output
+
+    def on_page_context(self, context, page, config, nav):
+        backlinks = self.backlinks.get(normalize_link(page.url))
+        if backlinks:
+            # Keep compatibility with https://github.com/danodic-dev/mkdocs-backlinks?tab=readme-ov-file#usage by naming the attrubutes the same
+            context[self.config.jinja_variable_name] = [{"url": url, "title": title} for url, title in backlinks]
+        return context
+
+
+def is_page_url(url: str) -> bool:
+    # Recognize directory urls and normal pages
+    return url.endswith("/") or url.lower().endswith(".html")
+
+def get_relative_path_from(current_page: str, absolute_destination_link: str) -> str:
+    level = current_page.count("/")
+    relative_url = ("../" * level) + absolute_destination_link
+
+    return relative_url
+
+
+def should_ignore_page(page: Page, ignore_pattern_list: list[str]) -> bool:
+    page_path = PurePath(page.file.src_uri)
+    for ignore_pattern in ignore_pattern_list:
+        if path_try_full_match(page_path, ignore_pattern, False):
+            return True
+    
+    return False
+
+def path_try_full_match(page_path: PurePath, ignore_pattern: str, case_sensitive: bool) -> bool:
+    # https://docs.python.org/3/library/pathlib.html#pathlib.PurePath.full_match
+    # Sadly it only exists in 3.13. So I need to use the worse version "match" if we have an old python version
+    if hasattr(page_path, "full_match"):
+        return page_path.full_match(ignore_pattern, case_sensitive=case_sensitive)
+    else:
+        # Case sensitive is only available from Python 3.12
+        return page_path.match(ignore_pattern)
+
+
+def normalize_link(path: str, base_url: str = "") -> str:
+    path = path.split("#", 1)[0] # Remove anything after a hashtag (exact anchor on page)
+
+    # decode URLs in case they contain URL encoded data
+    path = urllib.parse.unquote(path)
+
+    if path.startswith("/"):
+        # Absolute URL
+        path = os.path.normpath(path)[1:]
+    else:
+        if base_url:
+            base_url_decoded = urllib.parse.unquote(os.path.dirname(base_url))
+            path = os.path.join(base_url_decoded, path)
+    
+        path = os.path.normpath(path)
+
+    # Since some operating systems can have canse insensitive paths, we convert them to lowercase
+    path = path.lower()
+
+    if path.endswith("/index.html") or path == "index.html":
+        path = path[:-len("index.html")]
+
+    return path
+
+def is_valid_link(url: str) -> bool:
+    url = url.lower()
+    for bad_pattern in BAD_URL_STARTS:
+        if url.startswith(bad_pattern):
+            # Url is not a local path
+            return False
+    # Url is probably a local file
+    return True
+
+
+def parse_href_from_anchor_tag(anchor_tag: str, page_name: str) -> Optional[str]:
+    parser = AnchorHrefExtractor()
+    parser.feed(anchor_tag)
+    if parser.href:
+        return parser.href
+    elif parser.id and parser.id.startswith("__codelineno-"):
+        # These are created when you enable line numbers in listings (linenums="1"). We can silently ignore them
+        return ""
+    else:
+        LOGGER.warning(f"On page '{page_name}' an anchor tag has no href: {anchor_tag}")
+        return ""
+
+
+def parse_links_to_other_pages(html: str, page_name: str) -> list[str]:
+    results = []
+    for link_start_tag in LINK_START_TAG_REGEX.findall(html):
+        link = parse_href_from_anchor_tag(link_start_tag, page_name)
+        if link and is_valid_link(link):
+            results.append(link)
+    
+    return results
+
+
+# This should handle all edge cases properly, since it is a full HTML parser. But it luckily needs no external dependencies
+class AnchorHrefExtractor(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.href = ""
+        self.id = ""
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for attr_name, attr_value in attrs:
+                if attr_name == "href":
+                    self.href = attr_value
+                if attr_name == "id":
+                    self.id = attr_value
