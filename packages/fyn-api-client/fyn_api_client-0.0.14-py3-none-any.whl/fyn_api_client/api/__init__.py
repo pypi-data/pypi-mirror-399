ba6d4ad5f4993825e77609f09b5ea789cd317208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:05:22 +0000

    Add package fyn-api-client/0.0.14/fyn_api_client-0.0.14-py3-none-any.whl

---
+++ packages/fyn-api-client/fyn_api_client-0.0.14-py3-none-any.whl/fyn_api_client/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from fyn_api_client.api.accounts_api import AccountsApi
+from fyn_api_client.api.api_api import ApiApi
+from fyn_api_client.api.application_registry_api import ApplicationRegistryApi
+from fyn_api_client.api.auth_api import AuthApi
+from fyn_api_client.api.job_manager_api import JobManagerApi
+from fyn_api_client.api.runner_manager_api import RunnerManagerApi
+
