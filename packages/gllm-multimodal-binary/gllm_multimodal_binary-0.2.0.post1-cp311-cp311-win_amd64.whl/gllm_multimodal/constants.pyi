786836b1dc581335cdd36c558917e699184f7adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 20:05:09 +0000

    Add package gllm-multimodal-binary/0.2.0.post1/gllm_multimodal_binary-0.2.0.post1-cp311-cp311-win_amd64.whl

---
+++ packages/gllm-multimodal-binary/gllm_multimodal_binary-0.2.0.post1-cp311-cp311-win_amd64.whl/gllm_multimodal/constants.pyi
@@ -0,0 +1,27 @@
+class ImageToTextConstants:
+    """Constants for image-to-text operations in Gen AI applications."""
+    NOT_GIVEN: str
+    FILENAME: str
+    NO_IMAGE_FILENAME: str
+
+class CaptionConstants:
+    """Constants for caption operations in Gen AI applications."""
+    CAPTION_DEFAULT_JSON_KEY: str
+    DOMAIN_KNOWLEDGE: str
+    IMAGE_DESCRIPTION: str
+    IMAGE_METADATA: str
+    IMAGE_ONE_LINER: str
+    NUMBER_OF_CAPTIONS: str
+    ATTACHMENTS_CONTEXT: str
+    DEFAULT_NUMBER_OF_CAPTIONS: int
+
+class ExifConstants:
+    """Constants for EXIF tag operations in image metadata extraction."""
+    GPS_LATITUDE: str
+    GPS_LATITUDE_REF: str
+    GPS_LONGITUDE: str
+    GPS_LONGITUDE_REF: str
+    GPS_SOUTH: str
+    GPS_WEST: str
+    LATITUDE: str
+    LONGITUDE: str
---
+++ packages/gllm-multimodal-binary/gllm_multimodal_binary-0.2.0.post1-cp311-cp311-win_amd64.whl/gllm_multimodal/modality_converter/schema/__init__.pyi
@@ -0,0 +1,4 @@
+from gllm_multimodal.modality_converter.schema.caption import Caption as Caption
+from gllm_multimodal.modality_converter.schema.text_result import TextResult as TextResult
+
+__all__ = ['Caption', 'TextResult']
