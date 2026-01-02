2a8dacb4cb06ba11b883cfc39b561f1d71b180f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:04:34 +0000

    Add package mgraph-db/1.14.0/mgraph_db-1.14.0-py3-none-any.whl

---
+++ packages/mgraph-db/mgraph_db-1.14.0-py3-none-any.whl/mgraph_db/mgraph/schemas/index/Schema__MGraph__Index__Data__Labels.py
@@ -0,0 +1,13 @@
+from typing                                                         import Dict, Set
+from osbot_utils.type_safe.Type_Safe__On_Demand                     import Type_Safe__On_Demand
+from osbot_utils.type_safe.primitives.domains.identifiers.Edge_Id   import Edge_Id
+from osbot_utils.type_safe.primitives.domains.identifiers.Safe_Id   import Safe_Id
+
+# todo: check if adding Type_Safe__On_Demand adds any value here?
+class Schema__MGraph__Index__Data__Labels(Type_Safe__On_Demand):
+    edges_predicates       : Dict[Edge_Id, Safe_Id]                                           # edge_id -> predicate
+    edges_by_predicate     : Dict[Safe_Id, Set[Edge_Id]]                                      # predicate -> set of edge_ids
+    edges_incoming_labels  : Dict[Edge_Id, Safe_Id]                                           # edge_id -> incoming_label
+    edges_by_incoming_label: Dict[Safe_Id, Set[Edge_Id]]                                     # incoming_label -> set of edge_ids
+    edges_outgoing_labels  : Dict[Edge_Id, Safe_Id]                                           # edge_id -> outgoing_label
+    edges_by_outgoing_label: Dict[Safe_Id, Set[Edge_Id]]                                     # outgoing_label -> set of edge_ids
