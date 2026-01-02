e9d36f69fb27ac75c1839fdafb167f7fcf4edb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:06:26 +0000

    Add package fastapi-factory-utilities/0.13.7/fastapi_factory_utilities-0.13.7-py3-none-any.whl

---
+++ packages/fastapi-factory-utilities/fastapi_factory_utilities-0.13.7-py3-none-any.whl/fastapi_factory_utilities/core/security/jwt/configs.py
@@ -0,0 +1,32 @@
+"""Provides the configurations for the JWT bearer token."""
+
+from typing import ClassVar
+
+from jwt.algorithms import get_default_algorithms, requires_cryptography
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+
+
+class JWTBearerAuthenticationConfig(BaseModel):
+    """JWT bearer token authentication configuration."""
+
+    model_config: ClassVar[ConfigDict] = ConfigDict(frozen=True, extra="forbid")
+
+    authorized_algorithms: list[str] = Field(
+        default_factory=lambda: list(get_default_algorithms().keys()), description="The authorized algorithms."
+    )
+
+    audience: str = Field(description="The audience to be included in the JWT token.")
+    authorized_audiences: list[str] | None = Field(default=None, description="The authorized audiences.")
+    authorized_issuers: list[str] | None = Field(default=None, description="The authorized issuers.")
+
+    @field_validator("authorized_algorithms")
+    @classmethod
+    def validate_authorized_algorithms(cls, v: list[str]) -> list[str]:
+        """Validate the authorized algorithms."""
+        invalid_algorithms: list[str] = []
+        for algorithm in v:
+            if algorithm not in requires_cryptography:
+                invalid_algorithms.append(algorithm)
+        if invalid_algorithms:
+            raise ValueError(f"Invalid algorithms: {invalid_algorithms}")
+        return v
