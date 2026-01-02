d1c4f01d20c3b54911b5f8d3e203bedccf5a03a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 20:06:51 +0000

    Add package unidq/0.1.3/unidq-0.1.3-py3-none-any.whl

---
+++ packages/unidq/unidq-0.1.3-py3-none-any.whl/unidq/__init__.py
@@ -0,0 +1,19 @@
+"""
+UNIDQ: Unified Transformer for Multi-Task Data Quality
+"""
+
+__version__ = "0.1.3"
+__author__ = "shivakoreddi, sravanisowrupilli"
+
+from .model import UNIDQ, UNIDQConfig
+from .dataset import MultiTaskDataset
+from .trainer import UNIDQTrainer
+from .evaluation import evaluate_all_tasks
+
+__all__ = [
+    "UNIDQ",
+    "UNIDQConfig", 
+    "MultiTaskDataset",
+    "UNIDQTrainer",
+    "evaluate_all_tasks",
+]
