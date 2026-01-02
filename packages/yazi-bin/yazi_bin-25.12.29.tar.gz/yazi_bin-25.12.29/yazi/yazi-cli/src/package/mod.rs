45bc0390849e7f6176f41601092458e65f4c4d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:05:07 +0000

    Add package yazi-bin/25.12.29/yazi_bin-25.12.29.tar.gz

---
+++ packages/yazi-bin/yazi_bin-25.12.29.tar.gz/yazi_bin-25.12.29/yazi/yazi-cli/src/package/mod.rs
@@ -0,0 +1,12 @@
+yazi_macro::mod_flat!(add delete dependency deploy git hash install package upgrade);
+
+use anyhow::Context;
+use yazi_fs::Xdg;
+
+pub(super) fn init() -> anyhow::Result<()> {
+	let root = Xdg::state_dir().join("packages");
+	std::fs::create_dir_all(&root)
+		.with_context(|| format!("failed to create packages directory: {root:?}"))?;
+
+	Ok(())
+}
---
+++ packages/yazi-bin/yazi_bin-25.12.29.tar.gz/yazi_bin-25.12.29/yazi/yazi-vfs/src/provider/sftp/gate.rs
@@ -0,0 +1,90 @@
+use std::io;
+
+use yazi_config::vfs::{ServiceSftp, Vfs};
+use yazi_fs::provider::{Attrs, FileBuilder};
+use yazi_sftp::fs::Flags;
+use yazi_shared::url::{AsUrl, Url};
+
+use crate::provider::sftp::Conn;
+
+#[derive(Clone, Copy, Default)]
+pub struct Gate(crate::provider::Gate);
+
+impl From<Gate> for Flags {
+	fn from(Gate(g): Gate) -> Self {
+		let mut flags = Self::empty();
+		if g.append {
+			flags |= Self::APPEND;
+		}
+		if g.create {
+			flags |= Self::CREATE;
+		}
+		if g.create_new {
+			flags |= Self::CREATE | Self::EXCLUDE;
+		}
+		if g.read {
+			flags |= Self::READ;
+		}
+		if g.truncate {
+			flags |= Self::TRUNCATE;
+		}
+		if g.write {
+			flags |= Self::WRITE;
+		}
+		flags
+	}
+}
+
+impl FileBuilder for Gate {
+	type File = yazi_sftp::fs::File;
+
+	fn append(&mut self, append: bool) -> &mut Self {
+		self.0.append(append);
+		self
+	}
+
+	fn attrs(&mut self, attrs: Attrs) -> &mut Self {
+		self.0.attrs(attrs);
+		self
+	}
+
+	fn create(&mut self, create: bool) -> &mut Self {
+		self.0.create(create);
+		self
+	}
+
+	fn create_new(&mut self, create_new: bool) -> &mut Self {
+		self.0.create_new(create_new);
+		self
+	}
+
+	async fn open<U>(&self, url: U) -> io::Result<Self::File>
+	where
+		U: AsUrl,
+	{
+		let url = url.as_url();
+		let (path, (name, config)) = match url {
+			Url::Sftp { loc, domain } => (*loc, Vfs::service::<&ServiceSftp>(domain).await?),
+			_ => Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Not an SFTP URL: {url:?}")))?,
+		};
+
+		let flags = Flags::from(*self);
+		let attrs = super::Attrs(self.0.attrs).try_into().unwrap_or_default();
+		Ok(Conn { name, config }.roll().await?.open(path, flags, &attrs).await?)
+	}
+
+	fn read(&mut self, read: bool) -> &mut Self {
+		self.0.read(read);
+		self
+	}
+
+	fn truncate(&mut self, truncate: bool) -> &mut Self {
+		self.0.truncate(truncate);
+		self
+	}
+
+	fn write(&mut self, write: bool) -> &mut Self {
+		self.0.write(write);
+		self
+	}
+}
