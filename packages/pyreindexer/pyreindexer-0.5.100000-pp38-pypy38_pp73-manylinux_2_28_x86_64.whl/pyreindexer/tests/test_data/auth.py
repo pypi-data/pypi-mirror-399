13c91db4ad881d29420f0a9eb528ff8b9c4d5154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 20:06:03 +0000

    Add package pyreindexer/0.5.100000/pyreindexer-0.5.100000-pp38-pypy38_pp73-manylinux_2_28_x86_64.whl

---
+++ packages/pyreindexer/pyreindexer-0.5.100000-pp38-pypy38_pp73-manylinux_2_28_x86_64.whl/pyreindexer/tests/test_data/auth.py
@@ -0,0 +1,28 @@
+USER_ROLES = ["data_read", "data_write", "db_admin", "owner"]
+
+users_yml = '''
+data_read:
+    hash: dataread
+    roles:
+        *: data_read
+
+data_write:
+    hash: datawrite
+    roles:
+        *: data_write
+
+db_admin:
+    hash: dbadmin
+    roles:
+        *: db_admin
+
+owner:
+    hash: owner
+    roles:
+        *: owner
+        
+invalid_pass:
+    hash: invalid_pass
+    roles:
+        *: owner
+'''
