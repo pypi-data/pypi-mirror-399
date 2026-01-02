19c5485f4da5deff5b4ea4bac996eb30c67db4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:04:52 +0000

    Add package natsort-rs/0.1.17/natsort_rs-0.1.17-cp313-cp313-win32.whl

---
+++ packages/natsort-rs/natsort_rs-0.1.17-cp313-cp313-win32.whl/natsort_rs/__init__.py
@@ -0,0 +1,21 @@
+from .natsort_rs import get_sorted_indices
+
+
+def natsort(
+        vals: list, 
+        key = None, 
+        ignore_case = False, 
+        return_indices = False
+):
+    if key is None:
+        strs_for_sorting = vals
+    else:
+        strs_for_sorting = [key(item) for item in vals]
+    
+    sorted_indices = get_sorted_indices(strs_for_sorting, ignore_case)
+
+    if return_indices:
+        return sorted_indices
+    else:
+        return [vals[idx] for idx in sorted_indices]
+
