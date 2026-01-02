70b3f0e27ec92e5f081b48c557bf7b533340a972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:05:08 +0000

    Add package py-ai-toolkit/0.3.1/py_ai_toolkit-0.3.1-py3-none-any.whl

---
+++ packages/py-ai-toolkit/py_ai_toolkit-0.3.1-py3-none-any.whl/py_ai_toolkit/core/ports/workflow_port.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+from typing import Any, Type, TypeVar, Union
+
+from grafo import Node
+from pydantic import BaseModel
+
+from py_ai_toolkit.core.domain.models import BaseValidation
+
+T = TypeVar("T", bound=BaseModel)
+S = TypeVar("S", bound=BaseModel)
+V = TypeVar("V", bound=BaseValidation)
+
+
+class WorkflowPort(ABC):
+    """
+    Abstract base class for workflow operations.
+    """
+
+    @abstractmethod
+    async def task(
+        self,
+        path: str | None = None,
+        prompt: str | None = None,
+        response_model: Type[S] | None = None,
+        **kwargs: Any,
+    ) -> Union[str, bool, S]:
+        """
+        Execute a task.
+
+        Args:
+            path (str): The path to the prompt template file
+            prompt (str | None): The prompt to use for the task
+            response_model (Type[S] | None): The response model to return the response as
+            **kwargs: Additional arguments to pass to the prompt formatter
+
+        Returns:
+            Union[str, bool, S]: The response from the LLM with text content or a boolean indicating if the output is valid
+        """
+        pass
+
+    @abstractmethod
+    async def redirect(
+        self,
+        source_node: Node[S],
+        validation_node: Node[V],
+        target_nodes: list[Node[T]] | None = None,
+    ) -> None:
+        """
+        Redirect the workflow.
+
+        Args:
+            source_node (Node[S]): The source node.
+            validation_node (Node[V]): The validation node.
+            target_nodes (list[Node[T]], optional): The target nodes. Defaults to None.
+        """
+        pass
+
+    @abstractmethod
+    async def run(self, *args: Any, **kwargs: Any) -> Any:
+        """
+        Run the workflow.
+        """
+        pass
