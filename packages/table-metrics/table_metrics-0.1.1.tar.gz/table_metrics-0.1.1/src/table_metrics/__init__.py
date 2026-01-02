93889d3fe38bdc344f4d91e2a76bece2d9ab47be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 20:06:15 +0000

    Add package table-metrics/0.1.1/table_metrics-0.1.1.tar.gz

---
+++ packages/table-metrics/table_metrics-0.1.1.tar.gz/table_metrics-0.1.1/src/table_metrics/__init__.py
@@ -0,0 +1,15 @@
+from .teds import teds_score
+from .grits import (
+    html_to_cells,
+    grits_con_score,
+    grits_loc_score,
+    grits_top_score,
+)
+
+__all__ = [
+    "teds_score",
+    "html_to_cells",
+    "grits_con_score",
+    "grits_loc_score",
+    "grits_top_score",
+]
