8e6af50af9f29c4d91885b954d1f8446c5ec6ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:04:09 +0000

    Add package pocket_dimension/0.2.0/pocket_dimension-0.2.0-py3-none-any.whl

---
+++ packages/pocket_dimension/pocket_dimension-0.2.0-py3-none-any.whl/pocket_dimension/__init__.py
@@ -0,0 +1,33 @@
+"""
+Pocket Dimension provides a memory-efficient, dense, random projection of sparse vectors
+and then applies this to Term Frequency (TF) and Term Frequency, Inverse Document
+Frequency (TF-IDF) data.
+
+Copyright (C) 2022 Matthew Hendrey & Brendan Murphy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+__version__ = "0.2.0"
+
+
+from pocket_dimension.random_projection import (
+    random_projection,
+    distributional_johnson_lindenstrauss_optimal_delta,
+    random_sparse_vectors,
+    JustInTimeRandomProjection,
+)
+from pocket_dimension.vectorizer import (
+    TFVectorizer,
+    TFIDFVectorizer,
+)
