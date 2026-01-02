cfc96fe5d13c8610e90c1b71b904f99a8fa97ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 20:05:32 +0000

    Add package pyreindexer/0.5.100000/pyreindexer-0.5.100000-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/pyreindexer/pyreindexer-0.5.100000-cp312-cp312-manylinux_2_28_x86_64.whl/pyreindexer/lib/include/pyobjtools.cc
@@ -0,0 +1,245 @@
+#include "pyobjtools.h"
+
+#include "tools/serializer.h"
+#include "vendor/gason/gason.h"
+
+namespace pyreindexer {
+
+void pyValueSerialize(PyObject** value, reindexer::WrSerializer& wrSer);
+
+void pyListSerialize(PyObject** list, reindexer::WrSerializer& wrSer) {
+	if (!PyList_Check(*list)) {
+		throw reindexer::Error(ErrorCode::errParseJson, std::string("List expected, got ") + Py_TYPE(*list)->tp_name);
+	}
+
+	wrSer << '[';
+
+	Py_ssize_t sz = PyList_Size(*list);
+	for (Py_ssize_t i = 0; i < sz; ++i) {
+		if (i > 0) {
+			wrSer << ',';
+		}
+
+		PyObject* value = PyList_GetItem(*list, i);
+
+		pyValueSerialize(&value, wrSer);
+	}
+
+	wrSer << ']';
+}
+
+void pyDictSerialize(PyObject** dict, reindexer::WrSerializer& wrSer) {
+	if (!PyDict_Check(*dict)) {
+		throw reindexer::Error(ErrorCode::errParseJson,
+								std::string("Dictionary expected, got ") + Py_TYPE(*dict)->tp_name);
+	}
+
+	wrSer << '{';
+
+	Py_ssize_t sz = PyDict_Size(*dict);
+	if (sz) {
+		bool needSeparator = false;
+		PyObject *key = nullptr, *value = nullptr;
+		Py_ssize_t pos = 0;
+		while (PyDict_Next(*dict, &pos, &key, &value)) {
+			if (needSeparator) {
+				wrSer << ',';
+			} else {
+				needSeparator = true;
+			}
+			const char* k = PyUnicode_AsUTF8(key);
+			wrSer.PrintJsonString(k);
+			wrSer << ':';
+			pyValueSerialize(&value, wrSer);
+		}
+	}
+
+	wrSer << '}';
+}
+
+void pyValueSerialize(PyObject** value, reindexer::WrSerializer& wrSer) {
+	if (*value == Py_None) {
+		wrSer << "null";
+	} else if (PyBool_Check(*value)) {
+		bool v = PyLong_AsLong(*value) != 0;
+		wrSer << v;
+	} else if (PyFloat_Check(*value)) {
+		double v = PyFloat_AsDouble(*value);
+		wrSer << v;
+	} else if (PyLong_Check(*value)) {
+		long v = PyLong_AsLong(*value);
+		wrSer << v;
+	} else if (PyUnicode_Check(*value)) {
+		const char* v = PyUnicode_AsUTF8(*value);
+		wrSer.PrintJsonString(v);
+	} else if (PyList_Check(*value)) {
+		pyListSerialize(value, wrSer);
+	} else if (PyDict_Check(*value)) {
+		pyDictSerialize(value, wrSer);
+	} else {
+		throw reindexer::Error(ErrorCode::errParseJson,
+								std::string("Unable to parse value of type ") + Py_TYPE(*value)->tp_name);
+	}
+}
+
+void PyObjectToJson(PyObject** obj, reindexer::WrSerializer& wrSer) {
+	if (PyDict_Check(*obj)) {
+		pyDictSerialize(obj, wrSer);
+	} else if (PyList_Check(*obj)) {
+		pyListSerialize(obj, wrSer);
+	} else {
+		throw reindexer::Error(ErrorCode::errParseJson,
+								std::string("PyObject must be a dictionary or a list for JSON serializing, got ")
+								+ Py_TYPE(*obj)->tp_name);
+	}
+}
+
+reindexer::h_vector<std::string, 2> PyObjectToJson(PyObject** obj) {
+	reindexer::h_vector<std::string, 2> values;
+
+	reindexer::WrSerializer wrSer;
+	if (PyDict_Check(*obj)) {
+		Py_ssize_t sz = PyDict_Size(*obj);
+		if (sz) {
+			PyObject *key = nullptr, *value = nullptr;
+			Py_ssize_t pos = 0;
+			while (PyDict_Next(*obj, &pos, &key, &value)) {
+				const char* k = PyUnicode_AsUTF8(key);
+				wrSer.PrintJsonString(k);
+				wrSer << ':';
+				pyValueSerialize(&value, wrSer);
+				values.emplace_back(wrSer.Slice());
+				wrSer.Reset();
+			}
+		}
+	} else if (PyList_Check(*obj)) {
+		Py_ssize_t sz = PyList_Size(*obj);
+		for (Py_ssize_t i = 0; i < sz; ++i) {
+			PyObject* value = PyList_GetItem(*obj, i);
+			pyValueSerialize(&value, wrSer);
+			values.emplace_back(wrSer.Slice());
+			wrSer.Reset();
+		}
+	} else {
+		pyValueSerialize(obj, wrSer);
+		values.emplace_back(wrSer.Slice());
+	}
+	return values;
+}
+
+reindexer::Variant convert(PyObject** value) {
+	if (PyFloat_Check(*value)) {
+		return reindexer::Variant(PyFloat_AsDouble(*value));
+	} else if (PyBool_Check(*value)) {
+		return reindexer::Variant(PyLong_AsLong(*value) != 0);
+	} else if (PyLong_Check(*value)) {
+		return reindexer::Variant(int64_t(PyLong_AsLong(*value)));
+	} else if (PyUnicode_Check(*value)) {
+		return reindexer::Variant(std::string_view(PyUnicode_AsUTF8(*value)));
+	} else if (PyList_Check(*value)) {
+		auto size = PyList_Size(*value);
+		reindexer::VariantArray array;
+		array.reserve(size);
+		for (Py_ssize_t i = 0; i < size; ++i) {
+			auto item = PyList_GetItem(*value, i);
+			array.push_back(convert(&item));
+		}
+		return reindexer::Variant{array};
+	} else if (PyTuple_Check(*value)){
+		auto size = PyTuple_Size(*value);
+		reindexer::VariantArray array;
+		array.reserve(size);
+		for (Py_ssize_t i = 0; i < size; ++i) {
+			auto item = PyTuple_GetItem(*value, i);
+			array.push_back(convert(&item));
+		}
+		return reindexer::Variant{array};
+	} else {
+		throw reindexer::Error(ErrorCode::errParseJson,
+								std::string("Unexpected type, got ") + Py_TYPE(*value)->tp_name);
+	}
+	return {};
+}
+
+reindexer::VariantArray ParseListToVec(PyObject** list) {
+	reindexer::VariantArray result;
+
+	Py_ssize_t sz = PyList_Size(*list);
+	result.reserve(sz);
+	for (Py_ssize_t i = 0; i < sz; i++) {
+		PyObject* item = PyList_GetItem(*list, i);
+		result.push_back(convert(&item));
+	}
+
+	return result;
+}
+
+PyObject* pyValueFromJsonValue(const gason::JsonValue& value) {
+	PyObject* pyValue = nullptr;
+
+	switch (value.getTag()) {
+		case gason::JsonTag::NUMBER:
+			pyValue = PyLong_FromLongLong(value.toNumber()); // new ref
+			break;
+		case gason::JsonTag::DOUBLE:
+			pyValue = PyFloat_FromDouble(value.toDouble()); // new ref
+			break;
+		case gason::JsonTag::STRING: {
+			auto sv = value.toString();
+			pyValue = PyUnicode_FromStringAndSize(sv.data(), sv.size()); // new ref
+			break;
+		}
+		case gason::JsonTag::JSON_NULL:
+			pyValue = Py_None;
+			Py_INCREF(pyValue); // new ref
+			break;
+		case gason::JsonTag::JTRUE:
+			pyValue = Py_True;
+			Py_INCREF(pyValue); // new ref
+			break;
+		case gason::JsonTag::JFALSE:
+			pyValue = Py_False;
+			Py_INCREF(pyValue); // new ref
+			break;
+		case gason::JsonTag::ARRAY:
+			pyValue = PyList_New(0); // new ref
+			for (const auto& v : value) {
+				PyObject* dictFromJson = pyValueFromJsonValue(v.value); // stolen ref
+				PyList_Append(pyValue, dictFromJson); // new ref
+				Py_XDECREF(dictFromJson);
+			}
+			break;
+		case gason::JsonTag::OBJECT:
+			pyValue = PyDict_New(); // new ref
+			for (const auto& v : value) {
+				PyObject* dictFromJson = pyValueFromJsonValue(v.value); // stolen ref
+				PyObject* pyKey = PyUnicode_FromStringAndSize(v.key.data(), v.key.size()); // new ref
+				PyDict_SetItem(pyValue, pyKey, dictFromJson); // new refs
+				Py_XDECREF(pyKey);
+				Py_XDECREF(dictFromJson);
+			}
+			break;
+		case gason::JsonTag::EMPTY:
+			throw gason::Exception("Unexpected `JSON_EMPTY` tag");
+			break;
+	}
+
+	return pyValue;
+}
+
+PyObject* PyObjectFromJson(std::span<char> json) {
+	if (json.empty()) {
+		PyObject* pyValue = Py_None;
+		Py_INCREF(pyValue); // new ref
+		return pyValue;
+	}
+
+	try {
+		gason::JsonParser parser;
+		auto root = parser.Parse(json);
+		return pyValueFromJsonValue(root.value); // stolen ref
+	} catch (const gason::Exception& ex) {
+		throw reindexer::Error(ErrorCode::errParseJson, std::string("PyObjectFromJson: ") + ex.what());
+	}
+}
+}  // namespace pyreindexer
---
+++ packages/pyreindexer/pyreindexer-0.5.100000-cp312-cp312-manylinux_2_28_x86_64.whl/pyreindexer/tests/conftest.py
@@ -0,0 +1,239 @@
+import random
+import shutil
+
+import pytest
+
+from tests.helpers.api import ConnectorApi
+from tests.helpers.log_helper import log_fixture
+from tests.helpers.server_helper import ReindexerServer
+from tests.test_data.constants import composite_index_definition, index_definition, item_definition
+
+
+def pytest_addoption(parser):
+    parser.addoption("--mode", choices=["builtin", "cproto"], default="builtin", help="Connection mode")
+    parser.addoption("--rx_bin_path", action="store", help="path to reindexer_server", default="")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def log_setup(request):
+    log_fixture.info("Work with pyreindexer connector using {} mode".format(request.config.getoption("--mode")))
+
+
+@pytest.fixture(scope="module", autouse=True)
+def rx_server(request):
+    """
+    Start reindexer server for cproto mode
+    """
+    if request.config.getoption("--mode") == "builtin":
+        yield
+    else:
+        rx_bin_path = request.config.getoption("--rx_bin_path")
+        module = request.node.nodeid.replace(":", "_")
+        server = ReindexerServer(rx_bin_path=rx_bin_path, http_port=9088, rpc_port=6534,
+                                 storage=f"/tmp/reindex_test_{module}")
+        server.run(module=module)
+        yield
+        server.terminate()
+
+
+@pytest.fixture(scope="module")
+def db(request):
+    """
+    Create a database
+    """
+    mode = request.config.getoption("--mode")
+    prefix = "builtin://tmp/" if mode == "builtin" else "cproto://127.0.0.1:6534/"
+    db_name = "test_db"
+    db = ConnectorApi(f"{prefix}{db_name}")
+    yield db
+    db.close()
+    shutil.rmtree("tmp/", ignore_errors=True)
+
+
+@pytest.fixture
+def namespace(db):
+    """
+    Create a namespace
+    """
+    ns_name = 'new_ns'
+    db.namespace.open(ns_name)
+    yield ns_name
+    db.namespace.drop(ns_name)
+
+
+@pytest.fixture
+def index(db, namespace):
+    """
+    Create an index to namespace
+    """
+    db.index.create(namespace, index_definition)
+    yield
+
+
+@pytest.fixture
+def sparse_index(db, namespace):
+    """
+    Create sparse index to namespace
+    """
+    db.index.create(namespace, {"name": "val", "json_paths": ["val"], "field_type": "string", "index_type": "hash",
+                                "is_sparse": True})
+    yield
+
+
+@pytest.fixture
+def indexes(db, namespace):
+    """
+    Create two indexes to namespace
+    """
+    db.index.create(namespace, index_definition)
+    db.index.create(namespace, {"name": "val", "json_paths": ["val"], "field_type": "string", "index_type": "hash"})
+    yield
+
+
+@pytest.fixture
+def item(db, namespace):
+    """
+    Create an item to namespace
+    """
+    db.item.insert(namespace, item_definition)
+    yield item_definition
+
+
+@pytest.fixture
+def items(db, namespace):
+    """
+    Create items to namespace
+    """
+    items = [{"id": i, "val": f"testval{i}"} for i in range(10)]
+    for item in items:
+        db.item.insert(namespace, item)
+    yield items
+
+
+@pytest.fixture
+def items_shuffled(db, namespace):
+    """
+    Create items in random order
+    """
+    items = [{"id": i, "val": f"testval{i}"} for i in range(5)]
+    random.shuffle(items)
+    for item in items:
+        db.item.insert(namespace, item)
+    yield items
+
+
+@pytest.fixture
+def composite_index(db, namespace):
+    """
+    Create indexes and composite index from them
+    """
+    db.index.create(namespace, index_definition)
+    db.index.create(namespace, {"name": "val", "json_paths": ["val"], "field_type": "string", "index_type": "hash"})
+    db.index.create(namespace, composite_index_definition)
+    yield
+
+
+@pytest.fixture
+def rtree_index_and_items(db, namespace):
+    """
+    Create rtree index and items
+    """
+    db.index.create(namespace, {"name": "rtree", "json_paths": ["rtree"], "field_type": "point",
+                                "index_type": "rtree", "rtree_type": "rstar"})
+    items = [{"id": i, "rtree": [i, i]} for i in range(10)]
+    for item in items:
+        db.item.insert(namespace, item)
+    yield items
+
+
+@pytest.fixture
+def ft_index_and_items(db, namespace):
+    """
+    Create rtree index and items
+    """
+    db.index.create(namespace, {"name": "ft", "json_paths": ["ft"], "field_type": "string", "index_type": "text"})
+    content = ["one word", "sword two", "three work 333"]
+    items = [{"id": i, "ft": c} for i, c in enumerate(content)]
+    for item in items:
+        db.item.insert(namespace, item)
+    yield items
+
+
+@pytest.fixture
+def array_index_and_items(db, namespace):
+    """
+    Create rtree index and items
+    """
+    db.index.create(namespace, {"name": "arr", "json_paths": ["arr"], "field_type": "int", "index_type": "tree",
+                                "is_array": True})
+    items = [{"id": i, "arr": [i, i + 1]} for i in range(5)]
+    for item in items:
+        db.item.insert(namespace, item)
+    yield items
+
+
+@pytest.fixture
+def index_and_duplicate_items(db, namespace):
+    """
+    Create index and items with duplicate value
+    """
+    db.index.create(namespace, {"name": "idx", "json_paths": ["idx"], "field_type": "int", "index_type": "hash"})
+    items = [{"id": 0, "idx": 0}, {"id": 1, "idx": 1}, {"id": 2, "idx": 1}, {"id": 3, "idx": 3}]
+    for item in items:
+        db.item.insert(namespace, item)
+    yield items
+
+
+@pytest.fixture
+def array_indexes_and_items(db, namespace):
+    """
+    Create array indexes and items
+    """
+    db.index.create(namespace, {"name": "arr1", "json_paths": ["arr1"], "field_type": "int",
+                                "index_type": "tree", "is_array": True})
+    db.index.create(namespace, {"name": "arr2", "json_paths": ["arr2"], "field_type": "int",
+                                "index_type": "tree", "is_array": True})
+    items = [{"id": i, "arr1": [i, i % 2], "arr2": [i % 2, i]} for i in range(5)]
+    for item in items:
+        db.item.insert(namespace, item)
+    yield items
+
+
+@pytest.fixture
+def metadata(db, namespace):
+    """
+    Put metadata  to namespace
+    """
+    key, value = 'key', 'value'
+    db.meta.put(namespace, key, value)
+    yield key, value
+
+
+@pytest.fixture
+def second_namespace(db):
+    second_namespace_name = "test_ns_for_join"
+    db.namespace.open(second_namespace_name)
+    db.index.create(second_namespace_name, index_definition)
+    yield second_namespace_name
+    db.namespace.drop(second_namespace_name)
+
+
+@pytest.fixture
+def second_item(db, second_namespace):
+    """
+    Create item for the second namespace
+    """
+    item = {"id": 1, "second_ns_val": "second_ns_testval_1"}
+    db.item.insert(second_namespace, item)
+    yield item
+
+
+@pytest.fixture
+def second_items(db, second_namespace):
+    """
+    Create more items for the second namespace
+    """
+    items = [{"id": i, "second_ns_val": f"second_ns_testval_{i}"} for i in range(1, 6)]
+    for item in items:
+        db.item.insert(second_namespace, item)
+    yield items
