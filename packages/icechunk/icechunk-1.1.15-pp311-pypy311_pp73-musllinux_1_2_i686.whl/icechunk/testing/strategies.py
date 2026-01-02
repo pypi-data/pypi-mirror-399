8ef02a51cf27b41fb5fc4a96206c5ce964184bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:04:28 +0000

    Add package icechunk/1.1.15/icechunk-1.1.15-pp311-pypy311_pp73-musllinux_1_2_i686.whl

---
+++ packages/icechunk/icechunk-1.1.15-pp311-pypy311_pp73-musllinux_1_2_i686.whl/icechunk/testing/strategies.py
@@ -0,0 +1,56 @@
+from collections.abc import Iterable
+from typing import TYPE_CHECKING, Any
+
+import hypothesis.strategies as st
+
+import icechunk as ic
+import zarr
+
+if TYPE_CHECKING:
+    try:
+        from zarr.core.metadata import ArrayV3Metadata
+    except ImportError:
+        ArrayV3Metadata = Any  # type: ignore[misc,assignment]
+
+
+@st.composite
+def splitting_configs(
+    draw: st.DrawFn, *, arrays: "Iterable[zarr.Array[ArrayV3Metadata]]"
+) -> ic.ManifestSplittingConfig:
+    config_dict: dict[
+        ic.ManifestSplitCondition,
+        dict[
+            ic.ManifestSplitDimCondition.Axis
+            | ic.ManifestSplitDimCondition.DimensionName
+            | ic.ManifestSplitDimCondition.Any,
+            int,
+        ],
+    ] = {}
+    for array in arrays:
+        if draw(st.booleans()):
+            array_condition = ic.ManifestSplitCondition.name_matches(
+                array.path.split("/")[-1]
+            )
+        else:
+            array_condition = ic.ManifestSplitCondition.path_matches(array.path)
+        dimnames = (
+            getattr(array.metadata, "dimension_names", None) or (None,) * array.ndim
+        )
+        dimsize_axis_names = draw(
+            st.lists(
+                st.sampled_from(
+                    tuple(zip(array.shape, range(array.ndim), dimnames, strict=False))
+                ),
+                min_size=1,
+                unique=True,
+            )
+        )
+        for size, axis, dimname in dimsize_axis_names:
+            if dimname is None or draw(st.booleans()):
+                key = ic.ManifestSplitDimCondition.Axis(axis)
+            else:
+                key = ic.ManifestSplitDimCondition.DimensionName(dimname)  # type: ignore[assignment]
+            config_dict[array_condition] = {
+                key: draw(st.integers(min_value=1, max_value=size + 10))
+            }
+    return ic.ManifestSplittingConfig.from_dict(config_dict)
