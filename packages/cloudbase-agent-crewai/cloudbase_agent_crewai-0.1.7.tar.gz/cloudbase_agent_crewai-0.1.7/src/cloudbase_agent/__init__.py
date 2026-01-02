c46ba01ee863578db614c0ab80b273cad4fc8315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:06:01 +0000

    Add package cloudbase-agent-crewai/0.1.7/cloudbase_agent_crewai-0.1.7.tar.gz

---
+++ packages/cloudbase-agent-crewai/cloudbase_agent_crewai-0.1.7.tar.gz/cloudbase_agent_crewai-0.1.7/src/cloudbase_agent/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Cloudbase Agent CrewAI Package.
+
+This is a namespace package that extends the base Cloudbase Agent package with CrewAI integration.
+"""
+
+# Declare this as a namespace package to allow other cloudbase_agent subpackages to coexist
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+__all__ = []
