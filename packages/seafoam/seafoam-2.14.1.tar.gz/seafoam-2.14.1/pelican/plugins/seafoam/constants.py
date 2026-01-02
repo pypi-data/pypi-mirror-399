cf3d862d8c80e692ada7465c298266cce79d1855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:04:15 +0000

    Add package seafoam/2.14.1/seafoam-2.14.1.tar.gz

---
+++ packages/seafoam/seafoam-2.14.1.tar.gz/seafoam-2.14.1/pelican/plugins/seafoam/constants.py
@@ -0,0 +1,20 @@
+__title__ = "seafoam"
+__version__ = "2.14.1"
+__description__ = "Pelican theme, first used for Minchin.ca."
+__author__ = "W. Minchin"
+__email__ = "w_minchin@hotmail.com"
+__url__ = "http://blog.minchin.ca/label/seafoam/"
+__license__ = "MIT License"
+
+LOG_PREFIX = "[Seafoam]"
+PLUGIN_LIST = [
+    "pelican.plugins.seafoam",
+    "pelican.plugins.jinja_filters",
+]
+PRODUCTION_PLUGIN_LIST = PLUGIN_LIST + [
+    "pelican.plugins.image_process",
+]
+STYLES = [
+    "seafoam",
+    "strathcona",
+]
