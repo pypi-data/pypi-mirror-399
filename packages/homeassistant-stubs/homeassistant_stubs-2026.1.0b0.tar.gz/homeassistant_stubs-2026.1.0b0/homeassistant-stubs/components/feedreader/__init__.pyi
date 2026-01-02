0c7c3f1d7f13f794a820ab1857446d35ed40a085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 23:05:41 +0000

    Add package homeassistant-stubs/2026.1.0b0/homeassistant_stubs-2026.1.0b0.tar.gz

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2026.1.0b0.tar.gz/homeassistant_stubs-2026.1.0b0/homeassistant-stubs/components/feedreader/__init__.pyi
@@ -0,0 +1,11 @@
+from .const import DOMAIN as DOMAIN
+from .coordinator import FeedReaderConfigEntry as FeedReaderConfigEntry, FeedReaderCoordinator as FeedReaderCoordinator, StoredData as StoredData
+from homeassistant.const import Platform as Platform
+from homeassistant.core import HomeAssistant as HomeAssistant
+from homeassistant.util.hass_dict import HassKey as HassKey
+
+CONF_URLS: str
+MY_KEY: HassKey[StoredData]
+
+async def async_setup_entry(hass: HomeAssistant, entry: FeedReaderConfigEntry) -> bool: ...
+async def async_unload_entry(hass: HomeAssistant, entry: FeedReaderConfigEntry) -> bool: ...
