21a9a1482b2fd58a80ed8bbed6967b4be9cf723c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:05:59 +0000

    Add package manimera/0.4.13/manimera-0.4.13-py3-none-any.whl

---
+++ packages/manimera/manimera-0.4.13-py3-none-any.whl/manimera/components/__init__.py
@@ -0,0 +1,43 @@
+# ======================================================================================================================
+# IMPORTS
+# ======================================================================================================================
+
+# STANDARD IMPORTS =====================================================================================================
+
+# <None>
+
+# THIRD PARTY IMPORTS ==================================================================================================
+
+# <None>
+
+# MANIMERA IMPORTS =====================================================================================================
+
+from manimera.components.anatomical_eye import AnatomicalEye
+from manimera.components.atomic_clock import AtomicClock
+from manimera.components.bohr_atom import BohrAtom
+from manimera.components.brick import Brick
+from manimera.components.cathedral_lamp import CathedralLamp
+from manimera.components.clock import Clock
+from manimera.components.feather import Feather
+from manimera.components.network_tower import NetworkTower
+from manimera.components.pendulum import Pendulum
+
+# ======================================================================================================================
+# WILDCARD EXPORTS
+# ======================================================================================================================
+
+__all__ = [
+    "AnatomicalEye",
+    "AtomicClock",
+    "BohrAtom",
+    "Brick",
+    "CathedralLamp",
+    "Clock",
+    "Feather",
+    "NetworkTower",
+    "Pendulum",
+]
+
+# ======================================================================================================================
+# END
+# ======================================================================================================================
