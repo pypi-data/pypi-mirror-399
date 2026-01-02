febfa6e4e6de447ff8f64ea1e0a7f3dc224e4f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 20:08:59 +0000

    Add package android-notify/1.60.5/android_notify-1.60.5-py3-none-any.whl

---
+++ packages/android-notify/android_notify-1.60.5-py3-none-any.whl/android_notify/an_types.py
@@ -0,0 +1,328 @@
+"""For autocomplete Storing Reference to Available Methods"""
+from typing import Literal
+
+Importance = Literal['urgent', 'high', 'medium', 'low', 'none']
+"""
+    :argument urgent - Makes a sound and appears as a heads-up notification.
+    
+    :argument high - Makes a sound.
+    
+    :argument urgent - Makes no sound.
+    
+    :argument urgent - Makes no sound and doesn't appear in the status bar.
+    
+    :argument urgent - Makes no sound and doesn't in the status bar or shade.
+"""
+
+
+# For Dev
+# Idea for typing autocompletion and reference
+class Bundle:
+    def putString(self, key, value):
+        print(f"[MOCK] Bundle.putString called with key={key}, value={value}")
+
+    def putInt(self, key, value):
+        print(f"[MOCK] Bundle.putInt called with key={key}, value={value}")
+
+
+class String(str):
+    def __new__(cls, value):
+        print(f"[MOCK] String created with value={value}")
+        return str.__new__(cls, value)
+
+
+class Intent:
+    FLAG_ACTIVITY_NEW_TASK = 'FACADE_FLAG_ACTIVITY_NEW_TASK'
+    CATEGORY_DEFAULT = 'FACADE_FLAG_CATEGORY_DEFAULT'
+
+    def __init__(self, context='', activity=''):
+        self.obj = {}
+        print(f"[MOCK] Intent initialized with context={context}, activity={activity}")
+
+    def setAction(self, action):
+        print(f"[MOCK] Intent.setAction called with: {action}")
+        return self
+
+    def addFlags(self, *flags):
+        print(f"[MOCK] Intent.addFlags called with: {flags}")
+        return self
+
+    def setData(self, uri):
+        print(f"[MOCK] Intent.setData called with: {uri}")
+        return self
+
+    def setFlags(self, intent_flag):
+        print(f"[MOCK] Intent.setFlags called with: {intent_flag}")
+        return self
+
+    def addCategory(self, intent_category):
+        print(f"[MOCK] Intent.addCategory called with: {intent_category}")
+        return self
+
+    def getAction(self):
+        print("[MOCK] Intent.getAction called")
+        return self
+
+    def getStringExtra(self, key):
+        print(f"[MOCK] Intent.getStringExtra called with key={key}")
+        return self
+
+    def putExtra(self, key, value):
+        self.obj[key] = value
+        print(f"[MOCK] Intent.putExtra called with key={key}, value={value}")
+
+    def putExtras(self, bundle: Bundle):
+        self.obj['bundle'] = bundle
+        print(f"[MOCK] Intent.putExtras called with bundle={bundle}")
+
+
+class PendingIntent:
+    FLAG_IMMUTABLE = ''
+    FLAG_UPDATE_CURRENT = ''
+
+    def getActivity(self, context, value, action_intent, pending_intent_type):
+        print(
+            f"[MOCK] PendingIntent.getActivity called with context={context}, value={value}, action_intent={action_intent}, type={pending_intent_type}")
+
+
+class BitmapFactory:
+    def decodeStream(self, stream):
+        print(f"[MOCK] BitmapFactory.decodeStream called with stream={stream}")
+
+
+class BuildVersion:
+    SDK_INT = 0
+
+class Manifest:
+    POST_NOTIFICATIONS = 'FACADE_IMPORT'
+
+class Settings:
+    ACTION_APP_NOTIFICATION_SETTINGS = 'FACADE_IMPORT_ACTION_APP_NOTIFICATION_SETTINGS'
+    EXTRA_APP_PACKAGE = 'FACADE_IMPORT_EXTRA_APP_PACKAGE'
+    ACTION_APPLICATION_DETAILS_SETTINGS = 'FACADE_IMPORT_ACTION_APPLICATION_DETAILS_SETTINGS'
+
+class Uri:
+    def __init__(self,package_name):
+        print("FACADE_URI")
+
+class NotificationManager:
+    pass
+
+
+class NotificationChannel:
+    def __init__(self, channel_id, channel_name, importance):
+        self.description = None
+        self.channel_id = channel_id
+        self.channel = None
+        print(
+            f"[MOCK] NotificationChannel initialized with id={channel_id}, name={channel_name}, importance={importance}")
+
+    def createNotificationChannel(self, channel):
+        self.channel = channel
+        print(f"[MOCK] NotificationChannel.createNotificationChannel called with channel={channel}")
+
+    def getNotificationChannel(self, channel_id):
+        self.channel_id = channel_id
+        print(f"[MOCK] NotificationChannel.getNotificationChannel called with id={channel_id}")
+
+    def setDescription(self, description):
+        self.description = description
+        print(f"[MOCK] NotificationChannel.setDescription called with description={description}")
+
+    def getId(self):
+        print(f"[MOCK] NotificationChannel.getId called, returning {self.channel_id}")
+        return self.channel_id
+
+
+class IconCompat:
+    def createWithBitmap(self, bitmap):
+        print(f"[MOCK] IconCompat.createWithBitmap called with bitmap={bitmap}")
+
+
+class Color:
+    def __init__(self):
+        print("[MOCK] Color initialized")
+
+    def parseColor(self, color: str):
+        print(f"[MOCK] Color.parseColor called with color={color}")
+        return self
+
+
+class RemoteViews:
+    def __init__(self, package_name, small_layout_id):
+        print(f"[MOCK] RemoteViews initialized with package_name={package_name}, layout_id={small_layout_id}")
+
+    def createWithBitmap(self, bitmap):
+        print(f"[MOCK] RemoteViews.createWithBitmap called with bitmap={bitmap}")
+
+    def setTextViewText(self, id, text):
+        print(f"[MOCK] RemoteViews.setTextViewText called with id={id}, text={text}")
+
+    def setTextColor(self, id, color: Color):
+        print(f"[MOCK] RemoteViews.setTextColor called with id={id}, color={color}")
+
+
+class NotificationManagerCompat:
+    IMPORTANCE_HIGH = 4
+    IMPORTANCE_DEFAULT = 3
+    IMPORTANCE_LOW = ''
+    IMPORTANCE_MIN = ''
+    IMPORTANCE_NONE = ''
+
+
+class NotificationCompat:
+    DEFAULT_ALL = 3
+    PRIORITY_HIGH = 4
+    PRIORITY_DEFAULT = ''
+    PRIORITY_LOW = ''
+    PRIORITY_MIN = ''
+
+
+class MActions:
+    def clear(self):
+        """This Removes all buttons"""
+        print('[MOCK] MActions.clear called')
+
+
+class NotificationCompatBuilder:
+    def __init__(self, context, channel_id):
+        self.mActions = MActions()
+        pass
+    def setProgress(self,max_value,current_value,endless):
+        pass
+    def setStyle(self,style):
+        pass
+    def setContentTitle(self,title):
+        pass
+    def setContentText(self,text):
+        pass
+
+    def setSmallIcon(self, icon):
+        print(f"[MOCK] setSmallIcon called with icon={icon}")
+
+    def setLargeIcon(self, icon):
+        print(f"[MOCK] setLargeIcon called with icon={icon}")
+
+    def setAutoCancel(self, auto_cancel: bool):
+        print(f"[MOCK] setAutoCancel called with auto_cancel={auto_cancel}")
+
+    def setPriority(self, priority: Importance):
+        print(f"[MOCK] setPriority called with priority={priority}")
+
+    def setDefaults(self, defaults):
+        print(f"[MOCK] setDefaults called with defaults={defaults}")
+
+    def setOngoing(self, persistent: bool):
+        print(f"[MOCK] setOngoing called with persistent={persistent}")
+
+    def setOnlyAlertOnce(self, state):
+        print(f"[MOCK] setOnlyAlertOnce called with state={state}")
+
+    def build(self):
+        print("[MOCK] build called")
+
+    def setContentIntent(self, pending_action_intent: PendingIntent):
+        print(f"[MOCK] setContentIntent called with {pending_action_intent}")
+
+    def addAction(self, icon_int, action_text, pending_action_intent):
+        print(f"[MOCK] addAction called with icon={icon_int}, text={action_text}, intent={pending_action_intent}")
+
+    def setShowWhen(self, state):
+        print(f"[MOCK] setShowWhen called with state={state}")
+
+    def setWhen(self, time_ms):
+        print(f"[MOCK] setWhen called with time_ms={time_ms}")
+
+    def setCustomContentView(self, layout):
+        print(f"[MOCK] setCustomContentView called with layout={layout}")
+
+    def setCustomBigContentView(self, layout):
+        print(f"[MOCK] setCustomBigContentView called with layout={layout}")
+
+    def setSubText(self, text):
+        print(f"[MOCK] setSubText called with text={text}")
+
+    def setColor(self, color: Color) -> None:
+        print(f"[MOCK] setColor called with color={color}")
+
+
+class NotificationCompatBigTextStyle:
+    def bigText(self, body):
+        print(f"[MOCK] NotificationCompatBigTextStyle.bigText called with body={body}")
+        return self
+
+
+class NotificationCompatBigPictureStyle:
+    def bigPicture(self, bitmap):
+        print(f"[MOCK] NotificationCompatBigPictureStyle.bigPicture called with bitmap={bitmap}")
+        return self
+
+
+class NotificationCompatInboxStyle:
+    def addLine(self, line):
+        print(f"[MOCK] NotificationCompatInboxStyle.addLine called with line={line}")
+        return self
+
+
+class NotificationCompatDecoratedCustomViewStyle:
+    def __init__(self):
+        print("[MOCK] NotificationCompatDecoratedCustomViewStyle initialized")
+
+
+class Permission:
+    POST_NOTIFICATIONS = ''
+
+
+def check_permission(permission: Permission.POST_NOTIFICATIONS):
+    print(f"[MOCK] check_permission called with {permission}")
+    print(permission)
+
+
+def request_permissions(_list: [], _callback):
+    print(f"[MOCK] request_permissions called with {_list}")
+    _callback()
+
+
+class AndroidActivity:
+    def bind(self, on_new_intent):
+        print(f"[MOCK] AndroidActivity.bind called with {on_new_intent}")
+
+    def unbind(self, on_new_intent):
+        print(f"[MOCK] AndroidActivity.unbind called with {on_new_intent}")
+
+
+class PythonActivity:
+    mActivity = "[MOCK] mActivity used"
+    def __init__(self):
+        print("[MOCK] PythonActivity initialized")
+
+
+class DummyIcon:
+    icon = 101
+
+    def __init__(self):
+        print("[MOCK] DummyIcon initialized")
+
+
+class Context:
+    def __init__(self):
+        print("[MOCK] Context initialized")
+        pass
+
+    @staticmethod
+    def getApplicationInfo():
+        print("[MOCK] Context.getApplicationInfo called")
+        return DummyIcon
+
+    @staticmethod
+    def getResources():
+        print("[MOCK] Context.getResources called")
+        return None
+
+    @staticmethod
+    def getPackageName():
+        print("[MOCK] Context.getPackageName called")
+        return None  # TODO get package name from buildozer.spec file
+
+# Now writing Knowledge from errors
+# notify.(int, Builder.build()) # must be int
---
+++ packages/android-notify/android_notify-1.60.5-py3-none-any.whl/android_notify/config.py
@@ -0,0 +1,171 @@
+import os, traceback
+
+ON_ANDROID = False
+__version__ = "1.60.5"
+
+
+def is_platform_android():
+    if os.getenv("MAIN_ACTIVITY_HOST_CLASS_NAME"):
+        return True
+    kivy_build = os.environ.get('KIVY_BUILD', '')
+    if kivy_build in {'android'}:
+        return True
+    elif 'P4A_BOOTSTRAP' in os.environ:
+        return True
+    elif 'ANDROID_ARGUMENT' in os.environ:
+        return True
+
+    return False
+
+
+def on_flet_app():
+    return os.getenv("MAIN_ACTIVITY_HOST_CLASS_NAME")
+
+
+def get_activity_class_name():
+    ACTIVITY_CLASS_NAME = os.getenv("MAIN_ACTIVITY_HOST_CLASS_NAME")  # flet python
+    if not ACTIVITY_CLASS_NAME:
+        try:
+            from android import config  # type: ignore
+            ACTIVITY_CLASS_NAME = config.JAVA_NAMESPACE
+        except (ImportError, AttributeError):
+            ACTIVITY_CLASS_NAME = 'org.kivy.android'
+    return ACTIVITY_CLASS_NAME
+
+
+if is_platform_android():
+    try:
+        from jnius import cast, autoclass
+    except Exception as error_importing_frm_jnius:
+        print('android-notify: No pjnius, not on android? Error-',error_importing_frm_jnius)
+        # So commandline still works if java isn't installed and get pyjinus import error
+        cast = lambda x: x
+        autoclass = lambda x: None
+
+    try:
+        # Android Imports
+
+        # Get the required Java classes needs to on android to import
+        Bundle = autoclass('android.os.Bundle')
+        String = autoclass('java.lang.String')
+        Intent = autoclass('android.content.Intent')
+        PendingIntent = autoclass('android.app.PendingIntent')
+        BitmapFactory = autoclass('android.graphics.BitmapFactory')
+        BuildVersion = autoclass('android.os.Build$VERSION')
+        NotificationManager = autoclass('android.app.NotificationManager')
+        NotificationChannel = autoclass('android.app.NotificationChannel')
+        RemoteViews = autoclass('android.widget.RemoteViews')
+        Settings = autoclass("android.provider.Settings")
+        Uri = autoclass("android.net.Uri")
+        Manifest = autoclass('android.Manifest$permission')
+
+        ON_ANDROID = bool(RemoteViews)
+    except Exception as e:
+        from .an_types import *
+        print('Exception: ', e)
+        print(traceback.format_exc())
+else:
+    from .an_types import *
+    cast = lambda x: x
+    autoclass = lambda x: None
+
+if ON_ANDROID:
+    try:
+        NotificationManagerCompat = autoclass('androidx.core.app.NotificationManagerCompat')
+        NotificationCompat = autoclass('androidx.core.app.NotificationCompat')
+        IconCompat = autoclass('androidx.core.graphics.drawable.IconCompat')
+        Color = autoclass('android.graphics.Color')
+
+        # Notification Design
+        NotificationCompatBuilder = autoclass('androidx.core.app.NotificationCompat$Builder')
+        NotificationCompatBigTextStyle = autoclass('androidx.core.app.NotificationCompat$BigTextStyle')
+        NotificationCompatBigPictureStyle = autoclass('androidx.core.app.NotificationCompat$BigPictureStyle')
+        NotificationCompatInboxStyle = autoclass('androidx.core.app.NotificationCompat$InboxStyle')
+        NotificationCompatDecoratedCustomViewStyle = autoclass('androidx.core.app.NotificationCompat$DecoratedCustomViewStyle')
+
+    except Exception as dependencies_import_error:
+        print('dependencies_import_error: ', dependencies_import_error)
+        print("""
+        Dependency Error: Add the following in buildozer.spec:
+        * android.gradle_dependencies = androidx.core:core-ktx:1.15.0, androidx.core:core:1.6.0
+        * android.enable_androidx = True
+        """)
+
+        from .an_types import *
+else:
+    from .an_types import *
+
+
+def from_service_file():
+    return 'PYTHON_SERVICE_ARGUMENT' in os.environ
+
+
+run_on_ui_thread = None
+if on_flet_app() or from_service_file() or not ON_ANDROID:
+    def run_on_ui_thread(func):
+        """Fallback for Developing on PC"""
+
+        def wrapper(*args, **kwargs):
+            # print("Simulating run on UI thread")
+            return func(*args, **kwargs)
+
+        return wrapper
+else:  # TODO find var for kivy
+    from android.runnable import run_on_ui_thread
+
+
+def get_python_activity():
+    if not ON_ANDROID:
+        from .an_types import PythonActivity
+        return PythonActivity
+    ACTIVITY_CLASS_NAME = get_activity_class_name()
+    if on_flet_app():
+        PythonActivity = autoclass(ACTIVITY_CLASS_NAME)
+    else:
+        PythonActivity = autoclass(ACTIVITY_CLASS_NAME + '.PythonActivity')
+    return PythonActivity
+
+
+def get_python_service():
+    if not ON_ANDROID:
+        return None
+    PythonService = autoclass(get_activity_class_name() + '.PythonService')
+    return PythonService.mService
+
+
+def get_python_activity_context():
+    if not ON_ANDROID:
+        from .an_types import Context
+        return Context
+
+    PythonActivity = get_python_activity()
+    if from_service_file():
+        service = get_python_service()
+        context = service.getApplication().getApplicationContext()
+    else:
+        context = PythonActivity.mActivity
+    return context
+
+
+if ON_ANDROID:
+    context = get_python_activity_context()
+else:
+    context = None
+
+
+def get_notification_manager():
+    if not ON_ANDROID:
+        return None
+    notification_service = context.getSystemService(context.NOTIFICATION_SERVICE)
+    return cast(NotificationManager, notification_service)
+
+
+def app_storage_path():
+    if on_flet_app():
+        return os.path.join(context.getFilesDir().getAbsolutePath(), 'flet')
+    else:
+        try:
+            from android.storage import app_storage_path as kivy_app_storage_path  # type: ignore
+            return kivy_app_storage_path()
+        except Exception as e:
+            return './'  # TODO return file main.py path (not android)
