84bd20629762b9d6cab78fda10f5844497fa04e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:06:05 +0000

    Add package UnderAutomation.UniversalRobots/8.2.0.0/underautomation_universalrobots-8.2.0.0-py3-none-any.whl

---
+++ packages/UnderAutomation.UniversalRobots/underautomation_universalrobots-8.2.0.0-py3-none-any.whl/underautomation/universal_robots/dashboard/internal/dashboard_client_parameters_base.py
@@ -0,0 +1,36 @@
+import typing
+import clr
+import os
+clr.AddReference(os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..",  'lib', 'UnderAutomation.UniversalRobots.dll')))
+from UnderAutomation.UniversalRobots.Dashboard.Internal import DashboardClientParametersBase as dashboard_client_parameters_base
+
+class DashboardClientParametersBase:
+	def __init__(self, _internal = 0):
+		if(_internal == 0):
+			self._instance = dashboard_client_parameters_base()
+		else:
+			self._instance = _internal
+	@property
+	def port(self) -> int:
+		return self._instance.Port
+	@port.setter
+	def port(self, value: int):
+		self._instance.Port = value
+	@property
+	def receive_timeout_ms(self) -> int:
+		return self._instance.ReceiveTimeoutMs
+	@receive_timeout_ms.setter
+	def receive_timeout_ms(self, value: int):
+		self._instance.ReceiveTimeoutMs = value
+	@property
+	def send_timeout_ms(self) -> int:
+		return self._instance.SendTimeoutMs
+	@send_timeout_ms.setter
+	def send_timeout_ms(self, value: int):
+		self._instance.SendTimeoutMs = value
+
+DashboardClientParametersBase.defaul_t__port = DashboardClientParametersBase(dashboard_client_parameters_base.DEFAULT_PORT)
+
+DashboardClientParametersBase.defaul_t__receiv_e__timeou_t__ms = DashboardClientParametersBase(dashboard_client_parameters_base.DEFAULT_RECEIVE_TIMEOUT_MS)
+
+DashboardClientParametersBase.defaul_t__sen_d__timeou_t__ms = DashboardClientParametersBase(dashboard_client_parameters_base.DEFAULT_SEND_TIMEOUT_MS)
---
+++ packages/UnderAutomation.UniversalRobots/underautomation_universalrobots-8.2.0.0-py3-none-any.whl/underautomation/universal_robots/rtde/rtde_client.py
@@ -0,0 +1,24 @@
+import typing
+from underautomation.universal_robots.rtde.rtde_output_setup import RtdeOutputSetup
+from underautomation.universal_robots.rtde.rtde_input_setup import RtdeInputSetup
+from underautomation.universal_robots.rtde.rtde_versions import RtdeVersions
+from underautomation.universal_robots.rtde.rtde_outputs_description import RtdeOutputsDescription
+from underautomation.universal_robots.rtde.rtde_inputs_description import RtdeInputsDescription
+from underautomation.universal_robots.rtde.internal.rtde_client_base import RtdeClientBase
+import clr
+import os
+clr.AddReference(os.path.realpath(os.path.join(os.path.dirname(__file__), "..",  'lib', 'UnderAutomation.UniversalRobots.dll')))
+from UnderAutomation.UniversalRobots.Rtde import RtdeClient as rtde_client
+
+class RtdeClient(RtdeClientBase):
+	def __init__(self, _internal = 0):
+		if(_internal == 0):
+			self._instance = rtde_client()
+		else:
+			self._instance = _internal
+	def connect(self, ip: str, outputSetup: RtdeOutputSetup, inputSetup: RtdeInputSetup, version: RtdeVersions, frequency: float, port: int=30004) -> None:
+		self._instance.Connect(ip, outputSetup._instance if outputSetup else None, inputSetup._instance if inputSetup else None, version, frequency, port)
+
+RtdeClient.all_outputs_description = RtdeClient(rtde_client.AllOutputsDescription)
+
+RtdeClient.all_inputs_description = RtdeClient(rtde_client.AllInputsDescription)
