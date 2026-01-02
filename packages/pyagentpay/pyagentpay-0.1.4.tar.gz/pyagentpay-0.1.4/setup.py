b658e957ec37c70c9e489a5c3f474356f593ec8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:04:13 +0000

    Add package pyagentpay/0.1.4/pyagentpay-0.1.4.tar.gz

---
+++ packages/pyagentpay/pyagentpay-0.1.4.tar.gz/pyagentpay-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyagentpay",
+    version="0.1.4",
+    description="Infraestructura financiera universal para Agentes de IA",
+    author="Jairo Gelpi",
+    author_email="jairo.gelpi@agentpay.io",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",  # Instalará requests automáticamente
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
