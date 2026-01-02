13fb3cbb5b87350e6a3faeec3460656ca039e0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 23:05:38 +0000

    Add package egeometry/0.20.1/egeometry-0.20.1-cp313-cp313-win_amd64.whl

---
+++ packages/egeometry/egeometry-0.20.1-cp313-cp313-win_amd64.whl/egeometry/__init__.py
@@ -0,0 +1,113 @@
+# generated from codegen/templates/__init__.py
+
+from __future__ import annotations
+
+__all__ = [
+    "separating_axis_theorem",
+    "DBoundingBox2d",
+    "DBoundingBox2dOverlappable",
+    "HasDBoundingBox2d",
+    "FBoundingBox2d",
+    "FBoundingBox2dOverlappable",
+    "HasFBoundingBox2d",
+    "IBoundingBox2d",
+    "IBoundingBox2dOverlappable",
+    "HasIBoundingBox2d",
+    "DBoundingBox3d",
+    "DBoundingBox3dOverlappable",
+    "HasDBoundingBox3d",
+    "DBoundingBox3dRaycastResult",
+    "FBoundingBox3d",
+    "FBoundingBox3dOverlappable",
+    "HasFBoundingBox3d",
+    "FBoundingBox3dRaycastResult",
+    "IBoundingBox3d",
+    "IBoundingBox3dOverlappable",
+    "HasIBoundingBox3d",
+    "DCircle",
+    "DCircleOverlappable",
+    "FCircle",
+    "FCircleOverlappable",
+    "ICircle",
+    "ICircleOverlappable",
+    "DLineSegment3d",
+    "FLineSegment3d",
+    "DPlane",
+    "DPlaneRaycastResult",
+    "FPlane",
+    "FPlaneRaycastResult",
+    "DRectangle",
+    "DRectangleOverlappable",
+    "FRectangle",
+    "FRectangleOverlappable",
+    "IRectangle",
+    "IRectangleOverlappable",
+    "DRectangleFrustum",
+    "DRectangleFrustumOverlappable",
+    "FRectangleFrustum",
+    "FRectangleFrustumOverlappable",
+    "DTriangle2d",
+    "DTriangle2dOverlappable",
+    "FTriangle2d",
+    "FTriangle2dOverlappable",
+    "ITriangle2d",
+    "ITriangle2dOverlappable",
+    "DTriangleMesh3d",
+    "DTriangleMesh3dRaycastResult",
+    "FTriangleMesh3d",
+    "FTriangleMesh3dRaycastResult",
+]
+
+from ._dboundingbox2d import DBoundingBox2d
+from ._dboundingbox2d import DBoundingBox2dOverlappable
+from ._dboundingbox2d import HasDBoundingBox2d
+from ._dboundingbox3d import DBoundingBox3d
+from ._dboundingbox3d import DBoundingBox3dOverlappable
+from ._dboundingbox3d import DBoundingBox3dRaycastResult
+from ._dboundingbox3d import HasDBoundingBox3d
+from ._dcircle import DCircle
+from ._dcircle import DCircleOverlappable
+from ._dlinesegment3d import DLineSegment3d
+from ._dplane import DPlane
+from ._dplane import DPlaneRaycastResult
+from ._drectangle import DRectangle
+from ._drectangle import DRectangleOverlappable
+from ._drectanglefrustum import DRectangleFrustum
+from ._drectanglefrustum import DRectangleFrustumOverlappable
+from ._dtriangle2d import DTriangle2d
+from ._dtriangle2d import DTriangle2dOverlappable
+from ._dtrianglemesh3d import DTriangleMesh3d
+from ._dtrianglemesh3d import DTriangleMesh3dRaycastResult
+from ._fboundingbox2d import FBoundingBox2d
+from ._fboundingbox2d import FBoundingBox2dOverlappable
+from ._fboundingbox2d import HasFBoundingBox2d
+from ._fboundingbox3d import FBoundingBox3d
+from ._fboundingbox3d import FBoundingBox3dOverlappable
+from ._fboundingbox3d import FBoundingBox3dRaycastResult
+from ._fboundingbox3d import HasFBoundingBox3d
+from ._fcircle import FCircle
+from ._fcircle import FCircleOverlappable
+from ._flinesegment3d import FLineSegment3d
+from ._fplane import FPlane
+from ._fplane import FPlaneRaycastResult
+from ._frectangle import FRectangle
+from ._frectangle import FRectangleOverlappable
+from ._frectanglefrustum import FRectangleFrustum
+from ._frectanglefrustum import FRectangleFrustumOverlappable
+from ._ftriangle2d import FTriangle2d
+from ._ftriangle2d import FTriangle2dOverlappable
+from ._ftrianglemesh3d import FTriangleMesh3d
+from ._ftrianglemesh3d import FTriangleMesh3dRaycastResult
+from ._iboundingbox2d import HasIBoundingBox2d
+from ._iboundingbox2d import IBoundingBox2d
+from ._iboundingbox2d import IBoundingBox2dOverlappable
+from ._iboundingbox3d import HasIBoundingBox3d
+from ._iboundingbox3d import IBoundingBox3d
+from ._iboundingbox3d import IBoundingBox3dOverlappable
+from ._icircle import ICircle
+from ._icircle import ICircleOverlappable
+from ._irectangle import IRectangle
+from ._irectangle import IRectangleOverlappable
+from ._itriangle2d import ITriangle2d
+from ._itriangle2d import ITriangle2dOverlappable
+from ._separating_axis_theorem import separating_axis_theorem
