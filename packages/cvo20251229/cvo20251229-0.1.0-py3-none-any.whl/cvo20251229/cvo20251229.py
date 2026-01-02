93ab6ce02adb533c19aa2ee6010462bdea84341e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:04:45 +0000

    Add package cvo20251229/0.1.0/cvo20251229-0.1.0-py3-none-any.whl

---
+++ packages/cvo20251229/cvo20251229-0.1.0-py3-none-any.whl/cvo20251229/cvo20251229.py
@@ -0,0 +1,14 @@
+def hello(someone: str = "you") -> str:
+    """Greet someone.
+
+    Parameters
+    ----------
+    someone : str, default='you'
+        The name of the person to greet, by default 'you'
+
+    Returns
+    -------
+    str
+        A greeting message
+    """
+    return f"Hello {someone} from cvo20251229!"
