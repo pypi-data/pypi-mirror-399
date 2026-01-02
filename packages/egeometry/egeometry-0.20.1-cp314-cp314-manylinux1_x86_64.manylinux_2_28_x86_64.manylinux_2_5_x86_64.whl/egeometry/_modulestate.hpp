50ebe6006eb5accb38e54ceb5ed365bfa3c96758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 23:05:37 +0000

    Add package egeometry/0.20.1/egeometry-0.20.1-cp314-cp314-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/egeometry/egeometry-0.20.1-cp314-cp314-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl/egeometry/_modulestate.hpp
@@ -0,0 +1,71 @@
+
+// generated from codegen/templates/_modulestate.hpp
+
+#ifndef EGEOMETRY_MODULESTATE_HPP
+#define EGEOMETRY_MODULESTATE_HPP
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include "_module.hpp"
+#include "emath.h"
+
+struct ModuleState
+{
+    EMathApi *emath_api;
+
+    PyTypeObject *DBoundingBox2d_PyTypeObject;
+
+    PyTypeObject *FBoundingBox2d_PyTypeObject;
+
+    PyTypeObject *IBoundingBox2d_PyTypeObject;
+
+};
+
+
+static int
+ModuleState_traverse(
+    ModuleState *self,
+    visitproc visit,
+    void *arg
+)
+{
+
+    Py_VISIT(self->DBoundingBox2d_PyTypeObject);
+
+    Py_VISIT(self->FBoundingBox2d_PyTypeObject);
+
+    Py_VISIT(self->IBoundingBox2d_PyTypeObject);
+
+    return 0;
+}
+
+
+static int
+ModuleState_clear(ModuleState *self)
+{
+    if (self->emath_api)
+    {
+        EMathApi_Release();
+        PyErr_Clear();
+        self->emath_api = 0;
+    }
+
+    Py_CLEAR(self->DBoundingBox2d_PyTypeObject);
+
+    Py_CLEAR(self->FBoundingBox2d_PyTypeObject);
+
+    Py_CLEAR(self->IBoundingBox2d_PyTypeObject);
+
+    return 0;
+}
+
+
+static ModuleState *
+get_module_state()
+{
+    PyObject *module = get_module();
+    if (!module){ return 0; }
+    return (ModuleState *)PyModule_GetState(module);
+}
+
+#endif
