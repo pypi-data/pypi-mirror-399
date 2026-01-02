de76e0e1430848011d8054ce831061b5f66ea72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:05:42 +0000

    Add package optional-dependency-manager/0.2.0/optional_dependency_manager-0.2.0.tar.gz

---
+++ packages/optional-dependency-manager/optional_dependency_manager-0.2.0.tar.gz/optional_dependency_manager-0.2.0/src/optional_dependency_manager/__init__.py
@@ -0,0 +1,7 @@
+from optional_dependency_manager.odm import (
+    ModuleReport,
+    ModuleSpec,
+    OptionalDependencyManager,
+)
+
+__all__ = ["OptionalDependencyManager", "ModuleSpec", "ModuleReport"]
