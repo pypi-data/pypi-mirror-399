ae6467cc28078baa6a170dd549e1800c473983fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:04:28 +0000

    Add package cel-bind/0.8.2/cel_bind-0.8.2-cp314-cp314-macosx_14_0_arm64.whl

---
+++ packages/cel-bind/cel_bind-0.8.2-cp314-cp314-macosx_14_0_arm64.whl/cel/__init__.py
@@ -0,0 +1,37 @@
+from cel import _cel_python as __cel__
+
+BoolType = __cel__.BoolType
+BytesType = __cel__.BytesType
+CelExpression = __cel__.CelExpression
+CheckedExpr = __cel__.CheckedExpr
+Compiler = __cel__.Compiler
+Descriptor = __cel__.Descriptor
+DescriptorPool = __cel__.DescriptorPool
+DoubleType = __cel__.DoubleType
+FunctionRegistry = __cel__.FunctionRegistry
+IntType = __cel__.IntType
+Interpreter = __cel__.Interpreter
+NullType = __cel__.NullType
+StringType = __cel__.StringType
+UintType = __cel__.UintType
+ListType = __cel__.ListType
+
+__all__ = [
+    "Interpreter",
+    "BoolType",
+    "BytesType",
+    "CelExpression",
+    "CheckedExpr",
+    "Compiler",
+    "Descriptor",
+    "DescriptorPool",
+    "DoubleType",
+    "FunctionRegistry",
+    "IntType",
+    "Interpreter",
+    "NullType",
+    "StringType",
+    "UintType",
+    "ListType",
+]
+
