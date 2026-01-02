a78ac0edba4e4c4a07d0e2d289cb6e1022189504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:05:29 +0000

    Add package togather-event-sdk/1.0.61/togather_event_sdk-1.0.61-py3-none-any.whl

---
+++ packages/togather-event-sdk/togather_event_sdk-1.0.61-py3-none-any.whl/togather_event_sdk/user/v1/__init__.py
@@ -0,0 +1,7 @@
+# Auto-generated init to expose protobuf classes
+from .user_profile_created_pb2 import *
+from .user_email_verification_requested_pb2 import *
+from .user_forgot_password_pb2 import *
+from .user_account_created_pb2 import *
+
+__all__ = [name for name in globals() if not name.startswith('_')]
