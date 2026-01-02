6f8aec4293913d3201c07ff05f45d2db6127b6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:06:37 +0000

    Add package absfuyu/6.1.0/absfuyu-6.1.0-py3-none-any.whl

---
+++ packages/absfuyu/absfuyu-6.1.0-py3-none-any.whl/absfuyu/extra/__init__.py
@@ -0,0 +1,12 @@
+"""
+Absfuyu: Extra
+--------------
+Features that require additional libraries
+
+Version: 6.1.0
+Date updated: 28/12/2025 (dd/mm/yyyy)
+"""
+
+
+def is_loaded():
+    return True
---
+++ packages/absfuyu/absfuyu-6.1.0-py3-none-any.whl/absfuyu/extra/audio/__init__.py
@@ -0,0 +1,8 @@
+"""
+Absfuyu: Audio
+--------------
+Audio related
+
+Version: 6.1.0
+Date updated: 28/12/2025 (dd/mm/yyyy)
+"""
