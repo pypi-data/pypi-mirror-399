18fe8ca4dd0497fad8eee3ed6a62f8ccee7acb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:04:31 +0000

    Add package podgenai/0.17.0/podgenai-0.17.0-py3-none-any.whl

---
+++ packages/podgenai/podgenai-0.17.0-py3-none-any.whl/podgenai/config.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+from podgenai.util.dotenv_ import load_dotenv
+
+load_dotenv()
+
+CWD: Path = Path.cwd()
+PACKAGE_PATH: Path = Path(__file__).parent
+PACKAGE_NAME: str = PACKAGE_PATH.name
+REPO_PATH: Path = PACKAGE_PATH.parent.parent
+
+GiB = 1024**3
+MAX_CONCURRENT_WORKERS = int(os.environ.get("PODGENAI_OPENAI_MAX_WORKERS", 16))  # Note: Default value is documented in readme.
+assert MAX_CONCURRENT_WORKERS >= 1
+NUM_SECTIONS_MIN: int = 3  # Applies only to the `max_sections` argument. Does not apply to LLM output.
+NUM_SECTIONS_MAX: int = 100
+PROMPTS: dict[str, str] = {p.stem: p.read_text().strip() for p in (PACKAGE_PATH / "prompts").glob("*.txt")}
+WORK_PATH: Path = CWD / "work"
---
+++ packages/podgenai/podgenai-0.17.0-py3-none-any.whl/podgenai/content/topic.py
@@ -0,0 +1,54 @@
+import contextlib
+import io
+
+import podgenai.exceptions
+from podgenai.util.sys import print_error
+
+
+def is_topic_valid(topic: str) -> bool:
+    """Return true if the topic is structurally valid, otherwise false.
+
+    A validation error is printed if the topic is invalid.
+    """
+    if not isinstance(topic, str):  # Note: This happens if `-t` flag is provided without any value.
+        print_error("Topic must be a string.")
+        return False
+    if topic != topic.strip():
+        print_error("Topic must not have leading or trailing whitespace.")
+        return False
+    if len(topic) == 0:
+        print_error("No topic was provided.")
+        return False
+    if len(topic) < 2:
+        print_error("Topic must be at least two characters long.")
+        return False
+    if len(topic.splitlines()) > 1:
+        print_error("Topic must be in a single line.")
+        return False
+    if (topic[0] == topic[-1] == "'") or (topic[0] == topic[-1] == '"'):
+        print_error("Topic must not be quoted.")
+        return False
+    if topic[-1] == ":":
+        print_error("Topic must not end in a colon.")
+        return False
+    return True
+
+
+def ensure_topic_is_valid(topic: str) -> None:
+    """Raise `InputError` if the topic is structurally invalid."""
+    error = io.StringIO()
+    with contextlib.redirect_stderr(error):
+        if not is_topic_valid(topic):
+            error = error.getvalue().rstrip().removeprefix("Error: ")
+            raise podgenai.exceptions.InputError(error)
+
+
+def get_topic() -> str:
+    """Get topic for generation from user input."""
+    topic = None
+    while not topic:
+        topic = input("Specify the topic: ")
+        topic = topic.strip()
+        if not is_topic_valid(topic):
+            topic = None
+    return topic
