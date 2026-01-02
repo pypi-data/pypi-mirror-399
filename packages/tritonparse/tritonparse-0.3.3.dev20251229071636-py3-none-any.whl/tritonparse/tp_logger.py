b22d08a2db3dfe3dd0ea22136245de9762c30d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:04:57 +0000

    Add package tritonparse/0.3.3.dev20251229071636/tritonparse-0.3.3.dev20251229071636-py3-none-any.whl

---
+++ packages/tritonparse/tritonparse-0.3.3.dev20251229071636-py3-none-any.whl/tritonparse/tp_logger.py
@@ -0,0 +1,9 @@
+#  Copyright (c) Meta Platforms, Inc. and affiliates.
+
+import logging
+
+logger = logging.getLogger("tritonparse")
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
