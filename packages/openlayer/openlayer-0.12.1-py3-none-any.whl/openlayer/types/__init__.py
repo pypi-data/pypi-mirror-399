e10ffba8148556feef04a1b2c2ec5ca4416da44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:04:52 +0000

    Add package openlayer/0.12.1/openlayer-0.12.1-py3-none-any.whl

---
+++ packages/openlayer/openlayer-0.12.1-py3-none-any.whl/openlayer/types/__init__.py
@@ -0,0 +1,13 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .project_list_params import ProjectListParams as ProjectListParams
+from .project_create_params import ProjectCreateParams as ProjectCreateParams
+from .project_list_response import ProjectListResponse as ProjectListResponse
+from .project_create_response import ProjectCreateResponse as ProjectCreateResponse
+from .commit_retrieve_response import CommitRetrieveResponse as CommitRetrieveResponse
+from .inference_pipeline_update_params import InferencePipelineUpdateParams as InferencePipelineUpdateParams
+from .inference_pipeline_retrieve_params import InferencePipelineRetrieveParams as InferencePipelineRetrieveParams
+from .inference_pipeline_update_response import InferencePipelineUpdateResponse as InferencePipelineUpdateResponse
+from .inference_pipeline_retrieve_response import InferencePipelineRetrieveResponse as InferencePipelineRetrieveResponse
