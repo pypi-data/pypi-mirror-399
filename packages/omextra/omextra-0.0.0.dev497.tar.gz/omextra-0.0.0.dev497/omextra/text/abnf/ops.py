15321c9f7bfc61a625b30cb528105a431dda145e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:05:34 +0000

    Add package omextra/0.0.0.dev497/omextra-0.0.0.dev497.tar.gz

---
+++ packages/omextra/omextra-0.0.0.dev497.tar.gz/omextra-0.0.0.dev497/omextra/text/abnf/ops.py
@@ -0,0 +1,281 @@
+import typing as ta
+
+from omlish import check
+from omlish import dataclasses as dc
+from omlish import lang
+
+from .base import CompositeOp
+from .base import LeafOp
+from .base import Op
+
+
+##
+
+
+class Literal(LeafOp, lang.Abstract):
+    def _match_repr(self) -> str:
+        return repr(self)
+
+
+@ta.final
+class StringLiteral(Literal, lang.Final):
+    def __init__(self, value: str) -> None:
+        super().__init__()
+
+        self._value = check.non_empty_str(value)
+
+    @property
+    def value(self) -> str:
+        return self._value
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}@{id(self):x}({self._value!r})'
+
+
+@ta.final
+class CaseInsensitiveStringLiteral(Literal, lang.Final):
+    def __init__(self, value: str) -> None:
+        super().__init__()
+
+        self._value = check.non_empty_str(value).casefold()
+
+    @property
+    def value(self) -> str:
+        return self._value
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}@{id(self):x}({self._value!r})'
+
+
+@ta.final
+class RangeLiteral(Literal, lang.Final):
+    @dc.dataclass(frozen=True)
+    class Range:
+        lo: str
+        hi: str
+
+        def __post_init__(self) -> None:
+            for c in (self.lo, self.hi):
+                check.equal(len(check.non_empty_str(c)), 1)
+            check.state(self.hi >= self.lo)
+
+    def __init__(self, value: Range) -> None:
+        super().__init__()
+
+        self._value = check.isinstance(value, RangeLiteral.Range)
+
+    @property
+    def value(self) -> Range:
+        return self._value
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}@{id(self):x}({self._value!r})'
+
+
+@ta.overload
+def literal(s: str, *, case_sensitive: bool = False) -> StringLiteral:
+    ...
+
+
+@ta.overload
+def literal(lo: str, hi: str) -> RangeLiteral:
+    ...
+
+
+def literal(*args, case_sensitive=None):
+    if not args:
+        raise TypeError
+    elif len(args) == 1:
+        s = check.isinstance(check.single(args), str)
+        if case_sensitive:
+            return StringLiteral(s)
+        else:
+            return CaseInsensitiveStringLiteral(s)
+    elif len(args) == 2:
+        check.none(case_sensitive)
+        return RangeLiteral(RangeLiteral.Range(*map(check.of_isinstance(str), args)))
+    else:
+        raise TypeError(args)
+
+
+##
+
+
+@ta.final
+class Concat(CompositeOp, lang.Final):
+    def __init__(self, *children: Op) -> None:
+        super().__init__()
+
+        for c in check.not_empty(children):
+            check.isinstance(c, Op)
+        self._children = children
+
+    @property
+    def children(self) -> ta.Sequence[Op]:
+        return self._children
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}@{id(self):x}({", ".join(map(repr, self._children))})'
+
+
+concat = Concat
+
+
+##
+
+
+@ta.final
+class Repeat(CompositeOp, lang.Final):
+    @dc.dataclass(frozen=True)
+    class Times:
+        min: int = 0
+        max: int | None = None
+
+        def __post_init__(self) -> None:
+            if self.max is not None:
+                check.state(self.max >= self.min)
+
+        def __repr__(self) -> str:
+            if not self.min and self.max is None:
+                s = '*'
+            elif not self.min and self.max == 1:
+                s = '?'
+            elif self.min and self.max is None:
+                s = f'{self.min}+'
+            else:
+                s = f'{self.min}-{self.max!r}'
+            return f'{self.__class__.__name__}({s})'
+
+    def __init__(self, times: Times, child: Op) -> None:
+        super().__init__()
+
+        self._times = check.isinstance(times, Repeat.Times)
+        self._child = check.isinstance(child, Op)
+
+    @property
+    def times(self) -> Times:
+        return self._times
+
+    @property
+    def child(self) -> Op:
+        return self._child
+
+    @property
+    def children(self) -> ta.Sequence[Op]:
+        return (self._child,)
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}@{id(self):x}({self._times}, {self._child!r})'
+
+
+@ta.overload
+def repeat(child: Op) -> Repeat:  # noqa
+    ...
+
+
+@ta.overload
+def repeat(times: Repeat.Times, child: Op) -> Repeat:  # noqa
+    ...
+
+
+@ta.overload
+def repeat(min: int, child: Op) -> Repeat:  # noqa
+    ...
+
+
+@ta.overload
+def repeat(min: int, max: int | None, child: Op) -> Repeat:  # noqa
+    ...
+
+
+def repeat(*args):
+    min: int  # noqa
+    max: int | None  # noqa
+
+    if len(args) < 2:
+        [child] = args
+        min, max = 0, None  # noqa
+
+    elif len(args) > 2:
+        min, max, child = args  # noqa
+
+    else:
+        ti, child = args  # noqa
+
+        if isinstance(ti, Repeat.Times):
+            min, max = ti.min, ti.max  # noqa
+
+        else:
+            min, max = ti, None  # noqa
+
+    return Repeat(
+        Repeat.Times(
+            check.isinstance(min, int),
+            check.isinstance(max, (int, None)),
+        ),
+        check.isinstance(child, Op),
+    )
+
+
+ZERO_OR_ONE_TIMES = Repeat.Times(0, 1)
+
+
+def option(child: Op) -> Repeat:
+    return Repeat(ZERO_OR_ONE_TIMES, check.isinstance(child, Op))
+
+
+##
+
+
+@ta.final
+class Either(CompositeOp, lang.Final):
+    def __init__(self, *children: Op, first_match: bool = False) -> None:
+        super().__init__()
+
+        for c in check.not_empty(children):
+            check.isinstance(c, Op)
+        self._children = children
+        self._first_match = first_match
+
+    @property
+    def children(self) -> ta.Sequence[Op]:
+        return self._children
+
+    @property
+    def first_match(self) -> bool:
+        return self._first_match
+
+    def __repr__(self) -> str:
+        return (
+            f'{self.__class__.__name__}@{id(self):x}('
+            f'{", ".join(map(repr, self._children))}'
+            f'{", first_match=True" if self._first_match else ""})'
+        )
+
+
+either = Either
+
+
+##
+
+
+@ta.final
+class RuleRef(Op, lang.Final):
+    def __init__(self, name: str) -> None:
+        super().__init__()
+
+        self._name = check.non_empty_str(name)
+        self._name_f = name.casefold()
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}@{id(self):x}({self._name!r})'
+
+    def _match_repr(self) -> str:
+        return repr(self)
+
+
+rule = RuleRef
---
+++ packages/omextra/omextra-0.0.0.dev497.tar.gz/omextra-0.0.0.dev497/omextra/text/abnf/utils.py
@@ -0,0 +1,71 @@
+import itertools
+import textwrap
+import typing as ta
+
+from omlish import check
+
+from .base import Grammar
+from .base import Match
+from .ops import RuleRef
+
+
+##
+
+
+def strip_insignificant_match_rules(m: Match, g: Grammar) -> Match:
+    def rec(c: Match) -> Match:
+        return c.flat_map_children(
+            lambda x: (
+                (rec(x),) if not (
+                    isinstance((xp := x.op), RuleRef) and
+                    check.not_none(g.rule(xp.name)).insignificant
+                ) else ()
+            ),
+        )
+    return rec(m)
+
+
+def only_match_rules(m: Match) -> Match:
+    def rec(c: Match) -> ta.Iterable[Match]:
+        if isinstance(c.op, RuleRef):
+            return (c.flat_map_children(rec),)
+        else:
+            return itertools.chain.from_iterable(map(rec, c.children))
+    return m.flat_map_children(rec)
+
+
+#
+
+
+def parse_rules(
+        grammar: Grammar,
+        source: str,
+        root: str | None = None,
+        *,
+        start: int = 0,
+        **kwargs: ta.Any,
+) -> Match | None:
+    if (match := grammar.parse(
+            source,
+            root,
+            start=start,
+            **kwargs,
+    )) is None:
+        return None
+
+    match = only_match_rules(match)
+    match = strip_insignificant_match_rules(match, grammar)
+
+    return match
+
+
+##
+
+
+def fix_ws(s: str) -> str:
+    return (
+        textwrap.dedent(s)
+        .rstrip()
+        .replace('\r', '')
+        .replace('\n', '\r\n')
+    ) + '\r\n'
