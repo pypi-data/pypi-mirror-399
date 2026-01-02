f85174880e1d9bfc8d6a3b557c190c1b36c0cb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:03:47 +0000

    Add package OAM-KIST/0.2.0/oam_kist-0.2.0.tar.gz

---
+++ packages/OAM-KIST/oam_kist-0.2.0.tar.gz/oam_kist-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OAM_KIST",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy",
+    ],
+    author="Youngjun Kim",
+    author_email="kyjun0915@kist.re.kr",
+    description="Quantum information and technology using OAM states and SLM for KIST research",
+    python_requires=">=3.7",
+)
