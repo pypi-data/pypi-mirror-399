594076388a9da0f389d7bdb88bb0c1f8a5086e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:04:37 +0000

    Add package scadm/0.4.2/scadm-0.4.2.tar.gz

---
+++ packages/scadm/scadm-0.4.2.tar.gz/scadm-0.4.2/scadm/constants.py
@@ -0,0 +1,9 @@
+"""Version constants for OpenSCAD installations."""
+
+# renovate: datasource=github-releases depName=openscad/openscad
+OPENSCAD_STABLE_VERSION = "2021.01"
+
+# renovate: datasource=custom.openscad-snapshots depName=OpenSCAD versioning=loose
+OPENSCAD_NIGHTLY_VERSION_WINDOWS = "2025.12.28"
+# renovate: datasource=custom.openscad-snapshots depName=OpenSCAD versioning=loose
+OPENSCAD_NIGHTLY_VERSION_LINUX = "2025.12.28.ai30176"
