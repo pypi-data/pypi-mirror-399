7b19fd5ec4c3811989f1bfb53593e321c602175f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 23:05:36 +0000

    Add package dps-liumou-Stable/1.1.3/dps_liumou_stable-1.1.3-py3-none-any.whl

---
+++ packages/dps-liumou-Stable/dps_liumou_stable-1.1.3-py3-none-any.whl/dps_liumou_Stable/version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+版本信息模块
+"""
+
+__version__ = "1.1.3"
+__author__ = "坐公交也用券"
+__author_email__ = "liumou.site@qq.com"
+__description__ = "智能Docker镜像拉取工具 - 自动选择最优镜像源"
