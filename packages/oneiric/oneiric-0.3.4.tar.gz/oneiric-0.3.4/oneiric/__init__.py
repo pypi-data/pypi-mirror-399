b08f10a665480c62ad120b1434352ae1da59a1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:04:38 +0000

    Add package oneiric/0.3.4/oneiric-0.3.4.tar.gz

---
+++ packages/oneiric/oneiric-0.3.4.tar.gz/oneiric-0.3.4/oneiric/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from importlib import metadata
+
+__all__ = ["__version__"]
+
+try:
+    __version__ = metadata.version("oneiric")
+except metadata.PackageNotFoundError:  # pragma: no cover - fallback for local dev
+    __version__ = "0.0.0"
