ef481ee913bfb17b4e5843e508f0d081588f4bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 20:07:48 +0000

    Add package fluidattacks_zoho_sdk/2.0.0/fluidattacks_zoho_sdk-2.0.0.tar.gz

---
+++ packages/fluidattacks_zoho_sdk/fluidattacks_zoho_sdk-2.0.0.tar.gz/fluidattacks_zoho_sdk-2.0.0/build/default.nix
@@ -0,0 +1,7 @@
+{ nixpkgs, builders, scripts, src, }: {
+  inherit src;
+  root_path = "observes/sdk/zoho";
+  module_name = "fluidattacks_zoho_sdk";
+  pypi_token_var = "ZOHO_SDK_TOKEN";
+  override = b: b;
+}
