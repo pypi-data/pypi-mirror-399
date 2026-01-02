e83315ae870f054af75d728606f3c89d3224f233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:05:28 +0000

    Add package alforria/0.2.1/alforria-0.2.1.tar.gz

---
+++ packages/alforria/alforria-0.2.1.tar.gz/alforria-0.2.1/src/alforria/__init__.py
@@ -0,0 +1,11 @@
+"""Pacote Alforria"""
+
+__version__ = "0.2.1"
+
+from . import main
+
+
+def set_config_path(path: str):
+    main._PATHS_PATH = path + "/paths.cnf"
+    main._ALFCFG_PATH = path + "/alforria.cnf"
+    main._CONST_PATH = path + "/con stantes.cnf"
