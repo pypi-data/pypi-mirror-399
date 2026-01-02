26742a16da64029007b571c13c81b7dc11e18507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:05:10 +0000

    Add package rosettasciio/0.12.0/rosettasciio-0.12.0-cp314-cp314t-win_amd64.whl

---
+++ packages/rosettasciio/rosettasciio-0.12.0-cp314-cp314t-win_amd64.whl/rsciio/__init__.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# Copyright 2007-2025 The HyperSpy developers
+#
+# This file is part of RosettaSciIO.
+#
+# RosettaSciIO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RosettaSciIO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RosettaSciIO. If not, see <https://www.gnu.org/licenses/#GPL>.
+
+import functools
+import os
+from importlib.metadata import version
+from pathlib import Path
+
+import yaml
+
+from ._logger import set_log_level
+
+# Default to warning
+set_log_level("WARNING")
+
+IO_PLUGINS = []
+"""
+List of available IO plugins. Each entry is a dictionary with the following keys:
+
+- ``'name'``: The name of the plugin.
+- ``'name_aliases'``: A list of alternative names for the plugin.
+- ``'description'``: A brief description of the plugin.
+- ``'full_support'``: A boolean indicating if the plugin has full support.
+- ``'default_extension'``: The default file extension for the plugin.
+- ``'writes'``: A boolean indicating if the plugin supports writing files.
+- ``'non_uniform_axis'``: A boolean indicating if the plugin supports non-uniform axes.
+- ``'api'``: The API module path as a string (e.g., 'rsciio.nexus').
+
+:meta hide-value:
+"""
+
+
+for sub, _, _ in os.walk(os.path.abspath(os.path.dirname(__file__))):
+    _specsf = os.path.join(sub, "specifications.yaml")
+    if os.path.isfile(_specsf):
+        with open(_specsf, "r") as stream:
+            _specs = yaml.safe_load(stream)
+            # for testing purposes
+            _specs["api"] = "rsciio.%s" % os.path.split(sub)[1]
+            IO_PLUGINS.append(_specs)
+
+
+__all__ = ["__version__", "IO_PLUGINS", "set_log_level"]
+
+
+def __dir__():
+    return sorted(__all__)
+
+
+@functools.cache
+def _get_version():
+    version_ = version("rosettasciio")
+    # For development version, `setuptools_scm` will be used at build time
+    # to get the dev version, in case of missing vcs information (git archive,
+    # shallow repository), the fallback version defined in pyproject.toml will
+    # be used
+
+    # if we have a editable install from a git repository try to use
+    # `setuptools_scm` to find a more accurate version:
+    # `importlib.metadata` will provide the version at installation
+    # time and for editable version this may be different
+
+    # we only do that if we have enough git history, e.g. not shallow checkout
+    _root = Path(__file__).resolve().parents[1]
+    if (_root / ".git").exists() and not (_root / ".git/shallow").exists():
+        try:
+            # setuptools_scm may not be installed
+            from setuptools_scm import get_version
+
+            version_ = get_version(_root)
+        except ImportError:  # pragma: no cover
+            # setuptools_scm not install, we keep the existing __version__
+            pass
+
+    return version_
+
+
+def __getattr__(name):
+    if name == "__version__":
+        return _get_version()
+
+    if name in __all__:
+        return globals()[name]
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
