188108bb5b6a937fda97b545999500b0766a460b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:04:41 +0000

    Add package PepperPepper/0.0.9.34/pepperpepper-0.0.9.34.tar.gz

---
+++ packages/PepperPepper/pepperpepper-0.0.9.34.tar.gz/pepperpepper-0.0.9.34/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+setuptools.setup(
+    name="PepperPepper",
+    version="0.0.9.34",
+    python_requires='>=3.7',
+    author="Aohua Li",
+    author_email="liah24@mails.jlu.edu.cn",
+    description="It is a Deeplearning package to foster developed by Aohua Li",
+    url="",
+    packages=setuptools.find_packages(),
+    zip_safe=True,
+    # install_requires=requirements,
+)
+
+
+
+
+
+
+
+
+
+
+
