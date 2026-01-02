cffccba7af31baf9ce97e576a0ee251a424f18df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:06:07 +0000

    Add package aip-agents-binary/0.5.18/aip_agents_binary-0.5.18-py3-none-manylinux_2_31_x86_64.whl

---
+++ packages/aip-agents-binary/aip_agents_binary-0.5.18-py3-none-manylinux_2_31_x86_64.whl/aip_agents/examples/hello_world_langgraph_bosa_twitter.pyi
@@ -0,0 +1,5 @@
+from aip_agents.agent import LangGraphAgent as LangGraphAgent
+from aip_agents.tools import BOSA_AUTOMATED_TOOLS as BOSA_AUTOMATED_TOOLS
+
+async def langgraph_bosa_example() -> None:
+    """Demonstrates the LangGraphAgent's arun method."""
