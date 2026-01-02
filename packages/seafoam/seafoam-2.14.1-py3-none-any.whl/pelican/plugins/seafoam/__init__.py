a01843c5e4e65d5e818fd9a63c98fd4b02144edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:04:17 +0000

    Add package seafoam/2.14.1/seafoam-2.14.1-py3-none-any.whl

---
+++ packages/seafoam/seafoam-2.14.1-py3-none-any.whl/pelican/plugins/seafoam/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- #
+"""
+Seafoam.
+
+This is a Pelican theme. This module is the helper code to go with it.
+"""
+
+from pathlib import Path
+
+from pelican import signals
+
+from .constants import __version__  # NOQA
+from .formatting import table_fix
+from .initialize import check_settings, seafoam_version
+
+
+def get_path():
+    """
+    Shortcut for users whose theme is not next to their pelicanconf.py.
+
+    Returns
+    -------
+        str: filepath to folder containing theme
+
+    """
+    # Theme directory is defined as our parent directory
+    return str(Path(__file__).resolve().parent)
+
+
+def register():
+    """Register the plugin pieces with Pelican."""
+    signals.initialized.connect(check_settings)
+    signals.initialized.connect(seafoam_version)
+    signals.content_written.connect(table_fix)
