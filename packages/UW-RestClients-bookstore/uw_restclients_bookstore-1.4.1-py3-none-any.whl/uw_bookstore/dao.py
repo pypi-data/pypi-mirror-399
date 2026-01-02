2b6a5958652a44fa3fe67d92bc402b61bc9709dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:04:28 +0000

    Add package UW-RestClients-bookstore/1.4.1/uw_restclients_bookstore-1.4.1-py3-none-any.whl

---
+++ packages/UW-RestClients-bookstore/uw_restclients_bookstore-1.4.1-py3-none-any.whl/uw_bookstore/dao.py
@@ -0,0 +1,15 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+import os
+from os.path import abspath, dirname
+from restclients_core.dao import DAO
+
+
+class Bookstore_DAO(DAO):
+    def service_name(self):
+        return 'book'
+
+    def service_mock_paths(self):
+        return [abspath(os.path.join(dirname(__file__), "resources"))]
