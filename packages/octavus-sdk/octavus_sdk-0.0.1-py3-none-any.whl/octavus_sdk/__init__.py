3f50cb953189afdae9fa9b92a67255265ecd769c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 15:04:38 +0000

    Add package octavus-sdk/0.0.1/octavus_sdk-0.0.1-py3-none-any.whl

---
+++ packages/octavus-sdk/octavus_sdk-0.0.1-py3-none-any.whl/octavus_sdk/__init__.py
@@ -0,0 +1,9 @@
+"""
+Octavus SDK - Python SDK for AI agent orchestration.
+
+This package is currently under development.
+Note: This is an alias for the `octavus` package.
+"""
+
+__version__ = "0.0.1"
+
