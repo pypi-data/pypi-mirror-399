9a71c780f022891ab6948d0d18c3665fda49cd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 19:04:31 +0000

    Add package demolibrary/0.1.2/demolibrary-0.1.2.tar.gz

---
+++ packages/demolibrary/demolibrary-0.1.2.tar.gz/demolibrary-0.1.2/tests/test_demo_funcntions.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import TestCase
+
+
+class TestDemoFunctions(TestCase):
+    def test_calculate_statistics(self):
+        from src.demo_libry_lokesh.demo_functions import calculate_statistics
+        import numpy as np
+
+        data = [1, 2, 3, 4, 5]
+        stats = calculate_statistics(data)
+
+        assert np.isclose(stats['mean'], 3.0), "Mean calculation is incorrect"
+        assert np.isclose(stats['median'], 3.0), "Median calculation is incorrect"
+        assert np.isclose(stats['std_dev'], np.std(data)), "Standard deviation calculation is incorrect"
+        
+        
+if __name__ == "__main__":
+    unittest.main()
