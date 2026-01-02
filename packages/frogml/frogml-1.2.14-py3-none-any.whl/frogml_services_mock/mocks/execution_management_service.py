8fb85d5e816e062bfbe8105611b4e5263809737d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 18:04:33 +0000

    Add package frogml/1.2.14/frogml-1.2.14-py3-none-any.whl

---
+++ packages/frogml/frogml-1.2.14-py3-none-any.whl/frogml_services_mock/mocks/execution_management_service.py
@@ -0,0 +1,26 @@
+from grpc import RpcError
+
+from frogml._proto.qwak.execution.v1.execution_service_pb2 import TriggerBackfillResponse
+from frogml._proto.qwak.execution.v1.execution_service_pb2_grpc import (
+    FeatureStoreExecutionServiceServicer,
+)
+
+
+class ExecutionManagementServiceMock(FeatureStoreExecutionServiceServicer):
+    def __init__(self):
+        self._execution_id = None
+        self._raise_exception_on_request = False
+
+    def set_execution_id(self, execution_id: str) -> None:
+        self._execution_id = execution_id
+
+    def set_raise_exception(self):
+        self._raise_exception_on_request = True
+
+    def clear_raise_exception(self):
+        self._raise_exception_on_request = False
+
+    def TriggerBatchBackfill(self, request, context):
+        if self._raise_exception_on_request:
+            raise RpcError
+        return TriggerBackfillResponse(execution_id=self._execution_id)
