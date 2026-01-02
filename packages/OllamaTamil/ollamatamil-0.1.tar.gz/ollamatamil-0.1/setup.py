026e1a9cb6c0cd870bbafa02d0973c9e874a6174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 17:05:59 +0000

    Add package OllamaTamil/0.1/ollamatamil-0.1.tar.gz

---
+++ packages/OllamaTamil/ollamatamil-0.1.tar.gz/ollamatamil-0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import find_packages,setup
+setup(name="OllamaTamil",version="0.1",description="Ollama tamil helps you to build ollama api but with tamil interface",packages=find_packages())
