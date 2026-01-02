4e4a1e3d6dc05b78310bbce1c6ad08a7340c413f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 18:05:21 +0000

    Add package nlp-toolkit-services/0.1.8/nlp_toolkit_services-0.1.8-py3-none-any.whl

---
+++ packages/nlp-toolkit-services/nlp_toolkit_services-0.1.8-py3-none-any.whl/nlp_toolkit_services_py_client/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    "dependency_pb2",
+    "dependency_pb2_grpc",
+    "ner_pb2",
+    "ner_pb2_grpc",
+    "word_segment_pb2",
+    "word_segment_pb2_grpc",
+]
+
+__version__ = "0.1.3"
