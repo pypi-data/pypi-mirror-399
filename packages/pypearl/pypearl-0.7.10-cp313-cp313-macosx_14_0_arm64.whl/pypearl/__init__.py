75cba292c1fae4cb6bf1aede28b888c5f86d4605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:04:28 +0000

    Add package pypearl/0.7.10/pypearl-0.7.10-cp313-cp313-macosx_14_0_arm64.whl

---
+++ packages/pypearl/pypearl-0.7.10-cp313-cp313-macosx_14_0_arm64.whl/pypearl/__init__.py
@@ -0,0 +1,4 @@
+# pypearl/__init__.py
+from ._pypearl import add, Dense, TestActivation, ReLU, Linear, Sigmoid, LeakyReLU, Step, Softmax, ReverseReLU, ndarray, CCE, GradientDescent, Dihedral, ZNZ, DihedralTensor, Symmetric
+
+__all__ = ["add", "Dense", "TestActivation", "ReLU", "Linear", "Sigmoid", "LeakyReLU", "Step", "Softmax", "ReverseReLU", "ndarray", "CCE", "GradientDescent", "Dihedral", "ZNZ", "DihedralTensor", "Symmetric"]
