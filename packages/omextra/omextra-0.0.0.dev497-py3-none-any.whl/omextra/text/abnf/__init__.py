e0b3b2aed83eb153d1e6ff38b33529ea772e9a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:05:34 +0000

    Add package omextra/0.0.0.dev497/omextra-0.0.0.dev497-py3-none-any.whl

---
+++ packages/omextra/omextra-0.0.0.dev497-py3-none-any.whl/omextra/text/abnf/__init__.py
@@ -0,0 +1,94 @@
+"""
+Parser generator for ABNF grammars.
+
+Originally based on library by Charles Yeomans (see LICENSE file):
+
+  https://github.com/declaresub/abnf/tree/561ced67c0a8afc869ad0de5b39dbe4f6e71b0d8/src/abnf
+
+It has however been entirely rewritten.
+
+====
+
+TODO:
+ - opto
+ - error reporting
+ - codegen?
+ - fix_ws problem
+  - auto? no, need to keep lines / offsets accurate for errors
+  - relax CRLF rule by default?
+  - grammar transform? helper kwarg?
+"""
+from omlish import dataclasses as _dc  # noqa
+
+
+_dc.init_package(
+    globals(),
+    codegen=True,
+)
+
+
+##
+
+
+from .base import (  # noqa
+    Match,
+    longest_match,
+
+    Op,
+
+    Rule,
+    Grammar,
+
+    iter_parse,
+    parse,
+)
+
+from .core import (  # noqa
+    CORE_RULES,
+)
+
+from .errors import (  # noqa
+    AbnfError,
+    AbnfGrammarParseError,
+)
+
+from .meta import (  # noqa
+    META_GRAMMAR_RULES,
+    META_GRAMMAR,
+
+    parse_grammar,
+)
+
+from .ops import (  # noqa
+    Literal,
+    StringLiteral,
+    CaseInsensitiveStringLiteral,
+    RangeLiteral,
+    literal,
+
+    Concat,
+    concat,
+
+    Repeat,
+    repeat,
+    option,
+
+    Either,
+    either,
+
+    RuleRef,
+    rule,
+)
+
+from .opto import (  # noqa
+    optimize_op,
+)
+
+from .utils import (  # noqa
+    strip_insignificant_match_rules,
+    only_match_rules,
+
+    parse_rules,
+
+    fix_ws,
+)
---
+++ packages/omextra/omextra-0.0.0.dev497-py3-none-any.whl/omextra/text/abnf/core.py
@@ -0,0 +1,141 @@
+"""
+https://datatracker.ietf.org/doc/html/rfc5234
+"""
+import typing as ta
+
+from .base import Grammar
+from .base import Rule
+from .ops import concat
+from .ops import either
+from .ops import literal
+from .ops import repeat
+from .ops import rule
+
+
+##
+
+
+CORE_RULES: ta.Sequence[Rule] = [
+
+    Rule(
+        'ALPHA',
+        either(
+            literal('\x41', '\x5a'),
+            literal('\x61', '\x7a'),
+        ),
+    ),
+
+    Rule(
+        'BIT',
+        either(
+            literal('0'),
+            literal('1'),
+        ),
+    ),
+
+    Rule(
+        'CHAR',
+        literal('\x01', '\x7f'),
+    ),
+
+    Rule(
+        'CTL',
+        either(
+            literal('\x00', '\x1f'),
+            literal('\x7f', case_sensitive=True),
+        ),
+    ),
+
+    Rule(
+        'CR',
+        literal('\x0d', case_sensitive=True),
+        insignificant=True,
+    ),
+
+    Rule(
+        'CRLF',
+        concat(
+            rule('CR'),
+            rule('LF'),
+        ),
+        insignificant=True,
+    ),
+
+    Rule(
+        'DIGIT',
+        literal('\x30', '\x39'),
+    ),
+
+    Rule(
+        'DQUOTE',
+        literal('\x22', case_sensitive=True),
+    ),
+
+    Rule(
+        'HEXDIG',
+        either(
+            rule('DIGIT'),
+            literal('A'),
+            literal('B'),
+            literal('C'),
+            literal('D'),
+            literal('E'),
+            literal('F'),
+        ),
+    ),
+
+    Rule(
+        'HTAB',
+        literal('\x09', case_sensitive=True),
+        insignificant=True,
+    ),
+
+    Rule(
+        'LF',
+        literal('\x0a', case_sensitive=True),
+        insignificant=True,
+    ),
+
+    Rule(
+        'LWSP',
+        repeat(
+            either(
+                rule('WSP'),
+                concat(
+                    rule('CRLF'),
+                    rule('WSP'),
+                ),
+            ),
+        ),
+        insignificant=True,
+    ),
+
+    Rule(
+        'OCTET',
+        literal('\x00', '\xff'),
+    ),
+
+    Rule(
+        'SP',
+        literal('\x20', case_sensitive=True),
+        insignificant=True,
+    ),
+
+    Rule(
+        'VCHAR',
+        literal('\x21', '\x7e'),
+    ),
+
+    Rule(
+        'WSP',
+        either(
+            rule('SP'),
+            rule('HTAB'),
+        ),
+        insignificant=True,
+    ),
+
+]
+
+
+CORE_GRAMMAR = Grammar(*CORE_RULES)
