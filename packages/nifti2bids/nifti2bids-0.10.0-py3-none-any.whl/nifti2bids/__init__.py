76b6636c0ddc9402069606c645ffede133e639ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 16:04:29 +0000

    Add package nifti2bids/0.10.0/nifti2bids-0.10.0-py3-none-any.whl

---
+++ packages/nifti2bids/nifti2bids-0.10.0-py3-none-any.whl/nifti2bids/__init__.py
@@ -0,0 +1,23 @@
+"""
+Post-hoc BIDS conversion toolkit for NIfTI datasets without original DICOMs.
+----------------------------------------------------------------------------
+Documentation can be found at https://nifti2bids.readthedocs.io.
+
+Submodules
+----------
+bids -- Operations related to initializing and creating BIDS compliant files
+
+io -- Generic operations related to loading NIfTI data
+
+logging -- Set up a logger using ``RichHandler`` as the default handler if a root or
+module specific handler is not available
+
+metadata -- Operations related to extracting or creating metadata information from NIfTI images
+
+parsers -- Operations related to standardizing and parsing information logs created by stimulus
+presentation software such as Presentation and EPrime
+
+simulate -- Simulate a basic NIfTI image for testing purposes
+"""
+
+__version__ = "0.10.0"
