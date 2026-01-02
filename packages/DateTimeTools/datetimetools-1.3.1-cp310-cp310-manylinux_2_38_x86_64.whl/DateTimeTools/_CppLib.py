436d84b74be21066c69ef8aacb42b644e570a2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 14:07:33 +0000

    Add package DateTimeTools/1.3.1/datetimetools-1.3.1-cp310-cp310-manylinux_2_38_x86_64.whl

---
+++ packages/DateTimeTools/datetimetools-1.3.1-cp310-cp310-manylinux_2_38_x86_64.whl/DateTimeTools/_CppLib.py
@@ -0,0 +1,71 @@
+import os
+import platform
+import fnmatch
+
+def getLibFilename(isShort=False):
+	"""
+	Return library filename string
+	
+	Inputs
+	======
+	isShort : bool 
+		If False return filename with full path, if True return only filename
+		default - False
+	
+	Returns
+	=======
+	libFilename	: str
+	
+	"""
+	if(isShort):
+		libFilename = "libdatetime."
+	else:
+		vv = os.path.sep
+		libFilename = os.path.dirname(__file__) + vv + "__data" + vv + "datetime" + vv + "lib" + vv + "libdatetime."
+
+
+	systype = platform.system()
+	if systype == 'Linux':
+		extension = "so"
+	elif systype == 'Windows':
+		extension = "dll"
+	elif systype == 'Darwin':
+		extension = 'dylib'
+	else:
+		raise Exception("The Operating System is not supported")
+	
+	return libFilename + extension
+
+
+def checkLibExists():
+	"""Check if library file exist, and start compilation script if not."""
+	if not os.path.isfile(getLibFilename()):
+		print(getLibFilename(isShort=True)+" not found, try reinstalling")
+		raise SystemError
+
+
+def getWindowsSearchPaths():
+	'''Scan the directories within PATH and look for std C++ libs'''
+	paths = os.getenv('PATH')
+	paths = paths.split(';')
+
+	pattern = 'libstdc++*.dll'
+
+	out = []
+	for p in paths:
+		if os.path.isdir(p):
+			files = os.listdir(p)
+			mch = any(fnmatch.fnmatch(f,pattern) for f in files)
+			if mch:
+				out.append(p)
+	
+	return out
+
+
+
+def addWindowsSearchPaths():
+
+	paths = getWindowsSearchPaths()
+	for p in paths:
+		if os.path.isdir(p):
+			os.add_dll_directory(p)
