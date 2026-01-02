dcf79c62605dc1db7b854451a88231a97c1e5b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 22:05:02 +0000

    Add package nsj-rest-lib/6.0.4/nsj_rest_lib-6.0.4-py3-none-any.whl

---
+++ packages/nsj-rest-lib/nsj_rest_lib-6.0.4-py3-none-any.whl/nsj_rest_lib/service/service_base_update.py
@@ -0,0 +1,74 @@
+from typing import Any, Callable, Dict, List
+
+from nsj_rest_lib.dto.dto_base import DTOBase
+
+from .service_base_insert import ServiceBaseInsert
+
+
+class ServiceBaseUpdate(ServiceBaseInsert):
+
+    def update(
+        self,
+        dto: DTOBase,
+        id: Any,
+        aditional_filters: Dict[str, Any] = None,
+        custom_before_update: Callable = None,
+        custom_after_update: Callable = None,
+        upsert: bool = False,
+        manage_transaction: bool = True,
+        function_name: str | None = None,
+    ) -> DTOBase:
+        return self._save(
+            insert=False,
+            dto=dto,
+            manage_transaction=manage_transaction,
+            partial_update=False,
+            id=id,
+            aditional_filters=aditional_filters,
+            custom_before_update=custom_before_update,
+            custom_after_update=custom_after_update,
+            upsert=upsert,
+            function_name=function_name,
+        )
+
+    def update_list(
+        self,
+        dtos: List[DTOBase],
+        aditional_filters: Dict[str, Any] = None,
+        custom_before_update: Callable = None,
+        custom_after_update: Callable = None,
+        upsert: bool = False,
+        manage_transaction: bool = True,
+        function_name: str | None = None,
+    ) -> List[DTOBase]:
+        _lst_return = []
+        try:
+            if manage_transaction:
+                self._dao.begin()
+
+            for dto in dtos:
+                _return_object = self._save(
+                    insert=False,
+                    dto=dto,
+                    manage_transaction=False,
+                    partial_update=False,
+                    id=getattr(dto, dto.pk_field),
+                    aditional_filters=aditional_filters,
+                    custom_before_update=custom_before_update,
+                    custom_after_update=custom_after_update,
+                    upsert=upsert,
+                    function_name=function_name,
+                )
+
+                if _return_object is not None:
+                    _lst_return.append(_return_object)
+
+        except:
+            if manage_transaction:
+                self._dao.rollback()
+            raise
+        finally:
+            if manage_transaction:
+                self._dao.commit()
+
+        return _lst_return
