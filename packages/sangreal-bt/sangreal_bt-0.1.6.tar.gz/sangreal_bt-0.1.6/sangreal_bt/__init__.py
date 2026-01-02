f474b963a8798387e393e5c96f66a40189132e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-02 21:04:58 +0000

    Add package sangreal-bt/0.1.6/sangreal_bt-0.1.6.tar.gz

---
+++ packages/sangreal-bt/sangreal_bt-0.1.6.tar.gz/sangreal_bt-0.1.6/sangreal_bt/__init__.py
@@ -0,0 +1,13 @@
+"""
+Vector backtesting framework for quantitative strategies.
+"""
+
+# Core functionality
+from .strategy.strategy import Strategy
+from .datafeed.datafeed import DataPandas, DataBase
+
+__version__ = "0.0.34"
+
+__all__ = [
+    'Strategy', 'DataPandas', 'DataBase',
+]
