f9ad48bdb10b701ceca51512753542540bb19a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 16:05:59 +0000

    Add package mlstac/0.4.5/mlstac-0.4.5.tar.gz

---
+++ packages/mlstac/mlstac-0.4.5.tar.gz/mlstac-0.4.5/README.md
@@ -0,0 +1,17 @@
+# MLSTAC: Machine Learning with STAC
+
+[![PyPI version](https://img.shields.io/pypi/v/mlstac.svg)](https://pypi.org/project/mlstac/)
+[![Python Versions](https://img.shields.io/pypi/pyversions/mlstac.svg)](https://pypi.org/project/mlstac/)
+[![License](https://img.shields.io/pypi/l/mlstac.svg)](https://github.com/csaybar/isp-models/blob/main/LICENSE)
+[![Documentation](https://img.shields.io/badge/docs-latest-blue.svg)](https://csaybar.github.io/isp-models/)
+
+
+We take advantage of the new mlm STAC extension to provide a unified interface for working with machine learning models.
+**Experimental**
+
+
+## Installation
+
+```bash
+pip install mlstac
+```
---
+++ packages/mlstac/mlstac-0.4.5.tar.gz/mlstac-0.4.5/mlstac/utils.py
@@ -0,0 +1,55 @@
+"""
+Utility Functions
+
+This module provides utility functions used throughout the mlstac package.
+"""
+
+from pathlib import Path
+from typing import Literal
+from urllib.parse import urlparse
+
+# Define the valid scheme types for typing
+SchemeType = Literal["http", "https", "ftp", "s3", "gs", "local", "snippet"]
+
+
+def get_scheme(source: str | list) -> SchemeType:
+    """
+    Determine the protocol scheme for a given source.
+
+    This function analyzes a URL, file path, or list of paths and determines
+    how it should be accessed. It supports HTTP(S), FTP, cloud storage (S3, GS),
+    local files, special "snippet" references, and direct .pt2 model lists.
+
+    Args:
+        source: A URL, file path, or list of .pt2 model paths
+
+    Returns:
+        The identified scheme: 'http', 'https', 'ftp', 's3', 'gs', 'local',
+        'snippet', or 'pt2_list'
+
+    Examples:
+        >>> get_scheme("https://example.com/model.safetensor")
+        'https'
+        >>> get_scheme("/path/to/local/model")
+        'local'
+        >>> get_scheme("resnet50")  # A snippet reference
+        'snippet'
+        >>> get_scheme([Path("model1.pt2"), Path("model2.pt2")])
+        'pt2_list'
+    """
+    # Check if it's a list (direct .pt2 paths)
+    if isinstance(source, list):
+        return "pt2_list"
+
+    parsed = urlparse(source)
+
+    # Check for URL schemes
+    if parsed.scheme in {"http", "https", "ftp", "s3", "gs"}:
+        return parsed.scheme
+
+    # Check if it's a local path that exists
+    if Path(source).exists():
+        return "local"
+
+    # If not a URL or local path, assume it's a model snippet reference
+    return "snippet"
