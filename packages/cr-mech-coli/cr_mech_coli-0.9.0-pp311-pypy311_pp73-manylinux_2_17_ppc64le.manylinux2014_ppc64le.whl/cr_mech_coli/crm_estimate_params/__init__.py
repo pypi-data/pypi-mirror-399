a293eaf443e04b1bc53764c46efefa4b44636782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:29 +0000

    Add package cr-mech-coli/0.9.0/cr_mech_coli-0.9.0-pp311-pypy311_pp73-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/cr-mech-coli/cr_mech_coli-0.9.0-pp311-pypy311_pp73-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/cr_mech_coli/crm_estimate_params/__init__.py
@@ -0,0 +1,8 @@
+"""
+.. code-block:: text
+    :caption: Usage of the `crm_estimate_params` script
+"""
+
+from cr_mech_coli.crm_estimate_params.crm_estimate_params_rs import *
+
+from .main import crm_estimate_params_main
