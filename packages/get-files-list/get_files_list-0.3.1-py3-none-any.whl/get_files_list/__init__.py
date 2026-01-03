0d39987565b8f2942ad449e6eece35283d6d1fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:36 +0000

    Add package get-files-list/0.3.1/get_files_list-0.3.1-py3-none-any.whl

---
+++ packages/get-files-list/get_files_list-0.3.1-py3-none-any.whl/get_files_list/__init__.py
@@ -0,0 +1,10 @@
+"""
+get_files_list
+
+Python package that provides a utility function to recursively get files that match a pattern.
+"""
+
+from .get_dir_content import get_dir_content
+
+__version__ = "0.3.1"
+__author__ = "Luca Angioloni"
