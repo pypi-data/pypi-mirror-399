68840d98d8a109cff76d037568328b2844f8895b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:27 +0000

    Add package comfyui-frontend-package/1.37.2/comfyui_frontend_package-1.37.2.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.37.2.tar.gz/comfyui_frontend_package-1.37.2/comfyui_frontend_package/static/assets/index-CyS0uSKu.js
@@ -0,0 +1,2 @@
+import{c as o}from"./index-BODInyrK.js";const e=o(["not-disabled:bg-component-node-widget-background","not-disabled:text-component-node-foreground","border-none","rounded-lg"]);export{e as W};
+//# sourceMappingURL=index-CyS0uSKu.js.map
