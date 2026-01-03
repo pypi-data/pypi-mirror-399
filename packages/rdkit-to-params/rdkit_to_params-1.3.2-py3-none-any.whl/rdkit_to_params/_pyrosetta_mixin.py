cb9708fc06d60c2439290040ab3bcfa04229d437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:28 +0000

    Add package rdkit-to-params/1.3.2/rdkit_to_params-1.3.2-py3-none-any.whl

---
+++ packages/rdkit-to-params/rdkit_to_params-1.3.2-py3-none-any.whl/rdkit_to_params/_pyrosetta_mixin.py
@@ -0,0 +1,124 @@
+########################################################################################################################
+
+__doc__ = """
+``_PoserMixin`` is a base that adds pyrosetta functionality if avaliable. Adds:
+
+*the bound method ``.test(outfile=None)``, which returns a pose.
+*the static method ``.params_to_pose(paramsfile:str, name:str)``, which returns a pose.
+
+"""
+
+
+########################################################################################################################
+
+
+import logging
+from typing import TYPE_CHECKING, Optional
+
+import pyrosetta
+import pyrosetta.rosetta.core as prc
+
+if TYPE_CHECKING:
+    from rdkit_to_params.entries import Entries
+
+
+class _PoserMixin:
+    # Type hints for attributes that come from other mixins
+    log: logging.Logger
+    NAME: str
+    ATOM: "Entries"
+
+    def dumps(self) -> str:
+        return ""  # Implemented in _ParamsIoMixin
+    def test(self, outfile: Optional[str] = None) -> pyrosetta.Pose:
+        """
+        Makes a pose with the ligand to see if it works.
+
+        :param outfile: optionally save file.
+        :return: pose
+        """
+        self.log.debug(
+            f"Testing params {'(writing out as ' + outfile + ')' if outfile is not None else '(no saving)'}"
+        )
+        pose = self.to_pose(relax=True)
+        assert pose.total_residue() == 1, "Residue failed."
+        if outfile:
+            pose.dump_pdb(outfile)
+        return pose
+
+    def to_pose(self, relax: bool = False) -> pyrosetta.Pose:
+        pose = pyrosetta.Pose()
+        # add paramsblock
+        rts = self.add_residuetype(pose)
+        # add new residue
+        lig = pyrosetta.rosetta.core.conformation.ResidueFactory.create_residue(
+            rts.name_map(self.NAME)
+        )
+        pose.append_residue_by_jump(lig, 1)
+        if relax:
+            self._relax(pose)
+        return pose
+
+    def to_polymeric_pose(
+        self, relax: bool = False, sequence: Optional[str] = None
+    ) -> pyrosetta.Pose:
+        if sequence is None:
+            sequence = f"AX[{self.NAME}]A"
+        pose = pyrosetta.Pose()
+        rts = self.add_residuetype(pose)
+        pyrosetta.rosetta.core.pose.make_pose_from_sequence(pose, sequence, rts)
+        if relax:
+            self._relax(pose)
+        return pose
+
+    def _relax(
+        self,
+        pose: "pyrosetta.Pose",
+        scorefxn: Optional["pyrosetta.ScoreFunction"] = None,
+        cycles: Optional[int] = 15,
+    ) -> None:
+        if scorefxn is None:
+            scorefxn = pyrosetta.get_fa_scorefxn()
+        relax = pyrosetta.rosetta.protocols.relax.FastRelax(scorefxn, cycles)
+        relax.apply(pose)
+
+    def add_residuetype(
+        self, pose: "pyrosetta.Pose", reset: bool = True
+    ) -> prc.chemical.ResidueTypeSet:
+        """
+        Adds the params to a copy of a residue type set of a pose.
+        If reset is True it will also save it as the default RTS —it keeps other custom residue types.
+
+        :param pose:
+        :param reset:
+        :return:
+        """
+        rts = pose.conformation().modifiable_residue_type_set_for_conf(
+            pyrosetta.rosetta.core.chemical.FULL_ATOM_t
+        )
+        buffer = pyrosetta.rosetta.std.stringbuf(self.dumps())
+        stream = pyrosetta.rosetta.std.istream(buffer)
+        new = pyrosetta.rosetta.core.chemical.read_topology_file(stream, self.NAME, rts)
+        rts.add_base_residue_type(new)
+        if reset:
+            pose.conformation().reset_residue_type_set_for_conf(rts)
+        return rts
+
+    @staticmethod
+    def params_to_pose(paramsfile: str, name: str) -> pyrosetta.Pose:
+        """
+        Staticmethod to get a pose from a params file.
+
+        :param paramsfile: params file.
+        :param name: 3-letter residue name.
+        :return:
+        """
+        pose = pyrosetta.Pose()
+        params_paths = pyrosetta.rosetta.utility.vector1_string()
+        params_paths.extend([paramsfile])
+        resiset = pyrosetta.generate_nonstandard_residue_set(pose, params_paths)
+        lig = pyrosetta.rosetta.core.conformation.ResidueFactory.create_residue(
+            resiset.name_map(name)
+        )
+        pose.append_residue_by_jump(lig, 1)
+        return pose
