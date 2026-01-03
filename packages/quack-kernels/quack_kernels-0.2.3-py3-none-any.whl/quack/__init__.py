0adfa941a90fbbb499ccffb35ee8bbb78170e6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:15:11 +0000

    Add package quack-kernels/0.2.3/quack_kernels-0.2.3-py3-none-any.whl

---
+++ packages/quack-kernels/quack_kernels-0.2.3-py3-none-any.whl/quack/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.3"
+
+from quack.rmsnorm import rmsnorm
+from quack.softmax import softmax
+from quack.cross_entropy import cross_entropy
+
+__all__ = [
+    "rmsnorm",
+    "softmax",
+    "cross_entropy",
+]
