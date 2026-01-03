fa2028149e87c88a2b722ebfcf8177a95abd7c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:27 +0000

    Add package phasorpy/0.9/phasorpy-0.9-cp312-abi3-win_amd64.whl

---
+++ packages/phasorpy/phasorpy-0.9-cp312-abi3-win_amd64.whl/phasorpy/utils.py
@@ -0,0 +1,178 @@
+"""Utility functions.
+
+The ``phasorpy.utils`` module provides auxiliary and convenience functions
+that do not fit naturally into other modules.
+
+"""
+
+from __future__ import annotations
+
+__all__ = [
+    'logger',
+    'number_threads',
+    'versions',
+]
+
+import logging
+import os
+
+
+def logger() -> logging.Logger:
+    """Return PhasorPy logger instance.
+
+    Returns
+    -------
+    logging.Logger
+        Logger instance for 'phasorpy' namespace.
+
+    Examples
+    --------
+    >>> logger().info('This is a log message')
+
+    """
+    return logging.getLogger('phasorpy')
+
+
+def number_threads(
+    num_threads: int | None = None,
+    max_threads: int | None = None,
+    /,
+) -> int:
+    """Return number of threads for parallel computations across CPU cores.
+
+    This function is used to parse ``num_threads`` parameters in the API.
+
+    Parameters
+    ----------
+    num_threads : int, optional
+        Number of threads to use for parallel computations on CPU cores.
+        By default, return 1, disabling multithreading.
+        If greater than zero, return the value, limited to `max_threads` if
+        set.
+        If zero, return the value of the ``PHASORPY_NUM_THREADS``
+        environment variable if set, otherwise half the number of CPU cores
+        (up to `max_threads` if specified, or 32 if not).
+    max_threads : int, optional
+        Maximum number of threads to return.
+
+    Returns
+    -------
+    int
+        Number of threads for parallel computations.
+
+    Examples
+    --------
+    >>> number_threads()
+    1
+    >>> number_threads(0)  # doctest: +SKIP
+    8  # actual value depends on system
+
+    """
+    if num_threads is None or num_threads < 0:
+        # disable multithreading by default
+        return 1
+    if num_threads == 0:
+        # return default number of threads
+        max_threads = 32 if max_threads is None else max(max_threads, 1)
+        if 'PHASORPY_NUM_THREADS' in os.environ:
+            return min(
+                max_threads, max(1, int(os.environ['PHASORPY_NUM_THREADS']))
+            )
+        cpu_count: int | None
+        if hasattr(os, 'sched_getaffinity'):
+            cpu_count = len(os.sched_getaffinity(0))
+        else:
+            # sched_getaffinity not available on Windows
+            cpu_count = os.cpu_count()
+        if cpu_count is None:
+            return 1
+        return min(max_threads, max(1, cpu_count // 2))
+    # return num_threads up to max_threads
+    if max_threads is None:
+        return num_threads
+    return min(num_threads, max(max_threads, 1))
+
+
+def versions(
+    *,
+    sep: str = '\n',
+    dash: str = '-',
+    verbose: bool = False,
+) -> str:
+    """Return version information for PhasorPy and its dependencies.
+
+    Parameters
+    ----------
+    sep : str, optional, default: newline
+        Separator between version items. The default is newline.
+    dash : str, optional, default: -
+        Separator between module name and version. The default is a dash.
+    verbose : bool, optional, default: False
+        Include paths to Python interpreter and modules.
+
+    Returns
+    -------
+    str
+        Formatted string containing version information.
+        Format: ``"<package><dash><version>[<space>(<path>)]<sep>"``
+
+    Examples
+    --------
+    >>> print(versions())  # doctest: +SKIP
+    Python-3.14.1
+    phasorpy-0.8
+    numpy-2.3.5
+    ...
+
+    """
+    import importlib.metadata
+    import os
+    import sys
+
+    if verbose:
+        version_strings = [f'Python{dash}{sys.version}  ({sys.executable})']
+    else:
+        version_strings = [f'Python{dash}{sys.version.split()[0]}']
+
+    for module in (
+        'phasorpy',
+        'numpy',
+        'tifffile',
+        'imagecodecs',
+        'lfdfiles',
+        'fbdfile',
+        'sdtfile',
+        'ptufile',
+        'liffile',
+        'matplotlib',
+        'scipy',
+        'skimage',
+        'sklearn',
+        'pandas',
+        'xarray',
+        'pawflim',
+        'mkl_fft',
+        'click',
+        'pooch',
+    ):
+        try:
+            __import__(module)
+        except (ModuleNotFoundError, ImportError, ValueError):
+            version_strings.append(f'{module}{dash}n/a')
+            continue
+        lib = sys.modules[module]
+        try:
+            ver = importlib.metadata.version(module)
+        except importlib.metadata.PackageNotFoundError:
+            ver = getattr(lib, '__version__', 'unknown')
+        ver = f'{module}{dash}{ver}'
+        if verbose:
+            try:
+                path = lib.__file__
+            except NameError:
+                pass
+            else:
+                if path is not None:
+                    ver += f'  ({os.path.dirname(path)})'
+        version_strings.append(ver)
+    return sep.join(version_strings)
