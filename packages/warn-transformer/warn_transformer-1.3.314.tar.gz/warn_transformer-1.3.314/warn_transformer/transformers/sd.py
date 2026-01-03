8aa4240544e1dc64638b2ab8e17e860b161ce8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:06:01 +0000

    Add package warn-transformer/1.3.314/warn_transformer-1.3.314.tar.gz

---
+++ packages/warn-transformer/warn_transformer-1.3.314.tar.gz/warn_transformer-1.3.314/warn_transformer/transformers/sd.py
@@ -0,0 +1,19 @@
+from ..schema import BaseTransformer
+
+
+class Transformer(BaseTransformer):
+    """Transform South Dakota raw data for consolidation."""
+
+    postal_code = "SD"
+    fields = dict(
+        company="Company",
+        location="Location",
+        notice_date="Date Received",
+        jobs="Employees Affected",
+    )
+    date_format = "%m/%d/%Y"
+    jobs_corrections = {
+        "1-5": 1,
+        "324 (11 reside in South Dakota)": 11,
+        "n/a": None,
+    }
