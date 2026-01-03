4426f62c876eda0306f05c59234f4511991de5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:36:06 +0000

    Add package constructional-complexity-analyzer/0.1.1/constructional_complexity_analyzer-0.1.1-py3-none-any.whl

---
+++ packages/constructional-complexity-analyzer/constructional_complexity_analyzer-0.1.1-py3-none-any.whl/constructional_complexity_analyzer/__main__.py
@@ -0,0 +1,14 @@
+import sys
+from constructional_complexity_analyzer.core import run_constructional_analysis
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: constructional-analysis <input_text_dir>")
+        sys.exit(1)
+
+    input_text_dir = sys.argv[1]
+    df_diversity, df_complexity, df_verb, df_construction = run_constructional_analysis(input_text_dir)
+    print(df_diversity.head())
+    print(df_complexity.head())
+    print(df_verb.head())
+    print(df_construction.head())
