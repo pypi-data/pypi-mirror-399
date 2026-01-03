7d614be90f92f1565236e4824c6eaa3dfff51fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:49 +0000

    Add package prefect/3.6.9/prefect-3.6.9.tar.gz

---
+++ packages/prefect/prefect-3.6.9.tar.gz/prefect-3.6.9/src/prefect/server/ui/assets/404-M7IOp4Us.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,m as t,dz as r}from"./index-B0Ov_jWr.js";import{u as s}from"./usePageTitle-DOxz6387.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,m)=>(a(),o(t(r)))}});export{_ as default};
+//# sourceMappingURL=404-M7IOp4Us.js.map
