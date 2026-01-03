19772dd50a0f68bbc13400a1d1a8b56f5e72c244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:11 +0000

    Add package moat-ems/0.2.12/moat_ems-0.2.12.tar.gz

---
+++ packages/moat-ems/moat_ems-0.2.12.tar.gz/moat_ems-0.2.12/src/moat/ems/_main.py
@@ -0,0 +1,21 @@
+"""
+Basic tool support
+
+"""
+
+from __future__ import annotations
+
+import logging  # pylint: disable=wrong-import-position
+
+import asyncclick as click
+
+from moat.lib.run import load_subgroup
+
+log = logging.getLogger()
+
+
+@load_subgroup(sub_pre="moat.ems")
+@click.pass_obj
+async def cli(obj):
+    """Energy Management System"""
+    obj  # noqa:B018
