8ca1002a91d8384a10944364e56f192d29282ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:35:52 +0000

    Add package opensearchcli/0.2.2/opensearchcli-0.2.2-py3-none-any.whl

---
+++ packages/opensearchcli/opensearchcli-0.2.2-py3-none-any.whl/opensearchcli/commands/snapshot.py
@@ -0,0 +1,14 @@
+import typer
+from rich.console import Console
+
+app = typer.Typer(
+    rich_markup_mode="rich", no_args_is_help=True, help="Manage snapshots"
+)
+console = Console()
+
+
+@app.command()
+def repository(ctx: typer.Context):
+    """Get snapshot information."""
+    snapshot_info = ctx.obj.opensearch.snapshot.get_repository()
+    console.print(snapshot_info)
