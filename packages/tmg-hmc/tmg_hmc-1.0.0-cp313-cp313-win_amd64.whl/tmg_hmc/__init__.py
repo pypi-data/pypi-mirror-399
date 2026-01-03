71bbed4936e94b8125f9b1890e6a62aac0dc57ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 07:08:56 +0000

    Add package tmg-hmc/1.0.0/tmg_hmc-1.0.0-cp313-cp313-win_amd64.whl

---
+++ packages/tmg-hmc/tmg_hmc-1.0.0-cp313-cp313-win_amd64.whl/tmg_hmc/__init__.py
@@ -0,0 +1,33 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'tmg_hmc.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+__version__ = "1.0.0"
+
+try:
+    import torch
+    _TORCH_AVAILABLE = True
+except ImportError:
+    _TORCH_AVAILABLE = False
+
+class _TensorPlaceholder(object):
+        pass
+
+def get_torch():
+    if not _TORCH_AVAILABLE:
+        return None
+    return torch
+
+def get_tensor_type():
+    if not _TORCH_AVAILABLE:
+        return _TensorPlaceholder
+    return torch.Tensor
+
+from .sampler import TMGSampler
