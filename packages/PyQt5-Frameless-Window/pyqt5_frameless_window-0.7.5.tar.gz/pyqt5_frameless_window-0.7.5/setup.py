f1c36539aee23fe8930bbd2407c93dd6bc19a747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 14:06:20 +0000

    Add package PyQt5-Frameless-Window/0.7.5/pyqt5_frameless_window-0.7.5.tar.gz

---
+++ packages/PyQt5-Frameless-Window/pyqt5_frameless_window-0.7.5.tar.gz/pyqt5_frameless_window-0.7.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="PyQt5-Frameless-Window",
+    version="0.7.5",
+    keywords="pyqt frameless",
+    author="zhiyiYo",
+    author_email="shokokawaii@outlook.com",
+    description="A cross-platform frameless window based on pyqt5, support Win32, X11, Wayland and macOS.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="GPLv3",
+    url="https://github.com/zhiyiYo/PyQt-Frameless-Window",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pywin32;platform_system=='Windows'",
+        "xcffib;platform_system=='Linux'",
+        "pyobjc;platform_system=='Darwin'",
+        "PyCocoa;platform_system=='Darwin'",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
+    ]
+)
