3afbd5e695e16619da46a9afea6e0ad7146e793e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:14:16 +0000

    Add package theory-snake/0.2.0/theory_snake-0.2.0-py3-none-any.whl

---
+++ packages/theory-snake/theory_snake-0.2.0-py3-none-any.whl/theory_snake/consts.py
@@ -0,0 +1,21 @@
+#Notes
+Notes = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+
+#Scale Intervals
+MAJOR_SCALE_INTERVALS = [2, 2, 1, 2, 2, 2, 1]
+MINOR_SCALE_INTERVALS = [2, 1, 2, 2, 1, 2, 2]
+
+# Chord Types
+MAJOR_CHORD = ['1','3','5']
+MINOR_CHORD = ['1','3b','5']
+
+#Guitar constants
+FRET_COUNT = 18
+COMMON_TUNNINGS = {
+    "Standard": ["E", "A", "D", "G", "B", "E"],
+    "Drop D": ["D", "A", "D", "G", "B", "E"],
+    "DADGAD": ["D", "A", "D", "G", "A", "D"],
+    "Open G": ["D", "G", "D", "G", "B", "D"],
+    "Open D": ["D", "A", "D", "F#", "A", "D"],
+}
+
