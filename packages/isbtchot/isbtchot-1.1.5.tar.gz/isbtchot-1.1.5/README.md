1af5e64d04966d3171b2454e5008cb387e52c81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 17:12:39 +0000

    Add package isbtchot/1.1.5/isbtchot-1.1.5.tar.gz

---
+++ packages/isbtchot/isbtchot-1.1.5.tar.gz/isbtchot-1.1.5/README.md
@@ -0,0 +1,56 @@
+# isbtchot: BTC Hotness Index Dashboard
+
+The `isbtchot` package provides a minimalist terminal dashboard for visualizing Bitcoin's (BTC) hotness index. It utilizes historical BTC data to generate visualizations of BTC's price alongside the Hotness Index, directly in your terminal.
+
+## Features:
+
+- Visualizes BTC candlestick plots with log scale options.
+- Displays the Hotness Index based on proprietary indicators with buy/sell signals.
+- Supports different time frames and custom period back settings to tailor the data.
+- Offers multiple dashboard views including standard hotness index and power-law delta.
+- Data is cached locally to expedite subsequent visualizations.
+
+## Installation
+
+```bash
+pip install isbtchot
+```
+
+## Usage
+
+You can run the main dashboard with flexible command-line options:
+
+```bash
+isbtchot [--periods_back P] [--time T] [--dashboard D]
+```
+
+- `--periods_back`, `-p`: Specifies the number of periods back to process for generating the visualization. Defaults to 85.
+- `--time`, `-t`: Specifies the candlestick time frame (e.g., Weekly, Monthly). Defaults to Weekly.
+- `--dashboard`, `-d`: Choose the type of dashboard to display: `isbtchot` for the standard hotness index or `power_law` for the power-law distribution analysis. Defaults to `isbtchot`.
+
+### Command Line Arguments
+
+| Argument       | Short Form | Description                                                                     | Default   |
+|----------------|------------|---------------------------------------------------------------------------------|-----------|
+| `--periods_back` | `-p`       | Number of periods to be processed for the data visualization.                    | 85        |
+| `--time`        | `-t`       | Candlestick time to use (e.g., M for Monthly, W for Weekly).                   | W         |
+| `--dashboard`   | `-d`       | Dashboard type to display: `isbtchot` or `power_law`.                           | isbtchot  |
+
+## Demo
+
+### Hotness Index Dashboard
+Historically very reliable for bull market tops:
+![BTC Hotness Index Dashboard Demo](media/demo.png)
+
+### Power Law Delta Dashboard
+Historically very reliable for bear market bottoms:
+![BTC Power Law Delta Dashboard Demo](media/demo2.png)
+
+## Notes
+
+- For optimal visualization, maximize your terminal window.
+- Data is fetched from CryptoCompare API and cached to enhance performance and responsiveness.
+
+## Contributions
+
+Contributions are welcome! Please feel free to submit issues, enhancements, or pull requests. If you encounter a bug or have a feature suggestion, please open an issue on GitHub.
---
+++ packages/isbtchot/isbtchot-1.1.5.tar.gz/isbtchot-1.1.5/src/isbtchot/model.py
@@ -0,0 +1,162 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+import requests
+from sklearn.linear_model import LinearRegression
+
+import isbtchot
+from isbtchot.schemas.args import TypeTime
+
+CACHE_BTC_PATH = isbtchot.root_path / "cache" / "btc.csv"
+BTC_API = (
+    "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&allData=true"
+)
+
+
+def btc_historical_daily(options=None) -> pd.DataFrame:
+    if not options:
+        options = {}
+
+    use_cache = options.get("disable_cache") is None
+
+    if use_cache:
+        # Ensure the directory exists
+        CACHE_BTC_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+    # Check if the file exists and was modified today
+    if (
+        use_cache
+        and CACHE_BTC_PATH.is_file()
+        and datetime.datetime.fromtimestamp(CACHE_BTC_PATH.stat().st_mtime).date()
+        == datetime.datetime.today().date()
+    ):
+        df = pd.read_csv(CACHE_BTC_PATH)
+
+    # Fetch new data
+    else:
+        data = requests.get(BTC_API, verify=False).json()["Data"]["Data"]
+        df = pd.DataFrame(data)
+
+        if use_cache:
+            df.to_csv(CACHE_BTC_PATH, index=False)
+
+    df["time"] = pd.to_datetime(df["time"], unit="s")
+    df.set_index("time", inplace=True)
+    return df
+
+
+def btc_pi(time_grouping: TypeTime, periods_back: int | None = None, **kwargs):
+    df = btc_historical_daily(**kwargs)[["close"]].rename({"close": "price"}, axis=1)
+
+    df["sma111"] = df["price"].rolling(window=111).mean()
+    df["sma350x2"] = df["price"].rolling(window=350).mean() * 2
+    df["pi"] = df["sma111"] / df["sma350x2"]
+
+    df = df.drop(["sma111", "sma350x2"], axis=1)
+    df = df.dropna()
+
+    # Normalize pi
+    df["pi"] = (df["pi"] - 0.35) / (1 - 0.35)
+
+    # Sell Indicator
+    mask_pi_sell = (df["pi"].shift(-1) >= 1) & (df["pi"] < 1)
+    df["pi_sell"] = mask_pi_sell
+
+    # Buy indicator
+    mask_pi_buy = (df["pi"].shift(-1) <= 0) & (df["pi"] > 0)
+    df["pi_buy"] = mask_pi_buy
+
+    df = df.resample(time_grouping.value).agg(
+        {
+            "price": "last",
+            "pi": "max",
+            "pi_sell": "max",
+            "pi_buy": "max",
+        }
+    )
+
+    df = df.reset_index()
+    df.time = df.time.dt.strftime("%d/%m/%Y")
+
+    if periods_back:
+        periods_back = min(periods_back, len(df))
+        df = df.iloc[-periods_back:]
+
+    return df
+
+
+def btc_hist(
+    time_grouping: TypeTime | None = None, periods_back: int | None = None, **kwargs
+):
+    df = btc_historical_daily(**kwargs)
+    df = df.rename(
+        {"open": "Open", "close": "Close", "high": "High", "low": "Low"}, axis=1
+    )
+    if time_grouping:
+        df = df.resample(time_grouping.value).agg(
+            {
+                "Open": "first",
+                "High": "max",
+                "Low": "min",
+                "Close": "last",
+            }
+        )
+    if periods_back:
+        df = df.iloc[-periods_back:]
+    return df
+
+
+def btc_power_law(
+    time_grouping: TypeTime | None = None,
+    periods_back: int | None = None,
+    years_to_predict: int = 20,
+    **kwargs,
+) -> pd.DataFrame:
+    df = btc_hist(time_grouping=time_grouping, **kwargs)[["Close"]].rename(
+        {"Close": "price"}, axis=1
+    )
+
+    # Adjust for genesis block
+    genesis_delta = (df.index[0] - pd.Timestamp(year=2009, month=1, day=3)).days
+    if time_grouping is TypeTime.WEEK:
+        genesis_delta /= 7
+    elif time_grouping is TypeTime.MONTH:
+        genesis_delta /= 30
+
+    df["days"] = np.arange(len(df)) + genesis_delta
+    # Applying log transformation
+    X_log = np.log(df["days"].values.reshape(-1, 1))
+    y_log = np.log(df["price"].values)
+
+    # Fit the linear regression model on the log-transformed data
+    power_law_model = LinearRegression().fit(X_log, y_log)
+
+    # The coefficient 'b' is the slope of the line in the log-log space
+    b = power_law_model.coef_[0]
+    # The coefficient 'a' is obtained by taking the exponential of the intercept in the log-log space
+    a = np.exp(power_law_model.intercept_)
+
+    # Extend df
+    periods = 365 * years_to_predict
+    df = pd.concat(
+        [
+            df,
+            pd.DataFrame(
+                {
+                    "days": np.arange(periods) + df.days.iloc[-1],
+                },
+                index=pd.date_range(df.index[-1], periods=periods, freq="D"),
+            ),
+        ]
+    )
+
+    # Using the power law model to predict prices across the original range of days
+    df["power_law"] = a * (df.days**b)
+    df["power_law_bottom"] = df.power_law * 0.45
+    df["power_law_top"] = df.power_law * 3
+    df["delta"] = df.price / df.power_law
+
+    if periods_back:
+        df = df.dropna().iloc[-periods_back:]
+    return df
