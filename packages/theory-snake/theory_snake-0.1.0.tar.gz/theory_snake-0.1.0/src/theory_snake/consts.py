c41eb70bc7069dbcf29471c390bbc77339cd7ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 03:33:39 +0000

    Add package theory-snake/0.1.0/theory_snake-0.1.0.tar.gz

---
+++ packages/theory-snake/theory_snake-0.1.0.tar.gz/theory_snake-0.1.0/src/theory_snake/consts.py
@@ -0,0 +1,10 @@
+#Notes
+Notes = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
+
+#Scale Intervals
+MAJOR_SCALE_INTERVALS = [2, 2, 1, 2, 2, 2, 1]
+MINOR_SCALE_INTERVALS = [2, 1, 2, 2, 1, 2, 2]
+
+# Chord Types
+MAJOR_CHORD = ['1','3','5']
+MINOR_CHORD = ['1','3b','5']
