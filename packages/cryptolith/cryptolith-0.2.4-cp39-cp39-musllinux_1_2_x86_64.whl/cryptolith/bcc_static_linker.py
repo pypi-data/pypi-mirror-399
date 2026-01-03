e35bac657eda2a8102e765fa296113d4254ca69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:06:46 +0000

    Add package cryptolith/0.2.4/cryptolith-0.2.4-cp39-cp39-musllinux_1_2_x86_64.whl

---
+++ packages/cryptolith/cryptolith-0.2.4-cp39-cp39-musllinux_1_2_x86_64.whl/cryptolith/bcc_static_linker.py
@@ -0,0 +1,85 @@
+from . import cryptolith_runtime
+import sys as _obs_sys
+import os as _obs_os
+
+def _junk_func():
+    pass
+import base64
+RkpJJVRjHA = base64.b64decode('/l0RfrimRy6Jo4tTh7uJPaU3sRe+4I79TtUcT+dBjBM=')
+
+def NCsPozaCUX(i):
+    d = base64.b64decode(aowmYUfjHL[i])
+    return bytes([b ^ RkpJJVRjHA[j % len(RkpJJVRjHA)] for j, b in enumerate(d)]).decode('utf-8')
+aowmYUfjHL = ['9H0xXpjnMlrmzuon4sipSc1SkXTRjf6UIrRoJogvrHyYfWcf1M8jDsqD6Dzj3qlUy0PeN9CB+pQ4sDwfnjXkfJB9dAbMwyld4MzlIKfI+VjGXtd+3cD6km6hdCrHIvlhjDh/CpjWK0/9xeQh6pWDHYUXkVXHkO+OPbBvb4Mo/2eLKXgSy4k0S/3W+yfo1OVOhVjHcsyI65wq9XoglWHrdpA4Yx/MwyMOy+DIc+TU7ViLPZE3nsA=', 'lzNyEs3CIg==', 'jSl1EtHE', 'kjRzDQ==', 'tzNlG9/ULlrwg9066NfoScxY3y2er/6cP6B5b7cz6XeXPnAK3YYBT+DP7jc=', 'ky5nHQ==', 'ky5nHefTKUXnzPw9', 'tzNlG9/ULlrwg9066NfoScxY3y2er/6cP6B5b7cz6XeXPnAK3YYBT+DP7jc=', 'mT5y', 'ixRnC9rCLGDi1g==', 'hxddH8j1Jnf+8Q==', 'rSliBOvHBnTxwQ==', 'iTV4HdA=', 'rCh/CtHLImv70eQh', 'ky5nHQ==', 'jTh9GA==', 'iDJiL9XWKmLm5g==', 'iRlBDc/wPmvxxw==', 'jDRfKf3iC3rd6g==', 'mzx+C97TKGHnxQ==', 'sCdyMODhK2TL2g==', 'tRh2BN/BBXv+yQ==', 'swhAF9fsBkni5Q==', 'uDVdKezhEH/N+w==', 'hzpLKs/uAEvH6w==', '0TN+EtfBKA==', '0RFV', '0RIj', '0Qoi', 'nXY6', '0S5lGoLFbAW4lA==', '0RhZDds=', '0QlB', '0QlS', '0TF4ENM=', '0RFYPOjnE2az', '0DF4HA==', 'tzNlG9/ULlrwg9066NfoScxY3y2er/6cP6B5b7cz6XeXPnAK3YYBT+DP7jc=', '0RJEKoI=', 'pRF4ENPDNXOp8f496dLnWp8X', 'pRF4ENPDNXOp4OQ+99LlXNFe3nmepu+UIrB4dcc=', 'jDRfKf3iC3rd6g==', 'ixRnC9rCLGDi1g==', 'lTl4OevMP33gzQ==', 'mjRjENnLIg==', 'lApBLcz1NlrBxg==', 'myV4DczV', 'kzx6G9zPNV0=', 'mAxcH8vNDk3E0A==', 'swhAF9fsBkni5Q==', 'ny1hG9bC', 'mzx+C97TKGHnxQ==', 'jxBmHdbwKF7/xA==', 'jSlj', 'jTh9GA==', 'uzZoDOD3DF3R5A==', 'iRlBDc/wPmvxxw==', 'sCdyMODhK2TL2g==', 'sTRED/zDLG3r+w==', 'kS9cGM3uC33i5Q==', 'vSdIL9n8MWDa+g==', 'tRh2BN/BBXv+yQ==', 'jCRcP93KL13Ayg==', 'kjJmG8o=', 'mzN1Dc/PM0Y=', 'rCh/CtHLImv70eQh', 'mDF/HOHDPlnz5g==', 'lDJ4EA==', 'ji94EMw=', 'rDZHCczrKWnQzw==', 'qidhFuzkIk/Ayw==', 'vTx9Et3CF1zmwO4g9P77T8pF', 'sQd3LNn3Hl3u+g==', 'mT5y', '0y55H8rDIw==', '0xIi', '0ztBN/s=', 'nXY6', '0y5lGoXFbAW4lA==', 'pRF4ENPDNXOp8f496dLnWp8X', 'tzNlG9/ULlrwg9066NfoScxY3y2er/6cP6B5b7cz6XeXPnAK3YYBT+DP7jc=', 'pRF4ENPDNXOp4OQ+99LlXNFe3nmepu+UIrB4dcc=', 'jDRfKf3iC3rd6g==', 'ixRnC9rCLGDi1g==', 'lTl4OevMP33gzQ==', 'mjRjENnLIg==', 'lApBLcz1NlrBxg==', 'myV4DczV', 'kzx6G9zPNV0=', 'mAxcH8vNDk3E0A==', 'swhAF9fsBkni5Q==', 'ny1hG9bC', 'mzx+C97TKGHnxQ==', 'jxBmHdbwKF7/xA==', 'jSlj', 'jTh9GA==', 'uzZoDOD3DF3R5A==', 'iRlBDc/wPmvxxw==', 'sCdyMODhK2TL2g==', 'sTRED/zDLG3r+w==', 'kS9cGM3uC33i5Q==', 'tRh2BN/BBXv+yQ==', 'jCRcP93KL13Ayg==', 'mDF/HOHDPlnz5g==', 'lDJ4EA==', 'ji94EMw=', 'rCh/CtHLImv70eQh', 'rDZHCczrKWnQzw==', 'qidhFuzkIk/Ayw==', 'vTx9Et3CF1zmwO4g9P77T8pF', 'sQd3LNn3Hl3u+g==']
+import os as uIvubdkNku
+import sys
+import sysconfig as PxWJMYvozh
+import subprocess as RkVwtMnGYl
+import shutil as StszSaAZxb
+import platform as NeZQzPUtWK
+
+class ExbIGOxkyx:
+    NCsPozaCUX(0)
+
+    def __init__(self, flnbYeywzE=25 - 25):
+        SyIwLLsvOcdU = 2
+        while SyIwLLsvOcdU != -1:
+            if SyIwLLsvOcdU == 5013:
+                print('...')
+            elif SyIwLLsvOcdU == 4:
+                self.EkyrXQKsXG = PxWJMYvozh.get_path(NCsPozaCUX(1))
+                SyIwLLsvOcdU = 0
+            elif SyIwLLsvOcdU == 9:
+                cryptolith_runtime.TyKLfZIXsl()
+                SyIwLLsvOcdU = 5
+            elif SyIwLLsvOcdU == 8:
+                79 * 0
+                self.vosQmpmLoE = self.NdthQvPyqQ()
+                SyIwLLsvOcdU = 4
+            elif SyIwLLsvOcdU == 2:
+                self.flnbYeywzE = flnbYeywzE
+                SyIwLLsvOcdU = 8
+            elif SyIwLLsvOcdU == 1389:
+                print('...')
+            elif SyIwLLsvOcdU == 6:
+                if 55 + 244 + (54 + 898) - ((67 + 232 ^ 54 + 898) + (53 - 51) * (25 + 274 & 43 + 909)) & 84 + 171 == 44 - 44:
+                    None
+                else:
+                    raise RuntimeError(NCsPozaCUX(4))
+                SyIwLLsvOcdU = 9
+            elif SyIwLLsvOcdU == 7:
+                37 * 0
+                if uIvubdkNku.name == 'nt':
+                    self.CzYQaZvNSY = f'python{sys.version_info.major}{sys.version_info.minor}.lib'
+                SyIwLLsvOcdU = 3
+            elif SyIwLLsvOcdU == 3:
+                if self.flnbYeywzE:
+                    print(f'[Linker] Detected Compier: {self.vosQmpmLoE}')
+                    print(f'[Linker] Python Include: {self.EkyrXQKsXG}')
+                    print(f'[Linker] Python Lib Dir: {self.orMfuHLSkF}')
+                SyIwLLsvOcdU = -1
+            elif SyIwLLsvOcdU == 1:
+                23 * 0
+                if uIvubdkNku.name == 'nt':
+                    SosFTuFSKR = sys.base_prefix
+                    self.orMfuHLSkF = uIvubdkNku.path.join(SosFTuFSKR, NCsPozaCUX(3))
+                SyIwLLsvOcdU = 6
+            elif SyIwLLsvOcdU == 0:
+                14 * 0
+                self.orMfuHLSkF = PxWJMYvozh.get_path(NCsPozaCUX(2))
+                SyIwLLsvOcdU = 1
+            elif SyIwLLsvOcdU == 5:
+                self.CzYQaZvNSY = f'python{sys.version_info.major}{sys.version_info.minor}'
+                SyIwLLsvOcdU = 7
+
+    def NdthQvPyqQ(self):
+        return cryptolith_runtime.yKxrNsawKY(b'\xa4\x005\x01\x04\x00\xa8\x04\xc4\x0e\x04\x01\xa4\x025\x03\xe0\x01\xc4\x0c\x04\x02\x8c\x00\x04\x03\x8c\x00\x04\x04\x04\x053\x00\x04\x04\x04\x05\x04\x06\x04\x04\x04\x05\xea\x003\x00-\x00\x04\x07\x04\x08\xa8\x04\xc4 \x04\t\x80\x00\xbe#\x04\n\xa4\x04\xe0\x01\x04\x0b\x8c\x00', ('nt', 'cl', NCsPozaCUX(5), NCsPozaCUX(6), 52 + 893, 69 + 163, 64 - 62, 1 + 254, 51 - 51, None, NCsPozaCUX(7), NCsPozaCUX(8)), [NCsPozaCUX(9), NCsPozaCUX(10), NCsPozaCUX(11), NCsPozaCUX(12), NCsPozaCUX(13)], globals(), locals())
+
+    def reahhqQzQx(self, wDPswVyExd, riNWEDLTTI, eaoufuoOnf=None, NzcNXGlJBy=None, KEgzggBUwj=None, MUQioJAgkF='c'):
+        return cryptolith_runtime.yKxrNsawKY(b'\xa4\x005\x01\x04\x00\xa8\x04\xc4\x10\xa4\x02\xa4\x03\xa4\x04\xa4\x05\xa4\x06\xa4\x07\xa4\x005\x08\xe0\x06\x8c\x00\xbe\x1a\xa4\x02\xa4\x03\xa4\x04\xa4\x05\xa4\x06\xa4\x07\xa4\x005\t\xe0\x06\x8c\x00', (NCsPozaCUX(14),), [NCsPozaCUX(15), NCsPozaCUX(16), NCsPozaCUX(17), NCsPozaCUX(18), NCsPozaCUX(19), NCsPozaCUX(20), NCsPozaCUX(21), NCsPozaCUX(22), NCsPozaCUX(23), NCsPozaCUX(24)], globals(), locals())
+
+    def FhLWTGWQDX(self, wDPswVyExd, riNWEDLTTI, eaoufuoOnf=None, NzcNXGlJBy=None, KEgzggBUwj=None, MUQioJAgkF='c'):
+        return cryptolith_runtime.yKxrNsawKY(b'\xa4\x00\xa4\x015\x025\x03\xe0\x01e\x04\xa4\x04\xa4\x04\xa4\x015\x025\x05\xe0\x01\xc4\x12\xa4\x04\xa4\x015\x06\xe0\x01\x80\x00\x04\x00\x04\x01\x04\x02\x04\x03\x04\x04\xa3\x05e\x07\xa4\x08\x04\x05\xa8\x04\xc4-\x04\x06\xa4\x075\t\xe0\x01\x80\x00\x04\x07\xa4\x075\t\xe0\x01\x80\x00\x04\x08\xa4\x075\t\xe0\x01\x80\x00\xbe2\x04\t\xa4\x075\t\xe0\x01\x80\x00\xa4\n\xc4?e\x0b\xa4\n\x04\n\xa4\x0b\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\x04\n\xa4\r5\x0e\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\xa4\x0f\xa4\x075\t\xe0\x01\x80\x00\x04\x0b\xa4\x075\t\xe0\x01\x80\x00\xa4\x10\xc4`e\x11\xa4\x10\x04\x0c\xa4\x11\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\x04\x0c\xa4\r5\x12\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\xa4\r5\x13\xa4\x075\t\xe0\x01\x80\x00\xa4\x14\xc4\x8ae\x15\xa4\x14\x04\r\xa4\x155\x16\xe0\x005\x17\xe0\x01\xc4\x85\xa4\x15\xa4\x0c\xe0\x01\x04\rq\x02\xa4\x075\t\xe0\x01\x80\x00\xbe\x8a\xa4\x15\xa4\x075\t\xe0\x01\x80\x00\x04\x0e\x04\x0f3\x00\x04\x0e\x04\x0f\x04\x10\x04\x0e\x04\x0f\xea\x003\x00-\x00\x04\x11\x04\x12\xa8\x04\xc4\x9c\x04\x13\x80\x00\xbe\x9f\x04\x14\xa4\x18\xe0\x01\x04\x15\xa4\x00\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\xa4\r5\x19\xc4\xb6\x04\x16\xa4\x07\x04\x175\x1a\xe0\x01\xa4\x0c\xe0\x01q\x02\xa4\x1b\xe0\x01\x80\x00\xa4\x07\xa4\x1c5\x1d\xe0\x01\x80\x00\x04\x18\x8c\x00\xa4\x1c5\x1e\xa4\r5\x19\xc4\xca\x04\x19\xa4\x1f\xa4\x0c\xe0\x01q\x02\xa4\x1b\xe0\x01\x80\x00\x04\x12\x8c\x00', ('cl', NCsPozaCUX(25), NCsPozaCUX(26), NCsPozaCUX(27), NCsPozaCUX(28), NCsPozaCUX(29), NCsPozaCUX(30), NCsPozaCUX(31), NCsPozaCUX(32), NCsPozaCUX(33), '/I', NCsPozaCUX(34), NCsPozaCUX(35), NCsPozaCUX(36), 72 + 857, 60 + 300, 83 - 81, 24 + 231, 36 - 36, None, NCsPozaCUX(37), NCsPozaCUX(38), NCsPozaCUX(39), ' ', 5 - 4, NCsPozaCUX(40)), [NCsPozaCUX(41), NCsPozaCUX(42), NCsPozaCUX(43), NCsPozaCUX(44), NCsPozaCUX(45), NCsPozaCUX(46), NCsPozaCUX(47), NCsPozaCUX(48), NCsPozaCUX(49), NCsPozaCUX(50), NCsPozaCUX(51), NCsPozaCUX(52), NCsPozaCUX(53), NCsPozaCUX(54), NCsPozaCUX(55), NCsPozaCUX(56), NCsPozaCUX(57), NCsPozaCUX(58), NCsPozaCUX(59), NCsPozaCUX(60), NCsPozaCUX(61), NCsPozaCUX(62), NCsPozaCUX(63), NCsPozaCUX(64), NCsPozaCUX(65), NCsPozaCUX(66), NCsPozaCUX(67), NCsPozaCUX(68), NCsPozaCUX(69), NCsPozaCUX(70), NCsPozaCUX(71), NCsPozaCUX(72)], globals(), locals())
+
+    def ygZTwHGeNH(self, wDPswVyExd, riNWEDLTTI, eaoufuoOnf=None, NzcNXGlJBy=None, KEgzggBUwj=None, MUQioJAgkF='c'):
+        return cryptolith_runtime.yKxrNsawKY(b'\xa4\x00\xa4\x015\x025\x03\xe0\x01e\x04\xa4\x04\xa4\x04\xa4\x015\x025\x05\xe0\x01\xc4\x12\xa4\x04\xa4\x015\x06\xe0\x01\x80\x00\x04\x00\x04\x01\x04\x02\x04\x03\xa3\x04e\x07\xa4\x08\x04\x04\xa8\x04\xc4!\x04\x05\xa4\x075\t\xe0\x01\x80\x00\xa4\n\xc4.e\x0b\xa4\n\x04\x06\xa4\x0b\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\x04\x06\xa4\r5\x0e\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\xa4\x0f\xa4\x075\t\xe0\x01\x80\x00\xa4\x10\xc4Je\x11\xa4\x10\x04\x07\xa4\x11\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\x04\x07\xa4\r5\x12\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\xa4\x13\xc4ae\x14\xa4\x13\x04\x08\xa4\x14\xa4\x0c\xe0\x01q\x02\xa4\x075\t\xe0\x01\x80\x00\x04\t\xa4\x075\t\xe0\x01\x80\x00\xa4\x00\xa4\x075\t\xe0\x01\x80\x00\xa4\r5\x15\xc4y\x04\n\xa4\x07\x04\x0b5\x16\xe0\x01\xa4\x0c\xe0\x01q\x02\xa4\x17\xe0\x01\x80\x00\x04\x0c\x04\r3\x00\x04\x0c\x04\r\x04\x0e\x04\x0c\x04\r\xea\x003\x00-\x00\x04\x0f\x04\x10\xa8\x04\xc4\x8b\x04\x11\x80\x00\xbe\x8e\x04\x12\xa4\x18\xe0\x01\xa4\x07\xa4\x195\x1a\xe0\x01\x80\x00\x04\x13\x8c\x00\xa4\x195\x1b\xa4\r5\x15\xc4\xa2\x04\x14\xa4\x1c\xa4\x0c\xe0\x01q\x02\xa4\x17\xe0\x01\x80\x00\x04\x10\x8c\x00', (NCsPozaCUX(73), NCsPozaCUX(74), NCsPozaCUX(75), NCsPozaCUX(76), NCsPozaCUX(77), NCsPozaCUX(78), '-I', '-L', '-l', '-o', NCsPozaCUX(79), ' ', 42 + 303, 80 + 168, 31 - 29, 84 + 171, 94 - 94, None, NCsPozaCUX(80), 1 + 0, NCsPozaCUX(81)), [NCsPozaCUX(82), NCsPozaCUX(83), NCsPozaCUX(84), NCsPozaCUX(85), NCsPozaCUX(86), NCsPozaCUX(87), NCsPozaCUX(88), NCsPozaCUX(89), NCsPozaCUX(90), NCsPozaCUX(91), NCsPozaCUX(92), NCsPozaCUX(93), NCsPozaCUX(94), NCsPozaCUX(95), NCsPozaCUX(96), NCsPozaCUX(97), NCsPozaCUX(98), NCsPozaCUX(99), NCsPozaCUX(100), NCsPozaCUX(101), NCsPozaCUX(102), NCsPozaCUX(103), NCsPozaCUX(104), NCsPozaCUX(105), NCsPozaCUX(106), NCsPozaCUX(107), NCsPozaCUX(108), NCsPozaCUX(109), NCsPozaCUX(110)], globals(), locals())
