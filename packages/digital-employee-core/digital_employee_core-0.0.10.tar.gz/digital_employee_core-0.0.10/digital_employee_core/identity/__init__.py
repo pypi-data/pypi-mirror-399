6e794e54bc8b09a7f1ff8d53b98de3aa6d3e5a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:06:07 +0000

    Add package digital-employee-core/0.0.10/digital_employee_core-0.0.10.tar.gz

---
+++ packages/digital-employee-core/digital_employee_core-0.0.10.tar.gz/digital_employee_core-0.0.10/digital_employee_core/identity/__init__.py
@@ -0,0 +1,16 @@
+"""Identity module for digital employee.
+
+This module provides identity classes for digital employees.
+"""
+
+from digital_employee_core.identity.identity import (
+    DigitalEmployeeIdentity,
+    DigitalEmployeeJob,
+    DigitalEmployeeSupervisor,
+)
+
+__all__ = [
+    "DigitalEmployeeIdentity",
+    "DigitalEmployeeJob",
+    "DigitalEmployeeSupervisor",
+]
