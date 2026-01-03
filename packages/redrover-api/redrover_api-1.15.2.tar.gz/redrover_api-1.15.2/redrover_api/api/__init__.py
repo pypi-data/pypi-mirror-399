0b6ab7bb62330727e525ef634daf9c03b6700276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 03:31:43 +0000

    Add package redrover-api/1.15.2/redrover_api-1.15.2.tar.gz

---
+++ packages/redrover-api/redrover_api-1.15.2.tar.gz/redrover_api-1.15.2/redrover_api/api/__init__.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+# import apis into api package
+from redrover_api.api.absence_api import AbsenceApi
+from redrover_api.api.absence_reason_api import AbsenceReasonApi
+from redrover_api.api.absence_reason_balance_api import AbsenceReasonBalanceApi
+from redrover_api.api.accounting_code_api import AccountingCodeApi
+from redrover_api.api.application_template_api import ApplicationTemplateApi
+from redrover_api.api.assignment_api import AssignmentApi
+from redrover_api.api.attribute_api import AttributeApi
+from redrover_api.api.connection_api import ConnectionApi
+from redrover_api.api.contract_api import ContractApi
+from redrover_api.api.custom_fields_api import CustomFieldsApi
+from redrover_api.api.job_posting_api import JobPostingApi
+from redrover_api.api.job_posting_category_api import JobPostingCategoryApi
+from redrover_api.api.location_api import LocationApi
+from redrover_api.api.new_hire_api import NewHireApi
+from redrover_api.api.organization_api import OrganizationApi
+from redrover_api.api.pay_code_api import PayCodeApi
+from redrover_api.api.position_type_api import PositionTypeApi
+from redrover_api.api.user_api import UserApi
+from redrover_api.api.vacancy_api import VacancyApi
+from redrover_api.api.webhooks_api import WebhooksApi
+
