4cb6a893f7d2504d511b08ede4f827981ea57fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:36:05 +0000

    Add package de-console-client/0.2.3/de_console_client-0.2.3.tar.gz

---
+++ packages/de-console-client/de_console_client-0.2.3.tar.gz/de_console_client-0.2.3/de_console_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from de_console_client.api.chromia_entity_db_api import ChromiaEntityDBApi
+from de_console_client.api.chromia_file_storage_api import ChromiaFileStorageApi
+from de_console_client.api.chromia_vector_db_api import ChromiaVectorDBApi
+
