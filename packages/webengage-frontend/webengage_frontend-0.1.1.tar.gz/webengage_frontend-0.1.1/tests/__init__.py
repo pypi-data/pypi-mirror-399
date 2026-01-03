7003fd9b9343877e8c4abff7a8ecf780a61fe6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:06:02 +0000

    Add package webengage-frontend/0.1.1/webengage_frontend-0.1.1.tar.gz

---
+++ packages/webengage-frontend/webengage_frontend-0.1.1.tar.gz/webengage_frontend-0.1.1/tests/__init__.py
@@ -0,0 +1,4 @@
+"""
+Tests for webengageblack package.
+"""
+
---
+++ packages/webengage-frontend/webengage_frontend-0.1.1.tar.gz/webengage_frontend-0.1.1/tests/test_utils.py
@@ -0,0 +1,13 @@
+"""
+Tests for utility functions.
+"""
+
+import pytest
+from webengage_frontend import utils
+
+
+def test_example_function():
+    """Test the example function."""
+    result = utils.example_function()
+    assert result == "Hello from webengageblack!"
+
