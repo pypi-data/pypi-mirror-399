b110a9619f20e7346faf6e8894b476548de3e01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:36:07 +0000

    Add package asyncuvkit/0.3.23/asyncuvkit-0.3.23.tar.gz

---
+++ packages/asyncuvkit/asyncuvkit-0.3.23.tar.gz/asyncuvkit-0.3.23/asyncuvkit/__init__.py
@@ -0,0 +1,12 @@
+
+__version__ = "0.3.23"
+__author__ = "asyncuvkit"
+
+# 自动安装 import hook
+from . import asyncuvkit2
+from . import test
+from .EasyMinMaxScaler import EasyMinMaxScaler2
+#'asyncuvkit2'
+__all__ = ["test", "EasyMinMaxScaler2","asyncuvkit2"]
+
+
