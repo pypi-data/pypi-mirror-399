472d4aa7174bbcac597ee97c831e6d4f4fd80cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:08:53 +0000

    Add package hyperbrowser/0.80.0/hyperbrowser-0.80.0.tar.gz

---
+++ packages/hyperbrowser/hyperbrowser-0.80.0.tar.gz/hyperbrowser-0.80.0/hyperbrowser/client/managers/async_manager/session.py
@@ -0,0 +1,132 @@
+from typing import List, Union, IO
+from ....models.session import (
+    BasicResponse,
+    CreateSessionParams,
+    GetSessionDownloadsUrlResponse,
+    GetSessionRecordingUrlResponse,
+    GetSessionVideoRecordingUrlResponse,
+    SessionDetail,
+    SessionListParams,
+    SessionListResponse,
+    SessionRecording,
+    UploadFileResponse,
+    SessionEventLogListParams,
+    SessionEventLogListResponse,
+    SessionEventLog,
+)
+
+
+class SessionEventLogsManager:
+    def __init__(self, client):
+        self._client = client
+
+    async def list(
+        self,
+        session_id: str,
+        params: SessionEventLogListParams = SessionEventLogListParams(),
+    ) -> List[SessionEventLog]:
+        response = await self._client.transport.get(
+            self._client._build_url(f"/session/{session_id}/event-logs"),
+            params=params.model_dump(exclude_none=True, by_alias=True),
+        )
+        return SessionEventLogListResponse(**response.data)
+
+
+class SessionManager:
+    def __init__(self, client):
+        self._client = client
+        self.event_logs = SessionEventLogsManager(client)
+
+    async def create(self, params: CreateSessionParams = None) -> SessionDetail:
+        response = await self._client.transport.post(
+            self._client._build_url("/session"),
+            data=(
+                {}
+                if params is None
+                else params.model_dump(exclude_none=True, by_alias=True)
+            ),
+        )
+        return SessionDetail(**response.data)
+
+    async def get(self, id: str) -> SessionDetail:
+        response = await self._client.transport.get(
+            self._client._build_url(f"/session/{id}")
+        )
+        return SessionDetail(**response.data)
+
+    async def stop(self, id: str) -> BasicResponse:
+        response = await self._client.transport.put(
+            self._client._build_url(f"/session/{id}/stop")
+        )
+        return BasicResponse(**response.data)
+
+    async def list(
+        self, params: SessionListParams = SessionListParams()
+    ) -> SessionListResponse:
+        response = await self._client.transport.get(
+            self._client._build_url("/sessions"),
+            params=params.model_dump(exclude_none=True, by_alias=True),
+        )
+        return SessionListResponse(**response.data)
+
+    async def get_recording(self, id: str) -> List[SessionRecording]:
+        response = await self._client.transport.get(
+            self._client._build_url(f"/session/{id}/recording"), None, True
+        )
+        return [SessionRecording(**recording) for recording in response.data]
+
+    async def get_recording_url(self, id: str) -> GetSessionRecordingUrlResponse:
+        response = await self._client.transport.get(
+            self._client._build_url(f"/session/{id}/recording-url")
+        )
+        return GetSessionRecordingUrlResponse(**response.data)
+
+    async def get_video_recording_url(
+        self, id: str
+    ) -> GetSessionVideoRecordingUrlResponse:
+        response = await self._client.transport.get(
+            self._client._build_url(f"/session/{id}/video-recording-url")
+        )
+        return GetSessionVideoRecordingUrlResponse(**response.data)
+
+    async def get_downloads_url(self, id: str) -> GetSessionDownloadsUrlResponse:
+        response = await self._client.transport.get(
+            self._client._build_url(f"/session/{id}/downloads-url")
+        )
+        return GetSessionDownloadsUrlResponse(**response.data)
+
+    async def upload_file(
+        self, id: str, file_input: Union[str, IO]
+    ) -> UploadFileResponse:
+        response = None
+        if isinstance(file_input, str):
+            with open(file_input, "rb") as file_obj:
+                files = {"file": file_obj}
+                response = await self._client.transport.post(
+                    self._client._build_url(f"/session/{id}/uploads"),
+                    files=files,
+                )
+        else:
+            files = {"file": file_input}
+            response = await self._client.transport.post(
+                self._client._build_url(f"/session/{id}/uploads"),
+                files=files,
+            )
+
+        return UploadFileResponse(**response.data)
+
+    async def extend_session(self, id: str, duration_minutes: int) -> BasicResponse:
+        response = await self._client.transport.put(
+            self._client._build_url(f"/session/{id}/extend-session"),
+            data={"durationMinutes": duration_minutes},
+        )
+        return BasicResponse(**response.data)
+
+    async def update_profile_params(
+        self, id: str, persist_changes: bool
+    ) -> BasicResponse:
+        response = await self._client.transport.put(
+            self._client._build_url(f"/session/{id}/update"),
+            data={"type": "profile", "params": {"persistChanges": persist_changes}},
+        )
+        return BasicResponse(**response.data)
