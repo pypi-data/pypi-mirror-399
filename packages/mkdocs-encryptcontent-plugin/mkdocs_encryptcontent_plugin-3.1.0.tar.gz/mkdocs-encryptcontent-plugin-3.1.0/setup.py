849644f4a0029872bba446a535d77beffd2d6206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:06:16 +0000

    Add package mkdocs-encryptcontent-plugin/3.1.0/mkdocs_encryptcontent_plugin-3.1.0.tar.gz

---
+++ packages/mkdocs-encryptcontent-plugin/mkdocs_encryptcontent_plugin-3.1.0.tar.gz/mkdocs-encryptcontent-plugin-3.1.0/setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    with open(file_path, encoding='utf-8') as file:
+        content = file.read()
+    return content if content else 'no content read'
+
+
+setup(
+    name='mkdocs-encryptcontent-plugin',
+    version='3.1.0',
+    author='unverbuggt',
+    author_email='unverbuggt@xn--rthlein-n2a.de',
+    description='A MkDocs plugin that encrypt/decrypt markdown content with AES',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs python markdown encrypt decrypt content',
+    url='https://unverbuggt.github.io/mkdocs-encryptcontent-plugin/',
+    license='MIT',
+    python_requires='>=3.5',
+    install_requires=[
+        'mkdocs',
+        'pyyaml',
+        'pycryptodome',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    entry_points={
+        'mkdocs.plugins': [
+            'encryptcontent = encryptcontent.plugin:encryptContentPlugin'
+        ]
+    },
+    package_data={'encryptcontent': [
+        '*.tpl.js',
+        '*.tpl.html',
+        '*.tpl.py',
+        'contrib/templates/search/*.js'
+    ]},
+    include_package_data=True
+)
