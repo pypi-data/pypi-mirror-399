ff7905ae9939786a58ce88700e59c74d489c1de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:06:18 +0000

    Add package xc-api-py/0.1.16/xc_api_py-0.1.16-py3-none-any.whl

---
+++ packages/xc-api-py/xc_api_py-0.1.16-py3-none-any.whl/xeno_canto/schemas/api_response.py
@@ -0,0 +1,29 @@
+from .recording import XenoCantoRecordingSchema
+
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class ApiResponseSchema(BaseModel):
+  num_recordings: int = Field(
+    alias='numRecordings',
+    description='Total recordings fetched via the query',
+  )
+  num_species: int = Field(
+    alias='numSpecies',
+    description='Number of species represented in the results',
+  )
+  page: int = Field(
+    alias='page',
+    description='Current page number in the query result',
+  )
+  num_pages: int = Field(
+    alias='numPages',
+    description='Total pages of recordings fetched via the query',
+  )
+  recordings: List[XenoCantoRecordingSchema] = Field(
+    description='List of recording objects on this page',
+  )
+
+  def __bool__(self):
+    return bool(self.recordings)
