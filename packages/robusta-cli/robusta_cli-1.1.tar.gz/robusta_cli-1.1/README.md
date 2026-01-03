dae587a8d38c4fe7b1819620193738bce2301827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:10 +0000

    Add package robusta-cli/1.1/robusta_cli-1.1.tar.gz

---
+++ packages/robusta-cli/robusta_cli-1.1.tar.gz/robusta_cli-1.1/README.md
@@ -0,0 +1,13 @@
+# robusta-cli
+
+This project is a CLI tool for using [robusta](https://github.com/robusta-dev/robusta). With this cli tool you can easily:
+
+* Generate Helm values for the robusta helm chart, by answering a few questions in a wizard
+* Run diagnostic to see if robusta is running properly
+* Deploy and test your own extensions to Robusta like custom actions and playbooks
+
+For more information, checkout the [robusta installation docs](https://docs.robusta.dev/master/).
+
+## Requirements
+
+- Python 3.9 or higher
---
+++ packages/robusta-cli/robusta_cli-1.1.tar.gz/robusta_cli-1.1/robusta_cli/eula.py
@@ -0,0 +1,27 @@
+import requests
+import typer
+
+from robusta_cli.backend_profile import backend_profile
+
+
+def handle_eula(account_id, robusta_api_key):
+    require_eula = robusta_api_key
+    if not require_eula:
+        return
+
+    while True:
+        eula_url = f"{backend_profile.robusta_cloud_api_host}/eula.html"
+        typer.echo(f"Please read and approve our End User License Agreement: {eula_url}")
+        eula_approved = typer.confirm("Do you accept our End User License Agreement?")
+
+        if eula_approved:
+            try:
+                requests.get(f"{eula_url}?account_id={account_id}")
+            except Exception:
+                typer.echo(f"\nEula approval failed: {eula_url}")
+            return
+
+        typer.secho(
+            "End User License Agreement rejected. Sorry, you must either accept or restart the installation and disable the UI and cloud features",
+            fg="red",
+        )
