62c3630a4151fae8214248148446f889656f3562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:51 +0000

    Add package pyPDFServer/1.0.3/pypdfserver-1.0.3.tar.gz

---
+++ packages/pyPDFServer/pypdfserver-1.0.3.tar.gz/pypdfserver-1.0.3/pypdfserver/__init__.py
@@ -0,0 +1,11 @@
+"""
+pyPDFserver - A locally hosted scan destination to apply OCR and duplex to scans
+"""
+__author__ = "Andreas Brilka"
+__version__ = "1.0.3"
+
+from .core import *
+from .server import PDF_FTPServer
+from .cmd import start_pyPDFserver
+
+pdf_server: PDF_FTPServer|None = None
