7585396b8b9c6fdf1d340fd7802d2d06033848e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:08:28 +0000

    Add package amrita-plugin-exec/0.1.0/amrita_plugin_exec-0.1.0-py3-none-any.whl

---
+++ packages/amrita-plugin-exec/amrita_plugin_exec-0.1.0-py3-none-any.whl/amrita/plugins/exec/__init__.py
@@ -0,0 +1,15 @@
+from nonebot.plugin import PluginMetadata, require
+
+require("amrita.plugins.perm")
+
+from . import main
+
+__plugin_meta__ = PluginMetadata(
+    name="命令执行插件",
+    description="执行命令",
+    usage="/exec <command>",
+    type="application",
+)
+
+__all__ = ["main"]
+
