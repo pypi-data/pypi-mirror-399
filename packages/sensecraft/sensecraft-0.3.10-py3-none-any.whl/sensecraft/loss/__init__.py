9f750911c7aa130776b86729db0ddd646f032362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 15:07:27 +0000

    Add package sensecraft/0.3.10/sensecraft-0.3.10-py3-none-any.whl

---
+++ packages/sensecraft/sensecraft-0.3.10-py3-none-any.whl/sensecraft/loss/__init__.py
@@ -0,0 +1,76 @@
+# Config and registry
+from .config import (
+    LOSS_REGISTRY,
+    ValueRange,
+    LossConfig,
+    GeneralConfig,
+    DinoV3LossConfig,
+    ConvNextDinoV3LossConfig,
+    LPIPSConfig,
+    SSIMConfig,
+    MSSSIMConfig,
+    PatchFFTConfig,
+    register_loss,
+)
+
+# Perceptual losses
+from .convnext import ConvNextPerceptualLoss
+from .convnext_dinov3 import ConvNextDinoV3PerceptualLoss
+from .gram_dinov3 import ViTDinoV3PerceptualLoss
+from .lpips import LPIPS
+
+# General losses
+from .general import (
+    # Standard regression (PyTorch wrappers)
+    MSELoss,
+    L1Loss,
+    HuberLoss,
+    SmoothL1Loss,
+    # Custom regression
+    CharbonnierLoss,
+    RMSELoss,
+    MAPELoss,
+    SMAPELoss,
+    LogCoshLoss,
+    QuantileLoss,
+    # Robust regression
+    CauchyLoss,
+    GemanMcClureLoss,
+    WelschLoss,
+    TukeyBiweightLoss,
+    WingLoss,
+    # FFT
+    PatchFFTLoss,
+    FFTLoss,
+    NormType,
+    GaussianNoiseLoss,
+)
+
+# Edge losses
+from .edge import (
+    SobelEdgeLoss,
+    LaplacianEdgeLoss,
+    CannyStyleEdgeLoss,
+    GradientLoss,
+    HighFrequencyLoss,
+    MultiScaleGradientLoss,
+    StructureTensorLoss,
+)
+
+# SSIM losses
+from .ssim import SSIMLoss, MSSSIMLoss
+
+# Video/3D losses
+from .video import (
+    SSIM3D,
+    STSSIM,
+    TSSIM,
+    FDBLoss,
+    TemporalAccelerationLoss,
+    TemporalFFTLoss,
+    PatchFFT3DLoss,
+    TemporalGradientLoss,
+)
+
+# Compound loss
+from .sensecraft import SenseCraftLoss
