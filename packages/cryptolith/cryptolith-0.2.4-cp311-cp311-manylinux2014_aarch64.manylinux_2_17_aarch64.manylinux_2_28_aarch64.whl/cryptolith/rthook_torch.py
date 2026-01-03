84c498f0af2d7c1c9d98c91f2e4d242aebc25604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:06:33 +0000

    Add package cryptolith/0.2.4/cryptolith-0.2.4-cp311-cp311-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/cryptolith/cryptolith-0.2.4-cp311-cp311-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/cryptolith/rthook_torch.py
@@ -0,0 +1,20 @@
+from . import cryptolith_runtime
+import sys as _obs_sys
+import os as _obs_os
+
+def _junk_func():
+    pass
+import base64
+bOdrryXxiE = base64.b64decode('/l0RfrimRy6Jo4tTh7uJPaU3sRe+4I79TtUcT+dBjBM=')
+
+def XMtsMnIpHp(i):
+    d = base64.b64decode(enNPXcFKpG[i])
+    return bytes([b ^ bOdrryXxiE[j % len(bOdrryXxiE)] for j, b in enumerate(d)]).decode('utf-8')
+enNPXcFKpG = ['03A8XuzpFW3Bg80a35vBcup8kUTqodypbvgxYg==', 'oRBUN+jnFH0=', 'vCh/GtTDZ2zo0O5ppw==', 'nzl1IdzKK3Htyvk25M/mT9w=', 'ijJjHdA=', 'kjRz', 'kjRzF9fLNxvkx6U369c=', 'uDJkENyGCF7szcYDp9r9B4U=', 'sS10EPX2Z0Lmwu8245v6SMZU1GTNhvuRIqwy', 'qRxDMPHoABSpz+Ix7tTkTZBa1TnajOLdAJpIb6EO2V26fXgQmMQyQO3P7nP11OZJiw==', 'thJeNZjjFXzG8bFz', '03A8XuzpFW3Bg80a35vBcup8kVLwpK7QY/g=', 'ixRnC9rCLGDi1g==', 'hxddH8j1Jnf+8Q==', 'ji94EMw=', 'jSRi', 'oQJ3F9TDGHE=', 'lTl4OevMP33gzQ==', 'nz9iDtnSLw==', 'mjRjENnLIg==', 'mThlH8zSNQ==', 'iytFJMvDFUjm1Q==', 'jSlj', 'ljxiH8zSNQ==', 'nzl1IdzKK3Htyvk25M/mT9w=', 'lDJ4EA==', 'vy1FF9bFP2ft+g==', 'myV4DczV', 'tBBUL+HxCH7rzw==', 'iBpJL9LjMUTT0w==', 'vRldMg==', 'uyVyG8jSLkHn', 'sQd3LNn3Hl3u+g==']
+import os as uIvubdkNku
+import sys
+import ctypes as vGXQjEvjZp
+
+def qzIYTuqXuP():
+    return cryptolith_runtime.yKxrNsawKY(b'\xa4\x005\x01\x04\x00\xa8\x04\xc4l\x04\x01\xa4\x02\xe0\x01\x80\x00\xa4\x03\x04\x02\xa4\x04\xa4\x005\x055\x06\xe0\x01\xa4\x005\x055\x07\xe0\x01\xa4\x08\xe0\x03e\t\x04\x03\xa4\t\xa4\n\xe0\x01q\x02\xa4\x02\xe0\x01\x80\x00\xa4\x00\x04\x04\xa4\x0b\xe0\x02\xc4<\xa4\t\xa4\x005\x0c\xe0\x01\x80\x00\xa4\t\x04\x05\x04\x06\xa4\x005\x055\r\xe0\x03e\x0e\xa4\x0e\xa4\x005\x055\x0f\xe0\x01\xc4<\xa4\x0e\xa4\x005\x0c\xe0\x01\x80\x00\xa4\t\x04\x07\xa4\x005\x055\r\xe0\x02e\x10\xa4\x10\xa4\x005\x055\x0f\xe0\x01\xc4[\x04\x08\xa4\x10\xa4\n\xe0\x01q\x02\xa4\x02\xe0\x01\x80\x00\xa4\x10\xa4\x115\x12\xe0\x01\x80\x00\x04\t\xa4\x02\xe0\x01\x80\x00\xbe_\x04\n\xa4\x02\xe0\x01\x80\x00\xa4\x13\x04\x0b\xa4\x14\xa4\n\xe0\x01q\x02\xa4\x02\xe0\x01\x80\x00\x04\x0c\xa4\x02\xe0\x01\x80\x00\x04\r\x8c\x00', ('nt', XMtsMnIpHp(0), XMtsMnIpHp(1), XMtsMnIpHp(2), XMtsMnIpHp(3), XMtsMnIpHp(4), XMtsMnIpHp(5), XMtsMnIpHp(6), XMtsMnIpHp(7), XMtsMnIpHp(8), XMtsMnIpHp(9), XMtsMnIpHp(10), XMtsMnIpHp(11), None), [XMtsMnIpHp(12), XMtsMnIpHp(13), XMtsMnIpHp(14), XMtsMnIpHp(15), XMtsMnIpHp(16), XMtsMnIpHp(17), XMtsMnIpHp(18), XMtsMnIpHp(19), XMtsMnIpHp(20), XMtsMnIpHp(21), XMtsMnIpHp(22), XMtsMnIpHp(23), XMtsMnIpHp(24), XMtsMnIpHp(25), XMtsMnIpHp(26), XMtsMnIpHp(27), XMtsMnIpHp(28), XMtsMnIpHp(29), XMtsMnIpHp(30), XMtsMnIpHp(31), XMtsMnIpHp(32)], globals(), locals())
+qzIYTuqXuP()
