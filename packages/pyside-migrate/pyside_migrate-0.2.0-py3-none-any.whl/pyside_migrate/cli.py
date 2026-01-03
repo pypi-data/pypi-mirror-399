e36dc30a54cfe4c9d73d89aaaf7c546f8fc2d2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:15:22 +0000

    Add package pyside-migrate/0.2.0/pyside_migrate-0.2.0-py3-none-any.whl

---
+++ packages/pyside-migrate/pyside_migrate-0.2.0-py3-none-any.whl/pyside_migrate/cli.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function
+
+import sys
+
+
+def main():
+    import libcst.tool
+
+    args = [
+        "-x",
+        "--no-format",
+        "pyside_migrate.enum_codemod.MigrateEnumsCommand",
+    ] + sys.argv[1:]
+    sys.exit(libcst.tool._codemod_impl("pyside-migrate", args))
