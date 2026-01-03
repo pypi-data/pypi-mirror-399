2ed1ebf9e8f90e2365278dd824d2200379a1a095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:18 +0000

    Add package bubble-data-api-client/0.0.1a17/bubble_data_api_client-0.0.1a17-py3-none-any.whl

---
+++ packages/bubble-data-api-client/bubble_data_api_client-0.0.1a17-py3-none-any.whl/bubble_data_api_client/exceptions.py
@@ -0,0 +1,21 @@
+class BubbleError(Exception):
+    """Base class for all exceptions raised by the library."""
+
+
+class ConfigurationError(BubbleError):
+    """Raised when required configuration is missing."""
+
+    def __init__(self, key: str) -> None:
+        super().__init__(f"{key} is not configured")
+
+
+class BubbleHttpError(BubbleError):
+    """Base class for all high level HTTP errors."""
+
+
+class BubbleNotFoundError(BubbleHttpError):
+    """Raised when a resource is not found."""
+
+
+class BubbleUnauthorizedError(BubbleHttpError):
+    """Raised when the user is not authorized to access a resource."""
