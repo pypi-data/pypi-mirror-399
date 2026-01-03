44953241979d8cd21b4ba903949e4ca4b621a183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:05:54 +0000

    Add package robustinfer/0.1.0/robustinfer-0.1.0.tar.gz

---
+++ packages/robustinfer/robustinfer-0.1.0.tar.gz/robustinfer-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="robustinfer",
+    version="0.1.0",
+    packages=find_packages(),
+    author="sjnscythe",
+    author_email="scytheabhi97@gmail.com",
+    description="A poc by sjnscythe",
+    python_requires='>=3.6',
+)
