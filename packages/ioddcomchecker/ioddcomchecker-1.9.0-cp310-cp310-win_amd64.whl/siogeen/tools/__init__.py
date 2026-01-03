5ea51650addd7f3f3e0619eef863295fa6aa33ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:07:05 +0000

    Add package ioddcomchecker/1.9.0/ioddcomchecker-1.9.0-cp310-cp310-win_amd64.whl

---
+++ packages/ioddcomchecker/ioddcomchecker-1.9.0-cp310-cp310-win_amd64.whl/siogeen/tools/__init__.py
@@ -0,0 +1,17 @@
+'''
+Siogeen IoddCom tools.
+
+The Siogeen IoddCom is designed for various use cases for best working
+experience with IO-Link devices, simple usage and time saving thus both saving
+costs and offering time for the real tasks.
+
+Created on 15.11.2022
+'''
+
+__author__ = "Reimund Renner"
+__email__ = "reimund@siogeen.com"
+__contact__ = "contact@siogeen.com"
+__license__ = "proprietary and confidential"
+
+# classes for: from siogeen import *
+__all__ = ['IoddComChecker']
---
+++ packages/ioddcomchecker/ioddcomchecker-1.9.0-cp310-cp310-win_amd64.whl/siogeen/tools/cli/__init__.py
@@ -0,0 +1,17 @@
+'''
+Siogeen IoddCom tools cli's.
+
+The Siogeen IoddCom is designed for various use cases for best working
+experience with IO-Link devices, simple usage and time saving thus both saving
+costs and offering time for the real tasks.
+
+Created on 09.01.2023
+'''
+
+__author__ = "Reimund Renner"
+__email__ = "reimund@siogeen.com"
+__contact__ = "contact@siogeen.com"
+__license__ = "proprietary and confidential"
+
+# classes for: from siogeen import *
+__all__ = ['IoddComChecker']
