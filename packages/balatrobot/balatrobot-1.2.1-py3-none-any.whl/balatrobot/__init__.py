8fa24adae726c4c76637894d7ded1896a6e1a120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 15:07:31 +0000

    Add package balatrobot/1.2.1/balatrobot-1.2.1-py3-none-any.whl

---
+++ packages/balatrobot/balatrobot-1.2.1-py3-none-any.whl/balatrobot/__init__.py
@@ -0,0 +1,7 @@
+"""BalatroBot - API for developing Balatro bots."""
+
+from balatrobot.config import Config
+from balatrobot.manager import BalatroInstance
+
+__version__ = "1.2.1"
+__all__ = ["BalatroInstance", "Config", "__version__"]
