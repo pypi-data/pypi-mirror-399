55dce79bda4e16eb435de94c947c855200e3e7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:32 +0000

    Add package fuelwatcher/1.0.0/fuelwatcher-1.0.0-py3-none-any.whl

---
+++ packages/fuelwatcher/fuelwatcher-1.0.0-py3-none-any.whl/fuelwatcher/__init__.py
@@ -0,0 +1,7 @@
+from fuelwatcher.constants import BRAND as BRAND
+from fuelwatcher.constants import PRODUCT as PRODUCT
+from fuelwatcher.constants import REGION as REGION
+from fuelwatcher.constants import SUBURB as SUBURB
+from fuelwatcher.fuelwatch import FuelWatch as FuelWatch
+from fuelwatcher.models import FuelStation as FuelStation
+from fuelwatcher.models import FuelWatchError as FuelWatchError
