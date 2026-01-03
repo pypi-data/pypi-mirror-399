7786c80636551e1f380d44adec1e28c36f59e614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 14:05:51 +0000

    Add package cumulusci-plus-azure-devops/0.0.11/cumulusci_plus_azure_devops-0.0.11-py3-none-any.whl

---
+++ packages/cumulusci-plus-azure-devops/cumulusci_plus_azure_devops-0.0.11-py3-none-any.whl/cumulusci_ado/azure_devops.py
@@ -0,0 +1,60 @@
+from importlib.metadata import PackageNotFoundError, version
+
+from cumulusci.cli.utils import check_latest_version, parse_version
+from cumulusci.core.utils import import_global
+from cumulusci.plugins.plugin_base import PluginBase
+from cumulusci.vcs.vcs_source import VCSSource
+
+from cumulusci_ado import __version__
+from cumulusci_ado.utils.ado import get_ado_cci_plus_upgrade_command
+
+
+class AzureDevOpsPlugin(PluginBase):
+    """Plugin for Azure DevOps integration."""
+
+    plugin_config_file = "cumulusci_plugin.yml"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._api_name = "cumulusci-plus-azure-devops"
+
+    @property
+    def version(self) -> str:
+        """Returns the version of the plugin."""
+        try:
+            return version(self._api_name)
+        except PackageNotFoundError:
+            return "0.0.0"
+
+    def initialize(self) -> None:
+        """Initialize the plugin."""
+        super().initialize()
+
+        # Delay import of service to avoid circular import during plugin loading
+        import_global("cumulusci_ado.vcs.ado.service.AzureDevOpsService")
+
+        # Register VCS source
+        VCSSource.register(
+            "azure_devops", "cumulusci_ado.vcs.ado.source.azure_devops.ADOSource"
+        )
+
+        # Load dependencies and resolvers
+        from cumulusci_ado.vcs.ado.dependencies.ado_dependencies import VCS_ADO as dep
+        from cumulusci_ado.vcs.ado.dependencies.ado_resolvers import VCS_ADO as res
+
+        assert (
+            dep == res
+        ), "VCS_ADO must match in dependencies and resolvers. (Assertion done to load the ado dependencies and resolvers correctly)"
+
+    def teardown(self) -> None:
+        """Tear down the plugin."""
+        super().teardown()
+
+    def check_latest_version(self):
+        """Override this method to check for the latest version of the plugin."""
+        check_latest_version(
+            pkg=self._api_name,
+            installed_version=parse_version(__version__),
+            tstamp_file="cumulus_ado_timestamp",
+            message=f"An update to {self.name} is available. To install the update, run this command: {get_ado_cci_plus_upgrade_command()}",
+        )
---
+++ packages/cumulusci-plus-azure-devops/cumulusci_plus_azure_devops-0.0.11-py3-none-any.whl/cumulusci_ado/vcs/ado/dependencies/ado_dependencies.py
@@ -0,0 +1,174 @@
+import logging
+from abc import ABC
+from functools import lru_cache
+from typing import Optional, Type
+
+import cumulusci.core.dependencies.base as base_dependency
+from cumulusci.core.dependencies.dependencies import (
+    add_dependency_class,
+    add_dependency_pin_class,
+)
+from cumulusci.core.exceptions import DependencyResolutionError
+from cumulusci.core.utils import deep_merge_plugins
+from cumulusci.vcs.bootstrap import get_remote_project_config
+from pydantic.v1 import AnyUrl, root_validator
+
+from cumulusci_ado.utils.ado import parse_repo_url
+from cumulusci_ado.vcs.ado.adapter import ADORepository
+from cumulusci_ado.vcs.ado.exceptions import ADOApiNotFoundError
+
+logger = logging.getLogger("cumulusci_ado")
+
+VCS_ADO = "azure_devops"
+
+
+@lru_cache(50)
+def get_ado_repo(project_config, url) -> ADORepository:
+    from cumulusci.vcs.base import VCSService
+
+    from cumulusci_ado.vcs.ado.service import get_ado_service_for_url
+
+    vcs_service: Optional[VCSService] = get_ado_service_for_url(project_config, url)
+
+    if vcs_service is None:
+        raise DependencyResolutionError(f"Could not find a ADO service for URL: {url}")
+
+    try:
+        repo = vcs_service.get_repository(options={"repository_url": url})
+        if not repo:
+            raise ADOApiNotFoundError(f"Get ADO Repository found None. {url}")
+
+        # project_config is local configuration, we need the repo config on the remote.
+        remote_config = get_remote_project_config(repo, repo.default_branch)
+
+        # Remote config does not have the plugin configuration. Copying it from local it does not exist.
+        # Else update the missing key values.
+        if remote_config.plugins is None:
+            remote_config.config["plugins"] = project_config.plugins
+        else:
+            # Merge project plugins into remote plugins, keeping remote values for existing keys
+            remote_config.config["plugins"] = deep_merge_plugins(
+                remote_config.config["plugins"], project_config.plugins
+            )
+
+        repo.project_config = remote_config
+
+        return repo
+    except ADOApiNotFoundError as e:
+        raise DependencyResolutionError(
+            f"Could not find a ADO repository at {url}: {e}"
+        )
+
+
+def _validate_ado_parameters(values):
+    assert values.get("url") or values.get(
+        "azure_devops"
+    ), "Must specify `azure_devops`"
+
+    return values
+
+
+def _sync_ado_and_url(values):
+    # If only ado is provided, set url to ado
+    if values.get("azure_devops") and not values.get("url"):
+        values["url"] = values["azure_devops"]
+    # If only url is provided, set ado to url
+    elif values.get("url") and not values.get("azure_devops"):
+        values["azure_devops"] = values["url"]
+    return values
+
+
+class ADODependencyPin(base_dependency.VcsDependencyPin):
+    """Model representing a request to pin an ADO dependency to a specific tag"""
+
+    azure_devops: str
+
+    @property
+    def vcsTagResolver(self):  # -> Type["AbstractTagResolver"]:
+        from cumulusci_ado.vcs.ado.dependencies.ado_resolvers import (  # Circular imports
+            ADOTagResolver,
+        )
+
+        return ADOTagResolver
+
+    @root_validator(pre=True)
+    def sync_vcs_and_url(cls, values):
+        """Defined vcs should be assigned to url"""
+        return _sync_ado_and_url(values)
+
+
+ADODependencyPin.update_forward_refs()
+
+
+class BaseADODependency(base_dependency.BaseVcsDynamicDependency, ABC):
+    """Base class for dynamic dependencies that reference an ADO repo."""
+
+    azure_devops: Optional[AnyUrl] = None
+    vcs: str = VCS_ADO
+    pin_class = ADODependencyPin
+
+    @root_validator
+    def validate_ado_parameters(cls, values):
+        return _validate_ado_parameters(values)
+
+    @root_validator(pre=True)
+    def sync_vcs_and_url(cls, values):
+        """Defined vcs should be assigned to url"""
+        return _sync_ado_and_url(values)
+
+
+class ADODynamicSubfolderDependency(
+    BaseADODependency, base_dependency.VcsDynamicSubfolderDependency
+):
+    """A dependency expressed by a reference to a subfolder of a ADO repo, which needs
+    to be resolved to a specific ref. This is always an unmanaged dependency."""
+
+    @property
+    def unmanagedVcsDependency(self) -> Type["UnmanagedADORefDependency"]:
+        """A human-readable description of the dependency."""
+        return UnmanagedADORefDependency
+
+
+class ADODynamicDependency(BaseADODependency, base_dependency.VcsDynamicDependency):
+    """A dependency expressed by a reference to a ADO repo, which needs
+    to be resolved to a specific ref and/or package version."""
+
+    @property
+    def unmanagedVcsDependency(self) -> Type["UnmanagedADORefDependency"]:
+        """A human-readable description of the dependency."""
+        return UnmanagedADORefDependency
+
+    def get_repo(self, context, url) -> Optional["ADORepository"]:
+        return get_ado_repo(context, url)
+
+
+class UnmanagedADORefDependency(base_dependency.UnmanagedVcsDependency):
+    """Static dependency on unmanaged metadata in a specific ADO ref and subfolder."""
+
+    azure_devops: Optional[AnyUrl] = None
+
+    def get_repo(self, context, url) -> Optional["ADORepository"]:
+        return get_ado_repo(context, url)
+
+    @property
+    def package_name(self) -> str:
+        _owner, _repo_name, host, project = parse_repo_url((str(self.azure_devops)))
+        package_name = f"{_owner}/{_repo_name} {self.subfolder}"
+        return package_name
+
+    @root_validator
+    def validate(cls, values):
+        return _validate_ado_parameters(values)
+
+    @root_validator(pre=True)
+    def sync_vcs_and_url(cls, values):
+        """Defined vcs should be assigned to url"""
+
+        return _sync_ado_and_url(values)
+
+
+add_dependency_class(UnmanagedADORefDependency)
+add_dependency_class(ADODynamicDependency)
+add_dependency_class(ADODynamicSubfolderDependency)
+
+add_dependency_pin_class(ADODependencyPin)
