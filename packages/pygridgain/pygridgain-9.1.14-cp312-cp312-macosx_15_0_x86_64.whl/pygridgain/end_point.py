e9377fb1446492e4fc8f1e5760dfe59562db714b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:11 +0000

    Add package pygridgain/9.1.14/pygridgain-9.1.14-cp312-cp312-macosx_15_0_x86_64.whl

---
+++ packages/pygridgain/pygridgain-9.1.14-cp312-cp312-macosx_15_0_x86_64.whl/pygridgain/end_point.py
@@ -0,0 +1,18 @@
+# Copyright (C) GridGain Systems. All Rights Reserved.
+# _________        _____ __________________        _____
+# __  ____/___________(_)______  /__  ____/______ ____(_)_______
+# _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+# / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+# \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+
+from dataclasses import dataclass
+
+from pygridgain.constants import DEFAULT_PORT
+
+
+@dataclass
+class EndPoint:
+    """Represents a single address."""
+
+    host: str
+    port: int = DEFAULT_PORT
