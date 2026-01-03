37af93ac4c1137a5b5ed53249b3205191116fe27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:41 +0000

    Add package imsize/1.9.0/imsize-1.9.0-py3-none-any.whl

---
+++ packages/imsize/imsize-1.9.0-py3-none-any.whl/imsize/__init__.py
@@ -0,0 +1,22 @@
+"""
+Extracts image dimensions, bit depth, and other basic metadata.
+Runs very fast due to reading and parsing only the file header,
+with the exception of proprietary camera RAW files.
+
+Example:
+  info = imsize.read("myfile.jpg")
+  factor = info.nbytes / info.filesize
+  print(f"{info.filespec}: compression factor = {factor.1f}")
+
+https://github.com/toaarnio/imsize
+"""
+
+from .imsize import FILETYPES
+from .imsize import ImageInfo
+from .imsize import read
+from .imsize import guess_dims
+from .imsize import guess_packing
+from .imsize import ImageFileError
+from .version import __version__
+
+__all__ = ["FILETYPES", "ImageFileError", "ImageInfo", "__version__", "guess_dims", "guess_packing", "read"]
