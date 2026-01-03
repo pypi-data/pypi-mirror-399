80546ec5a241036e9604937aa6131d92d41e3edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 07:08:53 +0000

    Add package tmg-hmc/1.0.0/tmg_hmc-1.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/tmg-hmc/tmg_hmc-1.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tmg_hmc/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "1.0.0"
+
+try:
+    import torch
+    _TORCH_AVAILABLE = True
+except ImportError:
+    _TORCH_AVAILABLE = False
+
+class _TensorPlaceholder(object):
+        pass
+
+def get_torch():
+    if not _TORCH_AVAILABLE:
+        return None
+    return torch
+
+def get_tensor_type():
+    if not _TORCH_AVAILABLE:
+        return _TensorPlaceholder
+    return torch.Tensor
+
+from .sampler import TMGSampler
