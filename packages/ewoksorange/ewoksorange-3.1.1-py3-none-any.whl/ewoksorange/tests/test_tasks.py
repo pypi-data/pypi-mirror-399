2cda69730b440a1a5309d99f77504b39731054d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:35:45 +0000

    Add package ewoksorange/3.1.1/ewoksorange-3.1.1-py3-none-any.whl

---
+++ packages/ewoksorange/ewoksorange-3.1.1-py3-none-any.whl/ewoksorange/tests/test_tasks.py
@@ -0,0 +1,13 @@
+from orangecontrib.ewoksdemo.sumtask import OWSumTask
+
+from .utils import execute_task
+
+
+def test_sumtask_task():
+    result = execute_task(OWSumTask.ewokstaskclass, inputs={"a": 1, "b": 2})
+    assert result == {"result": 3}
+
+
+def test_sumtask_widget(qtapp):
+    result = execute_task(OWSumTask, inputs={"a": 1, "b": 2})
+    assert result == {"result": 3}
