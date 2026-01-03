2f2aae67a13e1dc8d84efcc9505fd3983868015e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 14:05:50 +0000

    Add package math3d-py/1.0.7/math3d_py-1.0.7.tar.gz

---
+++ packages/math3d-py/math3d_py-1.0.7.tar.gz/math3d_py-1.0.7/py/math3d/vector.hpp
@@ -0,0 +1,70 @@
+#pragma once
+#include "pybind11/pybind11.h"
+#include "pybind11/operators.h"
+#include "pybind11/stl.h"
+#include "util.h"
+#include "Vector.h"
+
+namespace py = pybind11;
+
+template<typename T, uint32_t Size>
+void bind_Vector(py::module_ const& module, char const* className) {
+    using vector = math3d::Vector<T, Size>;
+    auto vec_class =
+        py::class_<vector>(module, className)
+        // Construction
+        .def(py::init())
+        .def(py::init([](py::list const& list) {
+            auto const input = list.cast<std::vector<T>>();
+            return vector{input};
+        }))
+        // Member access
+        .def_property("x",
+            [](vector const& self) {
+                double const value = self.x;
+                return value;
+            },
+            [](vector& self, double const value) {
+                self.x = value;
+            }
+        )
+        .def_property("y",
+        [](vector const& self) {
+                T const value = self.y;
+                return value;
+            },
+            [](vector& self, double const value) {
+                self.y = value;
+            }
+        )
+        .def_property("z",
+        [](vector const& self) {
+                T const value = self.z;
+                return value;
+            },
+            [](vector& self, double const value) {
+                self.z = value;
+            }
+        )
+        // Formatted output
+        .def("__str__", [](vector const& v) {
+            return util::convertSpaceToNewLine(v.asString());
+        })
+        .def("__repr__", [](vector const& v) {
+            return util::convertSpaceToNewLine(v.asString());
+        })
+        // Operations
+        .def(py::self + py::self)
+        .def(py::self - py::self)
+        .def(py::self * T{})
+        .def(py::self / T{})
+        .def("dot", &vector::dot)
+        .def("normalize", [](vector& v) { v.normalize(); return v; })
+        .def("length", [](vector const& v) { return v.length(); })
+        .def("length_sqr", [](vector const& v) { return v.lengthSquared(); })
+        .def("projection", &vector::getVectorProjection);
+    if constexpr (Size == 3) {
+        vec_class
+        .def(py::self * py::self);
+    }
+}
