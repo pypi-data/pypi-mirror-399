8ca38eabfd8ccefdfcfef0855938d63dc977f61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:24 +0000

    Add package pyfrontkit/1.1.18/pyfrontkit-1.1.18.tar.gz

---
+++ packages/pyfrontkit/pyfrontkit-1.1.18.tar.gz/pyfrontkit-1.1.18/pyfrontkit/css/create_color/colors_templates/total_template.py
@@ -0,0 +1,24 @@
+total = {
+    "simple_selector": {
+        "body":   {"color": "white", "background-color": "principal"},
+        "header": {"color": "white", "background-color": "principal"},
+        "footer": {"color": "white", "background-color": "principal"},
+        
+        
+        "text_htags": {"color": "quaternary"},
+        "text_tags": {"color": "white"},
+        
+        
+        "block_tags": {"background-color": "quaternary", "color": "black"},
+        
+        "button": {"color": "black", "background-color": "tertiary"}
+    },
+
+    "doble_selector": {
+        "header>a": {"color":"quaternary"},
+        "footer>a": {"color": "quaternary"},
+        "block_tags>text_tags": {"color": "black"} 
+    },
+
+    "triple_selector": {}
+}
---
+++ packages/pyfrontkit/pyfrontkit-1.1.18.tar.gz/pyfrontkit-1.1.18/pyfrontkit/css/create_color/palettes.py
@@ -0,0 +1,254 @@
+
+
+
+
+
+
+# Paletas de colores homologous
+monochromatic = {
+        # REDS
+        "red_pure":    {"principal": "#FF0000", "secondary": "#FF4D4D", "tertiary": "#FF9999", "quaternary": "#FFCCCC"},
+        "red_dark":    {"principal": "#990000", "secondary": "#B22222", "tertiary": "#CC3333", "quaternary": "#FF6666"},
+        "red_pink":    {"principal": "#FF66CC", "secondary": "#FF99D9", "tertiary": "#FFB3E6", "quaternary": "#FFD6F0"},
+        "coral":       {"principal": "#FF7F50", "secondary": "#FF9980", "tertiary": "#FFB3A0", "quaternary": "#FFD1C1"},
+        "tomato":      {"principal": "#FF6347", "secondary": "#FF8266", "tertiary": "#FF9980", "quaternary": "#FFB3A0"},
+
+        # BLUES
+        "blue_pure":   {"principal": "#0000FF", "secondary": "#4D4DFF", "tertiary": "#9999FF", "quaternary": "#CCCCFF"},
+        "blue_medium": {"principal": "#0000CC", "secondary": "#3333FF", "tertiary": "#6666FF", "quaternary": "#9999FF"},
+        "aqua":        {"principal": "#00FFFF", "secondary": "#66FFFF", "tertiary": "#99FFFF", "quaternary": "#CCFFFF"},
+        "cyan":        {"principal": "#00CED1", "secondary": "#33D6D6", "tertiary": "#66E0E0", "quaternary": "#99EBEB"},
+        "sky_blue":    {"principal": "#87CEEB", "secondary": "#A0D9F7", "tertiary": "#B3E4FB", "quaternary": "#CCF0FF"},
+
+        # GREENS
+        "green_pure":  {"principal": "#00FF00", "secondary": "#66FF66", "tertiary": "#99FF99", "quaternary": "#CCFFCC"},
+        "green_light": {"principal": "#90EE90", "secondary": "#A6F0A6", "tertiary": "#B3F7B3", "quaternary": "#CCFFCC"},
+        "green_dark":  {"principal": "#006400", "secondary": "#228B22", "tertiary": "#33AA33", "quaternary": "#66CC66"},
+        "lime":        {"principal": "#32CD32", "secondary": "#66E066", "tertiary": "#99F099", "quaternary": "#CCFFCC"},
+        "sea_green":   {"principal": "#2E8B57", "secondary": "#4DA66E", "tertiary": "#66B380", "quaternary": "#80C99C"},
+
+        # YELLOWS
+        "yellow":      {"principal": "#FFFF00", "secondary": "#FFFF66", "tertiary": "#FFFF99", "quaternary": "#FFFFCC"},
+        "golden":      {"principal": "#FFD700", "secondary": "#FFE066", "tertiary": "#FFE680", "quaternary": "#FFF2B3"},
+        "light_yellow":{"principal": "#FFFFE0", "secondary": "#FFFFF0", "tertiary": "#FFFFF5", "quaternary": "#FFFFFF"},
+        "lemon":       {"principal": "#FFF44F", "secondary": "#FFF97F", "tertiary": "#FFFB99", "quaternary": "#FFFCCD"},
+
+        # ORANGES
+        "orange":      {"principal": "#FFA500", "secondary": "#FFB84D", "tertiary": "#FFC966", "quaternary": "#FFD699"},
+        "dark_orange": {"principal": "#FF8C00", "secondary": "#FFA033", "tertiary": "#FFB266", "quaternary": "#FFC299"},
+        "peach":       {"principal": "#FFE5B4", "secondary": "#FFEBCD", "tertiary": "#FFF2DC", "quaternary": "#FFF7EB"},
+        "tangerine":   {"principal": "#F28500", "secondary": "#FF9A33", "tertiary": "#FFB266", "quaternary": "#FFCC99"},
+
+        # VIOLETS
+        "violet":      {"principal": "#8A2BE2", "secondary": "#A063E2", "tertiary": "#B380EB", "quaternary": "#C39EF0"},
+        "lilac":       {"principal": "#C8A2C8", "secondary": "#D1B3D1", "tertiary": "#E0CCE0", "quaternary": "#F0E0F0"},
+        "purple":      {"principal": "#800080", "secondary": "#993399", "tertiary": "#B366B3", "quaternary": "#CC99CC"},
+        "orchid":      {"principal": "#DA70D6", "secondary": "#E28AD6", "tertiary": "#E6A0EB", "quaternary": "#F0C0F0"},
+
+        # NEUTRALS
+        "white":       {"principal": "#FFFFFF", "secondary": "#F2F2F2", "tertiary": "#E6E6E6", "quaternary": "#CCCCCC"},
+        "black":       {"principal": "#000000", "secondary": "#333333", "tertiary": "#666666", "quaternary": "#999999"},
+        "light_gray":  {"principal": "#D3D3D3", "secondary": "#E0E0E0", "tertiary": "#F0F0F0", "quaternary": "#FAFAFA"},
+        "dark_gray":   {"principal": "#A9A9A9", "secondary": "#BFBFBF", "tertiary": "#D3D3D3", "quaternary": "#E6E6E6"},
+        "beige":       {"principal": "#F5F5DC", "secondary": "#FAFAD2", "tertiary": "#FFFFE0", "quaternary": "#FFFEE0"},
+        "silver":      {"principal": "#C0C0C0", "secondary": "#D3D3D3", "tertiary": "#E6E6E6", "quaternary": "#F2F2F2"},
+    }
+
+triadic = {
+    # REDS
+    "red_pure":    {"principal": "#FF0000", "secondary": "#00FF00", "tertiary": "#0000FF"},
+    "red_dark":    {"principal": "#990000", "secondary": "#009900", "tertiary": "#000099"},
+    "red_pink":    {"principal": "#FF66CC", "secondary": "#CCFF65", "tertiary": "#65CCFF"},
+    "coral":       {"principal": "#FF7F50", "secondary": "#50FE7F", "tertiary": "#7F50FE"},
+    "tomato":      {"principal": "#FF6347", "secondary": "#46FF62", "tertiary": "#6246FF"},
+
+    # BLUES
+    "blue_pure":   {"principal": "#0000FF", "secondary": "#FF0000", "tertiary": "#00FF00"},
+    "blue_medium": {"principal": "#0000CC", "secondary": "#CC0000", "tertiary": "#00CC00"},
+    "aqua":        {"principal": "#00FFFF", "secondary": "#FE00FF", "tertiary": "#FFFE00"},
+    "cyan":        {"principal": "#00CED1", "secondary": "#D100CD", "tertiary": "#CED100"},
+    "sky_blue":    {"principal": "#87CEEB", "secondary": "#EB86CD", "tertiary": "#CEEB86"},
+
+    # GREENS
+    "green_pure":  {"principal": "#00FF00", "secondary": "#0000FF", "tertiary": "#FF0000"},
+    "green_light": {"principal": "#90EE90", "secondary": "#9090EE", "tertiary": "#EE9090"},
+    "green_dark":  {"principal": "#006400", "secondary": "#000064", "tertiary": "#640000"},
+    "lime":        {"principal": "#32CD32", "secondary": "#3131CD", "tertiary": "#CD3131"},
+    "sea_green":   {"principal": "#2E8B57", "secondary": "#562E8A", "tertiary": "#8A572E"},
+
+    # YELLOWS
+    "yellow":      {"principal": "#FFFF00", "secondary": "#00FEFF", "tertiary": "#FE00FF"},
+    "golden":      {"principal": "#FFD700", "secondary": "#00FFD7", "tertiary": "#D600FF"},
+    "light_yellow":{"principal": "#FFFFE0", "secondary": "#E0FFFF", "tertiary": "#FEE0FF"},
+    "lemon":       {"principal": "#FFF44F", "secondary": "#4FFEF4", "tertiary": "#F34FFE"},
+
+    # ORANGES
+    "orange":      {"principal": "#FFA500", "secondary": "#00FFA5", "tertiary": "#A400FF"},
+    "dark_orange": {"principal": "#FF8C00", "secondary": "#00FF8C", "tertiary": "#8B00FF"},
+    "peach":       {"principal": "#FFE5B4", "secondary": "#B4FFE5", "tertiary": "#E4B4FF"},
+    "tangerine":   {"principal": "#F28500", "secondary": "#00F285", "tertiary": "#8400F2"},
+
+    # VIOLETS
+    "violet":      {"principal": "#8A2BE2", "secondary": "#E2892B", "tertiary": "#2BE289"},
+    "lilac":       {"principal": "#C8A2C8", "secondary": "#C7C8A1", "tertiary": "#A1C7C8"},
+    "purple":      {"principal": "#800080", "secondary": "#7F8000", "tertiary": "#007F80"},
+    "orchid":      {"principal": "#DA70D6", "secondary": "#D6DA6F", "tertiary": "#6FD6DA"},
+
+    # NEUTRALS
+    "white":       {"principal": "#FFFFFF", "secondary": "#FFFFFF", "tertiary": "#FFFFFF"},
+    "black":       {"principal": "#000000", "secondary": "#000000", "tertiary": "#000000"},
+    "light_gray":  {"principal": "#D3D3D3", "secondary": "#D3D3D3", "tertiary": "#D3D3D3"},
+    "dark_gray":   {"principal": "#A9A9A9", "secondary": "#A9A9A9", "tertiary": "#A9A9A9"},
+    "beige":       {"principal": "#F5F5DC", "secondary": "#F5F5DC", "tertiary": "#F5F5DC"},
+    "silver":      {"principal": "#C0C0C0", "secondary": "#C0C0C0", "tertiary": "#C0C0C0"},
+}
+
+# Paleta de colores tetrádicos
+tetradic = {
+    # REDS
+    "red_pure":    {"principal": "#FF0000", "secondary": "#00FF00", "tertiary": "#0000FF", "quaternary": "#FFFF00"},
+    "red_dark":    {"principal": "#990000", "secondary": "#009900", "tertiary": "#000099", "quaternary": "#999900"},
+    "red_pink":    {"principal": "#FF66CC", "secondary": "#66FF99", "tertiary": "#9966FF", "quaternary": "#FFCC66"},
+    "coral":       {"principal": "#FF7F50", "secondary": "#50FF7F", "tertiary": "#7F50FF", "quaternary": "#FFFF50"},
+    "tomato":      {"principal": "#FF6347", "secondary": "#47FF63", "tertiary": "#6347FF", "quaternary": "#FFFF63"},
+
+    # BLUES
+    "blue_pure":   {"principal": "#0000FF", "secondary": "#FF0000", "tertiary": "#00FF00", "quaternary": "#FFFF00"},
+    "blue_medium": {"principal": "#0000CC", "secondary": "#CC0000", "tertiary": "#00CC00", "quaternary": "#CCCC00"},
+    "aqua":        {"principal": "#00FFFF", "secondary": "#FF00FF", "tertiary": "#FFFF00", "quaternary": "#00FF00"},
+    "cyan":        {"principal": "#00CED1", "secondary": "#D100CD", "tertiary": "#CDCD00", "quaternary": "#00D1CD"},
+    "sky_blue":    {"principal": "#87CEEB", "secondary": "#EB87CE", "tertiary": "#CEEB87", "quaternary": "#EBCF87"},
+
+    # GREENS
+    "green_pure":  {"principal": "#00FF00", "secondary": "#0000FF", "tertiary": "#FF0000", "quaternary": "#FFFF00"},
+    "green_light": {"principal": "#90EE90", "secondary": "#9090EE", "tertiary": "#EE9090", "quaternary": "#EEEE90"},
+    "green_dark":  {"principal": "#006400", "secondary": "#000064", "tertiary": "#640000", "quaternary": "#646400"},
+    "lime":        {"principal": "#32CD32", "secondary": "#3131CD", "tertiary": "#CD3131", "quaternary": "#CDCD31"},
+    "sea_green":   {"principal": "#2E8B57", "secondary": "#562E8A", "tertiary": "#8A572E", "quaternary": "#8A8A2E"},
+
+    # YELLOWS
+    "yellow":      {"principal": "#FFFF00", "secondary": "#FF0000", "tertiary": "#00FF00", "quaternary": "#0000FF"},
+    "golden":      {"principal": "#FFD700", "secondary": "#00FFD7", "tertiary": "#D600FF", "quaternary": "#FF6D00"},
+    "light_yellow":{"principal": "#FFFFE0", "secondary": "#E0FFFF", "tertiary": "#FEE0FF", "quaternary": "#FFE0FE"},
+    "lemon":       {"principal": "#FFF44F", "secondary": "#4FFEF4", "tertiary": "#F34FFE", "quaternary": "#F4FF4F"},
+
+    # ORANGES
+    "orange":      {"principal": "#FFA500", "secondary": "#00FFA5", "tertiary": "#A400FF", "quaternary": "#FF00A4"},
+    "dark_orange": {"principal": "#FF8C00", "secondary": "#00FF8C", "tertiary": "#8B00FF", "quaternary": "#FF008B"},
+    "peach":       {"principal": "#FFE5B4", "secondary": "#B4FFE5", "tertiary": "#E4B4FF", "quaternary": "#FFB4E4"},
+    "tangerine":   {"principal": "#F28500", "secondary": "#00F285", "tertiary": "#8400F2", "quaternary": "#F20084"},
+
+    # VIOLETS
+    "violet":      {"principal": "#8A2BE2", "secondary": "#E2892B", "tertiary": "#2BE289", "quaternary": "#89E22B"},
+    "lilac":       {"principal": "#C8A2C8", "secondary": "#C7C8A1", "tertiary": "#A1C7C8", "quaternary": "#A2C8A1"},
+    "purple":      {"principal": "#800080", "secondary": "#7F8000", "tertiary": "#007F80", "quaternary": "#808000"},
+    "orchid":      {"principal": "#DA70D6", "secondary": "#D6DA6F", "tertiary": "#6FD6DA", "quaternary": "#DA6FDA"},
+
+    # NEUTRALS
+    "white":       {"principal": "#FFFFFF", "secondary": "#FFFFFF", "tertiary": "#FFFFFF", "quaternary": "#FFFFFF"},
+    "black":       {"principal": "#000000", "secondary": "#000000", "tertiary": "#000000", "quaternary": "#000000"},
+    "light_gray":  {"principal": "#D3D3D3", "secondary": "#D3D3D3", "tertiary": "#D3D3D3", "quaternary": "#D3D3D3"},
+    "dark_gray":   {"principal": "#A9A9A9", "secondary": "#A9A9A9", "tertiary": "#A9A9A9", "quaternary": "#A9A9A9"},
+    "beige":       {"principal": "#F5F5DC", "secondary": "#F5F5DC", "tertiary": "#F5F5DC", "quaternary": "#F5F5DC"},
+    "silver":      {"principal": "#C0C0C0", "secondary": "#C0C0C0", "tertiary": "#C0C0C0", "quaternary": "#C0C0C0"},
+}
+
+homologous = {
+    # REDS
+    "red_pure":    {"principal": "#FF0000", "secondary": "#FFAA00", "tertiary": "#00FFFF"},
+    "red_dark":    {"principal": "#990000", "secondary": "#996600", "tertiary": "#009999"},
+    "red_pink":    {"principal": "#FF66CC", "secondary": "#FF6666", "tertiary": "#66FF99"},
+    "coral":       {"principal": "#FF7F50", "secondary": "#FFF450", "tertiary": "#50D0FF"},
+    "tomato":      {"principal": "#FF6347", "secondary": "#FFDE47", "tertiary": "#47E3FF"},
+
+    # BLUES
+    "blue_pure":   {"principal": "#0000FF", "secondary": "#AA00FF", "tertiary": "#FFFF00"},
+    "blue_medium": {"principal": "#0000CC", "secondary": "#8800CC", "tertiary": "#CCCC00"},
+    "aqua":        {"principal": "#00FFFF", "secondary": "#0055FF", "tertiary": "#FF0000"},
+    "cyan":        {"principal": "#00CED1", "secondary": "#0043D1", "tertiary": "#D10300"},
+    "sky_blue":    {"principal": "#87CEEB", "secondary": "#878BEB", "tertiary": "#EBA487"},
+
+    # GREENS
+    "green_pure":  {"principal": "#00FF00", "secondary": "#66FF66", "tertiary": "#FF00FF"},
+    "green_light": {"principal": "#90EE90", "secondary": "#B6F590", "tertiary": "#6F90FF"},
+    "green_dark":  {"principal": "#006400", "secondary": "#3C6400", "tertiary": "#FF9B99"},
+    "lime":        {"principal": "#32CD32", "secondary": "#64CD64", "tertiary": "#CD3296"},
+    "sea_green":   {"principal": "#2E8B57", "secondary": "#2E9F77", "tertiary": "#8B2E9A"},
+
+    # YELLOWS
+    "yellow":      {"principal": "#FFFF00", "secondary": "#FFAA00", "tertiary": "#0000FF"},
+    "golden":      {"principal": "#FFD700", "secondary": "#FFAA00", "tertiary": "#0052FF"},
+    "light_yellow":{"principal": "#FFFFE0", "secondary": "#FFF7AA", "tertiary": "#0000FF"},
+    "lemon":       {"principal": "#FFF44F", "secondary": "#FFD44F", "tertiary": "#4F4FFF"},
+
+    # ORANGES
+    "orange":      {"principal": "#FFA500", "secondary": "#FFCC66", "tertiary": "#0055FF"},
+    "dark_orange": {"principal": "#FF8C00", "secondary": "#FFB34D", "tertiary": "#0033FF"},
+    "peach":       {"principal": "#FFE5B4", "secondary": "#FFF1D1", "tertiary": "#B4D7FF"},
+    "tangerine":   {"principal": "#F28500", "secondary": "#F2AB55", "tertiary": "#0057D1"},
+
+    # VIOLETS
+    "violet":      {"principal": "#8A2BE2", "secondary": "#E22BC0", "tertiary": "#83E22B"},
+    "lilac":       {"principal": "#C8A2C8", "secondary": "#C8C0A2", "tertiary": "#A2C8A2"},
+    "purple":      {"principal": "#800080", "secondary": "#993366", "tertiary": "#008000"},
+    "orchid":      {"principal": "#DA70D6", "secondary": "#DAA0B6", "tertiary": "#70DA74"},
+
+    # NEUTRALS
+    "white":       {"principal": "#FFFFFF", "secondary": "#E0F7FF", "tertiary": "#001A66"},
+    "black":       {"principal": "#000000", "secondary": "#333333", "tertiary": "#66CCFF"},
+    "light_gray":  {"principal": "#D3D3D3", "secondary": "#D3D3D3", "tertiary": "#2D2DFF"},
+    "dark_gray":   {"principal": "#A9A9A9", "secondary": "#A9A9A9", "tertiary": "#5656FF"},
+    "beige":       {"principal": "#F5F5DC", "secondary": "#E4F5DC", "tertiary": "#DCDCF5"},
+    "silver":      {"principal": "#C0C0C0", "secondary": "#C0C0C0", "tertiary": "#8080FF"},
+}
+
+
+homologous_v2 = {
+    # REDS
+    "red_pure":    {"principal": "#FF0000", "secondary": "#FFAA00", "tertiary": "#00FFFF", "quaternary": "#A60000"},
+    "red_dark":    {"principal": "#990000", "secondary": "#996600", "tertiary": "#009999", "quaternary": "#330000"},
+    "red_pink":    {"principal": "#FF66CC", "secondary": "#FF6666", "tertiary": "#66FF99", "quaternary": "#A63F80"},
+    "coral":       {"principal": "#FF7F50", "secondary": "#FFF450", "tertiary": "#50D0FF", "quaternary": "#A64C30"},
+    "tomato":      {"principal": "#FF6347", "secondary": "#FFDE47", "tertiary": "#47E3FF", "quaternary": "#A63C2B"},
+
+    # BLUES
+    "blue_pure":   {"principal": "#0000FF", "secondary": "#AA00FF", "tertiary": "#FFFF00", "quaternary": "#0000A6"},
+    "blue_medium": {"principal": "#0000CC", "secondary": "#8800CC", "tertiary": "#CCCC00", "quaternary": "#000085"},
+    "aqua":        {"principal": "#00FFFF", "secondary": "#0055FF", "tertiary": "#FF0000", "quaternary": "#00A6A6"},
+    "cyan":        {"principal": "#00CED1", "secondary": "#0043D1", "tertiary": "#D10300", "quaternary": "#00898B"},
+    "sky_blue":    {"principal": "#87CEEB", "secondary": "#878BEB", "tertiary": "#EBA487", "quaternary": "#4C7E91"},
+
+    # GREENS
+    "green_pure":  {"principal": "#00FF00", "secondary": "#66FF66", "tertiary": "#FF00FF", "quaternary": "#00A600"},
+    "green_light": {"principal": "#90EE90", "secondary": "#B6F590", "tertiary": "#6F90FF", "quaternary": "#5E9C5E"},
+    "green_dark":  {"principal": "#006400", "secondary": "#3C6400", "tertiary": "#FF9B99", "quaternary": "#002400"},
+    "lime":        {"principal": "#32CD32", "secondary": "#64CD64", "tertiary": "#CD3296", "quaternary": "#1E7D1E"},
+    "sea_green":   {"principal": "#2E8B57", "secondary": "#2E9F77", "tertiary": "#8B2E9A", "quaternary": "#1B5334"},
+
+    # YELLOWS
+    "yellow":      {"principal": "#FFFF00", "secondary": "#FFAA00", "tertiary": "#0000FF", "quaternary": "#A6A600"},
+    "golden":      {"principal": "#FFD700", "secondary": "#FFAA00", "tertiary": "#0052FF", "quaternary": "#A69100"},
+    "light_yellow":{"principal": "#FFFFE0", "secondary": "#FFF7AA", "tertiary": "#0000FF", "quaternary": "#A6A68A"},
+    "lemon":       {"principal": "#FFF44F", "secondary": "#FFD44F", "tertiary": "#4F4FFF", "quaternary": "#A69A30"},
+
+    # ORANGES
+    "orange":      {"principal": "#FFA500", "secondary": "#FFCC66", "tertiary": "#0055FF", "quaternary": "#A66B00"},
+    "dark_orange": {"principal": "#FF8C00", "secondary": "#FFB34D", "tertiary": "#0033FF", "quaternary": "#A65A00"},
+    "peach":       {"principal": "#FFE5B4", "secondary": "#FFF1D1", "tertiary": "#B4D7FF", "quaternary": "#A69870"},
+    "tangerine":   {"principal": "#F28500", "secondary": "#F2AB55", "tertiary": "#0057D1", "quaternary": "#915300"},
+
+    # VIOLETS
+    "violet":      {"principal": "#8A2BE2", "secondary": "#E22BC0", "tertiary": "#83E22B", "quaternary": "#4F1A85"},
+    "lilac":       {"principal": "#C8A2C8", "secondary": "#C8C0A2", "tertiary": "#A2C8A2", "quaternary": "#7A657A"},
+    "purple":      {"principal": "#800080", "secondary": "#993366", "tertiary": "#008000", "quaternary": "#4D004D"},
+    "orchid":      {"principal": "#DA70D6", "secondary": "#DAA0B6", "tertiary": "#70DA74", "quaternary": "#8A437D"},
+
+    # NEUTRALS
+    "white":       {"principal": "#FFFFFF", "secondary": "#E0F7FF", "tertiary": "#001A66", "quaternary": "#A6A6A6"},
+    "black":       {"principal": "#000000", "secondary": "#333333", "tertiary": "#66CCFF", "quaternary": "#000000"},
+    "light_gray":  {"principal": "#D3D3D3", "secondary": "#D3D3D3", "tertiary": "#2D2DFF", "quaternary": "#888888"},
+    "dark_gray":   {"principal": "#A9A9A9", "secondary": "#A9A9A9", "tertiary": "#5656FF", "quaternary": "#676767"},
+    "beige":       {"principal": "#F5F5DC", "secondary": "#E4F5DC", "tertiary": "#DCDCF5", "quaternary": "#9E9E89"},
+    "silver":      {"principal": "#C0C0C0", "secondary": "#C0C0C0", "tertiary": "#8080FF", "quaternary": "#777777"},
+}
+    
