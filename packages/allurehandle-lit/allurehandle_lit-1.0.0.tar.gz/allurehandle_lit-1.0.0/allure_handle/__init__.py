a4b863cd98fcbbffde51923d2cded08120475193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:05:51 +0000

    Add package allurehandle-lit/1.0.0/allurehandle_lit-1.0.0.tar.gz

---
+++ packages/allurehandle-lit/allurehandle_lit-1.0.0.tar.gz/allurehandle_lit-1.0.0/allure_handle/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:UTF-8 -*-
+"""
+Allure Handle - 轻量级 Allure 报告处理工具
+
+一个简单易用的 Allure 报告工具，用于 pytest 测试框架。
+最小依赖，只需要 allure-pytest。
+"""
+from allure_handle.allure_handle import AllureHandle, allure_handle
+
+__version__ = '1.0.0'
+__all__ = ['AllureHandle', 'allure_handle']
+
