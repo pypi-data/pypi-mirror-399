b59b2a33285abf499925fd240add696924c09b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 16:07:59 +0000

    Add package mauztoolslib/0.1.1.post1/mauztoolslib-0.1.1.post1-py3-none-any.whl

---
+++ packages/mauztoolslib/mauztoolslib-0.1.1.post1-py3-none-any.whl/mauztoolslib/html/__init__.py
@@ -0,0 +1,13 @@
+from .reader import HTMLReader
+from .objects.script import Script
+from .objects.paragraph import Paragraph
+from .objects.image import Image
+from .objects.heading import Heading
+
+__all__ = [
+    "HTMLReader",
+    "Script",
+    "Paragraph",
+    "Image",
+    "Heading",
+]
