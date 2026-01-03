2f07e2cd09bcedaf9fd1521a8ba471305fc4230c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:34:55 +0000

    Add package CTkScrollableDropdownPP/2.2/ctkscrollabledropdownpp-2.2-py3-none-any.whl

---
+++ packages/CTkScrollableDropdownPP/ctkscrollabledropdownpp-2.2-py3-none-any.whl/CTkScrollableDropdownPP/__init__.py
@@ -0,0 +1,11 @@
+"""
+CustomTkinter Scrollable Dropdown Menu
+Original Author: Akash Bora (https://github.com/Akascape)
+Modified by: PLauncher-Team (https://github.com/PLauncher-Team) with pagination, search, and grouping features.
+
+License: MIT
+Homepage: https://github.com/PLauncher-Team/CTkScrollableDropdownPP
+"""
+
+__version__ = '2.0'
+from .ctk_scrollable_dropdown import CTkScrollableDropdown
