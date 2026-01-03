8420e4547f3fe8be5752eabb46be7d5dd32906a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:24 +0000

    Add package pyfrontkit/1.1.18/pyfrontkit-1.1.18-py3-none-any.whl

---
+++ packages/pyfrontkit/pyfrontkit-1.1.18-py3-none-any.whl/pyfrontkit/css/create_color/colors_templates/enfasis_main_template.py
@@ -0,0 +1,19 @@
+enfasis_main = {
+    "simple_selector": {
+        "body":   {"color": "black", "background-color": "secondary"},
+        "header": {"color": "white", "background-color": "principal"},
+        "footer": {"color": "white", "background-color": "principal"},
+        "text_htags": {"color": "principal"},
+        "h3": {"color": "black"},
+        "h4": {"color": "black"},
+        "button": {"color": "white", "background-color": "tertiary"}
+    },
+
+    "doble_selector": {
+        "header>text_tags": {"color":"white"},
+        "footer>text_tags": {"color": "white"},
+        "body>button": {"color": "white", "background-color": "principal"}
+    },
+
+    "triple_selector": {}
+}
