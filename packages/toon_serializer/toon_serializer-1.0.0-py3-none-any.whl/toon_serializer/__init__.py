e6051aa777fb1b56e10b26a7ea70a3c932a0eced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:42 +0000

    Add package toon_serializer/1.0.0/toon_serializer-1.0.0-py3-none-any.whl

---
+++ packages/toon_serializer/toon_serializer-1.0.0-py3-none-any.whl/toon_serializer/__init__.py
@@ -0,0 +1,9 @@
+"""toon_serializer Python library for working with TOON."""
+
+from toon_serializer.decoder import loads
+from toon_serializer.encoder import dumps
+
+__all__ = [
+    "dumps",
+    "loads",
+]
