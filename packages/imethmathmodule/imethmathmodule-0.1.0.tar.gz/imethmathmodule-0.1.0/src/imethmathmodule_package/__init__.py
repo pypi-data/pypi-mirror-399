b7f92c792a94afa44f5af2abb3d64ade91fec991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:55 +0000

    Add package imethmathmodule/0.1.0/imethmathmodule-0.1.0.tar.gz

---
+++ packages/imethmathmodule/imethmathmodule-0.1.0.tar.gz/imethmathmodule-0.1.0/src/imethmathmodule_package/__init__.py
@@ -0,0 +1,25 @@
+from .main import (
+    average,
+    add,
+    subtract,
+    multiply,
+    divide,
+    square,
+    cube,
+    power,
+    mod,
+    odd_even,
+)
+
+__all__ = [
+    "average",
+    "add",
+    "subtract",
+    "multiply",
+    "divide",
+    "square",
+    "cube",
+    "power",
+    "mod",
+    "odd_even",
+]
---
+++ packages/imethmathmodule/imethmathmodule-0.1.0.tar.gz/imethmathmodule-0.1.0/src/imethmathmodule_package/main.py
@@ -0,0 +1,35 @@
+
+def average(a, b, c):
+    x = (a + b + c) / 3
+    return x
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def power(x, y):
+    return x ** y
+
+def mod(x, y):
+    return x % y
+
+def odd_even(x):
+    if x % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
