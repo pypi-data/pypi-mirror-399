365c200f14417ebda14132f95311ea6944fc9f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:08:47 +0000

    Add package wisdem/4.0.5/wisdem-4.0.5-cp314-cp314t-macosx_15_0_arm64.whl

---
+++ packages/wisdem/wisdem-4.0.5-cp314-cp314t-macosx_15_0_arm64.whl/wisdem/floatingse/floating.py
@@ -0,0 +1,148 @@
+import openmdao.api as om
+
+from wisdem.floatingse.mooring import Mooring
+from wisdem.floatingse.constraints import RigidModes, FloatingConstraints
+from wisdem.commonse.cylinder_member import MemberDetailed
+from wisdem.floatingse.floating_frame import FloatingFrame
+from wisdem.floatingse.floating_system import FloatingSystem
+
+
+class FloatingSEProp(om.Group):
+    def initialize(self):
+        self.options.declare("modeling_options")
+
+    def setup(self):
+        opt = self.options["modeling_options"]
+
+        n_member = opt["floating"]["members"]["n_members"]
+        mem_prom = [
+            "E_mat",
+            "G_mat",
+            "sigma_y_mat",
+            "sigma_ult_mat",
+            "wohler_exp_mat",
+            "wohler_A_mat",
+            "rho_mat",
+            "rho_water",
+            "unit_cost_mat",
+            "material_names",
+            "painting_cost_rate",
+            "labor_cost_rate",
+        ]
+        for k in range(n_member):
+            kname = opt["floating"]["members"]["name"][k]
+            self.add_subsystem(
+                f"member{k}_{kname}",
+                MemberDetailed(
+                    column_options=opt["floating"]["members"],
+                    idx=k,
+                    n_mat=opt["materials"]["n_mat"],
+                    memmax=True,
+                    n_refine=2,
+                    member_shape=opt["floating"]["members"]["outer_shape"][k],
+                ),
+                promotes=mem_prom + [("joint1", f"member{k}_{kname}:joint1"),
+                                     ("joint2", f"member{k}_{kname}:joint2"),
+                                     ("total_mass_user", f"member{k}_{kname}:mass_user")],
+            )
+
+        self.add_subsystem(
+            "mm", Mooring(options=opt["mooring"], gamma=opt["WISDEM"]["FloatingSE"]["gamma_f"]), promotes=["*"]
+        )
+
+                
+class FloatingSEPerf(om.Group):
+    def initialize(self):
+        self.options.declare("modeling_options")
+
+    def setup(self):
+        opt = self.options["modeling_options"]
+
+        # Combine all members and tower into single system
+        self.add_subsystem("sys", FloatingSystem(modeling_options=opt), promotes=["*"])
+
+        # Do the load analysis over one or more load cases
+        self.add_subsystem("load", FloatingFrame(modeling_options=opt), promotes=["*"])
+
+        # Evaluate system constraints
+        self.add_subsystem("cons", FloatingConstraints(modeling_options=opt), promotes=["*"])
+
+        # Evaluate system constraints
+        self.add_subsystem("modal", RigidModes(), promotes=["*"])
+
+                
+class FloatingSE(om.Group):
+    def initialize(self):
+        self.options.declare("modeling_options")
+
+    def setup(self):
+        opt = self.options["modeling_options"]
+
+
+        self.add_subsystem("prop", FloatingSEProp(modeling_options=opt), promotes=["*"])
+        self.add_subsystem("perf", FloatingSEPerf(modeling_options=opt), promotes=["*"])
+
+        # Connect all input variables from all models
+        mem_vars = [
+            "section_t",
+            "section_A",
+            "section_Asx",
+            "section_Asy",
+            "section_Ixx",
+            "section_Iyy",
+            "section_J0",
+            "section_rho",
+            "section_E",
+            "section_G",
+            "section_TorsC",
+            "section_sigma_y",
+            "idx_cb",
+            "variable_ballast_capacity",
+            "variable_ballast_Vpts",
+            "variable_ballast_spts",
+            "buoyancy_force",
+            "displacement",
+            "center_of_buoyancy",
+            "center_of_mass",
+            "ballast_mass",
+            "total_mass",
+            "total_cost",
+            "I_total",
+            "Awater",
+            "Iwaterx",
+            "Iwatery",
+            "added_mass",
+            "waterline_centroid",
+        ]
+        
+        n_member = opt["floating"]["members"]["n_members"]
+        for k in range(n_member):
+            member_shape = opt["floating"]["members"]["outer_shape"][k]
+            kname = opt["floating"]["members"]["name"][k]
+
+            for var in mem_vars:
+                self.connect(f"member{k}_{kname}.{var}", f"member{k}_{kname}:{var}")
+
+            self.connect(f"member{k}_{kname}.nodes_xyz_all", f"member{k}_{kname}:nodes_xyz")
+            self.connect(f"member{k}_{kname}.constr_ballast_capacity", f"member{k}_{kname}:constr_ballast_capacity")
+
+            if member_shape == "circular":
+                self.connect(f"member{k}_{kname}.nodes_r_all", f"member{k}_{kname}:nodes_r")
+                self.connect(f"member{k}_{kname}.section_D", f"member{k}_{kname}:section_D")
+                self.connect(f"member{k}_{kname}.ca_usr_grid_full", f"memload{k}.ca_usr")
+                self.connect(f"member{k}_{kname}.cd_usr_grid_full", f"memload{k}.cd_usr")
+                self.connect(f"member{k}_{kname}.outer_diameter_full", f"memload{k}.outer_diameter_full")
+            elif member_shape == "rectangular":
+                # self.connect(f"member{k}_{kname}.nodes_a_all", f"member{k}_{kname}:nodes_a")
+                # self.connect(f"member{k}_{kname}.nodes_b_all", f"member{k}_{kname}:nodes_b")
+                self.connect(f"member{k}_{kname}.section_a", f"member{k}_{kname}:section_a") 
+                self.connect(f"member{k}_{kname}.section_b", f"member{k}_{kname}:section_b")
+                self.connect(f"member{k}_{kname}.ca_usr_grid_full", f"memload{k}.ca_usr")
+                self.connect(f"member{k}_{kname}.cay_usr_grid_full", f"memload{k}.cay_usr")
+                self.connect(f"member{k}_{kname}.cd_usr_grid_full", f"memload{k}.cd_usr")
+                self.connect(f"member{k}_{kname}.cdy_usr_grid_full", f"memload{k}.cdy_usr")
+                self.connect(f"member{k}_{kname}.side_length_a_full", f"memload{k}.side_length_a_full")
+                self.connect(f"member{k}_{kname}.side_length_b_full", f"memload{k}.side_length_b_full")
+
+            for var in ["z_global", "s_full", "s_all"]:
+                self.connect(f"member{k}_{kname}.{var}", f"memload{k}.{var}")
---
+++ packages/wisdem/wisdem-4.0.5-cp314-cp314t-macosx_15_0_arm64.whl/wisdem/test/test_floatingse/test_mooring.py
@@ -0,0 +1,94 @@
+import unittest
+
+import numpy as np
+import numpy.testing as npt
+
+import wisdem.floatingse.mooring as mm
+
+
+class TestMooring(unittest.TestCase):
+    def setUp(self):
+        self.inputs = {}
+        self.outputs = {}
+
+        myones = np.ones(1)
+        self.inputs["fairlead"] = 10.0 * myones
+        self.inputs["fairlead_radius"] = 11.0 * myones
+        self.inputs["anchor_radius"] = 250.0 * myones
+        self.inputs["anchor_mass"] = 100.0 * myones
+        self.inputs["anchor_cost"] = 10.0 * myones
+        self.inputs["anchor_max_vertical_load"] = np.array([ 1e10 ])
+        self.inputs["anchor_max_lateral_load"] = np.array([ 1e10 ])
+
+        self.inputs["rho_water"] = np.array([ 1e3 ])
+        self.inputs["water_depth"] = np.array([ 200.0 ])  # 100.0
+
+        self.inputs["line_length"] = 0.6 * (self.inputs["water_depth"] + self.inputs["anchor_radius"]) * myones
+        self.inputs["line_diameter"] = 0.1 * myones
+        self.inputs["line_mass_density_coeff"] = 10.0 * myones
+        self.inputs["line_stiffness_coeff"] = 2e6 * myones
+        self.inputs["line_breaking_load_coeff"] = 30.0 * myones
+        self.inputs["line_cost_rate_coeff"] = 40.0 * myones
+        self.inputs["max_surge_fraction"] = np.array([ 0.2 ])
+        self.inputs["survival_heel"] = np.array([ np.deg2rad(10.0) ])
+        self.inputs["operational_heel"] = np.array([ np.deg2rad(5.0) ])
+
+        opt = {}
+        opt["n_attach"] = 3
+        opt["n_anchors"] = 6
+        opt["line_anchor"] = ["custom"]  # ["drag_embedment"]
+        opt["line_material"] = ["custom"]  # ["chain"]
+
+        self.mymap = mm.Mooring(options=opt, gamma=1.35)
+
+    def testGeometry(self):
+        self.mymap.compute(self.inputs, self.outputs)
+        npt.assert_equal(self.outputs["constr_mooring_length"], 0.6 * 450 / (0.95 * 440))
+
+        self.inputs["line_length"] = np.array([ 150.0 ])
+        self.mymap.compute(self.inputs, self.outputs)
+        npt.assert_equal(self.outputs["constr_mooring_length"], 150 / (200 - 10 - 1.35 * 11 * np.sin(np.deg2rad(10))))
+
+    def testRunMap(self):
+        self.mymap.compute(self.inputs, self.outputs)
+
+        npt.assert_almost_equal(self.outputs["mooring_neutral_load"].sum(axis=0)[:2], 0.0, decimal=2)
+        npt.assert_equal(self.outputs["mooring_neutral_load"].shape[0], 3)
+        npt.assert_array_less(1.0, self.outputs["constr_axial_load"])
+        npt.assert_array_less(1e3, self.outputs["max_surge_restoring_force"])
+
+        npt.assert_almost_equal(self.outputs["operational_heel_restoring_force"][[1, 3, 5]], 0.0, 2)
+        npt.assert_array_less(0.0, self.outputs["operational_heel_restoring_force"][0])
+        npt.assert_array_less(1e3, np.abs(self.outputs["operational_heel_restoring_force"][2]))
+        npt.assert_array_less(1e3, np.abs(self.outputs["operational_heel_restoring_force"][4]))
+
+        npt.assert_almost_equal(self.outputs["survival_heel_restoring_force"][[1, 3, 5]], 0.0, 2)
+        npt.assert_array_less(
+            self.outputs["operational_heel_restoring_force"][0], self.outputs["survival_heel_restoring_force"][0]
+        )
+        npt.assert_array_less(1e3, np.abs(self.outputs["survival_heel_restoring_force"][2]))
+        npt.assert_array_less(
+            np.abs(self.outputs["operational_heel_restoring_force"][4]),
+            np.abs(self.outputs["survival_heel_restoring_force"][4]),
+        )
+
+        npt.assert_almost_equal(self.outputs["mooring_mass"], 6 * 270 * 0.1 + 6 * 100)
+        npt.assert_almost_equal(self.outputs["mooring_cost"], 6 * 270 * 0.4 + 6 * 10)
+
+    def testRunMap_MoorProps(self):
+        opt = {}
+        opt["n_attach"] = 3
+        opt["n_anchors"] = 6
+        opt["line_anchor"] = ["drag_embedment"]
+        opt["line_material"] = ["chain"]
+
+        mymap = mm.Mooring(options=opt, gamma=1.35)
+
+        mymap.compute(self.inputs, self.outputs)
+                                                          # Number of lines, length, mass density
+        npt.assert_almost_equal(self.outputs["mooring_mass"], 6 * 270 * 61.72839506 + 6 * 0, 3)
+        npt.assert_almost_equal(self.outputs["mooring_cost"], 6 * 270 * 159.5679012345679, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
