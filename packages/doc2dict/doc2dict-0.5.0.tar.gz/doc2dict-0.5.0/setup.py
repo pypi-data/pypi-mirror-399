54b059604c1a52189ed9fd8ff0204168730b9d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:06:00 +0000

    Add package doc2dict/0.5.0/doc2dict-0.5.0.tar.gz

---
+++ packages/doc2dict/doc2dict-0.5.0.tar.gz/doc2dict-0.5.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="doc2dict",
+    version="0.5.0",
+    packages=find_packages(),
+    install_requires=['selectolax','xmltodict','pypdfium2'
+    ]
+)
