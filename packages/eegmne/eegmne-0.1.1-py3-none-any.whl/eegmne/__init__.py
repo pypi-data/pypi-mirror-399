3d0a9e80fb862064e4c303a708d3f0c827fb629f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:45 +0000

    Add package eegmne/0.1.1/eegmne-0.1.1-py3-none-any.whl

---
+++ packages/eegmne/eegmne-0.1.1-py3-none-any.whl/eegmne/__init__.py
@@ -0,0 +1,13 @@
+from .table import (
+    ModelRow,
+    render_ascii_table,
+    print_fair_model_comparison,
+    eegsignal,
+)
+
+__all__ = [
+    "ModelRow",
+    "render_ascii_table",
+    "print_fair_model_comparison",
+    "eegsignal",
+]
