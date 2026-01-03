800ce8c01cf0fe8897cf77e6216c5c8808c4e26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 16:05:48 +0000

    Add package mlpyqtgraph/0.8.0/mlpyqtgraph-0.8.0-py3-none-any.whl

---
+++ packages/mlpyqtgraph/mlpyqtgraph-0.8.0-py3-none-any.whl/mlpyqtgraph/__init__.py
@@ -0,0 +1,35 @@
+"""
+__init__.py for mlpyqtgraph, only functions and classes which are meant to be
+used as interface
+"""
+
+from pqthreads import config as pqthreads_config
+from pqthreads.decorator import DecoratorCore, Decorator
+from mlpyqtgraph import windows
+from mlpyqtgraph import axes
+from mlpyqtgraph import workers
+from mlpyqtgraph import config
+
+from . import ml_functions
+from .ml_functions import *
+
+
+__version__ = '0.8.0'
+
+
+pqthreads_config.params.signal_slot_timeout = 10_000
+
+
+class OptionsDecoratorCore(DecoratorCore):
+    """ Decorator take also takes keyword arguments and sets them as config
+    options """
+
+    def __init__(self, **options):
+        super().__init__(**options)
+        if options:
+            config.options.set_options(**options)
+
+
+OptionsDecoratorCore.add_agent('figure', windows.FigureWindow, workers.FigureWorker)
+OptionsDecoratorCore.add_agent('axis',axes.Axis, workers.AxisWorker)
+plotter = Decorator(OptionsDecoratorCore)
