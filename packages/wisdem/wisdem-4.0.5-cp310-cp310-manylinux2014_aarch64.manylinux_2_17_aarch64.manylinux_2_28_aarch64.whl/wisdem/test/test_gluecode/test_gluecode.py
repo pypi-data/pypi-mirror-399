31145807dbfca68c4dfa0d3ce10f60ed20e4516f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:09:03 +0000

    Add package wisdem/4.0.5/wisdem-4.0.5-cp310-cp310-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/wisdem/wisdem-4.0.5-cp310-cp310-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/wisdem/test/test_gluecode/test_gluecode.py
@@ -0,0 +1,58 @@
+import os
+import unittest
+
+from wisdem.glue_code.runWISDEM import run_wisdem
+
+test_dir = (
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+    + os.sep
+    + "examples"
+    + os.sep
+    + "02_reference_turbines"
+    + os.sep
+)
+
+fname_analysis_options = test_dir + "analysis_options.yaml"
+
+
+class TestRegression(unittest.TestCase):
+    def test5MW(self):
+        ## NREL 5MW
+        fname_wt_input = test_dir + "nrel5mw.yaml"
+        fname_modeling_options = test_dir + "modeling_options_nrel5.yaml"
+
+        wt_opt, _, _ = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 23.8931681739, 2)
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 16485.0072740210, 2)  # new value: improved interpolation
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 53.1235615634, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 4.4785104986, 1)
+        self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 87.7, 2)
+
+    def test15MW(self):
+        ## IEA 15MW
+        fname_wt_input = test_dir + "IEA-15-240-RWT.yaml"
+        fname_modeling_options = test_dir + "modeling_options_iea15.yaml"
+        wt_opt, _, _ = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 77.90013659314998, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 68233.0936092383, -1) # new value: improved interpolation
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 75.25866160620044, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 25.98145796253223, 1)
+        self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 144.386, 3)
+
+    def test3p4MW(self):
+        ## IEA 3.4MW
+        fname_wt_input = test_dir + "IEA-3p4-130-RWT.yaml"
+        fname_modeling_options = test_dir + "modeling_options_iea3p4.yaml"
+        wt_opt, _, _ = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+        self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 13.591140338759166, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 14534.711602944584, 1)  # new value: improved interpolation
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 38.444833825078855, 1)
+        self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 8.031667548036724, 1)
+        self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 108.0, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
