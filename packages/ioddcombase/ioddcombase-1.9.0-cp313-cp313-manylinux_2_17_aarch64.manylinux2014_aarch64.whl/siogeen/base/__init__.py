c0f15cfd4e163976cd106653b85e8eb28eda5ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:06:50 +0000

    Add package ioddcombase/1.9.0/ioddcombase-1.9.0-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/ioddcombase/ioddcombase-1.9.0-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/siogeen/base/__init__.py
@@ -0,0 +1,15 @@
+'''
+IoddCom base package.
+
+The Siogeen IoddCom is designed for various use cases for best working
+experience with IO-Link devices, simple usage and time saving thus both saving
+costs and offering time for the real tasks.
+
+Created on 04.03.2017
+'''
+
+__author__ = "Reimund Renner"
+__email__ = "reimund@siogeen.com"
+__contact__ = "contact@siogeen.com"
+__license__ = "proprietary and confidential"
+__version__ = "1.9.0"
