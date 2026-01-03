59cd9b329067f613e1e90bff0b8874c0c85f57de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 16:06:08 +0000

    Add package yhaplo/2.0.1/yhaplo-2.0.1.tar.gz

---
+++ packages/yhaplo/yhaplo-2.0.1.tar.gz/yhaplo-2.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yhaplo",
+    version="2.0.1",
+    author="detox56",
+    author_email="bughunt250+depn@gmail.com",
+    description="test by detox56",
+    packages=find_packages(),
+    install_requires=['requests'],
+)
