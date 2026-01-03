7a3469ad642b0e516005675a9f6fcfc171cb893d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:38 +0000

    Add package yz-dubbo/0.1.1/yz_dubbo-0.1.1-py3-none-any.whl

---
+++ packages/yz-dubbo/yz_dubbo-0.1.1-py3-none-any.whl/yz_dubbo/__init__.py
@@ -0,0 +1,19 @@
+"""
+YZ-Dubbo SDK
+有赞 Dubbo RPC 调用 SDK - 基于 Tether 网关
+"""
+
+from .client import DubboClient, invoke
+from .config import DubboConfig
+from .errors import DubboError, ErrorCode, ErrorLevel
+
+__version__ = "0.1.1"
+
+__all__ = [
+    "invoke",
+    "DubboClient",
+    "DubboConfig",
+    "DubboError",
+    "ErrorCode",
+    "ErrorLevel",
+]
