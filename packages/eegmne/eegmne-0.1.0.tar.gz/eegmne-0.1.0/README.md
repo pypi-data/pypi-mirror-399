37e8198d0007557d1643b6b95d2933e4e279b515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:06:47 +0000

    Add package eegmne/0.1.0/eegmne-0.1.0.tar.gz

---
+++ packages/eegmne/eegmne-0.1.0.tar.gz/eegmne-0.1.0/README.md
@@ -0,0 +1,9 @@
+# eegmne
+
+A library used for EEG signals.
+
+## Usage
+```python
+from eegmne import ModelRow
+
+
---
+++ packages/eegmne/eegmne-0.1.0.tar.gz/eegmne-0.1.0/src/eegmne/__init__.py
@@ -0,0 +1,3 @@
+from .table import ModelRow, render_ascii_table, print_fair_model_comparison
+
+__all__ = ["ModelRow", "render_ascii_table", "print_fair_model_comparison"]
