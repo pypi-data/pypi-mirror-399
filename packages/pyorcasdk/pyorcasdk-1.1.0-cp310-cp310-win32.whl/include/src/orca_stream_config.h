18f9af509b5cdf959aab5e7b314fe93aaf1c34bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:41 +0000

    Add package pyorcasdk/1.1.0/pyorcasdk-1.1.0-cp310-cp310-win32.whl

---
+++ packages/pyorcasdk/pyorcasdk-1.1.0-cp310-cp310-win32.whl/include/src/orca_stream_config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace orcaSDK
+{
+
+/**
+  @brief Enum of all actuator specific function codes, in decimal.
+ */
+enum orca_function_codes_e {
+	motor_command = 100
+};
+
+enum connection_function_codes_e {
+	change_connection_status = 65
+};
+
+}
