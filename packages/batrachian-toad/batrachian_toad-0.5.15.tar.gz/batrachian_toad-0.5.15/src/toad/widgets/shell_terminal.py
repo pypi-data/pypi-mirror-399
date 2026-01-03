a22657bd4db5cc53a3beceb4f15bb6cff2dc8496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 16:05:44 +0000

    Add package batrachian-toad/0.5.15/batrachian_toad-0.5.15.tar.gz

---
+++ packages/batrachian-toad/batrachian_toad-0.5.15.tar.gz/batrachian_toad-0.5.15/src/toad/widgets/shell_terminal.py
@@ -0,0 +1,18 @@
+from typing import Iterable
+
+from toad.menus import MenuItem
+from toad.widgets.terminal import Terminal
+
+
+class ShellTerminal(Terminal):
+    """Subclass of Terminal used in the Shell view."""
+
+    def get_block_menu(self) -> Iterable[MenuItem]:
+        return
+        yield
+
+    def on_mount(self) -> None:
+        self.border_title = self.name
+
+    def get_block_content(self, destination: str) -> str | None:
+        return "\n".join(line.content.plain for line in self.state.buffer.lines)
