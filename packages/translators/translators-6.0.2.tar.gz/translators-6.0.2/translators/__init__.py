f21f15d7b8522a76e5753133ff4c90fe766cef6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:06:01 +0000

    Add package translators/6.0.2/translators-6.0.2.tar.gz

---
+++ packages/translators/translators-6.0.2.tar.gz/translators-6.0.2/translators/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "6.0.2"
+__author__ = "UlionTse"
+
+
+from translators.server import (
+    translate_text,
+    translate_html,
+    translators_pool,
+    get_languages,
+    get_region_of_server,
+    preaccelerate_and_speedtest,
+)
+
+
+__all__ = (
+    "__version__",
+    "__author__",
+    "translate_text",
+    "translate_html",
+    "translators_pool",
+    "get_languages",
+    "get_region_of_server",
+    "preaccelerate_and_speedtest",
+)
