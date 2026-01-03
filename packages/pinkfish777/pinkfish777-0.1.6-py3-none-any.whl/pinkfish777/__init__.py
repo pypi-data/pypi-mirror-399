9a082416c5c90538eb2c64c7fa9111c36b48d155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 07:07:59 +0000

    Add package pinkfish777/0.1.6/pinkfish777-0.1.6-py3-none-any.whl

---
+++ packages/pinkfish777/pinkfish777-0.1.6-py3-none-any.whl/pinkfish777/__init__.py
@@ -0,0 +1,95 @@
+from .fetch import (
+    fetch_timeseries,
+    select_tradeperiod,
+    finalize_timeseries,
+    remove_cache_symbols,
+    update_cache_symbols,
+    get_symbol_metadata,
+    get_quote
+)
+
+from .trade import (
+    Direction,
+    Margin,
+    TradeLog,
+    TradeState,
+    DailyBal
+)
+
+from .pfstatistics import (
+    ALPHA_BEGIN,
+    SP500_BEGIN,
+    get_trading_days,
+    currency_metrics,
+    stats,
+    currency,
+    summary,
+    optimizer_summary
+)
+
+from .plot import (
+    plot_equity_curve,
+    plot_equity_curves,
+    plot_trades,
+    plot_bar_graph,
+    optimizer_plot_bar_graph
+)
+
+from .benchmark import (
+    Benchmark
+)
+
+from .portfolio import (
+    Portfolio,
+    technical_indicator
+)
+
+from .indicator import (
+    SMA,
+    EMA,
+    CROSSOVER,
+    MOMENTUM,
+    VOLATILITY,
+    ANNUALIZED_RETURNS,
+    ANNUALIZED_STANDARD_DEVIATION,
+    ANNUALIZED_SHARPE_RATIO
+)
+
+from .pfcalendar import (
+    calendar
+)
+
+from .stock_market_calendar import (
+    stock_market_calendar
+)
+
+from .analysis import (
+    prettier_graphs,
+    volatility_graphs,
+    kelly_criterion
+)
+
+from .utility import (
+    ROOT,
+    import_strategy,
+    print_full,
+    read_config,
+    is_last_row,
+    get_previous_row,
+    sort_dict,
+    set_dict_values,
+    find_nan_rows
+)
+
+DEBUG = False
+"""
+bool : True to enable DBG() output.
+"""
+def DBG(s):
+    """
+    Debug print.  Enable by setting pf.DEBUG=True.
+    """
+    if DEBUG:
+        print(s)
+    else:
+        pass
---
+++ packages/pinkfish777/pinkfish777-0.1.6-py3-none-any.whl/pinkfish777/utility.py
@@ -0,0 +1,174 @@
+"""
+Utility functions.
+"""
+
+import importlib.util
+import inspect
+from configparser import ConfigParser
+from functools import wraps
+from pathlib import Path
+
+import pandas as pd
+from from_root import from_root
+
+ROOT = from_root()
+"""
+str: pinkfish project root dir.
+"""
+
+
+def import_strategy(strategy_name, top_level_dir="examples", module_name="strategy"):
+    """
+    Import a strategy from a python `.py` file.
+
+    Parameters
+    ----------
+    strategy_name : str
+        The leaf dir name that contains the strategy to import.
+    top_level_dir : str, optional
+        The top level dir name for the strategies
+        (default is 'examples').
+    module_name: str, optional
+        The name of the python module (default is 'strategy').
+
+    Returns
+    -------
+    module
+        The imported module.
+
+    Examples
+    --------
+    >>> strategy = import_strategy(strategy_name='190.momentum-dmsr-portfolio')
+    """
+
+    strategy_location = (
+        Path(ROOT)
+        / Path(top_level_dir)
+        / Path(strategy_name)
+        / Path(module_name + ".py")
+    )
+    print(strategy_location)
+    spec = importlib.util.spec_from_file_location(module_name, strategy_location)
+    strategy = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(strategy)
+    return strategy
+
+
+def print_full(x):
+    """
+    Print every row of list-like object.
+    """
+    pd.set_option("display.max_rows", len(x))
+    print(x)
+    pd.reset_option("display.max_rows")
+
+
+def read_config():
+    """
+    Read pinkfish configuration.
+    """
+    conf = {}
+    parser = ConfigParser()
+    parser.read(Path("~/.pinkfish").expanduser())
+    conf["base_dir"] = parser.get("global", "base_dir")
+    return conf
+
+
+def is_last_row(ts, index):
+    """
+    Return True for last row, False otherwise.
+    """
+    return True if (index == len(ts) - 1) else False
+
+
+def get_previous_row(ts, bars=1):
+    """
+    Returns the row from 'bars' bars ago (default: previous row).
+    """
+    if bars < 1 or bars >= len(ts):
+        return None
+    return ts.iloc[-bars]
+
+
+def sort_dict(d, reverse=False):
+    """
+    Return sorted dict; optionally reverse sort.
+    """
+    return dict(sorted(d.items(), key=lambda x: x[1], reverse=reverse))
+
+
+def set_dict_values(d, value):
+    """
+    Return dict with same keys as `d` and all values equal to `value'.
+    """
+    return dict.fromkeys(d, value)
+
+
+def find_nan_rows(ts):
+    """
+    Return a dataframe with the rows that contain NaN values.
+
+    This function can help you track down problems with a timeseries.
+    You may need to call `pd.set_option("display.max_columns", None)`
+    at the top of your notebook to display all columns.
+
+    Examples
+    --------
+    >>> pd.set_option("display.max_columns", None)
+    >>> df = pf.find_nan_rows(ts)
+    >>> df
+    """
+    is_NaN = ts.isnull()
+    row_has_NaN = is_NaN.any(axis=1)
+    rows_with_NaN = ts[row_has_NaN]
+    return rows_with_NaN
+
+
+def no_empty_container(container_name, default_ret_value):
+    """
+    Check if container is empty.  If so, return `default_ret_value`.
+
+    Parameters
+    ----------
+    container_name : str
+        The name of the container parameter to check.
+    default_ret_value : int
+        The return value the wrapped function if the container is empty.
+        (default is 'examples').
+    module_name: str, optional
+        The name of the python module (default is 'strategy').
+
+    Returns
+    -------
+    `default_ret_value` or `func` return value : type of return value
+        If the container is empty, `default_ret_value` is returned,
+        otherwise the return value of `func`.
+
+    Examples
+    --------
+    >>> @no_empty_container('my_list', 0)
+    >>> def my_func(my_list):
+    >>>     return 5
+    >>> my_func([])
+    0
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                container_obj = kwargs[container_name]
+            except KeyError:
+                container_arg_index = inspect.getfullargspec(func).args.index(
+                    container_name
+                )
+                container_obj = args[container_arg_index]
+
+            if len(container_obj) == 0:
+                return default_ret_value
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
