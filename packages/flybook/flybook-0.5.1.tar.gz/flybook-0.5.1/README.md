1d3bf7cd3bc7d84745835f96334f093ea800f5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 07:07:52 +0000

    Add package flybook/0.5.1/flybook-0.5.1.tar.gz

---
+++ packages/flybook/flybook-0.5.1.tar.gz/flybook-0.5.1/README.md
@@ -0,0 +1,13 @@
+# flybook
+
+A Python SDK for feishu API
+
+## Installation
+
+```bash
+pip install flybook
+```
+
+## Usage
+
+查看 test 目录下的测试文件获取使用示例
---
+++ packages/flybook/flybook-0.5.1.tar.gz/flybook-0.5.1/flybook/message/__init__.py
@@ -0,0 +1,4 @@
+from .prototype import Message
+from .text import TextMessage
+from .card import CardMessage, TemplateCardMessage, SimpleCardMessage
+from .markdown import MarkdownMessage
