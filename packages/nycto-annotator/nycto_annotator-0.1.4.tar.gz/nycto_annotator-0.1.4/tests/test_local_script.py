ff632e32756fa1a78a5b4668cbf40564db8be3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:07:06 +0000

    Add package nycto-annotator/0.1.4/nycto_annotator-0.1.4.tar.gz

---
+++ packages/nycto-annotator/nycto_annotator-0.1.4.tar.gz/nycto_annotator-0.1.4/tests/test_local_script.py
@@ -0,0 +1,14 @@
+# from auto_annotator.auto_annotate import AutoAnnotator
+
+# def test_auto_annotate():
+#     input_folder = "/mnt/nvme1/niharika/Test/Python-Package/tests/input"
+#     output_folder = "/mnt/nvme1/niharika/Test/Python-Package/tests/output"
+#     model_name = "grounding_dino"
+#     classes_prompt = {"person": "person", "car": "car"}
+#     annotator = AutoAnnotator(model_name)
+#     annotator.run(input_folder, output_folder, classes_prompt)
+
+# if __name__ == "__main__":
+#     test_auto_annotate()
+    
+
