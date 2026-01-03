72c7d39558b6af149906e5530e8a9d2287f50b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 07:07:39 +0000

    Add package GenomicRanges/0.8.0/genomicranges-0.8.0.tar.gz

---
+++ packages/GenomicRanges/genomicranges-0.8.0.tar.gz/genomicranges-0.8.0/tests/test_grl_initialize.py
@@ -0,0 +1,37 @@
+import pytest
+from biocframe import BiocFrame
+from iranges import IRanges
+
+from genomicranges import CompressedGenomicRangesList, GenomicRanges
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+a = GenomicRanges(
+    seqnames=["chr1", "chr2", "chr1", "chr3"],
+    ranges=IRanges([1, 3, 2, 4], [10, 30, 50, 60]),
+    strand=["-", "+", "*", "+"],
+    mcols=BiocFrame({"score": [1, 2, 3, 4]}),
+)
+
+b = GenomicRanges(
+    seqnames=["chr2", "chr4", "chr5"],
+    ranges=IRanges([3, 6, 4], [30, 50, 60]),
+    strand=["-", "+", "*"],
+    mcols=BiocFrame({"score": [2, 3, 4]}),
+)
+
+
+def test_create_grl():
+    grl = CompressedGenomicRangesList.from_list(lst=[a, b], names=["a", "b"])
+
+    assert isinstance(grl, CompressedGenomicRangesList)
+    assert isinstance(grl["a"], GenomicRanges)
+    assert grl["b"] is not None
+    assert len(grl) == 2
+
+
+def test_create_grl_should_fail():
+    with pytest.raises(Exception):
+        CompressedGenomicRangesList.from_list(lst=[a, 2])
