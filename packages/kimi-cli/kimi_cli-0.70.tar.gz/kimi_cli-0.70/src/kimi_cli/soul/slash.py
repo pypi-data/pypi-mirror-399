3d9edb777373af6c90c340f21ea3e3881c8f7ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:06 +0000

    Add package kimi-cli/0.70/kimi_cli-0.70.tar.gz

---
+++ packages/kimi-cli/kimi_cli-0.70.tar.gz/kimi_cli-0.70/src/kimi_cli/soul/slash.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import tempfile
+from collections.abc import Awaitable, Callable
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from kosong.message import Message, TextPart
+from loguru import logger
+
+import kimi_cli.prompts as prompts
+from kimi_cli.soul import wire_send
+from kimi_cli.soul.agent import load_agents_md
+from kimi_cli.soul.context import Context
+from kimi_cli.soul.message import system
+from kimi_cli.utils.slashcmd import SlashCommandRegistry
+
+if TYPE_CHECKING:
+    from kimi_cli.soul.kimisoul import KimiSoul
+
+type SoulSlashCmdFunc = Callable[[KimiSoul, list[str]], None | Awaitable[None]]
+"""
+A function that runs as a KimiSoul-level slash command.
+
+Raises:
+    Any exception that can be raised by `Soul.run`.
+"""
+
+registry = SlashCommandRegistry[SoulSlashCmdFunc]()
+
+
+@registry.command
+async def init(soul: KimiSoul, args: list[str]):
+    """Analyze the codebase and generate an `AGENTS.md` file"""
+    from kimi_cli.soul.kimisoul import KimiSoul
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        tmp_context = Context(file_backend=Path(temp_dir) / "context.jsonl")
+        tmp_soul = KimiSoul(soul.agent, context=tmp_context)
+        tmp_soul.set_thinking(soul.thinking)
+        await tmp_soul.run(prompts.INIT)
+
+    agents_md = load_agents_md(soul.runtime.builtin_args.KIMI_WORK_DIR)
+    system_message = system(
+        "The user just ran `/init` slash command. "
+        "The system has analyzed the codebase and generated an `AGENTS.md` file. "
+        f"Latest AGENTS.md file content:\n{agents_md}"
+    )
+    await soul.context.append_message(Message(role="user", content=[system_message]))
+
+
+@registry.command
+async def compact(soul: KimiSoul, args: list[str]):
+    """Compact the context"""
+    if soul.context.n_checkpoints == 0:
+        wire_send(TextPart(text="The context is empty."))
+        return
+
+    logger.info("Running `/compact`")
+    await soul.compact_context()
+    wire_send(TextPart(text="The context has been compacted."))
+
+
+@registry.command
+async def yolo(soul: KimiSoul, args: list[str]):
+    """Enable YOLO mode (auto approve all actions)"""
+    soul.runtime.approval.set_yolo(True)
+    wire_send(TextPart(text="You only live once! All actions will be auto-approved."))
