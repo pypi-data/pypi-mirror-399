92bb182012e5089e57635dbe5dde36b9bac6b6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:15:10 +0000

    Add package math-tols/1.0.8/math_tols-1.0.8.tar.gz

---
+++ packages/math-tols/math_tols-1.0.8.tar.gz/math_tols-1.0.8/math_tols/__init__.py
@@ -0,0 +1,29 @@
+#start libary v2
+
+
+
+def jam(x,y):
+	sum = (x+y)
+	return sum
+	
+def menha(x,y):
+	men = (x-y)
+	return men
+
+def zarb(x,y):
+	zar = (x*y)
+	return zar
+	
+def taghsim(x,y):
+	tag = (x/y)
+	return tag
+	
+	
+def tavan(x, y):
+    result = 1
+    for i in range(y):
+        result *= x
+    return result
+def jazr(x):
+	result = x ** 0.5
+	return result
