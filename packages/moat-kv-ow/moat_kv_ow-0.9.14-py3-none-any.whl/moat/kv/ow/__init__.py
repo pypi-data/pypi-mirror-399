2fe4a6b15a8103374505e2c2384100b0921e730f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:11 +0000

    Add package moat-kv-ow/0.9.14/moat_kv_ow-0.9.14-py3-none-any.whl

---
+++ packages/moat-kv-ow/moat_kv_ow-0.9.14-py3-none-any.whl/moat/kv/ow/__init__.py
@@ -0,0 +1,8 @@
+# noqa: D104
+from __future__ import annotations
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+from moat.lib.config import CfgStore
+
+CfgStore.with_(__name__)
