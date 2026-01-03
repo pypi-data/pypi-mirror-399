fcd8bfe2f905830423e23cb1691963651f610a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:59 +0000

    Add package MaaDebugger/1.15.0b1/maadebugger-1.15.0b1.tar.gz

---
+++ packages/MaaDebugger/maadebugger-1.15.0b1.tar.gz/maadebugger-1.15.0b1/src/MaaDebugger/maafw/__init__.py
@@ -0,0 +1,318 @@
+import re
+import io
+from pathlib import Path
+from typing import Callable, List, Optional, Tuple, Union
+
+from asyncify import asyncify
+from PIL import Image
+from maa.controller import AdbController, Win32Controller, CustomController
+from maa.context import Context, ContextEventSink
+from maa.tasker import Tasker, RecognitionDetail
+from maa.resource import Resource, ResourceEventSink
+from maa.toolkit import Toolkit, AdbDevice, DesktopWindow
+from maa.agent_client import AgentClient
+from maa.library import Library
+from maa.event_sink import NotificationType
+import numpy as np
+
+from ..utils.img_tools import cvmat_to_image, rgb_to_bgr
+
+
+class MyCustomController(CustomController):
+    def __init__(self, img_bytes: bytes):
+        super().__init__()
+
+        img = Image.open(io.BytesIO(img_bytes))
+        self.ndarray = rgb_to_bgr(np.array(img))
+
+    def connect(self) -> bool:
+        return True
+
+    def request_uuid(self) -> str:
+        return "0"
+
+    def screencap(self) -> np.ndarray:
+        return self.ndarray
+
+
+class MaaFW:
+
+    resource: Optional[Resource]
+    controller: Union[AdbController, Win32Controller, CustomController, None]
+    tasker: Optional[Tasker]
+    agent: Optional[AgentClient]
+    context_event_sink: Optional[ContextEventSink]
+    resource_event_sink: Optional[ResourceEventSink]
+
+    def __init__(self):
+        Toolkit.init_option("./")
+        Tasker.set_debug_mode(True)
+
+        self.resource = None
+        self.controller = None
+        self.tasker = None
+        self.agent = None
+
+        self.screenshotter = Screenshotter(self.screencap)
+
+    @property
+    def version(self) -> str:
+        return Library.version()
+
+    @staticmethod
+    @asyncify
+    def detect_adb() -> List[AdbDevice]:
+        return Toolkit.find_adb_devices()
+
+    @staticmethod
+    @asyncify
+    def detect_win32hwnd(window_regex: str) -> List[DesktopWindow]:
+        windows = Toolkit.find_desktop_windows()
+        result = []
+        for win in windows:
+            if re.search(window_regex, win.window_name):
+                result.append(win)
+
+        return result
+
+    @asyncify
+    def connect_adb(
+        self, path: Path, address: str, config: dict
+    ) -> Tuple[bool, Optional[str]]:
+        self.controller = AdbController(path, address, config=config)
+        connected = self.controller.post_connection().wait().succeeded
+        if not connected:
+            return False, f"Failed to connect {path} {address}"
+
+        return True, None
+
+    @asyncify
+    def connect_win32hwnd(
+        self,
+        hwnd: str,
+        screencap_method: int,
+        mouse_method: int,
+        keyboard_method: int,
+    ) -> Tuple[bool, Optional[str]]:
+        _hwnd = int(hwnd, 16)
+
+        self.controller = Win32Controller(
+            _hwnd,
+            screencap_method=screencap_method,
+            mouse_method=mouse_method,
+            keyboard_method=keyboard_method,
+        )
+        connected = self.controller.post_connection().wait().succeeded
+        if not connected:
+            return False, f"Failed to connect {hex(_hwnd)}"
+
+        return True, None
+
+    def connect_custom_controller(self, img_bytes) -> Tuple[bool, Optional[str]]:
+        self.controller = MyCustomController(img_bytes)
+        self.controller.post_connection().wait()
+
+        return True, None
+
+    @asyncify
+    def load_resource(self, dir: List[Path]) -> Tuple[bool, Optional[str]]:
+        if not self.resource:
+            self.resource = Resource()
+            self.resource.add_sink(self.resource_event_sink)  # type:ignore
+
+        self.resource.clear()
+        for d in dir:
+            if not d.exists():
+                return False, f"{d} does not exist."
+
+            status = self.resource.post_bundle(d).wait().succeeded
+            if not status:
+                return (
+                    False,
+                    "Fail to load resource, please check the outputs of CLI.",
+                )
+        return True, None
+
+    @asyncify
+    def create_agent(self, identifier: str) -> Optional[str]:
+        if not self.resource:
+            self.resource = Resource()
+
+        self.agent = AgentClient(identifier)
+        self.agent.bind(self.resource)
+        self.agent.set_timeout(3000)
+
+        return self.agent.identifier
+
+    @asyncify
+    def connect_agent(self) -> Tuple[bool, Optional[str]]:
+        if self.agent:
+            if self.agent.connect():
+                return True, None
+            else:
+                return False, "Failed to connect AgentClient."
+        else:
+            return False, "Internal Error: AgentClient is None."
+
+    @asyncify
+    def run_task(
+        self, entry: str, pipeline_override: dict = {}
+    ) -> Tuple[bool, Optional[str]]:
+        if not self.context_event_sink:
+            assert ValueError("EventSink is None.")
+
+        if not self.tasker:
+            self.tasker = Tasker()
+            self.tasker.add_context_sink(self.context_event_sink)
+
+        if not self.resource:
+            return False, "Resource is not initialized."
+
+        if not self.controller:
+            return False, "Controller is not initialized."
+
+        self.tasker.bind(self.resource, self.controller)
+        if not self.tasker.inited:
+            return False, "Failed to initialize Tasker."
+
+        if self.agent:
+            if not AgentClient(self.agent.identifier).register_sink(
+                self.resource, self.controller, self.tasker
+            ):
+                return False, "Failed to register AgentClientSink."
+
+        if isinstance(self.controller, CustomController):
+            # disable action
+            pipeline_override.update(
+                {entry: {"action": {"type": "DoNothing"}, "next": []}}
+            )
+            return (
+                self.tasker.post_task(entry, pipeline_override).wait().succeeded,
+                None,
+            )
+        else:
+            return (
+                self.tasker.post_task(entry, pipeline_override).wait().succeeded,
+                None,
+            )
+
+    @asyncify
+    def stop_task(self) -> None:
+        if not self.tasker:
+            return
+
+        self.tasker.post_stop().wait()
+
+    @asyncify
+    def screencap(self, capture: bool = True) -> Optional[Image.Image]:
+        if not self.controller:
+            return None
+
+        if capture:
+            self.controller.post_screencap().wait()
+        im = self.controller.cached_image
+        if im is None:
+            return None
+
+        return cvmat_to_image(im)
+
+    @asyncify
+    def click(self, x, y) -> bool:
+        if not self.controller:
+            return False
+
+        if isinstance(self.controller, CustomController):
+            return False
+
+        return self.controller.post_click(x, y).wait().succeeded
+
+    @asyncify
+    def get_reco_detail(self, reco_id: int) -> Optional[RecognitionDetail]:
+        if not self.tasker:
+            return None
+
+        return self.tasker.get_recognition_detail(reco_id)
+
+    @asyncify
+    def clear_cache(self) -> bool:
+        if not self.tasker:
+            return False
+
+        return self.tasker.clear_cache()
+
+    @asyncify
+    def get_node_list(self) -> list[str]:
+        if self.resource:
+            return self.resource.node_list
+        else:
+            return []
+
+    # @asyncify
+    def get_node_data(self, name: str) -> dict:
+        if self.resource:
+            return self.resource.get_node_data(name) or {}
+        else:
+            return {}
+
+
+class MyContextEventSink(ContextEventSink):
+    def __init__(
+        self,
+        on_next_list_starting: Optional[Callable],
+        on_recognized: Optional[Callable],
+    ) -> None:
+        self.on_next_list_starting = on_next_list_starting
+        self.on_recognized = on_recognized
+
+    def on_node_next_list(
+        self,
+        _: Context,
+        noti_type: NotificationType,
+        detail: ContextEventSink.NodeNextListDetail,
+    ):
+        if noti_type != NotificationType.Starting:
+            return
+
+        if self.on_next_list_starting is not None:
+            self.on_next_list_starting(detail.name, detail.next_list)
+
+    def on_node_recognition(
+        self,
+        _: Context,
+        noti_type: NotificationType,
+        detail: ContextEventSink.NodeRecognitionDetail,
+    ):
+        if (
+            noti_type != NotificationType.Succeeded
+            and noti_type != NotificationType.Failed
+        ):
+            return
+
+        if self.on_recognized is not None:
+            self.on_recognized(
+                detail.reco_id, detail.name, noti_type == NotificationType.Succeeded
+            )
+
+
+class MyResourceEventSink(ResourceEventSink):
+    def __init__(self, on_resource_loading: Callable) -> None:
+        self.on_resource_loading = on_resource_loading
+
+
+class Screenshotter:
+    source: Optional[Image.Image] = None
+    screencap_func: Callable
+
+    def __init__(self, screencap_func: Callable):
+        self.screencap_func = screencap_func
+
+    def __del__(self):
+        self.source = None
+
+    async def refresh(self, capture: bool = True):
+        im: Image.Image = await self.screencap_func(capture)
+        if im is not None:
+            self.source = im
+
+
+maafw = MaaFW()
