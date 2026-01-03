6a4e3bc6a7dbbb0bb9c5bc55199d8c3259e66708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:05:56 +0000

    Add package autogen-kafka/0.1.2/autogen_kafka-0.1.2.tar.gz

---
+++ packages/autogen-kafka/autogen_kafka-0.1.2.tar.gz/autogen_kafka-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autogen-kafka",
+    version="0.1.2",
+    packages=find_packages(),
+    author="sjnscythe",
+    author_email="scytheabhi97@gmail.com",
+    description="A poc by anupamas",
+    python_requires='>=3.6',
+)
