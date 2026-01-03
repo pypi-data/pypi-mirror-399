b0fcbe7856a22d902d4924a15b88b537b4babb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:42 +0000

    Add package pyorcasdk/1.1.0/pyorcasdk-1.1.0-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/pyorcasdk/pyorcasdk-1.1.0-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/lib64/cmake/orcaSDK/orcaSDKTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "orcaSDK::core" for configuration "Release"
+set_property(TARGET orcaSDK::core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(orcaSDK::core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/liborcaSDK_core.a"
+  )
+
+list(APPEND _cmake_import_check_targets orcaSDK::core )
+list(APPEND _cmake_import_check_files_for_orcaSDK::core "${_IMPORT_PREFIX}/lib64/liborcaSDK_core.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
