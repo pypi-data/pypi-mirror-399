03a492f6c51493919d66926be63800bab953f709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:45 +0000

    Add package SHARPlib/1.2.1.post1/sharplib-1.2.1.post1-cp310-cp310-win_amd64.whl

---
+++ packages/SHARPlib/sharplib-1.2.1.post1-cp310-cp310-win_amd64.whl/lib/cmake/SHARPlib/SHARPlibTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "SHARPlib::SHARPlib" for configuration "Release"
+set_property(TARGET SHARPlib::SHARPlib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(SHARPlib::SHARPlib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/SHARPlib.lib"
+  )
+
+list(APPEND _cmake_import_check_targets SHARPlib::SHARPlib )
+list(APPEND _cmake_import_check_files_for_SHARPlib::SHARPlib "${_IMPORT_PREFIX}/lib/SHARPlib.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
