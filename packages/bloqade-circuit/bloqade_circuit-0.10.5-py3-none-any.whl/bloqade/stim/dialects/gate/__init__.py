eaef548abd81dd04d7d09312915e69474485461a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:49 +0000

    Add package bloqade-circuit/0.10.5/bloqade_circuit-0.10.5-py3-none-any.whl

---
+++ packages/bloqade-circuit/bloqade_circuit-0.10.5-py3-none-any.whl/bloqade/stim/dialects/gate/__init__.py
@@ -0,0 +1,18 @@
+from .emit import EmitStimGateMethods as EmitStimGateMethods
+from .stmts import (
+    CX as CX,
+    CY as CY,
+    CZ as CZ,
+    SPP as SPP,
+    H as H,
+    S as S,
+    X as X,
+    Y as Y,
+    Z as Z,
+    Swap as Swap,
+    SqrtX as SqrtX,
+    SqrtY as SqrtY,
+    SqrtZ as SqrtZ,
+    Identity as Identity,
+)
+from ._dialect import dialect as dialect
