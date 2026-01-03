2aa8c956bc78785f54f20528ca5a4b99589a0a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:47 +0000

    Add package pyorcasdk/1.1.0/pyorcasdk-1.1.0-cp314-cp314-musllinux_1_2_aarch64.whl

---
+++ packages/pyorcasdk/pyorcasdk-1.1.0-cp314-cp314-musllinux_1_2_aarch64.whl/include/src/orca_modes.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace orcaSDK
+{
+
+typedef enum {
+
+	SleepMode = 1,
+	ForceMode = 2,
+	PositionMode = 3,
+	HapticMode = 4,
+	KinematicMode = 5,
+	AutoZeroMode = 55
+
+} MotorMode;
+
+}
