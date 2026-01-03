123f04346c26d651c2c8063f72d8256c05745a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 15:07:30 +0000

    Add package picassosr/0.9.3/picassosr-0.9.3.tar.gz

---
+++ packages/picassosr/picassosr-0.9.3.tar.gz/picassosr-0.9.3/picasso/__init__.py
@@ -0,0 +1,23 @@
+"""
+picasso.__init__.py
+~~~~~~~~~~~~~~~~~~~
+
+:authors: Joerg Schnitzbauer, Maximilian Thomas Strauss,
+    Rafal Kowalewski 2016-2025
+:copyright: Copyright (c) 2016-2025 Jungmann Lab, MPI of
+    Biochemistry
+"""
+
+import os.path
+import yaml
+from .version import __version__
+
+_this_file = os.path.abspath(__file__)
+_this_dir = os.path.dirname(_this_file)
+try:
+    with open(os.path.join(_this_dir, "config.yaml"), "r") as config_file:
+        CONFIG = yaml.full_load(config_file)
+    if CONFIG is None:
+        CONFIG = {}
+except FileNotFoundError:
+    CONFIG = {}
