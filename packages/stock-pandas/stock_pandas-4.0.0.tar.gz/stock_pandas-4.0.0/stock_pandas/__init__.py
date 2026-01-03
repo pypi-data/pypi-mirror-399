11a1bd523989c9459ae197d41e22d13758d4f847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:13 +0000

    Add package stock-pandas/4.0.0/stock_pandas-4.0.0.tar.gz

---
+++ packages/stock-pandas/stock_pandas-4.0.0.tar.gz/stock_pandas-4.0.0/stock_pandas/__init__.py
@@ -0,0 +1,29 @@
+from .dataframe import StockDataFrame
+
+from .exceptions import (
+    DirectiveSyntaxError,
+    DirectiveValueError
+)
+
+from .directive.command import (
+    CommandPreset,
+    CommandArgType,
+    CommandArg,
+    CommandDefinition
+)
+from .directive.types import ReturnType
+from .directive.cache import DirectiveCache
+
+from .meta.time_frame import (
+    TimeFrame,
+    TimeFrameArg
+)
+
+from .meta.cumulator import (
+    Cumulator,
+    Cumulators,
+    cumulators,
+    SubjectsToAppend
+)
+
+__version__ = '4.0.0'
