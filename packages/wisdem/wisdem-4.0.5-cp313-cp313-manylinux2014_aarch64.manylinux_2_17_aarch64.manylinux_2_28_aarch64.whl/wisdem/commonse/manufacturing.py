f70ee349462047a07d5f4590fac95e2ec23e3cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:08:35 +0000

    Add package wisdem/4.0.5/wisdem-4.0.5-cp313-cp313-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/wisdem/wisdem-4.0.5-cp313-cp313-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/wisdem/commonse/manufacturing.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+from wisdem.commonse import eps
+
+# Compute costs based on "Optimum Design of Steel Structures" by Farkas and Jarmai
+# All correlations are based in mm and all inputs are assumed to be in m and then converted within the functions
+
+
+def steel_cutting_plasma_time(length, thickness):
+    # Length input as meters, thickness in mm
+    # time = length / (-0.180150943 + 41.03815215/(1e3*thickness+eps)) # minutes
+    # Better conditioned polynomial fit to the above correlation
+    pp = np.array([2.44908121e02, 1.74461814e01, 7.05214799e-02])
+    time = length * np.polyval(pp, thickness)
+    return np.sum(time)
+
+
+def steel_rolling_time(theta, radius, thickness):
+    # Radius and thickness input as meters, converted to mm
+    time = theta * np.exp(
+        6.8582513 - 4.527217 / np.sqrt(1e3 * thickness + eps) + 0.009541996 * np.sqrt(2 * 1e3 * radius)
+    )
+    return np.sum(time)
+
+
+def steel_tube_cutgrind_time(theta, radius, thickness, angle):
+    # Radius and thickness input as meters, converted to mm
+    # time = theta * 2.5 * np.pi * (2.0*1e3*radius) / ((350.0 - 2.0*1e3*thickness)*0.3*np.sin(angle))
+    # Better conditioned polynomial fit to the above correlation
+    pp = np.array([7.84235859, 0.1428632, 0.07765389])
+    time = theta * (2.0 * 1e3 * radius) / (np.polyval(pp, thickness) * np.sin(angle))
+    return np.sum(time)
+
+
+def steel_welding_time(theta, npieces, mtotal, length, thickness, coeff):
+    # Length input as meters, thickness as mm
+    # Tw1
+    time = np.sum(theta * np.sqrt(npieces * mtotal))
+    # Tw2+Tw3
+    time += np.sum(1.3e-3 * coeff * (length) * (1e3 * thickness) ** 1.9358)
+    return time
+
+
+def steel_butt_welding_time(theta, npieces, mtotal, length, thickness):
+    # FCAW-MC Metal Cored Arc Welding
+    return steel_welding_time(theta, npieces, mtotal, length, thickness, 0.152)
+
+
+def steel_fillet_welding_time(theta, npieces, mtotal, length, thickness):
+    # Tw1
+    time = np.sum(theta * np.sqrt(npieces * mtotal))
+    # Tw2+Tw3
+    # coeff 0.2349 for SAW fillet weld
+    coeff = 0.2349
+    # SMAW fillet welds of size aw = 0.5t
+    aw = 0.5*thickness
+    time += np.sum(1.3e-3 * coeff * (length) * (1e3 * aw) ** 2)
+    return time
+
+
+def steel_tube_welding_time(theta, npieces, mtotal, length, thickness):
+    return steel_welding_time(theta, npieces, mtotal, length, thickness, 0.7889)
