7d9add707e293df8c2ef17b010d5701ad57f5c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:47 +0000

    Add package seemps/2.9.6/seemps-2.9.6-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/seemps/seemps-2.9.6-cp313-cp313-musllinux_1_2_x86_64.whl/seemps/register/__init__.py
@@ -0,0 +1,4 @@
+from .qubo import qubo_exponential_mpo, qubo_mpo
+from .transforms import twoscomplement, mpo_weighted_shifts
+
+__all__ = ["qubo_exponential_mpo", "qubo_mpo", "twoscomplement", "mpo_weighted_shifts"]
