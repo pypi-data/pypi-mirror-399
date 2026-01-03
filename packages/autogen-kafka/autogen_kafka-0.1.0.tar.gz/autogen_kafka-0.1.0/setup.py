ae3b334e63754a0019340c2531d815fa8af92690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:17 +0000

    Add package autogen-kafka/0.1.0/autogen_kafka-0.1.0.tar.gz

---
+++ packages/autogen-kafka/autogen_kafka-0.1.0.tar.gz/autogen_kafka-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autogen-kafka",
+    version="0.1.0",
+    packages=find_packages(),
+    author="sjnscythe",
+    author_email="scytheabhi97@gmail.com",
+    description="A poc for ledgerhq",
+    python_requires='>=3.6',
+)
