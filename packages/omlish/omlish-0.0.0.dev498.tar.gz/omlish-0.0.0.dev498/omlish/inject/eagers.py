b5d9c743cc3291867c775f2f6eda3dd15887f69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:55 +0000

    Add package omlish/0.0.0.dev498/omlish-0.0.0.dev498.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev498.tar.gz/omlish-0.0.0.dev498/omlish/inject/eagers.py
@@ -0,0 +1,20 @@
+"""
+TODO:
+ - SCOPED - eagers for EACH SCOPE
+"""
+from .. import check
+from .. import dataclasses as dc
+from .. import lang
+from .elements import Element
+from .keys import Key
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+@dc.extra_class_params(cache_hash=True)
+class Eager(Element, lang.Final):
+    key: Key = dc.xfield(coerce=check.of_isinstance(Key))
+
+    priority: int = dc.xfield(0, kw_only=True)
---
+++ packages/omlish/omlish-0.0.0.dev498.tar.gz/omlish-0.0.0.dev498/omlish/typedvalues/generic.py
@@ -0,0 +1,27 @@
+import typing as ta
+
+from .. import check
+from .. import lang
+from .. import reflect as rfl
+from .values import TypedValue  # noqa
+
+
+TypedValueT = ta.TypeVar('TypedValueT', bound='TypedValue')
+
+
+##
+
+
+class TypedValueGeneric(lang.Abstract, ta.Generic[TypedValueT]):
+    _typed_value_type: ta.ClassVar[rfl.Type]
+
+    def __init_subclass__(cls, **kwargs: ta.Any) -> None:
+        super().__init_subclass__(**kwargs)
+
+        if '_typed_value_type' in cls.__dict__:
+            return
+
+        g_mro = rfl.ALIAS_UPDATING_GENERIC_SUBSTITUTION.generic_mro(cls)
+        g_tvg = check.single(gb for gb in g_mro if isinstance(gb, rfl.Generic) and gb.cls is TypedValueGeneric)
+        tvt = check.single(g_tvg.args)
+        cls._typed_value_type = tvt
