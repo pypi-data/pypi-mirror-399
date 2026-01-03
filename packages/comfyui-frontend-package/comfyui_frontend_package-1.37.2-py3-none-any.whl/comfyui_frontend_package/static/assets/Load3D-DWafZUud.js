933869f2a8fab681845bd50caebca8b424c000a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:27 +0000

    Add package comfyui-frontend-package/1.37.2/comfyui_frontend_package-1.37.2-py3-none-any.whl

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.37.2-py3-none-any.whl/comfyui_frontend_package/static/assets/Load3D-DWafZUud.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./Load3D.vue_vue_type_script_setup_true_lang-_XswHXHt.js";import"./vendor-other-BxP-0xn6.js";import"./vendor-primevue-rR0TB_Js.js";import"./index-BODInyrK.js";import"./vendor-vue-BEnTqVKr.js";import"./vendor-xterm-CWYFmgbN.js";import"./vendor-three-BsnVSA6y.js";import"./vendor-tiptap-4vvRsqpM.js";export{o as default};
+//# sourceMappingURL=Load3D-DWafZUud.js.map
