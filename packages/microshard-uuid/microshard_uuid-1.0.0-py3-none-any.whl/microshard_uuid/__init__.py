8075c7804b974ff0c7260c71db4d2f12839b1b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:06:03 +0000

    Add package microshard-uuid/1.0.0/microshard_uuid-1.0.0-py3-none-any.whl

---
+++ packages/microshard-uuid/microshard_uuid-1.0.0-py3-none-any.whl/microshard_uuid/__init__.py
@@ -0,0 +1,15 @@
+from .core import (
+    Generator,
+    MicroShardUUID,
+    from_timestamp,
+    generate,
+)
+
+__all__ = [
+    "Generator",
+    "MicroShardUUID",
+    "from_timestamp",
+    "generate",
+]
+
+__version__ = "1.0.0"
