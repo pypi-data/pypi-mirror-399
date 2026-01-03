df308f6c98238601a293b13ae6002352c533d04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:35:53 +0000

    Add package credsweeper/1.14.2/credsweeper-1.14.2.tar.gz

---
+++ packages/credsweeper/credsweeper-1.14.2.tar.gz/credsweeper-1.14.2/credsweeper/scanner/scan_type/single_pattern.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from credsweeper.config.config import Config
+from credsweeper.credentials.candidate import Candidate
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.rules.rule import Rule
+from credsweeper.scanner.scan_type.scan_type import ScanType
+
+
+class SinglePattern(ScanType):
+    """Check if single line rule present in the line."""
+
+    @classmethod
+    def run(cls, config: Config, rule: Rule, target: AnalysisTarget) -> List[Candidate]:
+        """Check if regex pattern defined in a rule is present in a line.
+
+        Args:
+            config: config object of user configs
+            rule: Rule object to check current line
+            target: Analysis target
+
+        Return:
+            List of Candidate objects if pattern defined in a rule is present in a line
+            and filters defined in rule do not remove current line. Empty list - otherwise
+
+        """
+
+        return cls._get_candidates(config, rule, target)
