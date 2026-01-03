c751e0cb77d5996a869cf0d4fbe22982ccc9bfe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:06:00 +0000

    Add package total-connect-client/2025.12.1/total_connect_client-2025.12.1.tar.gz

---
+++ packages/total-connect-client/total_connect_client-2025.12.1.tar.gz/total_connect_client-2025.12.1/tests/test_device.py
@@ -0,0 +1,17 @@
+"""Test TotalConnectDevice."""
+
+from const import REST_RESULT_SESSION_DETAILS, SECURITY_DEVICE_ID
+
+from total_connect_client.device import TotalConnectDevice
+
+device_list = REST_RESULT_SESSION_DETAILS["SessionDetailsResult"]["Locations"][0]["DeviceList"]
+# [0] is a ProA7 panel
+# [1] is the built-in camera
+# [2] is a Skybell doorbell
+
+
+def tests_panel():
+    """Test an alarm panel."""
+    panel = TotalConnectDevice(device_list[0])
+    assert panel.deviceid == SECURITY_DEVICE_ID
+    assert panel.is_doorbell() is False
