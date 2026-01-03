ed9d595e1aeceaacaa4185ab653060fb8e859165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:49 +0000

    Add package dscience-tools/2.3.2/dscience_tools-2.3.2-py3-none-any.whl

---
+++ packages/dscience-tools/dscience_tools-2.3.2-py3-none-any.whl/ds_tools/__init__.py
@@ -0,0 +1,34 @@
+# src/ds_tools/__init__.py
+"""
+/*
+ * Copyright (c) [2025] [Sergii Kavun]
+ *
+ * This software is dual-licensed:
+ * - PolyForm Noncommercial 1.0.0 (default)
+ * - Commercial license available
+ *
+ * See LICENSE for details
+ */
+
+DSTools: A library of helpful functions for various data science research stages.
+"""
+
+__version__ = "2.3.2"
+
+from .ds_tool import DSTools
+from .models import (
+    CorrelationConfig,
+    DistributionConfig,
+    GrubbsTestResult,
+    MetricsConfig,
+    OutlierConfig,
+)
+
+__all__ = [
+    "DSTools",
+    "MetricsConfig",
+    "CorrelationConfig",
+    "OutlierConfig",
+    "DistributionConfig",
+    "GrubbsTestResult",
+]
