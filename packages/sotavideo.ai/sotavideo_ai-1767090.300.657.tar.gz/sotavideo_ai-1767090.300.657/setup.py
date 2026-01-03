a1a332e4dbc3a9aea566c6a5dcecfd2503b71105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:16 +0000

    Add package sotavideo.ai/1767090.300.657/sotavideo_ai-1767090.300.657.tar.gz

---
+++ packages/sotavideo.ai/sotavideo_ai-1767090.300.657.tar.gz/sotavideo_ai-1767090.300.657/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sotavideo.ai",
+    version="1767090.300.657",
+    description="High-quality integration for https://sotavideo.ai/",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="SuperMaker",
+    url="https://sotavideo.ai/",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
