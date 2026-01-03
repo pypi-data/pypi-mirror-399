2a4f1b744e4ef93e0a89efbd060982795be1199d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:35:58 +0000

    Add package endoreg-db/0.8.9.16/endoreg_db-0.8.9.16-py3-none-any.whl

---
+++ packages/endoreg-db/endoreg_db-0.8.9.16-py3-none-any.whl/endoreg_db/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+
+
+class EndoregDbConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "endoreg_db"
+
+    def ready(self):
+        """
+        Performs application startup tasks when the Django app is fully loaded.
+
+        This method imports media-related model modules to ensure they are registered
+        and ready for use when the application starts.
+        """
+        pass
