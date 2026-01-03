9d8bfc562751c020da0981afc5319b64e348cad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 15:06:54 +0000

    Add package easypygamewidgets/0.5.1/easypygamewidgets-0.5.1.tar.gz

---
+++ packages/easypygamewidgets/easypygamewidgets-0.5.1.tar.gz/easypygamewidgets-0.5.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easypygamewidgets",
+    version="0.5.1",
+    packages=find_packages(),
+    install_requires=[
+        "pygame",
+        "requests"
+    ],
+    author="PizzaPost",
+    description="Create GUIs for pygame.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PizzaPost/pywidgets ",
+)
