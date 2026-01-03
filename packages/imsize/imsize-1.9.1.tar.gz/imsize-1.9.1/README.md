d97a2426e406ab7373f8165aa85635ca0f052c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:56 +0000

    Add package imsize/1.9.1/imsize-1.9.1.tar.gz

---
+++ packages/imsize/imsize-1.9.1.tar.gz/imsize-1.9.1/README.md
@@ -0,0 +1,32 @@
+# imsize
+
+[![Build Status](https://travis-ci.com/toaarnio/imsize.svg?branch=master)](https://travis-ci.com/github/toaarnio/imsize)
+
+Lightning-fast extraction of image dimensions & bit depth. Tested on Python 3.8+ and Ubuntu 22.04.
+
+Supports PGM / PPM / PNM / PFM / PNG / BMP / JPG / TIFF / INSP / EXR / HDR / DNG / CR2 / NEF / RAW / NPY.
+
+**Installing on Linux:**
+```
+pip install imsize
+```
+
+**Documentation:**
+```
+pydoc imsize
+```
+
+**Command-line usage:**
+```
+imsize --help
+```
+
+**Building & installing from source:**
+```
+make install
+```
+
+**Building & releasing to PyPI:**
+```
+make release
+```
---
+++ packages/imsize/imsize-1.9.1.tar.gz/imsize-1.9.1/imsize/consoleapp.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3 -B
+
+"""
+Calculates the combined in-memory size of the given images by parsing
+their header information. Runs very fast because only the header part
+of a file is read from disk (except for headerless Camera RAW and Nikon
+NEF).
+"""
+
+import os                 # built-in library
+import sys                # built-in library
+import glob               # built-in library
+from pathlib import Path  # built-in library
+
+import imsize             # pip install imsize
+
+try:
+    # package mode
+    from imsize import argv
+except ImportError:
+    # stand-alone mode
+    import argv
+
+
+def main():
+    """
+    Entry point for the 'imsize' command-line application.
+    """
+    show_all = argv.exists("--all")
+    verbose = not argv.exists("--quiet")
+    show_help = argv.exists("--help")
+    argv.exitIfAnyUnparsedOptions()
+    if show_help:
+        print("Usage: imsize [options] [file-or-directory ...]")
+        print()
+        print("  Displays the dimensions and uncompressed sizes of the given images.")
+        print("  Runs very fast because only the header part of a file is read from")
+        print("  disk (except for headerless Camera RAW and Nikon NEF).")
+        print()
+        print("  options:")
+        print("    --all               show all extracted per-image metadata")
+        print("    --quiet             do not show per-image information")
+        print("    --help              show this help message")
+        print()
+        print("  example:")
+        print("    imsize ~/Pictures")
+        print()
+        print("  supported file types:")
+        print("   ", '\n    '.join(imsize.FILETYPES))
+        print()
+        print(f"imsize version {imsize.__version__}")
+        print()
+        sys.exit(-1)
+    else:
+        if verbose:
+            print("See 'imsize --help' for command-line options.")
+        paths = sys.argv[1:] or ["."]  # scan current directory if no arguments
+        filespecs = find_files(paths)
+        scan_sizes(filespecs, verbose, show_all)
+
+
+def find_files(paths):
+    """
+    Collects all files with known filetypes from the given list of directories.
+    """
+    allfiles = []
+    for path in paths:
+        if os.path.isdir(path):
+            for filetype in imsize.FILETYPES:
+                allfiles += glob.glob(os.path.join(path, filetype))
+                allfiles += glob.glob(os.path.join(path, filetype.upper()))
+        elif os.path.isfile(path):
+            allfiles += [path]
+    return allfiles
+
+
+def scan_sizes(filespecs, verbose, show_all):
+    """
+    Displays the dimensions of the given list of images.
+    """
+    total_compressed = 0
+    total_uncompressed = 0
+    num_processed = 0
+    max_namelen = 0
+    infos = []
+    for filespec in sorted(filespecs):
+        basename = os.path.basename(filespec)
+        try:
+            info = imsize.read(filespec)
+        except Exception as e:
+            print(f"{basename}: {type(e).__name__}: {e}")
+            continue
+        if info.width is None:
+            print(f"{basename}: Unable to guess dimensions. Maybe not an image? Skipping.")
+        else:
+            num_processed += 1
+            total_uncompressed += info.nbytes / 1024**2
+            total_compressed += info.filesize / 1024**2
+            max_namelen = max(max_namelen, len(basename))
+            infos.append(info)
+
+    if verbose:
+        maxw = max_namelen + 1
+        for info in infos:
+            basename = Path(info.filespec).name
+            megs = info.nbytes / 1024**2
+            mpix = info.width * info.height / 1000000
+            est = " [est.]" if info.uncertain else ""
+            packed = " [packed]" if info.packed_raw else ""
+            if info.rot90_ccw_steps in [0, 2]:
+                width, height = (info.width, info.height)
+            else:
+                width, height = (info.height, info.width)
+            stats = f"{width:>5} x {height:>5} x {info.nchan} x {info.bitdepth:>2} bits{packed:>9} => "
+            stats += f"{mpix:5.1f} MP, {megs:5.1f} MB{est}"
+            print(f"{basename:{maxw}.{maxw}}: {stats}")
+            if show_all:
+                print(info)
+
+    print(f"Scanned {num_processed} images, total {total_compressed:.1f} MB compressed, {total_uncompressed:.1f} MB uncompressed")
+
+
+if __name__ == "__main__":
+    main()
