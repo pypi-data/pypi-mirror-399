63b6127f9e5ec518ca37c7ab68d1e28ac762feb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:19 +0000

    Add package room-heater/0.0.1/room_heater-0.0.1.tar.gz

---
+++ packages/room-heater/room_heater-0.0.1.tar.gz/room_heater-0.0.1/src/room_heater/__init__.py
@@ -0,0 +1,24 @@
+__vesion__ = "0.0.1"
+
+import sys
+
+from .heater import SmartHeater
+
+def main():
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '--about':
+            print(__doc__)
+        elif sys.argv[1] == '--version':
+            print("Smart Room Heater 0.0.1")
+        elif sys.argv[1] == '--help':
+            print("Smart Room Heater")
+            print("Usage: python -m room_heater [option]")
+            print("  --about  : program information")
+            print("  --version: program version")
+            print("  --help   : help")
+            print("  (no option) : run room heater")
+    else:
+        heater = SmartHeater()
+        heater.run()
+
+__all__ = ["SmartHeater", "main", "__version__"]
