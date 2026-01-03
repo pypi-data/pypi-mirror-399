fb8f92f2d3522e2315274a9b962fbd15594dbc79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:30 +0000

    Add package tessng/4.1.0.2/tessng-4.1.0.2-cp36-cp36m-win_amd64.whl

---
+++ packages/tessng/tessng-4.1.0.2-cp36-cp36m-win_amd64.whl/tessng/__init__.py
@@ -0,0 +1,7 @@
+from PySide2.QtWidgets import QApplication
+from tessng.Tessng import *
+from tessng.Tessng import _Link, _Lane, _Connector, _LaneConnector, _DecisionPoint, _RoutingFLowRatio, _VehicleType
+
+
+print("\033[94m欢迎使用 TessNG Python 二次开发软件包！如需了解详细使用说明，请访问网址：http://jidatraffic.com:82/.\033[0m")
+print()
