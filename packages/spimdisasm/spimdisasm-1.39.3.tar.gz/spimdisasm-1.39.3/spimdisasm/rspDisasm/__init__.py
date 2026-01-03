8889c4926aa399188ee1f5a226d50a20f32585fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:06:08 +0000

    Add package spimdisasm/1.39.3/spimdisasm-1.39.3.tar.gz

---
+++ packages/spimdisasm/spimdisasm-1.39.3.tar.gz/spimdisasm-1.39.3/spimdisasm/rspDisasm/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022-2024 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+
+from .RspDisasmInternals import getToolDescription as getToolDescription
+from .RspDisasmInternals import addOptionsToParser as addOptionsToParser
+from .RspDisasmInternals import getArgsParser as getArgsParser
+from .RspDisasmInternals import applyArgs as applyArgs
+from .RspDisasmInternals import initializeContext as initializeContext
+from .RspDisasmInternals import processArguments as processArguments
+from .RspDisasmInternals import addSubparser as addSubparser
+from .RspDisasmInternals import rspDisasmMain as rspDisasmMain
