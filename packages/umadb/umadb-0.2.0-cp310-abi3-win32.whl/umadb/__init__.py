ef914159d4fa51fa87c5ee45d4403f3a02d1a2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:14:38 +0000

    Add package umadb/0.2.0/umadb-0.2.0-cp310-abi3-win32.whl

---
+++ packages/umadb/umadb-0.2.0-cp310-abi3-win32.whl/umadb/__init__.py
@@ -0,0 +1,34 @@
+"""
+UmaDB Python Client
+
+A Python client for UmaDB event store using Rust bindings via PyO3.
+"""
+
+
+from umadb._umadb import (
+    Client,
+    Event,
+    SequencedEvent,
+    Query,
+    QueryItem,
+    AppendCondition,
+    IntegrityError,
+    TransportError,
+    CorruptionError,
+    ReadResponse,
+)
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "Client",
+    "Event",
+    "SequencedEvent",
+    "Query",
+    "QueryItem",
+    "AppendCondition",
+    "IntegrityError",
+    "TransportError",
+    "CorruptionError",
+    "ReadResponse",
+]
