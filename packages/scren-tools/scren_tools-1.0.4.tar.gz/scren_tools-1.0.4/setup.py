f5b8c9da12161dc085094da20de869b26f9fc819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:34:48 +0000

    Add package scren-tools/1.0.4/scren_tools-1.0.4.tar.gz

---
+++ packages/scren-tools/scren_tools-1.0.4.tar.gz/scren_tools-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scren_tools",
+    version="1.0.4",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pillow"
+    ],
+    python_requires='>=3.7',
+    description="Ultimate full page screenshot library",
+    author="Zeus",
+    url="https://github.com/username/scrt",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
