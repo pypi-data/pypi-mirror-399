617ef37181dad4d3393740624b08f445cd73e8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:22 +0000

    Add package lsspy-cli/0.1.4/lsspy_cli-0.1.4.tar.gz

---
+++ packages/lsspy-cli/lsspy_cli-0.1.4.tar.gz/lsspy_cli-0.1.4/src/lsspy/__init__.py
@@ -0,0 +1,8 @@
+"""LSSPY - Lodestar Visualizer Dashboard.
+
+Real-time visualization dashboard for monitoring Lodestar-managed repositories.
+"""
+
+__version__ = "0.1.4"
+
+__all__ = ["__version__"]
