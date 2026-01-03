2e19b5408eb83741b210b51c380eef0997d8aff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:06:10 +0000

    Add package sandlersteam/0.7.0/sandlersteam-0.7.0-py3-none-any.whl

---
+++ packages/sandlersteam/sandlersteam-0.7.0-py3-none-any.whl/sandlersteam/unsatd.py
@@ -0,0 +1,298 @@
+# Author: Cameron F. Abrams <cfa22@drexel.edu>
+
+import numpy as np
+import pandas as pd
+from .util import add_headers, my_split
+from importlib.resources import files
+
+class UnsaturatedSteamTable:
+    data_path = files('sandlersteam') / 'resources' / 'data'
+    table_suph = data_path / 'SandlerSuphSteamTables.txt'
+    table_subc = data_path / 'SandlerSubcSteamTables.txt'
+    def __init__(self, phase: str = 'V'):
+        if phase == 'V':
+            with open(self.table_suph,'r') as f:
+                lines = f.read().split('\n')
+        elif phase == 'L':
+            with open(self.table_subc,'r') as f:
+                lines = f.read().split('\n')
+        # identify header
+        hder = lines[0].split()
+        # identify lines with pressures
+        plines = []
+        # save list of pressure values
+        Pval = []
+        for i, l in enumerate(lines):
+            if 'P = ' in l:
+                plines.append(i)
+        # extract and format all blocks as concurrent dataframes
+        DFS = []
+        for i, (l, r) in enumerate(zip(plines[:-1], plines[1:])):
+            # get pressures, and saturation temperatures, if present
+            tokens = lines[l].split()
+            kills = ['P', '=', 'MPa']
+            for k in kills:
+                while k in tokens:
+                    tokens.remove(k)
+            P = []
+            Tsat = []
+            for x in tokens:
+                if x[0] == '(':
+                    y = x[1:-1]
+                    Tsat.append(float(y))
+                else:
+                    P.append(float(x))
+            while len(Tsat) < len(P):
+                Tsat.append(None)
+            Pval.extend(P)
+            data = '\n'.join(lines[l + 1 : r])
+            ndf = my_split(data, hder, P, Tsat, fixw=(phase == 'L'))
+            DFS.append(ndf)
+        tokens = lines[plines[-1]].split()
+        kills = ['P', '=', 'MPa']
+        for k in kills:
+            while k in tokens:
+                tokens.remove(k)
+        P = []
+        Tsat = []
+        for x in tokens:
+            if x[0] == '(':
+                y = x[1:-1]
+                Tsat.append(float(y))
+            else:
+                P.append(float(x))
+        while len(Tsat) < len(P):
+            Tsat.append(None)
+        Pval.extend(P)
+        data = '\n'.join(lines[plines[-1] + 1 :])
+        ndf = my_split(data, hder, P, Tsat, fixw=(phase == 'L'))
+        DFS.append(ndf)
+        self.data = pd.concat(DFS, ignore_index=True)
+        dof = self.data.columns
+        self.uniqs = {}
+        for d in dof:
+            self.uniqs[d] = np.sort(np.array(list(set(self.data[d].to_list()))))
+
+    def TPBilinear(self, specdict: dict):
+        """
+        Bilinear interpolation given T and P
+        """
+        retdict = {}
+        xn, yn = specdict.keys()
+        assert [xn, yn] == ['TC', 'P']
+        xi, yi = specdict.values()
+        df: pd.DataFrame = self.data
+        dof = df.columns
+        retdict = {}
+        retdict['TC'] = xi
+        retdict['P'] = yi
+        tdf = df[df['P'] == yi]
+        if not tdf.empty:
+            X = np.array(tdf['TC'])
+            for d in dof:
+                if d not in ['TC','P']:
+                    Y = np.array(tdf[d])
+                    retdict[d] = np.interp(xi, X, Y, left=np.nan, right=np.nan)
+        else:
+            for PL, PR in zip(self.uniqs['P'][:-1], self.uniqs['P'][1:]):
+                if PL < yi < PR:
+                    break
+            else:
+                raise Exception(f'P {yi} not between {self.uniqs["P"][0]} and {self.uniqs["P"][-1]} at {xi} C')
+            X = np.array([PL, PR])
+            LDF = df[df['P'] == PL]
+            RDF = df[df['P'] == PR]
+            LT = np.array(LDF['TC'])
+            RT = np.array(RDF['TC'])
+            CT = np.array([T for T in LT if T in RT])
+            if xi in CT:
+                for d in dof:
+                    if d not in ['TC','P']:
+                        Y = np.array([LDF[LT == xi][d].values[0], RDF[RT == xi][d].values[0]])
+                        retdict[d] = np.interp(yi, X, Y, left=np.nan, right=np.nan)
+            else:
+                for TL, TR in zip(CT[:-1], CT[1:]):
+                    if TL < xi < TR:
+                        break
+                else:
+                    raise Exception(f'T {xi} not between {CT[0]} and {CT[-1]} at {yi} MPa')
+                LTDF = LDF[(LDF['TC'] == TL) | (LDF['TC'] == TR)].sort_values(by='TC')
+                RTDF = RDF[(RDF['TC'] == TL) | (RDF['TC'] == TR)].sort_values(by='TC')
+                iv = np.zeros(2)
+                for p in dof:
+                    if not p in ['TC','P']:
+                        for i in range(2):
+                            Lp = LTDF[p].values[i]
+                            Rp = RTDF[p].values[i]
+                            Y = np.array([Lp, Rp])
+                            iv[i] = np.interp(yi, X, Y, left=np.nan, right=np.nan)
+                        retdict[p] = np.interp(xi, np.array([TL, TR]), iv)
+        return retdict
+
+    def to_latex(self, P):
+        # generates latex version of a P-block of the superheated/subcooled steam table
+        block  = self.data[self.data['P'] == P][['TC','V','U','H','S']]
+        if not block.empty:
+            block_floatsplit =  pd.DataFrame()
+            for c in ['TC', 'V', 'U', 'H', 'S']:
+                w = block[c].astype(int)
+                dd = np.round((block[c] - w), 10).astype(str)
+                d = []
+                block_floatsplit[c+'w'] = w
+                for a, s in zip(w, dd):
+                    if '.' in s:
+                        ss = s[1:]
+                    if ss == '.0' and c == 'TC':
+                        d.append('')
+                    else:
+                        if a == 0: # this is a fractional number
+                            while len(ss) < 6:
+                                ss = ss + '0'
+                            iss = int(ss[1:])
+                            if iss > 19999:
+                                ss = ss[:-1]
+                        elif a < 10:
+                            while len(ss) < 5:
+                                ss = ss + '0'
+                        d.append(ss)
+                    
+                block_floatsplit[c+'d'] = d
+            title = r'\noindent\begin{minipage}{0.6\textwidth}' + '\n' + r'\footnotesize\vspace{5mm}' + '\n' + r'\begin{center}' + '\n' + r'$P$ = ' + f'{P}' + r' MPa\\*[1ex]' + '\n'
+            fmts = r'>{\raggedleft}p{8mm}@{}p{5mm}' # T
+            fmts += r'>{\raggedleft}p{4mm}@{}p{10mm}' # V
+            fmts += r'>{\raggedleft}p{10mm}@{}p{3mm}' # U
+            fmts += r'>{\raggedleft}p{10mm}@{}p{3mm}' # H
+            fmts += r'>{\raggedleft\arraybackslash}p{3mm}@{}p{8mm}' # S
+            tbl = block_floatsplit.to_latex(escape=False, header=False, column_format=fmts, index=False, float_format='%g')
+            hdrs = [r'\multicolumn{2}{c}{$T$~($^\circ$C)}',
+                  r'\multicolumn{2}{c}{$\hat{V}$}',
+                  r'\multicolumn{2}{c}{$\hat{U}$}',
+                  r'\multicolumn{2}{c}{$\hat{H}$}',
+                  r'\multicolumn{2}{c}{$\hat{S}$}']
+            tbl = add_headers(tbl, [hdrs], [''])
+            return title + tbl  + r'\end{center}'+'\n'+r'\end{minipage}'+'\n'
+        else:
+            return None
+
+    def TThBilinear(self,specdict):
+        """
+        Bilinear interpolation given T and another property 
+        (V, U, S, H, but not P)
+        """
+        xn, yn = specdict.keys()
+        assert xn == 'TC'
+        xi, yi = specdict.values()
+        df = self.data
+        dof = list(df.columns)
+        dof.remove(yn)
+        dof.remove(xn)
+        retdict = {}
+        retdict['TC'] = xi
+        retdict[yn] = yi
+        LLdat = {}
+        LLdat[yn] = []
+        for d in dof:
+            LLdat[d] = []
+        for P in self.uniqs['P'][::-1]:  # VUSH properties decrease with increasing P
+            tdf = df[df['P'] == P]
+            X = np.array(tdf['TC'])
+            Y = np.array(tdf[yn])
+            if Y.min() < yi < Y.max():
+                LLdat['P'].append(P)
+                for d in 'VUSH':
+                    Y = np.array(tdf[d])
+                    LLdat[d].append(np.interp(xi, X, Y, left=np.nan, right=np.nan))
+        X = np.array(LLdat[yn])
+        retdict['P'] = np.interp(yi, X, np.array(LLdat['P']))
+        for d in 'VUSH':
+            if d != yn:
+                retdict[d] = np.interp(yi, X, LLdat[d])
+        return retdict
+
+    def PThBilinear(self,specdict):
+        """
+        Bilinear interpolation given P and another property 
+        (V, U, S, H, but not TC)
+        """
+        xn, yn = specdict.keys()
+        xi, yi = specdict.values()
+        assert xn == 'P'
+        df = self.data
+        dof = list(df.columns)
+        dof.remove(yn)
+        dof.remove(xn)
+        retdict = {}
+        retdict['TC'] = xi
+        retdict[yn] = yi
+        if xi in self.uniqs['P']:
+            tdf = df[df['P'] == xi]
+            X = np.array(tdf[yn])
+            for pp in dof:
+                Y = np.array(tdf[pp])
+                retdict[pp] = np.interp(yi, X, Y, left=np.nan, right=np.nan)
+        else:
+            for PL, PR in zip(self.uniqs['P'][:-1], self.uniqs['P'][1:]):
+                if PL < xi < PR:
+                    break
+            else:
+                raise Exception(f'Error: no two blocks bracket P={xi}')
+            ldf, rdf = df[df['P'] == PL], df[df['P'] == PR]
+            ldict, rdict = {}, {}
+            ldict['P'], rdict['P'] = PL, PR
+            ldict[yn], rdict[yn] = yi, yi
+            for d, xdf in zip([ldict, rdict], [ldf, rdf]):
+                X = xdf[yn]
+                for pp in dof:
+                    Y = np.array(xdf[pp])
+                    d[pp] = np.interp(yi, X, Y, left=np.nan, right=np.nan)
+            X = np.array([PL, PR])
+            for pp in dof:
+                Y = np.array([ldict[pp], rdict[pp]])
+                retdict[pp] = np.interp(xi, X, Y, left=np.nan, right=np.nan)
+        return retdict
+    
+    def ThThBilinear(self,specdict):
+        """
+        Bilinear interpolation given two properties from V, U, S, H 
+        (not T or P)
+        """
+        xn, yn =specdict.keys()
+        assert not xn in ['TC','P'] and not yn in ['TC','P']
+        xi, yi = specdict.values()
+        df = self.data
+        dof = self.data.columns
+        LLdat = {}
+        for d in dof:
+            if d not in ['TC','P']:
+                LLdat[d] = []
+        LLdat['TC'] = self.uniqs['TC']
+        for TC in LLdat['TC']:
+            tdf = df[df['TC'] == TC]
+            X = np.array(tdf[xn])
+            for d in dof:
+                if d!='TC' and d!=xn:
+                    Y = np.array(tdf[d])
+                    if Y.min() < yi < Y.max():
+                        LLdat[d] = np.interp(xi, X, Y, left=np.nan, right=np.nan)
+        X = LLdat[yn]
+        retdict = {}
+        retdict[xn] = xi
+        retdict[yn] = yi
+        for d in dof:
+            if d!=xn and d!=yn:
+                retdict[d] = np.interp(yi, X, LLdat[d])
+        return retdict
+
+    def Bilinear(self, specdict):
+        """
+        General bilinear interpolation dispatcher
+        """
+        xn, yn = specdict.keys()
+        if [xn, yn] == ['TC', 'P']:
+            return self.TPBilinear(specdict)
+        elif xn == 'TC':
+            return self.TThBilinear(specdict)
+        elif xn == 'P':
+            return self.PThBilinear(specdict)
+        else:
+            return self.ThThBilinear(specdict)
