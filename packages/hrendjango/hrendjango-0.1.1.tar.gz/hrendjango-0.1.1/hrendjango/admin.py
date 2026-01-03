ce6277948853020d3de5d7507c4f378533a837ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 15:08:07 +0000

    Add package hrendjango/0.1.1/hrendjango-0.1.1.tar.gz

---
+++ packages/hrendjango/hrendjango-0.1.1.tar.gz/hrendjango-0.1.1/hrendjango/admin.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.contrib import admin
+from .models import get_file_model, get_image_model
+
+
+if getattr(settings, 'HRENDJANGO_ACTIVE_MODELS', None):
+    admin.site.register(get_file_model())
+    admin.site.register(get_image_model())
