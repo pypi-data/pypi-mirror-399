08a5702e48530c929a7ee1f723507c200c439e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:07:11 +0000

    Add package ngxgen-app/1.0.1/ngxgen_app-1.0.1-py3-none-any.whl

---
+++ packages/ngxgen-app/ngxgen_app-1.0.1-py3-none-any.whl/ngxgen_app/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2024-2025, Maciej Barć <xgqt@xgqt.org>
+
+
+"""
+NgxGen - generate Nginx configuration from YAML files.
+"""
+
+
+__app__ = "NgxGen"
+__version__ = "1.0.1"
+__description__ = "Generate Nginx configuration from YAML files"
