85cbba821378c9c99ef8bbd1571e4955828b56ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:32 +0000

    Add package ypricemagic/5.1.3/ypricemagic-5.1.3-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/ypricemagic/ypricemagic-5.1.3-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/y/utils/checks.py
@@ -0,0 +1,30 @@
+"""
+Utility functions for performing various checks.
+"""
+
+from typing import Any
+from collections.abc import Iterable
+
+
+def hasall(obj: Any, attrs: Iterable[str]) -> bool:
+    """
+    Check if an object has all the specified attributes.
+
+    Args:
+        obj: The object to check.
+        attrs: An iterable of attribute names to check for.
+
+    Returns:
+        True if the object has all the specified attributes, False otherwise.
+
+    Example:
+        >>> class TestClass:
+        ...     attr1 = 1
+        ...     attr2 = 2
+        >>> test_obj = TestClass()
+        >>> hasall(test_obj, ['attr1', 'attr2'])
+        True
+        >>> hasall(test_obj, ['attr1', 'attr3'])
+        False
+    """
+    return all(hasattr(obj, attr) for attr in attrs)
