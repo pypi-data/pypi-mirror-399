7d4929a55c81b07b4214c1aaf593f7dcaedd34da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 03:33:07 +0000

    Add package uv/0.9.21/uv-0.9.21.tar.gz

---
+++ packages/uv/uv-0.9.21.tar.gz/uv-0.9.21/crates/uv-cli/src/options.rs
@@ -0,0 +1,501 @@
+use anstream::eprintln;
+
+use uv_cache::Refresh;
+use uv_configuration::{BuildIsolation, Reinstall, Upgrade};
+use uv_distribution_types::{ConfigSettings, PackageConfigSettings, Requirement};
+use uv_resolver::{ExcludeNewer, ExcludeNewerPackage, PrereleaseMode};
+use uv_settings::{Combine, PipOptions, ResolverInstallerOptions, ResolverOptions};
+use uv_warnings::owo_colors::OwoColorize;
+
+use crate::{
+    BuildOptionsArgs, FetchArgs, IndexArgs, InstallerArgs, Maybe, RefreshArgs, ResolverArgs,
+    ResolverInstallerArgs,
+};
+
+/// Given a boolean flag pair (like `--upgrade` and `--no-upgrade`), resolve the value of the flag.
+pub fn flag(yes: bool, no: bool, name: &str) -> Option<bool> {
+    match (yes, no) {
+        (true, false) => Some(true),
+        (false, true) => Some(false),
+        (false, false) => None,
+        (..) => {
+            eprintln!(
+                "{}{} `{}` and `{}` cannot be used together. \
+                Boolean flags on different levels are currently not supported \
+                (https://github.com/clap-rs/clap/issues/6049)",
+                "error".bold().red(),
+                ":".bold(),
+                format!("--{name}").green(),
+                format!("--no-{name}").green(),
+            );
+            // No error forwarding since should eventually be solved on the clap side.
+            #[allow(clippy::exit)]
+            {
+                std::process::exit(2);
+            }
+        }
+    }
+}
+
+impl From<RefreshArgs> for Refresh {
+    fn from(value: RefreshArgs) -> Self {
+        let RefreshArgs {
+            refresh,
+            no_refresh,
+            refresh_package,
+        } = value;
+
+        Self::from_args(flag(refresh, no_refresh, "no-refresh"), refresh_package)
+    }
+}
+
+impl From<ResolverArgs> for PipOptions {
+    fn from(args: ResolverArgs) -> Self {
+        let ResolverArgs {
+            index_args,
+            upgrade,
+            no_upgrade,
+            upgrade_package,
+            index_strategy,
+            keyring_provider,
+            resolution,
+            prerelease,
+            pre,
+            fork_strategy,
+            config_setting,
+            config_settings_package,
+            no_build_isolation,
+            no_build_isolation_package,
+            build_isolation,
+            exclude_newer,
+            link_mode,
+            no_sources,
+            exclude_newer_package,
+        } = args;
+
+        Self {
+            upgrade: flag(upgrade, no_upgrade, "no-upgrade"),
+            upgrade_package: Some(upgrade_package),
+            index_strategy,
+            keyring_provider,
+            resolution,
+            fork_strategy,
+            prerelease: if pre {
+                Some(PrereleaseMode::Allow)
+            } else {
+                prerelease
+            },
+            config_settings: config_setting
+                .map(|config_settings| config_settings.into_iter().collect::<ConfigSettings>()),
+            config_settings_package: config_settings_package.map(|config_settings| {
+                config_settings
+                    .into_iter()
+                    .collect::<PackageConfigSettings>()
+            }),
+            no_build_isolation: flag(no_build_isolation, build_isolation, "build-isolation"),
+            no_build_isolation_package: Some(no_build_isolation_package),
+            exclude_newer,
+            exclude_newer_package: exclude_newer_package.map(ExcludeNewerPackage::from_iter),
+            link_mode,
+            no_sources: if no_sources { Some(true) } else { None },
+            ..Self::from(index_args)
+        }
+    }
+}
+
+impl From<InstallerArgs> for PipOptions {
+    fn from(args: InstallerArgs) -> Self {
+        let InstallerArgs {
+            index_args,
+            reinstall,
+            no_reinstall,
+            reinstall_package,
+            index_strategy,
+            keyring_provider,
+            config_setting,
+            config_settings_package,
+            no_build_isolation,
+            build_isolation,
+            exclude_newer,
+            link_mode,
+            compile_bytecode,
+            no_compile_bytecode,
+            no_sources,
+            exclude_newer_package,
+        } = args;
+
+        Self {
+            reinstall: flag(reinstall, no_reinstall, "reinstall"),
+            reinstall_package: Some(reinstall_package),
+            index_strategy,
+            keyring_provider,
+            config_settings: config_setting
+                .map(|config_settings| config_settings.into_iter().collect::<ConfigSettings>()),
+            config_settings_package: config_settings_package.map(|config_settings| {
+                config_settings
+                    .into_iter()
+                    .collect::<PackageConfigSettings>()
+            }),
+            no_build_isolation: flag(no_build_isolation, build_isolation, "build-isolation"),
+            exclude_newer,
+            exclude_newer_package: exclude_newer_package.map(ExcludeNewerPackage::from_iter),
+            link_mode,
+            compile_bytecode: flag(compile_bytecode, no_compile_bytecode, "compile-bytecode"),
+            no_sources: if no_sources { Some(true) } else { None },
+            ..Self::from(index_args)
+        }
+    }
+}
+
+impl From<ResolverInstallerArgs> for PipOptions {
+    fn from(args: ResolverInstallerArgs) -> Self {
+        let ResolverInstallerArgs {
+            index_args,
+            upgrade,
+            no_upgrade,
+            upgrade_package,
+            reinstall,
+            no_reinstall,
+            reinstall_package,
+            index_strategy,
+            keyring_provider,
+            resolution,
+            prerelease,
+            pre,
+            fork_strategy,
+            config_setting,
+            config_settings_package,
+            no_build_isolation,
+            no_build_isolation_package,
+            build_isolation,
+            exclude_newer,
+            link_mode,
+            compile_bytecode,
+            no_compile_bytecode,
+            no_sources,
+            exclude_newer_package,
+        } = args;
+
+        Self {
+            upgrade: flag(upgrade, no_upgrade, "upgrade"),
+            upgrade_package: Some(upgrade_package),
+            reinstall: flag(reinstall, no_reinstall, "reinstall"),
+            reinstall_package: Some(reinstall_package),
+            index_strategy,
+            keyring_provider,
+            resolution,
+            prerelease: if pre {
+                Some(PrereleaseMode::Allow)
+            } else {
+                prerelease
+            },
+            fork_strategy,
+            config_settings: config_setting
+                .map(|config_settings| config_settings.into_iter().collect::<ConfigSettings>()),
+            config_settings_package: config_settings_package.map(|config_settings| {
+                config_settings
+                    .into_iter()
+                    .collect::<PackageConfigSettings>()
+            }),
+            no_build_isolation: flag(no_build_isolation, build_isolation, "build-isolation"),
+            no_build_isolation_package: Some(no_build_isolation_package),
+            exclude_newer,
+            exclude_newer_package: exclude_newer_package.map(ExcludeNewerPackage::from_iter),
+            link_mode,
+            compile_bytecode: flag(compile_bytecode, no_compile_bytecode, "compile-bytecode"),
+            no_sources: if no_sources { Some(true) } else { None },
+            ..Self::from(index_args)
+        }
+    }
+}
+
+impl From<FetchArgs> for PipOptions {
+    fn from(args: FetchArgs) -> Self {
+        let FetchArgs {
+            index_args,
+            index_strategy,
+            keyring_provider,
+            exclude_newer,
+        } = args;
+
+        Self {
+            index_strategy,
+            keyring_provider,
+            exclude_newer,
+            ..Self::from(index_args)
+        }
+    }
+}
+
+impl From<IndexArgs> for PipOptions {
+    fn from(args: IndexArgs) -> Self {
+        let IndexArgs {
+            default_index,
+            index,
+            index_url,
+            extra_index_url,
+            no_index,
+            find_links,
+        } = args;
+
+        Self {
+            index: default_index
+                .and_then(Maybe::into_option)
+                .map(|default_index| vec![default_index])
+                .combine(index.map(|index| {
+                    index
+                        .iter()
+                        .flat_map(std::clone::Clone::clone)
+                        .filter_map(Maybe::into_option)
+                        .collect()
+                })),
+            index_url: index_url.and_then(Maybe::into_option),
+            extra_index_url: extra_index_url.map(|extra_index_urls| {
+                extra_index_urls
+                    .into_iter()
+                    .filter_map(Maybe::into_option)
+                    .collect()
+            }),
+            no_index: if no_index { Some(true) } else { None },
+            find_links: find_links.map(|find_links| {
+                find_links
+                    .into_iter()
+                    .filter_map(Maybe::into_option)
+                    .collect()
+            }),
+            ..Self::default()
+        }
+    }
+}
+
+/// Construct the [`ResolverOptions`] from the [`ResolverArgs`] and [`BuildOptionsArgs`].
+pub fn resolver_options(
+    resolver_args: ResolverArgs,
+    build_args: BuildOptionsArgs,
+) -> ResolverOptions {
+    let ResolverArgs {
+        index_args,
+        upgrade,
+        no_upgrade,
+        upgrade_package,
+        index_strategy,
+        keyring_provider,
+        resolution,
+        prerelease,
+        pre,
+        fork_strategy,
+        config_setting,
+        config_settings_package,
+        no_build_isolation,
+        no_build_isolation_package,
+        build_isolation,
+        exclude_newer,
+        link_mode,
+        no_sources,
+        exclude_newer_package,
+    } = resolver_args;
+
+    let BuildOptionsArgs {
+        no_build,
+        build,
+        no_build_package,
+        no_binary,
+        binary,
+        no_binary_package,
+    } = build_args;
+
+    ResolverOptions {
+        index: index_args
+            .default_index
+            .and_then(Maybe::into_option)
+            .map(|default_index| vec![default_index])
+            .combine(index_args.index.map(|index| {
+                index
+                    .into_iter()
+                    .flat_map(|v| v.clone())
+                    .filter_map(Maybe::into_option)
+                    .collect()
+            })),
+        index_url: index_args.index_url.and_then(Maybe::into_option),
+        extra_index_url: index_args.extra_index_url.map(|extra_index_url| {
+            extra_index_url
+                .into_iter()
+                .filter_map(Maybe::into_option)
+                .collect()
+        }),
+        no_index: if index_args.no_index {
+            Some(true)
+        } else {
+            None
+        },
+        find_links: index_args.find_links.map(|find_links| {
+            find_links
+                .into_iter()
+                .filter_map(Maybe::into_option)
+                .collect()
+        }),
+        upgrade: Upgrade::from_args(
+            flag(upgrade, no_upgrade, "no-upgrade"),
+            upgrade_package.into_iter().map(Requirement::from).collect(),
+        ),
+        index_strategy,
+        keyring_provider,
+        resolution,
+        prerelease: if pre {
+            Some(PrereleaseMode::Allow)
+        } else {
+            prerelease
+        },
+        fork_strategy,
+        dependency_metadata: None,
+        config_settings: config_setting
+            .map(|config_settings| config_settings.into_iter().collect::<ConfigSettings>()),
+        config_settings_package: config_settings_package.map(|config_settings| {
+            config_settings
+                .into_iter()
+                .collect::<PackageConfigSettings>()
+        }),
+        build_isolation: BuildIsolation::from_args(
+            flag(no_build_isolation, build_isolation, "build-isolation"),
+            no_build_isolation_package,
+        ),
+        extra_build_dependencies: None,
+        extra_build_variables: None,
+        exclude_newer: ExcludeNewer::from_args(
+            exclude_newer,
+            exclude_newer_package.unwrap_or_default(),
+        ),
+        link_mode,
+        torch_backend: None,
+        no_build: flag(no_build, build, "build"),
+        no_build_package: Some(no_build_package),
+        no_binary: flag(no_binary, binary, "binary"),
+        no_binary_package: Some(no_binary_package),
+        no_sources: if no_sources { Some(true) } else { None },
+    }
+}
+
+/// Construct the [`ResolverInstallerOptions`] from the [`ResolverInstallerArgs`] and [`BuildOptionsArgs`].
+pub fn resolver_installer_options(
+    resolver_installer_args: ResolverInstallerArgs,
+    build_args: BuildOptionsArgs,
+) -> ResolverInstallerOptions {
+    let ResolverInstallerArgs {
+        index_args,
+        upgrade,
+        no_upgrade,
+        upgrade_package,
+        reinstall,
+        no_reinstall,
+        reinstall_package,
+        index_strategy,
+        keyring_provider,
+        resolution,
+        prerelease,
+        pre,
+        fork_strategy,
+        config_setting,
+        config_settings_package,
+        no_build_isolation,
+        no_build_isolation_package,
+        build_isolation,
+        exclude_newer,
+        exclude_newer_package,
+        link_mode,
+        compile_bytecode,
+        no_compile_bytecode,
+        no_sources,
+    } = resolver_installer_args;
+
+    let BuildOptionsArgs {
+        no_build,
+        build,
+        no_build_package,
+        no_binary,
+        binary,
+        no_binary_package,
+    } = build_args;
+
+    let default_index = index_args
+        .default_index
+        .and_then(Maybe::into_option)
+        .map(|default_index| vec![default_index]);
+    let index = index_args.index.map(|index| {
+        index
+            .into_iter()
+            .flat_map(|v| v.clone())
+            .filter_map(Maybe::into_option)
+            .collect()
+    });
+
+    ResolverInstallerOptions {
+        index: default_index.combine(index),
+        index_url: index_args.index_url.and_then(Maybe::into_option),
+        extra_index_url: index_args.extra_index_url.map(|extra_index_url| {
+            extra_index_url
+                .into_iter()
+                .filter_map(Maybe::into_option)
+                .collect()
+        }),
+        no_index: if index_args.no_index {
+            Some(true)
+        } else {
+            None
+        },
+        find_links: index_args.find_links.map(|find_links| {
+            find_links
+                .into_iter()
+                .filter_map(Maybe::into_option)
+                .collect()
+        }),
+        upgrade: Upgrade::from_args(
+            flag(upgrade, no_upgrade, "upgrade"),
+            upgrade_package.into_iter().map(Requirement::from).collect(),
+        ),
+        reinstall: Reinstall::from_args(
+            flag(reinstall, no_reinstall, "reinstall"),
+            reinstall_package,
+        ),
+        index_strategy,
+        keyring_provider,
+        resolution,
+        prerelease: if pre {
+            Some(PrereleaseMode::Allow)
+        } else {
+            prerelease
+        },
+        fork_strategy,
+        dependency_metadata: None,
+        config_settings: config_setting
+            .map(|config_settings| config_settings.into_iter().collect::<ConfigSettings>()),
+        config_settings_package: config_settings_package.map(|config_settings| {
+            config_settings
+                .into_iter()
+                .collect::<PackageConfigSettings>()
+        }),
+        build_isolation: BuildIsolation::from_args(
+            flag(no_build_isolation, build_isolation, "build-isolation"),
+            no_build_isolation_package,
+        ),
+        extra_build_dependencies: None,
+        extra_build_variables: None,
+        exclude_newer,
+        exclude_newer_package: exclude_newer_package.map(ExcludeNewerPackage::from_iter),
+        link_mode,
+        compile_bytecode: flag(compile_bytecode, no_compile_bytecode, "compile-bytecode"),
+        no_build: flag(no_build, build, "build"),
+        no_build_package: if no_build_package.is_empty() {
+            None
+        } else {
+            Some(no_build_package)
+        },
+        no_binary: flag(no_binary, binary, "binary"),
+        no_binary_package: if no_binary_package.is_empty() {
+            None
+        } else {
+            Some(no_binary_package)
+        },
+        no_sources: if no_sources { Some(true) } else { None },
+        torch_backend: None,
+    }
+}
---
+++ packages/uv/uv-0.9.21.tar.gz/uv-0.9.21/crates/uv/src/commands/help.rs
@@ -0,0 +1,256 @@
+use std::ffi::OsString;
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::{fmt::Display, fmt::Write};
+
+use anstream::{ColorChoice, stream::IsTerminal};
+use anyhow::{Result, anyhow};
+use clap::CommandFactory;
+use itertools::Itertools;
+use owo_colors::OwoColorize;
+use which::which;
+
+use super::ExitStatus;
+use crate::printer::Printer;
+use uv_cli::Cli;
+use uv_static::EnvVars;
+
+// hidden subcommands to show in the help command
+const SHOW_HIDDEN_COMMANDS: &[&str] = &["generate-shell-completion"];
+
+pub(crate) fn help(query: &[String], printer: Printer, no_pager: bool) -> Result<ExitStatus> {
+    let mut uv: clap::Command = SHOW_HIDDEN_COMMANDS
+        .iter()
+        .fold(Cli::command(), |uv, &name| {
+            uv.mut_subcommand(name, |cmd| cmd.hide(false))
+        });
+
+    // It is very important to build the command before beginning inspection or subcommands
+    // will be missing all of the propagated options.
+    uv.build();
+
+    let command = find_command(query, &uv).map_err(|(unmatched, nearest)| {
+        let missing = if unmatched.len() == query.len() {
+            format!("`{}` for `uv`", query.join(" "))
+        } else {
+            format!("`{}` for `uv {}`", unmatched.join(" "), nearest.get_name())
+        };
+        anyhow!(
+            "There is no command {}. Did you mean one of:\n    {}",
+            missing,
+            nearest
+                .get_subcommands()
+                .filter(|cmd| !cmd.is_hide_set())
+                .map(clap::Command::get_name)
+                .filter(|name| *name != "help")
+                .join("\n    "),
+        )
+    })?;
+
+    let name = command.get_name();
+    let is_root = name == uv.get_name();
+    let mut command = command.clone();
+
+    let help = if is_root {
+        command
+            .after_help(format!(
+                "Use `{}` for more information on a specific command.",
+                "uv help <command>".bold()
+            ))
+            .render_help()
+    } else {
+        if command.has_subcommands() {
+            command.after_long_help(format!(
+                "Use `{}` for more information on a specific command.",
+                format!("uv help {name} <command>").bold()
+            ))
+        } else {
+            command
+        }
+        .render_long_help()
+    };
+
+    let want_color = match anstream::Stdout::choice(&std::io::stdout()) {
+        ColorChoice::Always | ColorChoice::AlwaysAnsi => true,
+        ColorChoice::Never => false,
+        // We just asked anstream for a choice, that can't be auto
+        ColorChoice::Auto => unreachable!(),
+    };
+
+    let is_terminal = std::io::stdout().is_terminal();
+    let should_page = !no_pager && !is_root && is_terminal;
+
+    if should_page && let Some(pager) = Pager::try_from_env() {
+        let query = query.join(" ");
+        if want_color && pager.supports_colors() {
+            pager.spawn(format!("{}: {query}", "uv help".bold()), help.ansi())?;
+        } else {
+            pager.spawn(format!("uv help: {query}"), help)?;
+        }
+    } else {
+        if want_color {
+            writeln!(printer.stdout(), "{}", help.ansi())?;
+        } else {
+            writeln!(printer.stdout(), "{help}")?;
+        }
+    }
+
+    Ok(ExitStatus::Success)
+}
+
+/// Find the command corresponding to a set of arguments, e.g., `["uv", "pip", "install"]`.
+///
+/// If the command cannot be found, the nearest command is returned.
+fn find_command<'a>(
+    query: &'a [String],
+    cmd: &'a clap::Command,
+) -> Result<&'a clap::Command, (&'a [String], &'a clap::Command)> {
+    let Some(next) = query.first() else {
+        return Ok(cmd);
+    };
+
+    let subcommand = cmd.find_subcommand(next).ok_or((query, cmd))?;
+    find_command(&query[1..], subcommand)
+}
+
+#[derive(Debug)]
+enum PagerKind {
+    Less,
+    More,
+    Other(String),
+}
+
+#[derive(Debug)]
+struct Pager {
+    kind: PagerKind,
+    args: Vec<String>,
+    path: Option<PathBuf>,
+}
+
+impl PagerKind {
+    fn default_args(&self) -> Vec<String> {
+        match self {
+            Self::Less => vec!["-R".to_string()],
+            Self::More => vec![],
+            Self::Other(_) => vec![],
+        }
+    }
+}
+
+impl Display for PagerKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Less => write!(f, "less"),
+            Self::More => write!(f, "more"),
+            Self::Other(name) => write!(f, "{name}"),
+        }
+    }
+}
+
+impl FromStr for Pager {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split_ascii_whitespace();
+
+        // Empty string
+        let Some(first) = split.next() else {
+            return Err(());
+        };
+
+        match first {
+            "less" => Ok(Self {
+                kind: PagerKind::Less,
+                args: split.map(str::to_string).collect(),
+                path: None,
+            }),
+            "more" => Ok(Self {
+                kind: PagerKind::More,
+                args: split.map(str::to_string).collect(),
+                path: None,
+            }),
+            _ => Ok(Self {
+                kind: PagerKind::Other(first.to_string()),
+                args: split.map(str::to_string).collect(),
+                path: None,
+            }),
+        }
+    }
+}
+
+impl Pager {
+    /// Display `contents` using the pager.
+    fn spawn(self, heading: String, contents: impl Display) -> Result<()> {
+        use std::io::Write;
+
+        let command = self
+            .path
+            .as_ref()
+            .map(|path| path.as_os_str().to_os_string())
+            .unwrap_or(OsString::from(self.kind.to_string()));
+
+        let args = if self.args.is_empty() {
+            self.kind.default_args()
+        } else {
+            self.args
+        };
+
+        let mut child = std::process::Command::new(command)
+            .args(args)
+            .stdin(std::process::Stdio::piped())
+            .spawn()?;
+
+        let mut stdin = child
+            .stdin
+            .take()
+            .ok_or_else(|| anyhow!("Failed to take child process stdin"))?;
+
+        let contents = contents.to_string();
+        let writer = std::thread::spawn(move || {
+            let _ = write!(stdin, "{heading}\n\n");
+            let _ = stdin.write_all(contents.as_bytes());
+        });
+
+        drop(child.wait());
+        drop(writer.join());
+
+        Ok(())
+    }
+
+    /// Get a pager to use and its path, if available.
+    ///
+    /// Supports the `PAGER` environment variable, otherwise checks for `less` and `more` in the
+    /// search path.
+    fn try_from_env() -> Option<Self> {
+        if let Some(pager) = std::env::var_os(EnvVars::PAGER) {
+            if !pager.is_empty() {
+                return Self::from_str(&pager.to_string_lossy()).ok();
+            }
+        }
+
+        if let Ok(less) = which("less") {
+            Some(Self {
+                kind: PagerKind::Less,
+                args: vec![],
+                path: Some(less),
+            })
+        } else if let Ok(more) = which("more") {
+            Some(Self {
+                kind: PagerKind::More,
+                args: vec![],
+                path: Some(more),
+            })
+        } else {
+            None
+        }
+    }
+
+    fn supports_colors(&self) -> bool {
+        match self.kind {
+            // The `-R` flag is required for color support. We will provide it by default.
+            PagerKind::Less => self.args.is_empty() || self.args.iter().any(|arg| arg == "-R"),
+            PagerKind::More => false,
+            PagerKind::Other(_) => false,
+        }
+    }
+}
