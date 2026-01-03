836bfab6844ff85f1ee7a239b0aecf67a571442a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:36 +0000

    Add package skypilot-nightly/1.0.0.dev20251231/skypilot_nightly-1.0.0.dev20251231.tar.gz

---
+++ packages/skypilot-nightly/skypilot_nightly-1.0.0.dev20251231.tar.gz/skypilot_nightly-1.0.0.dev20251231/sky/dashboard/out/_next/static/chunks/7325.b4bc99ce0892dcd5.js
@@ -0,0 +1,6 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7325],{27325:function(e,t,c){c.d(t,{Z:function(){return h}});/**
+ * @license lucide-react v0.407.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let h=(0,c(60998).Z)("Copy",[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]])}}]);
