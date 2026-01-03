269dfca9d36552f47eba07c0e3b38f57c432adcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:57 +0000

    Add package bendev-tools/1.0.0/bendev_tools-1.0.0.tar.gz

---
+++ packages/bendev-tools/bendev_tools-1.0.0.tar.gz/bendev_tools-1.0.0/README.md
@@ -0,0 +1,14 @@
+# Operations
+
+A simple Python library for basic mathematical operations and statistics.
+
+## Features
+- `csq(a, b,c)` → returns the two values of x
+- `circle(r)` → returns the area of a square when given radius
+- `length(string)` → returns the length of a string
+- `reverse(string)` → returns the reverse of a string and value
+
+## Installation
+You can install the package from PyPI (after publishing):
+```bash
+pip install bendev-tools
---
+++ packages/bendev-tools/bendev_tools-1.0.0.tar.gz/bendev_tools-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bendev-tools",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    description= "A simple math operations library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">3.6",
+)
