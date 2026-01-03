2de790ba3169b7919c6112e75ec5cb579a787f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 03:33:42 +0000

    Add package iqcc-cloud-client/0.13.8/iqcc_cloud_client-0.13.8.tar.gz

---
+++ packages/iqcc-cloud-client/iqcc_cloud_client-0.13.8.tar.gz/iqcc_cloud_client-0.13.8/src/iqcc_cloud_client/__init__.py
@@ -0,0 +1,5 @@
+from iqcc_cloud_client.computers import IQCC_Cloud
+from iqcc_cloud_client.qmm_cloud import CloudQuantumMachinesManager
+
+__version__ = "0.13.8"
+__all__ = [IQCC_Cloud, CloudQuantumMachinesManager]
