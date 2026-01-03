4df48313a814fd5b41ae3a310e7c9653cc9e7eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:06:30 +0000

    Add package termella/0.0.9.dev8/termella-0.0.9.dev8.tar.gz

---
+++ packages/termella/termella-0.0.9.dev8.tar.gz/termella-0.0.9.dev8/termella/renderer.py
@@ -0,0 +1,15 @@
+from .widgets import grid
+
+def render_to_string(renderable):
+    """
+    Converts any renderable object (String, List, Widget) into a final string.
+    """
+    if isinstance(renderable, (list, tuple)):
+        return grid(renderable, cols=1, render=True)
+
+    text = str(renderable)
+
+    if text.endswith('\n'):
+        text = text.rstrip('\n')
+
+    return text
