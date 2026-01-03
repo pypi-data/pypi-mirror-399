0dc35cb74bc812912cf45232fb9e6f16f8d82c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:07:55 +0000

    Add package rsp-jupyter-extensions/0.19.2/rsp_jupyter_extensions-0.19.2.tar.gz

---
+++ packages/rsp-jupyter-extensions/rsp_jupyter_extensions-0.19.2.tar.gz/rsp_jupyter_extensions-0.19.2/src/tokens.ts
@@ -0,0 +1,11 @@
+/**
+ * Namespace for everything
+ */
+export const NS = 'rsp-jupyterlab';
+
+export const PLUGIN_ID = `${NS}:plugin`;
+
+export const DISPLAYVERSION_ID = `${NS}:displayversion`;
+export const SAVEQUIT_ID = `${NS}:savequit`;
+export const QUERY_ID = `${NS}:query`;
+export const TUTORIALS_ID = `${NS}:tutorials`;
