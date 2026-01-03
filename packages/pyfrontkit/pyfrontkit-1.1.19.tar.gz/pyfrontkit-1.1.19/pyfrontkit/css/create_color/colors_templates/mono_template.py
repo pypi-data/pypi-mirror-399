0df93af7b92f33e73aff8c37336a6b46cd39c2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:43 +0000

    Add package pyfrontkit/1.1.19/pyfrontkit-1.1.19.tar.gz

---
+++ packages/pyfrontkit/pyfrontkit-1.1.19.tar.gz/pyfrontkit-1.1.19/pyfrontkit/css/create_color/colors_templates/mono_template.py
@@ -0,0 +1,19 @@
+mono = {
+    "simple_selector": {
+        "body":   {"color": "black", "background-color": "quaternary"},
+        "header": {"color": "principal", "background-color": "quaternary"},
+        "footer": {"color": "secondary", "background-color": "quaternary"},
+        "text_htags": {"color": "principal"},
+        "h3": {"color": "secondary"},
+        "h4": {"color": "secondary"},
+        "button": {"color": "white", "background-color": "principal"}
+    },
+
+    "doble_selector": {
+        "header>text_tags": {"color":"black"},
+        "footer>text_tags": {"color": "black"},
+        "header>button": {"color": "white", "background-color": "secondary"}
+    },
+
+    "triple_selector": {}
+}
