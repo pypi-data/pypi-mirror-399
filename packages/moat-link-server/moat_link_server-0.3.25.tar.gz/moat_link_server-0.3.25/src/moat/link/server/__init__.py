38f4fa732d0732559e43ca7695ae966edcc467d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:10 +0000

    Add package moat-link-server/0.3.25/moat_link_server-0.3.25.tar.gz

---
+++ packages/moat-link-server/moat_link_server-0.3.25.tar.gz/moat_link_server-0.3.25/src/moat/link/server/__init__.py
@@ -0,0 +1,23 @@
+"""
+The base of MoaT-Link's server part.
+"""
+
+from __future__ import annotations
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+try:
+    from importlib.metadata import version
+
+    _version = version("moat.link.server")
+    _version_tuple = tuple(int(x) for x in _version.split("."))
+
+except Exception:  # pragma: no cover
+    _version = "0.0.1"
+    _version_tuple = (0, 0, 1)
+
+from moat.lib.config import CfgStore
+
+from ._server import Server as Server
+
+CfgStore.with_(__name__)
