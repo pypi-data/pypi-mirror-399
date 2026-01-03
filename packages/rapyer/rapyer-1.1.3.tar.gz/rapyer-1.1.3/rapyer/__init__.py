49fc52bce320e9a46e79b957a6361209cf5c1685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:06:55 +0000

    Add package rapyer/1.1.3/rapyer-1.1.3.tar.gz

---
+++ packages/rapyer/rapyer-1.1.3.tar.gz/rapyer-1.1.3/rapyer/__init__.py
@@ -0,0 +1,14 @@
+"""Redis Pydantic - Pydantic models with Redis as the backend."""
+
+from rapyer.base import AtomicRedisModel, aget, find_redis_models, ainsert
+from rapyer.init import init_rapyer, teardown_rapyer
+
+__all__ = [
+    "AtomicRedisModel",
+    "init_rapyer",
+    "teardown_rapyer",
+    "aget",
+    "get",
+    "find_redis_models",
+    "ainsert",
+]
