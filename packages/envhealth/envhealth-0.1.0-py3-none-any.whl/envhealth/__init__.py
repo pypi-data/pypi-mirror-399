8507373599e72e52a28856655c34fd256332037e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:05:49 +0000

    Add package envhealth/0.1.0/envhealth-0.1.0-py3-none-any.whl

---
+++ packages/envhealth/envhealth-0.1.0-py3-none-any.whl/envhealth/__init__.py
@@ -0,0 +1,12 @@
+from .checker import EnvironmentChecker
+from .reporter import ConsoleReporter, HTMLReporter, JSONReporter, MarkdownReporter
+
+__all__ = [
+    "EnvironmentChecker",
+    "ConsoleReporter",
+    "HTMLReporter",
+    "JSONReporter",
+    "MarkdownReporter",
+]
+
+__version__ = "0.1.0"
