8c217748325222f8c51f8906ea31dfe1759f6a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:06:12 +0000

    Add package prometrix/0.2.9/prometrix-0.2.9-py3-none-any.whl

---
+++ packages/prometrix/prometrix-0.2.9-py3-none-any.whl/prometrix/models/prometheus_config.py
@@ -0,0 +1,82 @@
+from enum import Enum
+from typing import Dict, List, Optional
+
+try:
+    # Works if Pydantic v2 is installed
+    from pydantic.v1 import BaseModel, SecretStr
+except ImportError:
+    # Fallback if running under Pydantic v1
+    from pydantic import BaseModel, SecretStr
+
+
+class PrometheusApis(Enum):
+    QUERY = 0
+    QUERY_RANGE = 1
+    LABELS = 2
+    FLAGS = 3
+    VM_FLAGS = 4
+
+
+class PrometheusConfig(BaseModel):
+    url: str
+    disable_ssl: bool = False
+    headers: Dict[str, str] = {}
+    prometheus_auth: Optional[SecretStr] = None
+    prometheus_url_query_string: Optional[str] = None
+    additional_labels: Optional[Dict[str, str]] = None
+    supported_apis: List[PrometheusApis] = [
+        PrometheusApis.QUERY,
+        PrometheusApis.QUERY_RANGE,
+        PrometheusApis.LABELS,
+        PrometheusApis.FLAGS,
+    ]
+    query_step: str = "5m"
+    query_interval: str = "1d"
+
+
+class AWSPrometheusConfig(PrometheusConfig):
+    access_key: Optional[str] = None
+    secret_access_key: Optional[str] = None
+    token: Optional[str] = None
+    service_name: str = "aps"
+    aws_region: str
+    assume_role_arn: Optional[str] = None
+    supported_apis: List[PrometheusApis] = [
+        PrometheusApis.QUERY,
+        PrometheusApis.QUERY_RANGE,
+        PrometheusApis.LABELS,
+    ]
+
+
+class CoralogixPrometheusConfig(PrometheusConfig):
+    prometheus_token: str
+    supported_apis: List[PrometheusApis] = [
+        PrometheusApis.QUERY,
+        PrometheusApis.QUERY_RANGE,
+        PrometheusApis.LABELS,
+    ]
+
+
+class VictoriaMetricsPrometheusConfig(PrometheusConfig):
+    supported_apis: List[PrometheusApis] = [
+        PrometheusApis.QUERY,
+        PrometheusApis.QUERY_RANGE,
+        PrometheusApis.LABELS,
+        PrometheusApis.VM_FLAGS,
+    ]
+
+
+# Does not support labels according to the docs, See below for apis
+# https://learn.microsoft.com/en-us/azure/azure-monitor/essentials/prometheus-api-promql#supported-apis
+class AzurePrometheusConfig(PrometheusConfig):
+    azure_resource: str
+    azure_metadata_endpoint: str
+    azure_token_endpoint: str
+    azure_use_managed_id: Optional[str] = None
+    azure_client_id: Optional[str] = None
+    azure_tenant_id: Optional[str] = None
+    azure_client_secret: Optional[str] = None
+    supported_apis: List[PrometheusApis] = [
+        PrometheusApis.QUERY,
+        PrometheusApis.QUERY_RANGE,
+    ]
