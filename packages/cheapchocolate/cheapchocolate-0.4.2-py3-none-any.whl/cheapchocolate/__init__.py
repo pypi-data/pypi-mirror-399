a60f38c7e5df6281f7e612b517ec3e6d24e858a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 02:34:50 +0000

    Add package cheapchocolate/0.4.2/cheapchocolate-0.4.2-py3-none-any.whl

---
+++ packages/cheapchocolate/cheapchocolate-0.4.2-py3-none-any.whl/cheapchocolate/__init__.py
@@ -0,0 +1,38 @@
+import argparse
+from cheapchocolate.modules.imap import get_mails, get_folders
+
+# import getpass
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="cheapchocolate")
+    parser.add_argument("--version", action="version", version="%(prog)s v0.4.2")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    start_parser = subparsers.add_parser(
+        "start",
+        help="📨 Get today's emails.",
+    )
+    start_parser.add_argument(
+        "--folder",
+        default="mail_folders",
+        help="Choose an specific mailbox folder to check.",
+    )
+
+    folder_parser = subparsers.add_parser(
+        "folders",
+        help="🗂️ Look for folder of my mailbox.",
+    )
+    args = parser.parse_args()
+
+    if args.command == "start":
+        get_mails(args.folder)
+
+    if args.command == "folders":
+        get_folders()
+
+    return
+
+
+if __name__ == "__main__":
+    main()
