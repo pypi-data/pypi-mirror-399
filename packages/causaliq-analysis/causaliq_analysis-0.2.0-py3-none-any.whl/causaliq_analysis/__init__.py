6deb2d7acfc1534edf11be3f4c836c188cd9594b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 03:32:13 +0000

    Add package causaliq-analysis/0.2.0/causaliq_analysis-0.2.0-py3-none-any.whl

---
+++ packages/causaliq-analysis/causaliq_analysis-0.2.0-py3-none-any.whl/causaliq_analysis/__init__.py
@@ -0,0 +1,24 @@
+"""
+causaliq-analysis: Tools for analysing and visualising causal graphs
+"""
+
+__version__ = "0.2.0"
+__author__ = "CausalIQ"
+__email__ = "info@causaliq.com"
+
+# Package metadata
+__title__ = "causaliq-analysis"
+__description__ = "Tools for analysing and visualising causal graphs"
+
+__url__ = "https://github.com/causaliq/causaliq-analysis"
+__license__ = "MIT"
+
+# Version tuple for programmatic access
+VERSION = tuple(map(int, __version__.split(".")))
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__email__",
+    "VERSION",
+]
