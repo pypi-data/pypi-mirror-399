d750eff87d35eb545b9d2e23e7e5cc852108ca18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:50 +0000

    Add package Rubio/1.0.0/rubio-1.0.0.tar.gz

---
+++ packages/Rubio/rubio-1.0.0.tar.gz/rubio-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Rubio",
+    version="1.0.0",
+    author="Rubio Team",
+    description="The Ultimate Elite Rubika Bot Framework. @RubioLib",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://t.me/RubioLib",
+    packages=find_packages(),
+    install_requires=["httpx"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
