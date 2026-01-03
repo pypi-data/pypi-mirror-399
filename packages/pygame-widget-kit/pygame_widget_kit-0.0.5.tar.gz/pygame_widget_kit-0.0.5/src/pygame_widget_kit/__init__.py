21e38addf00b57d0e1b79ff541c345e88d461ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:14:36 +0000

    Add package pygame-widget-kit/0.0.5/pygame_widget_kit-0.0.5.tar.gz

---
+++ packages/pygame-widget-kit/pygame_widget_kit-0.0.5.tar.gz/pygame_widget_kit-0.0.5/src/pygame_widget_kit/__init__.py
@@ -0,0 +1,11 @@
+from .UIComponent import *
+from .Widget import *
+from .Text import *
+from .Button import *
+from .UIManager import*
+from .Select import*
+from .TextInput import*
+from .Radio import*
+from .Slider import*
+from .TextArea import*
+from .TextArea import*
