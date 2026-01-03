ae83b5aa6536cf895805ed5d52ce451ee66b9d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:06:28 +0000

    Add package distributions-avol/0.1/distributions_avol-0.1.tar.gz

---
+++ packages/distributions-avol/distributions_avol-0.1.tar.gz/distributions_avol-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="distributions_avol",
+    version="0.1",
+    description="Gaussian distributions",
+    packages=["distributions"],
+    zip_safe=False,
+    install_requires=[
+        'matplotlib'
+    ]
+)
