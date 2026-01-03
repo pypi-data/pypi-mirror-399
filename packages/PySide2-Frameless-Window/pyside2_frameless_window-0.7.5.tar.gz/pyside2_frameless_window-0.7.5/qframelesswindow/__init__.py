a82f77faa1ec300845ba3090f6097512614ab38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 14:06:23 +0000

    Add package PySide2-Frameless-Window/0.7.5/pyside2_frameless_window-0.7.5.tar.gz

---
+++ packages/PySide2-Frameless-Window/pyside2_frameless_window-0.7.5.tar.gz/pyside2_frameless_window-0.7.5/qframelesswindow/__init__.py
@@ -0,0 +1,75 @@
+"""
+PySide2-Frameless-Window
+========================
+A cross-platform frameless window based on pyside2, support Win32, Linux and macOS.
+
+Documentation is available in the docstrings and
+online at https://pyqt-frameless-window.readthedocs.io.
+
+Examples are available at https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/Pyside2/examples.
+
+:copyright: (c) 2021 by zhiyiYo.
+:license: LGPLv3, see LICENSE for more details.
+"""
+
+__version__ = "0.7.5"
+__author__ = "zhiyiYo"
+
+import sys
+
+from PySide2.QtWidgets import QDialog, QMainWindow
+
+from .titlebar import TitleBar, TitleBarButton, SvgTitleBarButton, StandardTitleBar, TitleBarBase
+
+if sys.platform == "win32":
+    from .windows import AcrylicWindow as AcrylicWindowBase
+    from .windows import WindowsFramelessWindow as FramelessWindowBase
+    from .windows import WindowsWindowEffect as WindowEffect
+elif sys.platform == "darwin":
+    from .mac import AcrylicWindow as AcrylicWindowBase
+    from .mac import MacFramelessWindow as FramelessWindowBase
+    from .mac import MacWindowEffect as WindowEffect
+else:
+    from .linux import LinuxFramelessWindow as FramelessWindowBase
+    from .linux import LinuxWindowEffect as WindowEffect
+
+    AcrylicWindowBase = FramelessWindowBase
+
+
+class FramelessWindow(FramelessWindowBase):
+    """ Frameless window """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self._initFrameless()
+
+
+class FramelessDialog(QDialog, FramelessWindowBase):
+    """ Frameless dialog """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self._initFrameless()
+        self.titleBar.minBtn.hide()
+        self.titleBar.maxBtn.hide()
+        self.titleBar.setDoubleClickEnabled(False)
+        self.windowEffect.disableMaximizeButton(self.winId())
+
+    def resizeEvent(self, e):
+        self.titleBar.resize(self.width(), self.titleBar.height())
+
+
+class FramelessMainWindow(QMainWindow, FramelessWindowBase):
+    """ Frameless main window """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self._initFrameless()
+
+
+class AcrylicWindow(AcrylicWindowBase):
+    """ Acrylic window """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self._initFrameless()
---
+++ packages/PySide2-Frameless-Window/pyside2_frameless_window-0.7.5.tar.gz/pyside2_frameless_window-0.7.5/qframelesswindow/webengine/__init__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import sys
+
+from PySide2.QtCore import Qt
+from PySide2.QtWebEngineWidgets import QWebEngineView
+from qframelesswindow import AcrylicWindow, FramelessWindowBase
+
+
+class FramelessWebEngineView(QWebEngineView):
+    """ Frameless web engine view """
+
+    def __init__(self, parent):
+        if sys.platform == "win32" and isinstance(parent.window(), AcrylicWindow):
+            parent.window().setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        super().__init__(parent=parent)
+
+        if sys.platform in ("win32", "darwin"):
+            self.setHtml("")
+
+        if isinstance(self.window(), FramelessWindowBase):
+            self.window().updateFrameless()
