d47f29b635c569faf346e818d2e8268a1a98ea9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:11:36 +0000

    Add package deepseek-ocr-ollama/1.2/deepseek_ocr_ollama-1.2.tar.gz

---
+++ packages/deepseek-ocr-ollama/deepseek_ocr_ollama-1.2.tar.gz/deepseek_ocr_ollama-1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+setup(
+  name="deepseek-ocr-ollama",
+  version="1.2",
+  packages=find_packages(),
+  description="A simple script that uses the Ollama API to get the Markdown text from a PDF or image file from the DeepSeek-OCR model",
+  entry_points={
+    'console_scripts': [
+      'deepseek-ocr-ollama = deepseek_ocr_ollama.__main__:main',
+    ],
+  },
+  url="https://github.com/jfhack/deepseek-ocr-ollama",
+  install_requires=[
+    'ollama',
+    'pymupdf',
+    'python-dotenv',
+    'tqdm',
+    'pillow'
+  ],
+  long_description=long_description,
+  long_description_content_type="text/markdown"
+)
