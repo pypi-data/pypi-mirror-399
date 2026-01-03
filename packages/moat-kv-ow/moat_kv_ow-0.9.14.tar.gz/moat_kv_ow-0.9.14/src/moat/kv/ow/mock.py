e7482a81e6de663da5ff502418dc0adce3e7456c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:11 +0000

    Add package moat-kv-ow/0.9.14/moat_kv_ow-0.9.14.tar.gz

---
+++ packages/moat-kv-ow/moat_kv_ow-0.9.14.tar.gz/moat_kv_ow-0.9.14/src/moat/kv/ow/mock.py
@@ -0,0 +1,32 @@
+from __future__ import annotations  # noqa: D100
+
+import anyio
+import contextlib
+from functools import partial
+
+from asyncowfs.mock import some_server
+
+from .task import task
+
+
+async def server(client, tree: dict | None = None, options: dict | None = None, evt=None):  # noqa: D103
+    async with anyio.create_task_group() as tg:
+        listener = await anyio.create_tcp_listener(
+            local_host="127.0.0.1",
+            local_port=0,
+            reuse_port=True,
+        )
+
+        async def may_close():
+            with contextlib.suppress(anyio.ClosedResourceError, anyio.BrokenResourceError):
+                await listener.serve(partial(some_server, tree or {}, options or {}))
+
+        addr = listener.extra(anyio.abc.SocketAttribute.raw_socket).getsockname()
+        tg.start_soon(may_close)
+
+        await client.set(
+            client._cfg.ow.prefix + ("server", "127.0.0.1"),  # noqa: SLF001
+            value=dict(server=dict(host="127.0.0.1", port=addr[1])),
+        )
+
+        await task(client, client._cfg, "127.0.0.1", evt)  # noqa: SLF001
