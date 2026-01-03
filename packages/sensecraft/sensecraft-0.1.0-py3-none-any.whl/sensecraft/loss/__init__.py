cec2bc948dbbe7c0cf47fb2d5ba62e437465486a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:58 +0000

    Add package sensecraft/0.1.0/sensecraft-0.1.0-py3-none-any.whl

---
+++ packages/sensecraft/sensecraft-0.1.0-py3-none-any.whl/sensecraft/loss/__init__.py
@@ -0,0 +1,5 @@
+from .convnext import ConvNextPerceptualLoss
+from .convnext_dinov3 import ConvNextDinoV3PerceptualLoss
+from .gram_dinov3 import ViTDinoV3PerceptualLoss
+from .lpips import LPIPS
+from .general import CharbonnierLoss, PatchFFTLoss, FFTLoss, NormType, GaussianNoiseLoss
