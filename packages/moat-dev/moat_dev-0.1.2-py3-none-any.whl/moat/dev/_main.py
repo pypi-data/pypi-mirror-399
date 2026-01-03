045d84c70eb776b6646dc362b354a7460ee8ec20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:13 +0000

    Add package moat-dev/0.1.2/moat_dev-0.1.2-py3-none-any.whl

---
+++ packages/moat-dev/moat_dev-0.1.2-py3-none-any.whl/moat/dev/_main.py
@@ -0,0 +1,21 @@
+"""
+Basic tool support
+
+"""
+
+from __future__ import annotations
+
+import logging  # pylint: disable=wrong-import-position
+
+import asyncclick as click
+
+from moat.lib.run import load_subgroup
+
+log = logging.getLogger()
+
+
+@load_subgroup(prefix="moat.dev")
+@click.pass_obj
+async def cli(obj):
+    """Device Manager"""
+    obj  # noqa:B018
