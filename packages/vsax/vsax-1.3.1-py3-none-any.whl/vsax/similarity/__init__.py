aad22fa52886c0a721eb732f2115bcbad5c57a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 16:09:01 +0000

    Add package vsax/1.3.1/vsax-1.3.1-py3-none-any.whl

---
+++ packages/vsax/vsax-1.3.1-py3-none-any.whl/vsax/similarity/__init__.py
@@ -0,0 +1,11 @@
+"""Similarity metrics for hypervectors."""
+
+from vsax.similarity.cosine import cosine_similarity
+from vsax.similarity.dot import dot_similarity
+from vsax.similarity.hamming import hamming_similarity
+
+__all__ = [
+    "cosine_similarity",
+    "dot_similarity",
+    "hamming_similarity",
+]
