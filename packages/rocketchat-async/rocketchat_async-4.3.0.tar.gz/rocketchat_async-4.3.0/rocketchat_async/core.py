badf57a22aee266bce6433132829514f94c7c679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:06:13 +0000

    Add package rocketchat-async/4.3.0/rocketchat_async-4.3.0.tar.gz

---
+++ packages/rocketchat-async/rocketchat_async-4.3.0.tar.gz/rocketchat_async-4.3.0/rocketchat_async/core.py
@@ -0,0 +1,168 @@
+import asyncio
+import websockets
+
+from rocketchat_async.dispatcher import Dispatcher
+from rocketchat_async.methods import Connect, Login, Resume, GetChannels, GetChannelsRaw,\
+        SendMessage, SendReaction, SendTypingEvent, SubscribeToChannelMessages,\
+        SubscribeToChannelMessagesRaw, SubscribeToChannelChanges, SubscribeToChannelChangesRaw,\
+        Unsubscribe
+
+
+class RocketChat:
+    """Represents a connection to RocketChat, exposing the API."""
+
+    class ConnectionClosed(Exception):
+        pass
+
+    class ConnectCallFailed(Exception):
+        pass
+
+    def __init__(self, verbose=False):
+        self.user_id = None
+        self.username = None
+        self._dispatcher = Dispatcher(verbose)
+
+    async def start(self, address, username, password):
+        ws_connected = asyncio.get_event_loop().create_future()
+        ws_connection = self._start(address, ws_connected)
+        self._ws_connection_task = asyncio.create_task(ws_connection)
+        try:
+            await ws_connected
+        except (OSError, websockets.InvalidMessage) as e:
+            # Exceptions that can arise during temporary network glitches or
+            # outages on the remote side.
+            # See also https://github.com/aaugustin/websockets/issues/593
+            raise self.ConnectCallFailed(e)
+
+        # Connect and login.
+        await self._connect()
+        self.user_id = await self._login(username, password)
+        self.username = username
+
+    async def resume(self, address, username, token):
+        ws_connected = asyncio.get_event_loop().create_future()
+        ws_connection = self._start(address, ws_connected)
+        self._ws_connection_task = asyncio.create_task(ws_connection)
+        try:
+            await ws_connected
+        except (OSError, websockets.InvalidMessage) as e:
+            # Exceptions that can arise during temporary network glitches or
+            # outages on the remote side.
+            # See also https://github.com/aaugustin/websockets/issues/593
+            raise self.ConnectCallFailed(e)
+
+        # Connect and login.
+        await self._connect()
+        self.user_id = await self._resume(token)
+        self.username = username
+
+    async def run_forever(self):
+        try:
+            await self.dispatch_task
+        except websockets.ConnectionClosed as e:
+            raise self.ConnectionClosed(e)
+
+    async def _start(self, address, connected_fut):
+        try:
+            async with websockets.connect(address) as websocket:
+                self.dispatch_task = self._dispatcher.run(websocket)
+                # Notify the caller that login has succeeded.
+                connected_fut.set_result(True)
+                # Finally, create the ever-running dispatcher loop.
+                await self.dispatch_task
+        except Exception as e:
+            if not connected_fut.done():
+                connected_fut.set_exception(e)
+
+    async def _connect(self):
+        await Connect.call(self._dispatcher)
+
+    async def _login(self, username, password):
+        return await Login.call(self._dispatcher, username, password)
+
+    async def _resume(self, token):
+        return await Resume.call(self._dispatcher, token)
+
+    # --> Public API methods start here. <--
+
+    async def get_channels(self):
+        """
+        Get a list of channels user is currently member of.
+
+        Returns a list of (channel id, channel type) pairs.
+
+        """
+        return await GetChannels.call(self._dispatcher)
+
+    async def get_channels_raw(self):
+        """
+        Get a list of channels user is currently member of.
+
+        Returns a list of channel objects.
+
+        """
+        return await GetChannelsRaw.call(self._dispatcher)
+
+    async def send_message(self, text, channel_id, thread_id=None):
+        """Send a text message to a channel."""
+        await SendMessage.call(self._dispatcher, text, channel_id, thread_id)
+
+    async def send_reaction(self, orig_msg_id, emoji):
+        """Send a reaction to a specific message."""
+        await SendReaction.call(self._dispatcher, orig_msg_id, emoji)
+
+    async def send_typing_event(self, channel_id, thread_id=None):
+        """Send the `typing` event to a channel."""
+        await SendTypingEvent.call(self._dispatcher, channel_id, self.username, thread_id)
+
+    async def subscribe_to_channel_messages(self, channel_id, callback):
+        """
+        Subscribe to all messages in the given channel.
+
+        Returns the subscription ID.
+
+        """
+        sub_id = await SubscribeToChannelMessages.call(self._dispatcher,
+                                                       channel_id, callback)
+        return sub_id
+
+    async def subscribe_to_channel_messages_raw(self, channel_id, callback):
+        """
+        Subscribe to all messages in the given channel.
+
+        The callback is passed the full message object as provided by the API.
+
+        Returns the subscription ID.
+
+        """
+        sub_id = await SubscribeToChannelMessagesRaw.call(self._dispatcher,
+                                                          channel_id, callback)
+        return sub_id
+
+    async def subscribe_to_channel_changes(self, callback):
+        """
+        Subscribe to all changes in channels.
+
+        Returns the subscription ID.
+
+        """
+        sub_id = await SubscribeToChannelChanges.call(self._dispatcher,
+                                                      self.user_id, callback)
+        return sub_id
+
+    async def subscribe_to_channel_changes_raw(self, callback):
+        """
+        Subscribe to all changes in channels.
+
+        The callback is passed the full message object as provided by the API.
+
+        Returns the subscription ID.
+
+        """
+        sub_id = await SubscribeToChannelChangesRaw.call(self._dispatcher,
+                                                         self.user_id, callback)
+        return sub_id
+
+    async def unsubscribe(self, subscription_id):
+        """Cancel a subscription."""
+        await Unsubscribe.call(self._dispatcher, subscription_id)
---
+++ packages/rocketchat-async/rocketchat_async-4.3.0.tar.gz/rocketchat_async-4.3.0/rocketchat_async/methods.py
@@ -0,0 +1,337 @@
+import hashlib
+import time
+
+
+class RealtimeRequest:
+    """Method call or subscription in the RocketChat realtime API."""
+    _max_id = 0
+
+    @staticmethod
+    def _get_new_id():
+        RealtimeRequest._max_id += 1
+        return f'{RealtimeRequest._max_id}'
+
+
+class Connect(RealtimeRequest):
+    """Initialize the connection."""
+
+    REQUEST_MSG = {
+        'msg': 'connect',
+        'version': '1',
+        'support': ['1'],
+    }
+
+    @classmethod
+    async def call(cls, dispatcher):
+        await dispatcher.call_method(cls.REQUEST_MSG)
+
+
+class Resume(RealtimeRequest):
+    """Log in to the service with a token."""
+
+    @staticmethod
+    def _get_request_msg(msg_id, token):
+        return {
+            "msg": "method",
+            "method": "login",
+            "id": msg_id,
+            "params": [
+                {
+                    "resume": token,
+                }
+            ]
+        }
+
+    @staticmethod
+    def _parse(response):
+        return response['result']['id']
+
+    @classmethod
+    async def call(cls, dispatcher, token):
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id, token)
+        response = await dispatcher.call_method(msg, msg_id)
+        return cls._parse(response)
+
+
+class Login(RealtimeRequest):
+    """Log in to the service."""
+
+    @staticmethod
+    def _get_request_msg(msg_id, username, password):
+        pwd_digest = hashlib.sha256(password.encode()).hexdigest()
+        return {
+            "msg": "method",
+            "method": "login",
+            "id": msg_id,
+            "params": [
+                {
+                    "user": {"username": username},
+                    "password": {
+                        "digest": pwd_digest,
+                        "algorithm": "sha-256"
+                    }
+                }
+            ]
+        }
+
+    @staticmethod
+    def _parse(response):
+        if "error" in response:
+            raise RuntimeError(response['error']['reason'])
+        return response['result']['id']
+
+    @classmethod
+    async def call(cls, dispatcher, username, password):
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id, username, password)
+        response = await dispatcher.call_method(msg, msg_id)
+        return cls._parse(response)
+
+
+class GetChannelsRaw(RealtimeRequest):
+    """
+    Get a list of channels user is currently member of.
+
+    Returns the complete channel objects.
+
+    """
+
+    @staticmethod
+    def _get_request_msg(msg_id):
+        return {
+            'msg': 'method',
+            'method': 'rooms/get',
+            'id': msg_id,
+            'params': [],
+        }
+
+    @staticmethod
+    def _parse(response):
+        return response['result']
+
+    @classmethod
+    async def call(cls, dispatcher):
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id)
+        response = await dispatcher.call_method(msg, msg_id)
+        return cls._parse(response)
+
+
+class GetChannels(GetChannelsRaw):
+    """
+    Get a list of channels user is currently member of.
+
+    Returns a list of (channel id, channel type) pairs.
+
+    """
+
+    @classmethod
+    def _parse(cls, response):
+        # Return channel IDs and channel types.
+        return [(r['_id'], r['t']) for r in super()._parse(response)]
+
+
+class SendMessage(RealtimeRequest):
+    """Send a text message to a channel."""
+
+    @staticmethod
+    def _get_request_msg(msg_id, channel_id, msg_text, thread_id=None):
+        id_seed = f'{msg_id}:{time.time()}'
+        msg = {
+            "msg": "method",
+            "method": "sendMessage",
+            "id": msg_id,
+            "params": [
+                {
+                    "_id": hashlib.md5(id_seed.encode()).hexdigest()[:12],
+                    "rid": channel_id,
+                    "msg": msg_text
+                }
+            ]
+        }
+        if thread_id is not None:
+            msg["params"][0]["tmid"] = thread_id
+        return msg
+
+    @classmethod
+    async def call(cls, dispatcher, msg_text, channel_id, thread_id=None):
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id, channel_id, msg_text, thread_id)
+        await dispatcher.call_method(msg, msg_id)
+
+
+class SendReaction(RealtimeRequest):
+    """Send a reaction to a specific message."""
+
+    @staticmethod
+    def _get_request_msg(msg_id, orig_msg_id, emoji):
+        return {
+            "msg": "method",
+            "method": "setReaction",
+            "id": msg_id,
+            "params": [
+                emoji,
+                orig_msg_id,
+            ]
+        }
+
+    @classmethod
+    async def call(cls, dispatcher, orig_msg_id, emoji):
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id, orig_msg_id, emoji)
+        await dispatcher.call_method(msg)
+
+
+class SendTypingEvent(RealtimeRequest):
+    """Send the `typing` event to a channel."""
+
+    @staticmethod
+    def _get_request_msg(msg_id, channel_id, username, thread_id=None):
+        msg = {
+            "msg": "method",
+            "method": "stream-notify-room",
+            "id": msg_id,
+            "params": [
+                f'{channel_id}/user-activity',
+                username,
+                ["user-typing"],
+                {}
+            ]
+        }
+        if(thread_id):
+            msg["params"][-1]["tmid"] = thread_id
+        return msg
+
+    @classmethod
+    async def call(cls, dispatcher, channel_id, username, thread_id=None):
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id, channel_id, username, thread_id)
+        await dispatcher.call_method(msg, msg_id)
+
+
+class SubscribeToChannelMessagesRaw(RealtimeRequest):
+    """
+    Subscribe to all messages in the given channel.
+
+    Passes the raw message object to the callback.
+
+    """
+
+    @staticmethod
+    def _get_request_msg(msg_id, channel_id):
+        return {
+            "msg": "sub",
+            "id": msg_id,
+            "name": "stream-room-messages",
+            "params": [
+                channel_id,
+                {
+                    "useCollection": False,
+                    "args": []
+                }
+            ]
+        }
+
+    @staticmethod
+    def _wrap(callback):
+        def fn(msg):
+            event = msg["fields"]["args"][0]  # TODO: This looks suspicious.
+            return callback(event)
+
+        return fn
+
+    @classmethod
+    async def call(cls, dispatcher, channel_id, callback):
+        # TODO: document the expected interface of the callback.
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id, channel_id)
+        await dispatcher.create_subscription(msg, msg_id, cls._wrap(callback))
+        return msg_id  # Return the ID to allow for later unsubscription.
+
+
+class SubscribeToChannelMessages(SubscribeToChannelMessagesRaw):
+    """Subscribe to all messages in the given channel."""
+
+    @staticmethod
+    def _wrap(callback):
+        def fn(msg):
+            event = msg['fields']['args'][0]  # TODO: This looks suspicious.
+            msg_id = event['_id']
+            channel_id = event['rid']
+            thread_id = event.get('tmid')
+            sender_id = event['u']['_id']
+            msg = event['msg']
+            qualifier = event.get('t')
+            unread = event.get('unread', False)
+            repeated = bool(event.get('replies')) or bool(event.get('reactions'))
+            return callback(channel_id, sender_id, msg_id, thread_id, msg,
+                            qualifier, unread, repeated)
+        return fn
+
+
+class SubscribeToChannelChangesRaw(RealtimeRequest):
+    """
+    Subscribe to all messages in the given channel.
+
+    Passes the raw message object to the callback.
+
+    """
+
+    @staticmethod
+    def _get_request_msg(msg_id, user_id):
+        return {
+            "msg": "sub",
+            "id": msg_id,
+            "name": "stream-notify-user",
+            "params": [
+                f'{user_id}/rooms-changed',
+                False
+            ]
+        }
+
+    @staticmethod
+    def _wrap(callback):
+        def fn(msg):
+            payload = msg['fields']['args']
+            return callback(payload)
+        return fn
+
+    @classmethod
+    async def call(cls, dispatcher, user_id, callback):
+        # TODO: document the expected interface of the callback.
+        msg_id = cls._get_new_id()
+        msg = cls._get_request_msg(msg_id, user_id)
+        await dispatcher.create_subscription(msg, msg_id, cls._wrap(callback))
+        return msg_id  # Return the ID to allow for later unsubscription.
+
+
+class SubscribeToChannelChanges(SubscribeToChannelChangesRaw):
+    """Subscribe to all changes in channels."""
+
+    @staticmethod
+    def _wrap(callback):
+        def fn(msg):
+            payload = msg['fields']['args']
+            if payload[0] == 'removed':
+                return  # Nothing else to do - channel has just been deleted.
+            channel_id = payload[1]['_id']
+            channel_type = payload[1]['t']
+            return callback(channel_id, channel_type)
+        return fn
+
+
+class Unsubscribe(RealtimeRequest):
+    """Cancel a subscription"""
+
+    @staticmethod
+    def _get_request_msg(subscription_id):
+        return {
+            "msg": "unsub",
+            "id": subscription_id,
+        }
+
+    @classmethod
+    async def call(cls, dispatcher, subscription_id):
+        msg = cls._get_request_msg(subscription_id)
+        await dispatcher.call_method(msg)
