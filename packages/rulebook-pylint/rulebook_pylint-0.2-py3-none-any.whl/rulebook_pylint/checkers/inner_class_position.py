e46fafc2ab7ab81f74f73118762e870cbc946cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:53 +0000

    Add package rulebook-pylint/0.2/rulebook_pylint-0.2-py3-none-any.whl

---
+++ packages/rulebook-pylint/rulebook_pylint-0.2-py3-none-any.whl/rulebook_pylint/checkers/inner_class_position.py
@@ -0,0 +1,35 @@
+from astroid.nodes import NodeNG, ClassDef, FunctionDef, AssignName, Assign
+from pylint.typing import TYPE_CHECKING, MessageDefinitionTuple
+from rulebook_pylint.checkers.rulebook_checkers import RulebookChecker
+from rulebook_pylint.messages import _Messages
+
+if TYPE_CHECKING:
+    from pylint.lint import PyLinter
+
+
+class InnerClassPositionChecker(RulebookChecker):
+    """See detail: https://hanggrian.github.io/rulebook/rules/#inner-class-position"""
+    MSG: str = 'inner-class-position'
+
+    name: str = 'inner-class-position'
+    msgs: dict[str, MessageDefinitionTuple] = _Messages.of(MSG)
+
+    def visit_classdef(self, node: ClassDef) -> None:
+        # consider only inner class
+        if node.parent and not isinstance(node.parent, ClassDef):
+            return
+
+        next2: NodeNG = node
+        while next2:
+            next2 = next2.next_sibling()
+
+            # checks for violation
+            if isinstance(next2, FunctionDef) or \
+                isinstance(next2, Assign) or \
+                isinstance(next2, AssignName):
+                self.add_message(self.MSG, node=node)
+                return
+
+
+def register(linter: 'PyLinter') -> None:
+    linter.register_checker(InnerClassPositionChecker(linter))
---
+++ packages/rulebook-pylint/rulebook_pylint-0.2-py3-none-any.whl/rulebook_pylint/checkers/rulebook_checkers.py
@@ -0,0 +1,21 @@
+from abc import ABC
+
+from astroid.nodes import Module
+from pylint.checkers import BaseChecker, BaseTokenChecker, BaseRawFileChecker
+
+
+class RulebookChecker(BaseChecker):
+    """Standard checker with `visit_` prefix functions."""
+
+
+class RulebookTokenChecker(BaseTokenChecker, ABC):
+    """Override `process_tokens` to iterate each token."""
+
+
+class RulebookFileChecker(BaseRawFileChecker, ABC):
+    """Override `process_module` to capture file at once."""
+    lines: list[bytes]
+
+    def process_module(self, node: Module) -> None:
+        with node.stream() as stream:
+            self.lines = [s.strip() for s in stream.readlines()]
