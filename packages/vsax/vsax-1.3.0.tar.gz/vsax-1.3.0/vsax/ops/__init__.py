d2386867d32f54846c0f6bf5e4268a515c8fecf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:46 +0000

    Add package vsax/1.3.0/vsax-1.3.0.tar.gz

---
+++ packages/vsax/vsax-1.3.0.tar.gz/vsax-1.3.0/vsax/ops/__init__.py
@@ -0,0 +1,11 @@
+"""VSA operation sets."""
+
+from vsax.ops.binary import BinaryOperations
+from vsax.ops.fhrr import FHRROperations
+from vsax.ops.map import MAPOperations
+
+__all__ = [
+    "FHRROperations",
+    "MAPOperations",
+    "BinaryOperations",
+]
---
+++ packages/vsax/vsax-1.3.0.tar.gz/vsax-1.3.0/vsax/representations/__init__.py
@@ -0,0 +1,11 @@
+"""Hypervector representations."""
+
+from vsax.representations.binary_hv import BinaryHypervector
+from vsax.representations.complex_hv import ComplexHypervector
+from vsax.representations.real_hv import RealHypervector
+
+__all__ = [
+    "ComplexHypervector",
+    "RealHypervector",
+    "BinaryHypervector",
+]
