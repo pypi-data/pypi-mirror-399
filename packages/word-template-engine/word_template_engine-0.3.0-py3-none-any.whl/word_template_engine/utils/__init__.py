b4a26793e1a1e2e8304606782dd4100842c154ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 08:06:17 +0000

    Add package word-template-engine/0.3.0/word_template_engine-0.3.0-py3-none-any.whl

---
+++ packages/word-template-engine/word_template_engine-0.3.0-py3-none-any.whl/word_template_engine/utils/__init__.py
@@ -0,0 +1,16 @@
+"""
+Utils package for word-template-engine.
+
+This package contains the internal processors used by `WordTemplateEngine`:
+- text_processor: text and block placeholders
+- table_processor: table loops
+- image_processor: image placeholders
+"""
+
+__all__ = [
+    "text_processor",
+    "table_processor",
+    "image_processor",
+    "block_processor",
+    "list_processor",
+]
