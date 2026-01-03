84c1dc3679fc4169a5c13d7931541463461511b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 05:06:55 +0000

    Add package pai-sdk/0.0.1/pai_sdk-0.0.1.tar.gz

---
+++ packages/pai-sdk/pai_sdk-0.0.1.tar.gz/pai_sdk-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="pai-sdk",  # Der Name, den du willst
+    version="0.0.1",
+    description="Reserved for PAI-OS SDK",
+    url="https://github.com/aurintex/pai-os",
+    author="Aurintex",
+    author_email="contact@aurintex.com", # Deine Email
+    license="MIT",
+    packages=[], # Leer lassen für Reservierung
+    install_requires=[],
+)
