2d56d4b3d7931929a796a4358ad912ea8c274de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:03 +0000

    Add package systemeval/0.1.0/systemeval-0.1.0.tar.gz

---
+++ packages/systemeval/systemeval-0.1.0.tar.gz/systemeval-0.1.0/systemeval/__init__.py
@@ -0,0 +1,3 @@
+"""SystemEval - Unified test runner CLI with framework-agnostic adapters."""
+
+__version__ = "0.1.0"
---
+++ packages/systemeval/systemeval-0.1.0.tar.gz/systemeval-0.1.0/systemeval/adapters/__init__.py
@@ -0,0 +1,17 @@
+"""Test framework adapters for systemeval."""
+
+from .base import BaseAdapter, TestFailure, TestItem, TestResult
+from .registry import get_adapter, is_registered, list_adapters, register_adapter
+
+__all__ = [
+    # Base classes and data structures
+    "BaseAdapter",
+    "TestItem",
+    "TestResult",
+    "TestFailure",
+    # Registry functions
+    "register_adapter",
+    "get_adapter",
+    "list_adapters",
+    "is_registered",
+]
