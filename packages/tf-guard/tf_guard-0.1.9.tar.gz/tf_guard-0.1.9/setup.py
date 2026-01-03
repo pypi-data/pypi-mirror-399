914307753597218dc3b265c9fc7155a620ae6b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:08:55 +0000

    Add package tf-guard/0.1.9/tf_guard-0.1.9.tar.gz

---
+++ packages/tf-guard/tf_guard-0.1.9.tar.gz/tf_guard-0.1.9/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="tf-guard",
+    version="0.1.9",
+    py_modules=["main", "parser", "analyzer", "naming", "utils"],
+    install_requires=["click", "openai", "rich", "python-dotenv"],
+    entry_points={
+        "console_scripts": [
+            "ftltf=main:cli",
+        ],
+    },
+)
