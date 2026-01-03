ca9eed611a145e953973ec98e25a7d4c0125a483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 17:12:00 +0000

    Add package madsci.experiment_application/0.6.1/madsci_experiment_application-0.6.1-py3-none-any.whl

---
+++ packages/madsci.experiment_application/madsci_experiment_application-0.6.1-py3-none-any.whl/madsci/experiment_application/__init__.py
@@ -0,0 +1,5 @@
+"""Experiment Application framework for MADSci."""
+
+from .experiment_application import ExperimentApplication, ExperimentApplicationConfig
+
+__all__ = ["ExperimentApplication", "ExperimentApplicationConfig"]
