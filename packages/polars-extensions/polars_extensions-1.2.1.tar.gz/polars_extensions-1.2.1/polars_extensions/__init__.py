5c4af814c5738501473bd50e79a3555fb56e069e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:14:18 +0000

    Add package polars-extensions/1.2.1/polars_extensions-1.2.1.tar.gz

---
+++ packages/polars-extensions/polars_extensions-1.2.1.tar.gz/polars_extensions-1.2.1/polars_extensions/__init__.py
@@ -0,0 +1,24 @@
+from .io import *
+from .name import *
+from .numeric import *
+from .string import *
+from .geo import *
+from .units import *
+from .url import *
+from .biology import *
+from .technical_analysis import *
+
+__all__ = [
+    "NameExtensionNameSpace",
+    "NumericExtensionNamespace",
+    "StringExtensionNamespace",
+    "GeometryExtensionNamespace",
+    "UnitExtensionNamespace",
+    "UrlExtensionNamespace",
+    "BioExtensionNamespace",
+    "TechnicalAnalysisNamespace",
+    "write_schema",
+    "read_schema",
+    "read_xml",
+]
+ 
