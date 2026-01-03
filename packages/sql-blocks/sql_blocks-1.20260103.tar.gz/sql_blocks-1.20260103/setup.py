a844dfdb4ea227f2ed2d63d7143922cd5dd558e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:06:33 +0000

    Add package sql-blocks/1.20260103/sql_blocks-1.20260103.tar.gz

---
+++ packages/sql-blocks/sql_blocks-1.20260103.tar.gz/sql_blocks-1.20260103/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name = 'sql_blocks',
+    version = '1.20260103',
+    author = 'Júlio Cascalles',
+    author_email = 'julio.cascalles@outlook.com',
+    packages = ['sql_blocks'],
+    url = 'https://github.com/julio-cascalles/sql_blocks',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ]
+)
