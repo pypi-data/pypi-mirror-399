2cb5820c18a5db9c7506fdfcaf0d77161af18a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:14:22 +0000

    Add package subtask-manager/0.1.1/subtask_manager-0.1.1-cp39-abi3-win_amd64.whl

---
+++ packages/subtask-manager/subtask_manager-0.1.1-cp39-abi3-win_amd64.whl/subtask_manager/__init__.py
@@ -0,0 +1,21 @@
+from ._core import (
+    EtlStage,
+    FileClassifier,
+    FileScanner,
+    ParamType,
+    Subtask,
+    SubtaskManager,
+    SystemType,
+    TaskType,
+)
+
+__all__ = [
+    "EtlStage",
+    "FileClassifier",
+    "FileScanner",
+    "ParamType",
+    "SubtaskManager",
+    "Subtask",
+    "SystemType",
+    "TaskType",
+]
