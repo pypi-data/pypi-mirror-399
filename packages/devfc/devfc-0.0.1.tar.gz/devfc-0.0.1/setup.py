7a65da28035ba319888b21e0b99045c1130f1d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 14:06:26 +0000

    Add package devfc/0.0.1/devfc-0.0.1.tar.gz

---
+++ packages/devfc/devfc-0.0.1.tar.gz/devfc-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="devfc", 
+    version="0.0.1",
+    description="The Flight Controller for Developers",
+    author="Dhanush",
+    author_email="dhanushken@gmail.com", 
+    packages=[],
+    install_requires=[],
+)
