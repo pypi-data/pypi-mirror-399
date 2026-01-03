2dd7a7ac171d83a6c6b769ae9a33c2ca78cde42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 07:08:23 +0000

    Add package rpycdec/0.1.8/rpycdec-0.1.8-py3-none-any.whl

---
+++ packages/rpycdec/rpycdec-0.1.8-py3-none-any.whl/rpycdec/cli.py
@@ -0,0 +1,122 @@
+import argparse
+import logging
+import os
+from rpycdec.decompile import decompile
+from rpycdec.rpa import extract_rpa
+from rpycdec.save import extract_save, restore_save
+from rpycdec.translate import extract_translate
+
+
+logger = logging.getLogger(__name__)
+
+
+def decompile_files(srcs: list[str], **kwargs):
+    """
+    decompile rpyc file or directory.
+    """
+    for src in srcs:
+        decompile(src, dis=kwargs.get("disassemble", False))
+
+
+def extract_rpa_files(srcs: list[str], **kwargs):
+    """
+    extract rpa archive.
+    """
+    for src in srcs:
+        with open(src, "rb") as f:
+            extract_rpa(f, dir=os.path.dirname(src))
+
+
+def run_extract_translations(srcs: list[str], dest: str, **kwargs):
+    """
+    extract translations from rpy files.
+    """
+    for src in srcs:
+        extract_translate(src, dest, **kwargs)
+
+
+def main():
+    """
+    command line tool entry.
+    """
+    logging.basicConfig(level=logging.INFO)
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "--verbose", "-v", action="store_true", help="verbose output"
+    )
+    subparsers = argparser.add_subparsers(
+        title="subcommands", dest="command", help="subcommand help"
+    )
+
+    decompile_parser = subparsers.add_parser("decompile", help="decompile rpyc file")
+    decompile_parser.add_argument("src", nargs=1, help="rpyc file or directory")
+    decompile_parser.add_argument(
+        "--disassemble", "-d", action="store_true", help="disassemble rpyc file"
+    )
+    decompile_parser.set_defaults(
+        func=lambda args: decompile_files(args.src, **vars(args))
+    )
+
+    unrpa_parser = subparsers.add_parser("unrpa", help="extract rpa archive")
+    unrpa_parser.add_argument("file", nargs=1, help="rpa archive")
+    unrpa_parser.set_defaults(
+        func=lambda args: extract_rpa_files(args.file, **vars(args))
+    )
+
+    extract_translate_parser = subparsers.add_parser(
+        "extract-translate",
+        help="""extract translations from rpy files and save to tl/<language> directory.""",
+    )
+    extract_translate_parser.add_argument(
+        "src", nargs="+", help="source game directory"
+    )
+    extract_translate_parser.add_argument(
+        "--output",
+        "-o",
+        required=True,
+        help="directory to save translations",
+    )
+    extract_translate_parser.add_argument(
+        "--src-lang",
+        default="en",
+        help="source language (default: en)",
+    )
+    extract_translate_parser.add_argument(
+        "--dest-lang",
+        required=True,
+        help="destination language",
+    )
+    extract_translate_parser.set_defaults(
+        func=lambda args: run_extract_translations(args.src, args.output, **vars(args))
+    )
+
+    save_parser = subparsers.add_parser("save", help="save operations")
+    save_subparsers = save_parser.add_subparsers(
+        title="save subcommands", dest="save_command", help="save subcommand help"
+    )
+    save_extract_parser = save_subparsers.add_parser(
+        "extract", help="extract save data"
+    )
+    save_extract_parser.add_argument("path", nargs=1, help="save-file path")
+    save_extract_parser.add_argument("dest", nargs="?", help="extract to directory")
+    save_extract_parser.set_defaults(
+        func=lambda args: extract_save(args.path[0], args.dest, **vars(args))
+    )
+
+    save_restore_parser = save_subparsers.add_parser(
+        "restore", help="restore save data from extracted directory"
+    )
+    save_restore_parser.add_argument("path", nargs=1, help="extracted directory")
+    save_restore_parser.add_argument("dest", nargs=1, help="restore to save-file")
+    save_restore_parser.set_defaults(
+        func=lambda args: restore_save(args.path[0], args.dest[0])
+    )
+
+    args = argparser.parse_args()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    if not args.command:
+        argparser.print_help()
+        return
+    args.func(args)
---
+++ packages/rpycdec/rpycdec-0.1.8-py3-none-any.whl/rpycdec/decompile.py
@@ -0,0 +1,50 @@
+"""Module for decompiling Ren'Py compiled files (.rpyc) back to source code (.rpy)."""
+
+import logging
+import os
+
+from renpy import util
+from rpycdec import stmts, utils
+
+logger = logging.getLogger(__name__)
+
+
+def decompile_file(input_file, output_file=None, **kwargs):
+    """
+    decompile rpyc file into rpy file and write to output.
+    """
+    if not output_file:
+        name, _ = os.path.splitext(input_file)
+        output_file = f"{name}.rpy"
+    stmt = stmts.load_file(input_file, **kwargs)
+    try:
+        code = util.get_code(stmt)
+    except Exception as e:
+        logger.error("decode file %s failed: %s", input_file, e)
+        raise e
+    utils.write_file(output_file, code)
+    logger.info("decompile %s -> %s", input_file, output_file)
+
+
+def decompile(input_path, output_path=None, **kwargs):
+    """
+    decompile rpyc file or directory into rpy
+
+    Parameters
+    ----------
+    input_path : str
+        path to rpyc file or directory contains rpyc files
+    output_path : str, optional
+        output path, by default it's same path of input_path.
+    """
+    if not os.path.isdir(input_path):
+        decompile_file(input_path, output_path, **kwargs)
+        return
+    if not output_path:
+        output_path = input_path
+    for filename in utils.match_files(input_path, r".*\.rpym?c$"):
+        decompile_file(
+            os.path.join(input_path, filename),
+            os.path.join(output_path, filename.removesuffix("c")),
+            **kwargs,
+        )
