7281705b2c9e03f2fd7f937c217dde141f8dcef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:08:41 +0000

    Add package texjam/0.4.1/texjam-0.4.1-py3-none-any.whl

---
+++ packages/texjam/texjam-0.4.1-py3-none-any.whl/texjam/__init__.py
@@ -0,0 +1,13 @@
+from .config import JinjaConfig, MetaField, TemplateConfig, TexJamConfig
+from .path import TempPath
+from .scaffold import TexJam, TexJamPlugin
+
+__all__ = [
+    'JinjaConfig',
+    'MetaField',
+    'TemplateConfig',
+    'TexJamConfig',
+    'TempPath',
+    'TexJam',
+    'TexJamPlugin',
+]
