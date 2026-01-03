064ea046bfda4383796eb9ff704a4f21a4867d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:07:21 +0000

    Add package urlssser/0.2/urlssser-0.2.tar.gz

---
+++ packages/urlssser/urlssser-0.2.tar.gz/urlssser-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="urlssser",
+    version="0.2",
+    packages=find_packages(),
+    author="fuckkkkk you 2 3 4",
+    description="Simple Scopper Library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
