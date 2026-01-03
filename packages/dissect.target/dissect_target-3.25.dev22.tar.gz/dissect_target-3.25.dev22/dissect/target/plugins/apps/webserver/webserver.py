6dce7a143258ed80635b78c946a330568bc26edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 04:13:31 +0000

    Add package dissect.target/3.25.dev22/dissect_target-3.25.dev22.tar.gz

---
+++ packages/dissect.target/dissect_target-3.25.dev22.tar.gz/dissect_target-3.25.dev22/dissect/target/plugins/apps/webserver/webserver.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from dissect.target.helpers.record import TargetRecordDescriptor
+from dissect.target.plugin import NamespacePlugin, export
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+WebserverAccessLogRecord = TargetRecordDescriptor(
+    "application/log/webserver/access",
+    [
+        ("datetime", "ts"),
+        ("string", "remote_user"),
+        ("net.ipaddress", "remote_ip"),
+        ("net.ipaddress", "local_ip"),
+        ("varint", "pid"),
+        ("string", "method"),
+        ("uri", "uri"),
+        ("string", "query"),
+        ("string", "protocol"),
+        ("varint", "status_code"),
+        ("varint", "bytes_sent"),
+        ("uri", "referer"),
+        ("string", "useragent"),
+        ("varint", "response_time_ms"),
+        ("path", "source"),
+    ],
+)
+
+WebserverErrorLogRecord = TargetRecordDescriptor(
+    "application/log/webserver/error",
+    [
+        ("datetime", "ts"),
+        ("net.ipaddress", "remote_ip"),
+        ("varint", "pid"),
+        ("string", "module"),
+        ("string", "level"),
+        ("string", "error_source"),
+        ("string", "error_code"),
+        ("string", "message"),
+        ("path", "source"),
+    ],
+)
+
+WebserverHostRecord = TargetRecordDescriptor(
+    "application/log/webserver/host",
+    [
+        ("datetime", "ts"),
+        ("string", "server_name"),
+        ("varint", "server_port"),
+        ("path", "root_path"),
+        ("path", "access_log_config"),
+        ("path", "error_log_config"),
+        ("path", "source"),
+    ],
+)
+
+
+class WebserverPlugin(NamespacePlugin):
+    __namespace__ = "webserver"
+
+    @export(record=[WebserverAccessLogRecord, WebserverErrorLogRecord])
+    def logs(self) -> Iterator[WebserverAccessLogRecord | WebserverErrorLogRecord]:
+        """Returns log file records from installed webservers."""
+        yield from self.access()
+        yield from self.error()
---
+++ packages/dissect.target/dissect_target-3.25.dev22.tar.gz/dissect_target-3.25.dev22/tests/helpers/test_cache.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from dissect.target.helpers.cache import Cache
+from dissect.target.plugins.os.windows.amcache import AmcachePlugin
+from dissect.target.plugins.os.windows.ual import UalPlugin
+
+if TYPE_CHECKING:
+    from dissect.target.target import Target
+
+
+def test_cache_namespace(target_bare: Target) -> None:
+    cache1 = Cache(AmcachePlugin.__call__, cls=AmcachePlugin)
+    cache2 = Cache(UalPlugin.__call__, cls=UalPlugin)
+
+    assert cache1.fname != cache2.fname
+    assert cache1.fname == "dissect.target.plugins.os.windows.amcache.AmcachePlugin.__call__"
+    assert cache2.fname == "dissect.target.plugins.os.windows.ual.UalPlugin.__call__"
+
+    target_bare._config.CACHE_DIR = "/tmp"
+    assert (
+        cache1.cache_path(target_bare, ("a", 1234)).stem
+        == "dissect.target.plugins.os.windows.amcache.AmcachePlugin.__call__.KCdhJywgMTIzNCk="
+    )
+    assert (
+        cache2.cache_path(target_bare, ("b", 5678)).stem
+        == "dissect.target.plugins.os.windows.ual.UalPlugin.__call__.KCdiJywgNTY3OCk="
+    )
+
+
+def test_cache_filename(target_win: Target) -> None:
+    plugin1 = AmcachePlugin(target_win)
+    plugin2 = UalPlugin(target_win)
+
+    cache1 = Cache(AmcachePlugin.applications)
+    cache2 = Cache(UalPlugin.client_access)
+    cache3 = Cache(plugin1.applications)
+    cache4 = Cache(plugin2.client_access)
+
+    assert cache1.fname == cache3.fname == "dissect.target.plugins.os.windows.amcache.AmcachePlugin.applications"
+    assert cache2.fname == cache4.fname == "dissect.target.plugins.os.windows.ual.UalPlugin.client_access"
+
+    target_win._config.CACHE_DIR = "/tmp"
+    assert (
+        cache1.cache_path(target_win, ()).stem
+        == "dissect.target.plugins.os.windows.amcache.AmcachePlugin.applications.KCk="
+    )
+    assert (
+        cache2.cache_path(target_win, ()).stem == "dissect.target.plugins.os.windows.ual.UalPlugin.client_access.KCk="
+    )
+    assert (
+        cache3.cache_path(target_win, ()).stem
+        == "dissect.target.plugins.os.windows.amcache.AmcachePlugin.applications.KCk="
+    )
+    assert (
+        cache4.cache_path(target_win, ()).stem == "dissect.target.plugins.os.windows.ual.UalPlugin.client_access.KCk="
+    )
