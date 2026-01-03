136254578fe466af4dfc9f615e7234fdeb951f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:34 +0000

    Add package pypm-manager/2.0.2/pypm_manager-2.0.2.tar.gz

---
+++ packages/pypm-manager/pypm_manager-2.0.2.tar.gz/pypm_manager-2.0.2/pypm/__init__.py
@@ -0,0 +1,18 @@
+"""
+PyPM - Python Package Manager
+Works like venv + pip but with centralized package storage (zero duplication)
+"""
+
+__version__ = "2.0.2"
+__author__ = "Avishek"
+__description__ = "Python package manager with centralized storage and zero duplication"
+__url__ = "https://github.com/Avishek8136/pypm"
+
+from .central_store import CentralPackageStore
+from .environment_manager import EnvironmentManager
+
+__all__ = [
+    'CentralPackageStore',
+    'EnvironmentManager',
+    '__version__',
+]
