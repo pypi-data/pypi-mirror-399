b8f68493abc16f58cb478d5da0aad3ee6f2e9570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 00:08:56 +0000

    Add package typing-runtime/0.0.0/typing_runtime-0.0.0.tar.gz

---
+++ packages/typing-runtime/typing_runtime-0.0.0.tar.gz/typing_runtime-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="typing-runtime",
+    version="0.0.0",
+    description="WIP; beta to be published soon.",
+    packages=["typing_runtime"],
+    python_requires=">=3.15",
+)
