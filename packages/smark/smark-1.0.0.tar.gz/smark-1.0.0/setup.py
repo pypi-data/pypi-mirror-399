cba0d282de8ab97f7572cadff9c13cb16a652152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 11:06:17 +0000

    Add package smark/1.0.0/smark-1.0.0.tar.gz

---
+++ packages/smark/smark-1.0.0.tar.gz/smark-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smark",
+    version="1.0.0",
+    author="SMARK",
+    author_email="SMARKpy@gmail.com",
+    description="SMARK logo",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
