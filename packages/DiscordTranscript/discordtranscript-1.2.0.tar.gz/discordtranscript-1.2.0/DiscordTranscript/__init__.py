9c9a892e370ee6024090212c8a2b3e349d0b0826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:47 +0000

    Add package DiscordTranscript/1.2.0/discordtranscript-1.2.0.tar.gz

---
+++ packages/DiscordTranscript/discordtranscript-1.2.0.tar.gz/discordtranscript-1.2.0/DiscordTranscript/__init__.py
@@ -0,0 +1,19 @@
+from DiscordTranscript.chat_exporter import (
+    export,
+    raw_export,
+    quick_export,
+    AttachmentHandler,
+    AttachmentToDataURIHandler,
+    AttachmentToDiscordChannelHandler,
+)
+
+__version__ = "1.2.0"
+
+__all__ = (
+    export,
+    raw_export,
+    quick_export,
+    AttachmentHandler,
+    AttachmentToDataURIHandler,
+    AttachmentToDiscordChannelHandler,
+)
