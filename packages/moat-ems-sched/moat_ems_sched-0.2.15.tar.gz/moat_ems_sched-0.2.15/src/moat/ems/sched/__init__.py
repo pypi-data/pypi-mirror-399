8dc6c912d4e86cf44be7f117230f99798f382c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 09:06:13 +0000

    Add package moat-ems-sched/0.2.15/moat_ems_sched-0.2.15.tar.gz

---
+++ packages/moat-ems-sched/moat_ems_sched-0.2.15.tar.gz/moat_ems_sched-0.2.15/src/moat/ems/sched/__init__.py
@@ -0,0 +1,13 @@
+"""
+This module calculates optimal battery charge/discharge based on usage and
+pricing prediction.
+
+"""
+
+from __future__ import annotations
+
+from moat.lib.config import CfgStore
+
+from .control import Model  # noqa:F401
+
+CfgStore.with_(__name__)
