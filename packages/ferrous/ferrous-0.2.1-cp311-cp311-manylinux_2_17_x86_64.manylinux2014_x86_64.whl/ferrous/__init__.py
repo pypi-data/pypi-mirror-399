6044f446ba7dbb39d1dbfb2980b652344689cb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 16:05:45 +0000

    Add package ferrous/0.2.1/ferrous-0.2.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ferrous/ferrous-0.2.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ferrous/__init__.py
@@ -0,0 +1,3 @@
+from ._ferrous import hello_world, FuzzyCache, PyMarkdownChunker as MarkdownChunker, PyContextPacker as ContextPacker
+
+__all__ = ["hello_world", "FuzzyCache", "MarkdownChunker", "ContextPacker"]
