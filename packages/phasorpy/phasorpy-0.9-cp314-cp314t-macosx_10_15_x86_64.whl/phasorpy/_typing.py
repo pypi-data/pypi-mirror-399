2ada1d5d55651f65291c94f615ec521aee8cb1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 13:14:28 +0000

    Add package phasorpy/0.9/phasorpy-0.9-cp314-cp314t-macosx_10_15_x86_64.whl

---
+++ packages/phasorpy/phasorpy-0.9-cp314-cp314t-macosx_10_15_x86_64.whl/phasorpy/_typing.py
@@ -0,0 +1,73 @@
+"""Type annotations.
+
+This module should only be imported when type-checking, for example::
+
+    from __future__ import annotations
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from ._typing import Any, ArrayLike, PathLike
+
+"""
+
+from __future__ import annotations
+
+__all__ = [
+    'IO',
+    'Any',
+    'ArrayLike',
+    'Callable',
+    'Collection',
+    'Container',
+    'DTypeLike',
+    'DataArray',
+    'EllipsisType',
+    'ItemsView',
+    'Iterable',
+    'Iterator',
+    'KeysView',
+    'Literal',
+    'Mapping',
+    'NDArray',
+    'Optional',
+    'PathLike',
+    'Pooch',
+    'Sequence',
+    'TextIO',
+    'Union',
+    'ValuesView',
+    'cast',
+    'final',
+    'overload',
+]
+
+from collections.abc import (
+    Callable,
+    Collection,
+    Container,
+    ItemsView,
+    Iterable,
+    Iterator,
+    KeysView,
+    Mapping,
+    Sequence,
+    ValuesView,
+)
+from os import PathLike
+from types import EllipsisType
+from typing import (
+    IO,
+    Any,
+    Literal,
+    Optional,
+    TextIO,
+    Union,
+    cast,
+    final,
+    overload,
+)
+
+from numpy.typing import ArrayLike, DTypeLike, NDArray
+from pooch import Pooch
+from xarray import DataArray
