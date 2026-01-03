aaaacce8e842324c4145db89d40c9f22dfa5ea5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 06:05:28 +0000

    Add package python-paddle/0.2.12/python_paddle-0.2.12.tar.gz

---
+++ packages/python-paddle/python_paddle-0.2.12.tar.gz/python_paddle-0.2.12/paddle/client.py
@@ -0,0 +1,27 @@
+import httpx
+
+from paddle.operations.customers import CustomerOperationsMixin
+from paddle.operations.transactions import TransactionOperationsMixin
+
+
+class Paddle(CustomerOperationsMixin, TransactionOperationsMixin):
+    """A Paddle client."""
+
+    _client = httpx.AsyncClient()
+
+    def __init__(self, token: str, production: bool = True) -> None:
+        """Initialize a Paddle client.
+
+        Args:
+            token (str): The API token for authentication.
+            production (bool, optional): Whether to use the production environment. Defaults to
+                True.
+        """
+
+        self._token = token
+
+        if production:
+            self._endpoint = "api.paddle.com"
+
+        else:
+            self._endpoint = "sandbox-api.paddle.com"
