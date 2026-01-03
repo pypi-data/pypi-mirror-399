d39953e0f81902349cf24940ec428e4b34945e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 01:33:28 +0000

    Add package aip-agents-binary/0.5.19/aip_agents_binary-0.5.19-py3-none-any.whl

---
+++ packages/aip-agents-binary/aip_agents_binary-0.5.19-py3-none-any.whl/aip_agents/tools/__init__.py
@@ -0,0 +1,6 @@
+"""Initialize Tools Module for AIP Agents."""
+
+from aip_agents.tools.bosa_tools import BOSA_AUTOMATED_TOOLS
+from aip_agents.tools.gl_connector import GLConnectorTool
+
+__all__ = ["BOSA_AUTOMATED_TOOLS", "GLConnectorTool"]
---
+++ packages/aip-agents-binary/aip_agents_binary-0.5.19-py3-none-any.whl/aip_agents/tools/__init__.pyi
@@ -0,0 +1,4 @@
+from aip_agents.tools.bosa_tools import BOSA_AUTOMATED_TOOLS as BOSA_AUTOMATED_TOOLS
+from aip_agents.tools.gl_connector import GLConnectorTool as GLConnectorTool
+
+__all__ = ['BOSA_AUTOMATED_TOOLS', 'GLConnectorTool']
