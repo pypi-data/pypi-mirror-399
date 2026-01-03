b26bae148c8c86411537b8689e27f58d9ffbc24e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-03 12:06:47 +0000

    Add package aep8/0.3.0/aep8-0.3.0-cp314-cp314t-win_amd64.whl

---
+++ packages/aep8/aep8-0.3.0-cp314-cp314t-win_amd64.whl/aep8/__init__.py
@@ -0,0 +1,17 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'aep8.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from importlib.metadata import version as _version
+
+from ._core import flux
+
+__all__ = ("flux",)
+__version__ = _version(__package__)
